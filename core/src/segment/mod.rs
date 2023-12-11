4490380ea76f78ae34521575c4a335a9bd8eb45f
Tamir Hemo <tamir@succinct.xyz>
2023-12-10 17:21:31 -0800

    interpreter

--- core/src/segment/mod.rs
+++ core/src/segment/mod.rs
@@ -7,14 +7,3 @@ pub struct Segment {
     pub alu_events: Vec<AluEvent>,
     pub program: ProgramROM<i32>,
 }
-
-impl Segment {
-    fn prove() {
-        // Generate the traces based on each set of events.
-        todo!()
-    }
-
-    fn verify() {
-        todo!()
-    }
-}
