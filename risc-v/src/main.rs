3cee621c880f5132d6b84a2ff26d865f8a8fa607
Tamir Hemo <tamir@succinct.xyz>
2023-12-11 12:02:07 -0800

    compiling tendermint

---
+++ risc-v/src/main.rs
@@ -0,0 +1,33 @@
+#![no_std]
+// #![no_main]
+
+extern crate alloc;
+
+use cortex_m_rt::entry;
+use embedded_alloc::Heap;
+use my_riscv::my_bench;
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[entry]
+fn main() -> ! {
+    // Initialize the allocator BEFORE you use it
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+    }
+
+    // now the allocator is ready types like Box, Vec can be used.
+
+    loop {
+        my_bench()
+    }
+}
