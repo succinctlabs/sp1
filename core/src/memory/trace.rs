399bd5cb59729a2c285d16818731e0f1fc5dcb4d
Tamir Hemo <tamir@succinct.xyz>
2023-12-12 10:50:32 -0800

    intermediate push

--- core/src/memory/trace.rs
+++ core/src/memory/trace.rs
@@ -1,16 +1,15 @@
 use p3_field::PrimeField;
 use p3_matrix::dense::RowMajorMatrix;
 
-use alloc::collections::BTreeSet;
-
 use super::MemoryEvent;
 
 #[derive(Debug, Clone)]
 pub struct MemoryTable<T>(pub RowMajorMatrix<T>);
 
 impl<F: PrimeField> MemoryTable<F> {
-    pub fn generate(events: &[MemoryEvent]) -> Self {
-        let mut event_map = events.into_iter().collect::<BTreeSet<_>>();
+    pub fn generate(events: &mut [MemoryEvent]) -> Self {
+        // Sort the events by address and then by clock cycle.
+        events.sort_by_key(|event| (event.addr, event.clk));
         todo!()
     }
 }
