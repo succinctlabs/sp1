fa591915be8fe841b65d9e788e86327ea1e05845
John Guibas <john@succinct.xyz>
2023-12-12 13:14:58 -0800

    resolve merge conflicts

--- core/src/alu/mod.rs
+++ core/src/alu/mod.rs
@@ -1,10 +1,10 @@
+use crate::runtime::Opcode;
+
+#[derive(Debug, Clone, Copy)]
 pub struct AluEvent {
     pub clk: u32,
-    pub opcode: u32,
-    pub addr_d: usize,
-    pub addr_1: usize,
-    pub addr_2: usize,
-    pub v_d: i32,
-    pub v_1: i32,
-    pub v_2: i32,
+    pub opcode: Opcode,
+    pub a: u32,
+    pub b: u32,
+    pub c: u32,
 }
