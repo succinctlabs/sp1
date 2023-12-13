c8872236905b82d35308482eeb1a90c814158a59
Tamir Hemo <tamir@succinct.xyz>
2023-12-12 17:27:53 -0800

    feat: memory air (#9)
    * address
    
    * interaction
    
    * mem interaction
    
    * number the interaction kind
    
    * interaction v2
    
    * nuke address
    
    * add interaction kind

--- core/src/memory/air.rs
+++ core/src/memory/air.rs
@@ -1,5 +1,4 @@
-use p3_air::{AirBuilder, BaseAir};
-use p3_field::PrimeField;
+use p3_air::AirBuilder;
 
 use crate::air::{AirConstraint, AirVariable, Bool, Word};
 
@@ -36,5 +35,7 @@ impl<AB: AirBuilder> AirConstraint<AB> for MemoryCols<AB::Var> {
         // REMARK: This might get automated.
         self.is_read.eval_is_valid(builder);
         self.is_init.eval_is_valid(builder);
+
+        // Assert that the value is zero when the memory is not initialized.
     }
 }
--- core/src/memory/mod.rs
+++ core/src/memory/mod.rs
@@ -5,31 +5,36 @@ use crate::runtime::Register;
 mod air;
 pub mod trace;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum MemOp {
-    Read,
-    Write,
+    Read = 0,
+    Write = 1,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct MemoryEvent {
-    pub clk: u32,
     pub addr: u32,
+    pub clk: u32,
     pub op: MemOp,
     pub value: u32,
 }
 
 pub struct Memory {
-    MAX_MEMORY: u32,
+    max_memory: u32,
     memory: BTreeMap<u32, u32>,
     registers: [u32; 32],
     memory_events: Vec<MemoryEvent>,
 }
 
 impl Memory {
-    pub fn new(MAX_MEMORY: u32) -> Self {
+    pub fn new(max_memory: u32) -> Self {
+        assert_eq!(max_memory % 4, 0, "Memory size must be a multiple of 4");
+        assert!(
+            max_memory < u32::MAX - 31,
+            "Memory size must be smaller than 2^32 - 32"
+        );
         Self {
-            MAX_MEMORY,
+            max_memory,
             memory: BTreeMap::new(),
             registers: [0; 32],
             memory_events: Vec::new(),
@@ -57,9 +62,9 @@ impl Memory {
         self.memory.insert(addr, value);
     }
 
-    pub fn read_register(&mut self, clk: u32, reg: Register, value: u32) -> u32 {
+    pub fn read_register(&mut self, clk: u32, reg: Register) -> u32 {
         let value = self.registers[reg as usize];
-        let addr = self.MAX_MEMORY + reg as u32;
+        let addr = self.max_memory + reg as u32;
         self.memory_events.push(MemoryEvent {
             clk,
             addr,
@@ -71,7 +76,7 @@ impl Memory {
 
     pub fn write_register(&mut self, clk: u32, reg: Register, value: u32) {
         self.registers[reg as usize] = value;
-        let addr = self.MAX_MEMORY + reg as u32;
+        let addr = self.max_memory + reg as u32;
         self.memory_events.push(MemoryEvent {
             clk,
             addr,
