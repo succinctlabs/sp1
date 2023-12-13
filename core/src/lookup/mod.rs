fc266df8728a24ee597cdc0f0fe6ec489fec03e8
John Guibas <jtguibas@gmail.com>
2023-12-13 10:24:18 -0800

    feat: working ALU tables for ADD + SUB + AND + XOR + OR minus interactions (#10)
    * feat: basic ALU tables
    
    * feat: basic ALU tables
    
    * feat: basic ALU tables
    
    * generate trace
    
    * generate trace
    
    * generate trace
    
    * generate trace
    
    * generate trace
    
    * generate trace
    
    * working bitwise
    
    * working bitwise
    
    * working bitwise
    
    * working bitwise

--- core/src/lookup/mod.rs
+++ core/src/lookup/mod.rs
@@ -5,9 +5,9 @@ use crate::memory::MemOp;
 
 /// An interaction for a lookup or a permutation argument.
 pub struct Interaction<F: Field> {
-    values: Vec<VirtualPairCol<F>>,
-    multiplicity: VirtualPairCol<F>,
-    kind: InteractionKind,
+    pub values: Vec<VirtualPairCol<F>>,
+    pub multiplicity: VirtualPairCol<F>,
+    pub kind: InteractionKind,
 }
 
 /// The type of interaction for a lookup argument.
