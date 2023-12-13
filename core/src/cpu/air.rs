85877d3eb150cc0ff6f97c3c528df5a8b05b3ed4
Uma Roy <uma.roy.us@gmail.com>
2023-12-13 11:39:13 -0800

    Better structure cpu cols

--- core/src/cpu/air.rs
+++ core/src/cpu/air.rs
@@ -6,26 +6,12 @@ use p3_field::AbstractField;
 use p3_field::PrimeField;
 use p3_matrix::MatrixRowSlices;
 use p3_util::indices_arr;
+use valida_derive::AlignedBorrow;
 
-#[derive(Debug, Clone, Copy)]
-pub struct CpuAir;
-
-/// An AIR table for memory accesses.
-#[derive(Debug, Clone)]
-pub struct CpuCols<T> {
-    /// The clock cycle value.
-    pub clk: T,
-    /// The program counter value.
-    pub pc: T,
-    /// The opcode for this cycle.
-    pub opcode: T,
-    /// The first operand for this instruction.
-    pub op_a: T,
-    /// The second operand for this instruction.
-    pub op_b: T,
-    /// The third operand for this instruction.
-    pub op_c: T,
-    // Whether op_b is an immediate value.
+#[derive(AlignedBorrow, Default)]
+#[repr(C)]
+pub struct OpcodeSelectors<T> {
+    // // Whether op_b is an immediate value.
     pub imm_b: T,
     // Whether op_c is an immediate value.
     pub imm_c: T,
@@ -45,17 +31,39 @@ pub struct CpuCols<T> {
     pub system_instruction: T,
     // Whether this is a multiply instruction.
     pub multiply_instruction: T,
-    // Selectors for load/store instructions and their types.
+    // // Selectors for load/store instructions and their types.
     pub byte: Bool<T>,
     pub half: Bool<T>,
     pub word: Bool<T>,
     pub unsigned: Bool<T>,
-    // TODO: we might need a selector for "MULSU" since no other instruction has "SU"
+    // // TODO: we might need a selector for "MULSU" since no other instruction has "SU"
     pub JALR: T,
     pub JAL: T,
     pub AUIPC: T,
+    // // Whether this instruction is reading from register A.
+    pub reg_a_read: T,
+}
+/// An AIR table for memory accesses.
+#[derive(AlignedBorrow, Default)]
+#[repr(C)]
+pub struct CpuCols<T> {
+    /// The clock cycle value.
+    pub clk: T,
+    // /// The program counter value.
+    pub pc: T,
+    // /// The opcode for this cycle.
+    pub opcode: T,
+    // /// The first operand for this instruction.
+    pub op_a: T,
+    // /// The second operand for this instruction.
+    pub op_b: T,
+    // /// The third operand for this instruction.
+    pub op_c: T,
 
-    // Operand values, either from registers or immediate values.
+    // Selectors for the opcode.
+    pub selectors: OpcodeSelectors<T>,
+
+    // // Operand values, either from registers or immediate values.
     pub op_a_val: Word<T>,
     pub op_b_val: Word<T>,
     pub op_c_val: Word<T>,
@@ -75,28 +83,6 @@ pub(crate) const CPU_COL_MAP: CpuCols<usize> = make_col_map();
 const fn make_col_map() -> CpuCols<usize> {
     let indices_arr = indices_arr::<NUM_CPU_COLS>();
     unsafe { transmute::<[usize; NUM_CPU_COLS], CpuCols<usize>>(indices_arr) }
-}
-
-impl<T> Borrow<CpuCols<T>> for [T] {
-    fn borrow(&self) -> &CpuCols<T> {
-        // TODO: Double check if this is correct & consider making asserts debug-only.
-        let (prefix, shorts, suffix) = unsafe { self.align_to::<CpuCols<T>>() };
-        assert!(prefix.is_empty(), "Data was not aligned");
-        assert!(suffix.is_empty(), "Data was not aligned");
-        assert_eq!(shorts.len(), 1);
-        &shorts[0]
-    }
-}
-
-impl<T> BorrowMut<CpuCols<T>> for [T] {
-    fn borrow_mut(&mut self) -> &mut CpuCols<T> {
-        // TODO: Double check if this is correct & consider making asserts debug-only.
-        let (prefix, shorts, suffix) = unsafe { self.align_to_mut::<CpuCols<T>>() };
-        assert!(prefix.is_empty(), "Data was not aligned");
-        assert!(suffix.is_empty(), "Data was not aligned");
-        assert_eq!(shorts.len(), 1);
-        &mut shorts[0]
-    }
 }
 
 impl<AB: AirBuilder> AirConstraint<AB> for CpuCols<AB::Var> {
@@ -119,16 +105,22 @@ impl<AB: AirBuilder> AirConstraint<AB> for CpuCols<AB::Var> {
         // Constraint the op_b_val and op_c_val columns when imm_b and imm_c are true.
         // TODO: modify these to be bit-decomposition constraints
         builder
-            .when(local.imm_b)
+            .when(local.selectors.imm_b)
             .assert_eq(reduce::<AB>(local.op_b_val), local.op_b);
         builder
-            .when(local.imm_c)
+            .when(local.selectors.imm_c)
             .assert_eq(reduce::<AB>(local.op_c_val), local.op_c);
 
         // We only read from the first register if there is a store or branch instruction. In all other cases we write.
+<<<<<<< Updated upstream
         let reg_a_read =
             local.store_instruction + local.branch_instruction + local.multiply_instruction;
         // TODO: lookup (clk, op_a, op_a_val, is_read=reg_a_read) in the register table with multiplicity 1.
+=======
+        let reg_a_read = local.selectors.store_instruction
+            + local.selectors.branch_instruction
+            + local.selectors.multiply_instruction;
+>>>>>>> Stashed changes
 
         //// For r-type, i-type and multiply instructions, we must constraint by an "opcode-oracle" table
         // TODO: lookup (clk, op_a_val, op_b_val, op_c_val) in the "opcode-oracle" table with multiplicity (register_instruction + immediate_instruction + multiply_instruction)
@@ -154,7 +146,7 @@ impl<AB: AirBuilder> AirConstraint<AB> for CpuCols<AB::Var> {
         //// For branch instructions
         /// TODO: lookup (clk, branch_cond_val, op_a_val, op_b_val) in the "branch" table with multiplicity branch_instruction
         // Increment the pc by 4 + op_c_val * branch_cond_val where we interpret the first result as a bool that it is.
-        builder.when(local.branch_instruction).assert_eq(
+        builder.when(local.selectors.branch_instruction).assert_eq(
             local.pc
                 + AB::F::from_canonical_u8(4)
                 + reduce::<AB>(local.op_c_val) * local.branch_cond_val.0[0],
@@ -162,23 +154,23 @@ impl<AB: AirBuilder> AirConstraint<AB> for CpuCols<AB::Var> {
         );
 
         //// For jump instructions
-        builder.when(local.jump_instruction).assert_eq(
+        builder.when(local.selectors.jump_instruction).assert_eq(
             reduce::<AB>(local.op_a_val),
             local.pc + AB::F::from_canonical_u8(4),
         );
-        builder.when(local.JAL).assert_eq(
+        builder.when(local.selectors.JAL).assert_eq(
             local.pc + AB::F::from_canonical_u8(4) + reduce::<AB>(local.op_b_val),
             next.pc,
         );
         builder
-            .when(local.JALR)
+            .when(local.selectors.JALR)
             .assert_eq(reduce::<AB>(local.op_b_val) + local.op_c, next.pc);
 
         //// For system instructions
 
         //// Upper immediate instructions
         // lookup(clk, op_c_val, imm, 12) in SLT table with multiplicity AUIPC
-        builder.when(local.AUIPC).assert_eq(
+        builder.when(local.selectors.AUIPC).assert_eq(
             reduce::<AB>(local.op_a_val),
             reduce::<AB>(local.op_c_val) + local.pc,
         );
