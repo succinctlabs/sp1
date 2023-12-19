c6f790e854d0e675acc5d27c4ca775009501d63b
Tamir Hemo <tamir@succinct.xyz>
2023-12-18 16:46:59 -0800

    trace

--- core/src/bytes/trace.rs
+++ core/src/bytes/trace.rs
@@ -3,9 +3,7 @@ use std::collections::BTreeMap;
 use p3_field::Field;
 use p3_matrix::dense::RowMajorMatrix;
 
-use crate::bytes::air::NUM_BYTE_COLS;
-
-use super::{ByteChip, ByteLookupEvent};
+use super::{air::BYTE_MULT_INDICES, ByteChip, ByteLookupEvent};
 
 pub const NUM_ROWS: usize = 1 << 16;
 
@@ -16,14 +14,14 @@ impl<F: Field> ByteChip<F> {
     ) -> RowMajorMatrix<F> {
         let mut trace = self.initial_trace.clone();
 
-        // for (lookup, mult) in byte_lookups.iter() {
-        //     let (row, index) = self.table_map[lookup];
+        for (lookup, mult) in byte_lookups.iter() {
+            let (row, index) = self.table_map[lookup];
 
-        //     // Get the column index for the multiplicity.
-        //     let mult_idx = row * NUM_BYTE_COLS + BYTE_MULT_INDICES[index];
-        //     // Update the trace value
-        //     trace_rows[mult_idx] = F::from_canonical_usize(*mult);
-        // }
+            // Get the column index for the multiplicity.
+            let idx = BYTE_MULT_INDICES[index];
+            // Update the trace value
+            trace.row_mut(row)[idx] += F::from_canonical_usize(*mult);
+        }
 
         trace
     }
