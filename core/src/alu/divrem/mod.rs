c032fbd71fa29961e9e15404a412e5b392d6414b
Hidenori Shinohara <hidenori@succinct.xyz>
2024-01-09 18:38:20 -0500

    debugging

--- core/src/alu/divrem/mod.rs
+++ core/src/alu/divrem/mod.rs
@@ -231,6 +231,7 @@ impl<F: PrimeField> Chip<F> for DivRemChip {
                     debug_assert_eq!(b_word[i] as u32, result[i]);
                 }
 
+                println!("{:#?}", cols);
                 row
             })
             .collect::<Vec<_>>();
@@ -271,13 +272,15 @@ where
         // Use the mul table to compute c * quotient and compare it to local.c_times_quotient.
 
         // Add remainder to product c * quotient.
-        for i in 0..WORD_SIZE {
-            result[i] = local.c_times_quotient[i].clone() + local.remainder[i].clone();
-        }
         let sign_extension = local.rem_neg.clone() * AB::F::from_canonical_u32(0xff);
-        for i in WORD_SIZE..LONG_WORD_SIZE {
-            // If rem is negative, add 0xff to the upper 4 bytes.
-            result[i] = local.c_times_quotient[i].clone() + sign_extension.clone();
+        for i in 0..LONG_WORD_SIZE {
+            result[i] = local.c_times_quotient[i].into();
+            if i < WORD_SIZE {
+                result[i] += local.remainder[i].into();
+            } else {
+                // If rem is negative, add 0xff to the upper 4 bytes.
+                result[i] += sign_extension.clone();
+            }
         }
 
         // Propagate carry.
@@ -480,37 +483,37 @@ mod tests {
 
         let divrems: Vec<(Opcode, u32, u32, u32)> = vec![
             (Opcode::DIVU, 3, 20, 6),
-            (Opcode::DIVU, 715827879, neg(20), 6),
-            (Opcode::DIVU, 0, 20, neg(6)),
-            (Opcode::DIVU, 0, neg(20), neg(6)),
-            (Opcode::DIVU, 1 << 31, 1 << 31, 1),
-            (Opcode::DIVU, 0, 1 << 31, neg(1)),
-            (Opcode::DIVU, u32::MAX, 1 << 31, 0),
-            (Opcode::DIVU, u32::MAX, 1, 0),
-            (Opcode::DIVU, u32::MAX, 0, 0),
-            (Opcode::REMU, 4, 18, 7),
-            (Opcode::REMU, 6, neg(20), 11),
-            (Opcode::REMU, 23, 23, neg(6)),
-            (Opcode::REMU, neg(21), neg(21), neg(11)),
-            (Opcode::REMU, 5, 5, 0),
-            (Opcode::REMU, neg(1), neg(1), 0),
-            (Opcode::REMU, 0, 0, 0),
-            (Opcode::REM, 7, 16, 9),
-            (Opcode::REM, neg(4), neg(22), 6),
-            (Opcode::REM, 1, 25, neg(3)),
-            (Opcode::REM, neg(2), neg(22), neg(4)),
-            (Opcode::REM, 0, 873, 1),
-            (Opcode::REM, 0, 873, neg(1)),
-            (Opcode::REM, 5, 5, 0),
-            (Opcode::REM, neg(5), neg(5), 0),
-            (Opcode::REM, 0, 0, 0),
-            (Opcode::REM, 0, 0x80000001, neg(1)),
-            (Opcode::DIV, 3, 18, 6),
-            (Opcode::DIV, neg(6), neg(24), 4),
-            (Opcode::DIV, neg(2), 16, neg(8)),
-            (Opcode::DIV, neg(1), 0, 0),
-            (Opcode::DIV, 1 << 31, neg(1), 1 << 31),
-            (Opcode::REM, 1 << 31, neg(1), 0),
+            //(Opcode::DIVU, 715827879, neg(20), 6),
+            //(Opcode::DIVU, 0, 20, neg(6)),
+            //(Opcode::DIVU, 0, neg(20), neg(6)),
+            //(Opcode::DIVU, 1 << 31, 1 << 31, 1),
+            //(Opcode::DIVU, 0, 1 << 31, neg(1)),
+            //(Opcode::DIVU, u32::MAX, 1 << 31, 0),
+            //(Opcode::DIVU, u32::MAX, 1, 0),
+            //(Opcode::DIVU, u32::MAX, 0, 0),
+            //(Opcode::REMU, 4, 18, 7),
+            //(Opcode::REMU, 6, neg(20), 11),
+            //(Opcode::REMU, 23, 23, neg(6)),
+            //(Opcode::REMU, neg(21), neg(21), neg(11)),
+            //(Opcode::REMU, 5, 5, 0),
+            //(Opcode::REMU, neg(1), neg(1), 0),
+            //(Opcode::REMU, 0, 0, 0),
+            //(Opcode::REM, 7, 16, 9),
+            //(Opcode::REM, neg(4), neg(22), 6),
+            //(Opcode::REM, 1, 25, neg(3)),
+            //(Opcode::REM, neg(2), neg(22), neg(4)),
+            //(Opcode::REM, 0, 873, 1),
+            //(Opcode::REM, 0, 873, neg(1)),
+            //(Opcode::REM, 5, 5, 0),
+            //(Opcode::REM, neg(5), neg(5), 0),
+            //(Opcode::REM, 0, 0, 0),
+            //(Opcode::REM, 0, 0x80000001, neg(1)),
+            //(Opcode::DIV, 3, 18, 6),
+            //(Opcode::DIV, neg(6), neg(24), 4),
+            //(Opcode::DIV, neg(2), 16, neg(8)),
+            //(Opcode::DIV, neg(1), 0, 0),
+            //(Opcode::DIV, 1 << 31, neg(1), 1 << 31),
+            //(Opcode::REM, 1 << 31, neg(1), 0),
         ];
         for t in divrems.iter() {
             divrem_events.push(AluEvent::new(0, t.0, t.1, t.2, t.3));
@@ -518,7 +521,7 @@ mod tests {
 
         // Append more events until we have 1000 tests.
         for _ in 0..(1000 - divrems.len()) {
-            divrem_events.push(AluEvent::new(0, Opcode::DIVU, 1, 1, 1));
+            //            divrem_events.push(AluEvent::new(0, Opcode::DIVU, 1, 1, 1));
         }
 
         runtime.divrem_events = divrem_events;
