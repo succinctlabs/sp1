cda10717e43dfa8cea2dd8e94cd4b21918903700
John Guibas <john@succinct.xyz>
2024-01-12 12:54:41 -0800

    feat: move runtime execution to precompile module

--- core/src/precompiles/sha256/extend/execute.rs
+++ core/src/precompiles/sha256/extend/execute.rs
@@ -7,46 +7,47 @@ use super::ShaExtendChip;
 
 impl ShaExtendChip {
     pub fn execute(rt: &mut Runtime) -> (u32, u32, u32) {
-        let t0 = Register::X5;
-        let a0 = Register::X10;
-
         // The number of cycles it takes to perform this precompile.
         const NB_SHA_EXTEND_CYCLES: u32 = 48 * 20;
 
-        // Temporarily set the clock to the number of cycles it takes to perform
-        // this precompile as reading `w_ptr` happens on this clock.
+        // Initialize the registers.
+        let t0 = Register::X5;
+        let a0 = Register::X10;
+
+        // Temporarily set the clock to the number of cycles it takes to perform this precompile as
+        // reading `w_ptr` happens on this clock.
         rt.clk += NB_SHA_EXTEND_CYCLES;
 
         // Read `w_ptr` from register a0 or x5.
         let w_ptr = rt.register(a0);
-        let mut w = Vec::new();
-        for i in 0..64 {
-            w.push(rt.word(w_ptr + i * 4));
-        }
+        let w: [u32; 64] = (0..64)
+            .map(|i| rt.word(w_ptr + i * 4))
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
 
         // Set the CPU table values with some dummy values.
         let (a, b, c) = (w_ptr, rt.rr(t0, AccessPosition::B), 0);
         rt.rw(a0, a);
 
-        // We'll save the current record and restore it later so that the CPU
-        // event gets emitted correctly.
+        // We'll save the current record and restore it later so that the CPU event gets emitted
+        // correctly.
         let t = rt.record;
 
-        // Set the clock back to the original value and begin executing the
-        // precompile.
+        // Set the clock back to the original value and begin executing the precompile.
         rt.clk -= NB_SHA_EXTEND_CYCLES;
-        let saved_clk = rt.clk;
-        let saved_w_ptr = w_ptr;
-        let saved_w = w.clone();
-        let mut w_i_minus_15_records = Vec::new();
-        let mut w_i_minus_2_records = Vec::new();
-        let mut w_i_minus_16_records = Vec::new();
-        let mut w_i_minus_7_records = Vec::new();
-        let mut w_i_records = Vec::new();
+        let clk_init = rt.clk;
+        let w_ptr_init = w_ptr;
+        let w_init = w.clone();
+        let mut w_i_minus_15_reads = Vec::new();
+        let mut w_i_minus_2_reads = Vec::new();
+        let mut w_i_minus_16_reads = Vec::new();
+        let mut w_i_minus_7_reads = Vec::new();
+        let mut w_i_writes = Vec::new();
         for i in 16..64 {
             // Read w[i-15].
             let w_i_minus_15 = rt.mr(w_ptr + (i - 15) * 4, AccessPosition::Memory);
-            w_i_minus_15_records.push(rt.record.memory);
+            w_i_minus_15_reads.push(rt.record.memory);
             rt.clk += 4;
 
             // Compute `s0`.
@@ -55,7 +56,7 @@ impl ShaExtendChip {
 
             // Read w[i-2].
             let w_i_minus_2 = rt.mr(w_ptr + (i - 2) * 4, AccessPosition::Memory);
-            w_i_minus_2_records.push(rt.record.memory);
+            w_i_minus_2_reads.push(rt.record.memory);
             rt.clk += 4;
 
             // Compute `s1`.
@@ -64,12 +65,12 @@ impl ShaExtendChip {
 
             // Read w[i-16].
             let w_i_minus_16 = rt.mr(w_ptr + (i - 16) * 4, AccessPosition::Memory);
-            w_i_minus_16_records.push(rt.record.memory);
+            w_i_minus_16_reads.push(rt.record.memory);
             rt.clk += 4;
 
             // Read w[i-7].
             let w_i_minus_7 = rt.mr(w_ptr + (i - 7) * 4, AccessPosition::Memory);
-            w_i_minus_7_records.push(rt.record.memory);
+            w_i_minus_7_reads.push(rt.record.memory);
             rt.clk += 4;
 
             // Compute `w_i`.
@@ -81,20 +82,20 @@ impl ShaExtendChip {
             // Write w[i].
             rt.mr(w_ptr + i * 4, AccessPosition::Memory);
             rt.mw(w_ptr + i * 4, w_i, AccessPosition::Memory);
-            w_i_records.push(rt.record.memory);
+            w_i_writes.push(rt.record.memory);
             rt.clk += 4;
         }
 
         // Push the SHA extend event.
         rt.segment.sha_extend_events.push(ShaExtendEvent {
-            clk: saved_clk,
-            w_ptr: saved_w_ptr,
-            w: saved_w.try_into().unwrap(),
-            w_i_minus_15_records: w_i_minus_15_records.try_into().unwrap(),
-            w_i_minus_2_records: w_i_minus_2_records.try_into().unwrap(),
-            w_i_minus_16_records: w_i_minus_16_records.try_into().unwrap(),
-            w_i_minus_7_records: w_i_minus_7_records.try_into().unwrap(),
-            w_i_records: w_i_records.try_into().unwrap(),
+            clk: clk_init,
+            w_ptr: w_ptr_init,
+            w: w_init,
+            w_i_minus_15_reads: w_i_minus_15_reads.try_into().unwrap(),
+            w_i_minus_2_reads: w_i_minus_2_reads.try_into().unwrap(),
+            w_i_minus_16_reads: w_i_minus_16_reads.try_into().unwrap(),
+            w_i_minus_7_reads: w_i_minus_7_reads.try_into().unwrap(),
+            w_i_writes: w_i_writes.try_into().unwrap(),
         });
 
         // Restore the original record.
