fef902ea956f023c7b4a8b92d26aa40f5240e50e
Hidenori Shinohara <hidenori@succinct.xyz>
2024-01-16 12:08:33 -0500

    add MUL test, failing

--- core/src/prover/runtime.rs
+++ core/src/prover/runtime.rs
@@ -434,6 +434,17 @@ pub mod tests {
     }
 
     #[test]
+    fn test_mul_prove() {
+        let instructions = vec![
+            Instruction::new(Opcode::ADD, 29, 0, 5, false, true),
+            Instruction::new(Opcode::ADD, 30, 0, 8, false, true),
+            Instruction::new(Opcode::MUL, 31, 30, 29, false, false),
+        ];
+        let program = Program::new(instructions, 0, 0);
+        prove(program);
+    }
+
+    #[test]
     fn test_fibonacci_prove() {
         if env_logger::try_init().is_err() {
             debug!("Logger already initialized")
