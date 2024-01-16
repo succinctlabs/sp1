b6d5de677d65e0986e5f8a5bdab5b12650926e87
John Guibas <john@succinct.xyz>
2024-01-16 14:10:02 -0800

    fix print

--- examples/sha_extend/src/main.rs
+++ examples/sha_extend/src/main.rs
@@ -7,9 +7,6 @@ curta_zkvm::entrypoint!(main);
 
 pub fn main() {
     let mut w = [1u32; 64];
+    syscall_sha256_extend(w.as_mut_ptr());
     println!("{:?}", w);
-    // syscall_sha256_extend(w.as_mut_ptr());
-    // for i in 0..64 {
-    //     println!("{}", w[i]);
-    // }
 }
