74347af767972b3df83c88fe8281849bf73ddb81
Hidenori <hidenori@succinct.xyz>
2024-01-16 16:10:05 -0500

    chore: Fix MUL ALU table interactions (#78)

--- core/src/prover/runtime.rs
+++ core/src/prover/runtime.rs
@@ -1,3 +1,5 @@
+use crate::alu::divrem::DivRemChip;
+use crate::alu::mul::MulChip;
 use crate::bytes::ByteChip;
 use crate::cpu::trace::CpuChip;
 use crate::memory::MemoryGlobalChip;
@@ -78,8 +80,9 @@ impl Runtime {
 
 struct Prover {}
 
+const NUM_CHIPS: usize = 12;
 impl Prover {
-    pub fn segment_chips<F, EF, SC>() -> [Box<dyn AirChip<SC>>; 11]
+    pub fn segment_chips<F, EF, SC>() -> [Box<dyn AirChip<SC>>; NUM_CHIPS]
     where
         F: PrimeField + TwoAdicField + PrimeField32,
         EF: ExtensionField<F>,
@@ -91,20 +94,26 @@ impl Prover {
         let add = AddChip::new();
         let sub = SubChip::new();
         let bitwise = BitwiseChip::new();
-        let right_shift = RightShiftChip::new();
-        let left_shift = LeftShiftChip::new();
+        let mul = MulChip::new();
+        let _divrem = DivRemChip::new();
+        let shift_right = RightShiftChip::new();
+        let shift_left = LeftShiftChip::new();
         let lt = LtChip::new();
         let bytes = ByteChip::<F>::new();
         let sha_extend = ShaExtendChip::new();
         let sha_compress = ShaCompressChip::new();
+        // This is where we create a vector of chips.
         [
             Box::new(program),
             Box::new(cpu),
             Box::new(add),
             Box::new(sub),
             Box::new(bitwise),
-            Box::new(right_shift),
-            Box::new(left_shift),
+            Box::new(mul),
+            // TODO: We need to add this here, but it doesn't work yet.
+            // Box::new(divrem),
+            Box::new(shift_right),
+            Box::new(shift_left),
             Box::new(lt),
             Box::new(sha_extend),
             Box::new(sha_compress),
@@ -316,6 +325,8 @@ pub mod tests {
     use crate::runtime::tests::ecall_lwa_program;
     use crate::runtime::tests::fibonacci_program;
     use crate::runtime::tests::simple_program;
+    use crate::runtime::Instruction;
+    use crate::runtime::Opcode;
     use crate::runtime::Program;
     use crate::runtime::Runtime;
     use log::debug;
@@ -396,6 +407,53 @@ pub mod tests {
     fn test_ecall_lwa_prove() {
         let program = ecall_lwa_program();
         prove(program);
+    }
+
+    #[test]
+    fn test_sll_prove() {
+        let instructions = vec![
+            Instruction::new(Opcode::ADD, 29, 0, 5, false, true),
+            Instruction::new(Opcode::ADD, 30, 0, 8, false, true),
+            Instruction::new(Opcode::SLL, 31, 30, 29, false, false),
+        ];
+        let program = Program::new(instructions, 0, 0);
+        prove(program);
+    }
+
+    #[test]
+    fn test_sub_prove() {
+        let instructions = vec![
+            Instruction::new(Opcode::ADD, 29, 0, 5, false, true),
+            Instruction::new(Opcode::ADD, 30, 0, 8, false, true),
+            Instruction::new(Opcode::SUB, 31, 30, 29, false, false),
+        ];
+        let program = Program::new(instructions, 0, 0);
+        prove(program);
+    }
+
+    #[test]
+    fn test_add_prove() {
+        let instructions = vec![
+            Instruction::new(Opcode::ADD, 29, 0, 5, false, true),
+            Instruction::new(Opcode::ADD, 30, 0, 8, false, true),
+            Instruction::new(Opcode::ADD, 31, 30, 29, false, false),
+        ];
+        let program = Program::new(instructions, 0, 0);
+        prove(program);
+    }
+
+    #[test]
+    fn test_mul_prove() {
+        let mul_ops = [Opcode::MUL, Opcode::MULH, Opcode::MULHU, Opcode::MULHSU];
+        for mul_op in mul_ops.iter() {
+            let instructions = vec![
+                Instruction::new(Opcode::ADD, 29, 0, 5, false, true),
+                Instruction::new(Opcode::ADD, 30, 0, 8, false, true),
+                Instruction::new(*mul_op, 31, 30, 29, false, false),
+            ];
+            let program = Program::new(instructions, 0, 0);
+            prove(program);
+        }
     }
 
     #[test]
