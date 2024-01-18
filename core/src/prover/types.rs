26ef34173392cf617d32600352a501cff781fdc4
Tamir Hemo <tamir@succinct.xyz>
2024-01-18 11:59:45 -0800

    proof types

--- core/src/prover/types.rs
+++ core/src/prover/types.rs
@@ -3,7 +3,7 @@ use p3_matrix::dense::RowMajorMatrix;
 use p3_uni_stark::StarkConfig;
 
 type Val<SC> = <SC as StarkConfig>::Val;
-type Proof<SC> = <<SC as StarkConfig>::Pcs as Pcs<Val<SC>, ValMat<SC>>>::Proof;
+type OpenningProof<SC> = <<SC as StarkConfig>::Pcs as Pcs<Val<SC>, ValMat<SC>>>::Proof;
 type Challenge<SC> = <SC as StarkConfig>::Challenge;
 type ValMat<SC> = RowMajorMatrix<Val<SC>>;
 type ChallengeMat<SC> = RowMajorMatrix<Challenge<SC>>;
@@ -44,5 +44,6 @@ pub struct SegmentOpenedValues<T> {
 pub struct SegmentProof<SC: StarkConfig> {
     pub commitment: SegmentCommitment<Com<SC>>,
     pub opened_values: SegmentOpenedValues<Val<SC>>,
-    pub openning_proof: Proof<SC>,
+    pub openning_proof: OpenningProof<SC>,
+    pub degree_bits: Vec<usize>,
 }
