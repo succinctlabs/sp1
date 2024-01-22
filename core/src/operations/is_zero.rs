3f350a06f716bed0a225d09885aa52856c1abc95
Hidenori <hidenori@succinct.xyz>
2024-01-22 18:54:04 -0500

    feat: implement `isEqualWordOperation` and use it in `DivRemChip` (#103)

--- core/src/operations/is_zero.rs
+++ core/src/operations/is_zero.rs
@@ -27,25 +27,29 @@ pub struct IsZeroOperation<T> {
 
 impl<F: Field> IsZeroOperation<F> {
     pub fn populate(&mut self, a: u32) -> u32 {
-        if a == 0 {
+        self.populate_from_field_element(F::from_canonical_u32(a))
+    }
+
+    pub fn populate_from_field_element(&mut self, a: F) -> u32 {
+        if a == F::zero() {
             self.inverse = F::zero();
             self.result = F::one();
         } else {
-            self.inverse = F::from_canonical_u32(a).inverse();
+            self.inverse = a.inverse();
             self.result = F::zero();
         }
-        let prod = self.inverse * F::from_canonical_u32(a);
+        let prod = self.inverse * a;
         debug_assert!(prod == F::one() || prod == F::zero());
-        (a == 0) as u32
+        (a == F::zero()) as u32
     }
 
     pub fn eval<AB: CurtaAirBuilder>(
         builder: &mut AB,
-        a: AB::Var,
+        a: AB::Expr,
         cols: IsZeroOperation<AB::Var>,
-        is_real: AB::Var,
+        is_real: AB::Expr,
     ) {
-        builder.assert_bool(is_real);
+        builder.assert_bool(is_real.clone());
         let one: AB::Expr = AB::F::one().into();
 
         // 1. Input == 0 => is_zero = 1 regardless of the inverse.
@@ -58,11 +62,16 @@ impl<F: Field> IsZeroOperation<F> {
 
         // If the input is 0, then any product involving it is 0. If it is nonzero and its inverse
         // is correctly set, then the product is 1.
-        let is_zero = one.clone() - cols.inverse * a;
-        builder.when(is_real).assert_eq(is_zero, cols.result);
-        builder.when(is_real).assert_bool(cols.result);
+        let is_zero = one.clone() - cols.inverse * a.clone();
+        builder
+            .when(is_real.clone())
+            .assert_eq(is_zero, cols.result);
+        builder.when(is_real.clone()).assert_bool(cols.result);
 
         // If the result is 1, then the input is 0.
-        builder.when(is_real).when(cols.result).assert_zero(a);
+        builder
+            .when(is_real.clone())
+            .when(cols.result)
+            .assert_zero(a.clone());
     }
 }
