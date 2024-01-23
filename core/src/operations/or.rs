06279fa8badf380ea76c636f09793ab0983b5139
Hidenori Shinohara <hidenori@succinct.xyz>
2024-01-23 17:05:00 -0500

    or

--- core/src/operations/or.rs
+++ core/src/operations/or.rs
@@ -1,12 +1,16 @@
 use core::borrow::Borrow;
 use core::borrow::BorrowMut;
+use p3_field::AbstractField;
 use p3_field::Field;
 use std::mem::size_of;
 use valida_derive::AlignedBorrow;
 
 use crate::air::CurtaAirBuilder;
 use crate::air::Word;
+use crate::bytes::ByteLookupEvent;
+use crate::bytes::ByteOpcode;
 use crate::disassembler::WORD_SIZE;
+use crate::runtime::Segment;
 
 /// A set of columns needed to compute the or of two words.
 #[derive(AlignedBorrow, Default, Debug, Clone, Copy)]
@@ -17,12 +21,26 @@ pub struct OrOperation<T> {
 }
 
 impl<F: Field> OrOperation<F> {
-    pub fn populate(&mut self, x: u32, y: u32) -> u32 {
+    pub fn populate(&mut self, segment: &mut Segment, x: u32, y: u32) -> u32 {
         let expected = x | y;
         let x_bytes = x.to_le_bytes();
         let y_bytes = y.to_le_bytes();
         for i in 0..WORD_SIZE {
-            self.value[i] = F::from_canonical_u8(x_bytes[i] | y_bytes[i]);
+            let and = x_bytes[i] & y_bytes[i];
+            self.value[i] = F::from_canonical_u8(and);
+
+            let byte_event = ByteLookupEvent {
+                opcode: ByteOpcode::OR,
+                a1: and,
+                a2: 0,
+                b: x_bytes[i],
+                c: y_bytes[i],
+            };
+            segment
+                .byte_lookups
+                .entry(byte_event)
+                .and_modify(|j| *j += 1)
+                .or_insert(1);
         }
         expected
     }
@@ -33,7 +51,16 @@ impl<F: Field> OrOperation<F> {
         a: Word<AB::Var>,
         b: Word<AB::Var>,
         cols: OrOperation<AB::Var>,
+        is_real: AB::Var,
     ) {
-        // TODO:
+        for i in 0..WORD_SIZE {
+            builder.send_byte(
+                AB::F::from_canonical_u32(ByteOpcode::OR as u32),
+                cols.value[i],
+                a[i],
+                b[i],
+                is_real,
+            );
+        }
     }
 }
