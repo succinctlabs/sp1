52c6f1b14d13b06d5f0b20cd5de518495bb0d33b
Hidenori <hidenori@succinct.xyz>
2024-01-24 17:30:43 -0500

    feat: Implement `F_p` division [WIP/Draft] (#115)

--- core/src/operations/field/util.rs
+++ core/src/operations/field/util.rs
@@ -1,3 +1,5 @@
+use std::cmp::{max, min};
+
 use crate::air::polynomial::Polynomial;
 
 use num::BigUint;
@@ -13,6 +15,21 @@ fn biguint_to_field<F: PrimeField32>(num: BigUint) -> F {
         power *= base;
     }
     x
+}
+
+/// Asserts that two polynomials are equal while ignoring leading 0's.
+pub fn assert_eq_polynomial<F: PrimeField32>(p1: &Polynomial<F>, p2: &Polynomial<F>) {
+    let coef1 = p1.coefficients();
+    let coef2 = p2.coefficients();
+    for i in 0..max(coef1.len(), coef2.len()) {
+        if i < min(coef1.len(), coef2.len()) {
+            assert_eq!(coef1[i], coef2[i]);
+        } else if i < coef1.len() {
+            assert_eq!(coef1[i], F::zero());
+        } else {
+            assert_eq!(coef2[i], F::zero());
+        }
+    }
 }
 
 #[inline]
@@ -44,11 +61,8 @@ pub fn compute_root_quotient_and_shift<F: PrimeField32>(
     }
 
     // Sanity Check #2: w(x) * (x - 2^nb_bits_per_limb) = vanishing(x).
-    // let x_minus_root = Polynomial::<F>::from_coefficients_slice(&[-root_monomial, F::one()]);
-    // debug_assert_eq!(
-    //     (&p_quotient * &x_minus_root).coefficients(),
-    //     p_vanishing.coefficients()
-    // );
+    let x_minus_root = Polynomial::<F>::from_coefficients_slice(&[-root_monomial, F::one()]);
+    assert_eq_polynomial(&(&p_quotient * &x_minus_root), p_vanishing);
 
     // Shifting the witness polynomial to make it positive
     p_quotient
