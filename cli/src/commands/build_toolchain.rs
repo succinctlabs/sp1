66881e1ce8d6bbcdd1faab34e89db03c513edd24
John Guibas <john@succinct.xyz>
2024-01-23 16:16:35 -0800

    feat: build-toolchain cli tool

--- cli/src/commands/build_toolchain.rs
+++ cli/src/commands/build_toolchain.rs
@@ -17,6 +17,15 @@ impl BuildToolChainCmd {
             .stdin(Stdio::inherit())
             .output()?;
 
+        // Checkout the correct branch.
+        Command::new("git")
+            .args(["checkout", "succinct"])
+            .stderr(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stdin(Stdio::inherit())
+            .current_dir("rust")
+            .output()?;
+
         // Install our config.toml.
         let config_toml = include_str!("config.toml");
         std::fs::write("rust/config.toml", config_toml)?;
@@ -39,9 +48,32 @@ impl BuildToolChainCmd {
             .current_dir("rust")
             .output()?;
 
+        // Remove the existing toolchain from rustup, if it exists.
+        match Command::new("rustup")
+            .args(["toolchain", "remove", "riscv32im-succinct-zkvm-elf"])
+            .stderr(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stdin(Stdio::inherit())
+            .output()
+        {
+            Ok(_) => println!("Succesfully removed existing toolchain."),
+            Err(_) => println!("No existing toolchain to remove."),
+        }
+
+        let mut toolchain_dir = None;
+        for wentry in std::fs::read_dir("rust/build")? {
+            let entry = wentry?;
+            let toolchain_dir_candidate = entry.path().join("stage2");
+            if toolchain_dir_candidate.is_dir() {
+                toolchain_dir = Some(toolchain_dir_candidate);
+                break;
+            }
+        }
+
         // Link the toolchain to rustup.
         Command::new("rustup")
-            .args(["toolchain", "link", "riscv32im-succinct-zkvm-elf", "build/"])
+            .args(["toolchain", "link", "riscv32im-succinct-zkvm-elf"])
+            .arg(toolchain_dir.unwrap())
             .stderr(Stdio::inherit())
             .stdout(Stdio::inherit())
             .stdin(Stdio::inherit())
