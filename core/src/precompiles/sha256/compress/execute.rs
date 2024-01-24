19504e537fdbd594ec1b0b6079cff6d0d9fc60f0
puma314 <puma314@users.noreply.github.com>
2024-01-24 11:57:39 -0800

    chore: precompile runtime refactor (#112)

--- core/src/precompiles/sha256/compress/execute.rs
+++ core/src/precompiles/sha256/compress/execute.rs
@@ -1,43 +1,23 @@
 use crate::{
-    precompiles::sha256::{ShaCompressEvent, SHA_COMPRESS_K},
-    runtime::{AccessPosition, Register, Runtime},
+    precompiles::{
+        sha256::{ShaCompressEvent, SHA_COMPRESS_K},
+        PrecompileRuntime,
+    },
+    runtime::Register,
 };
 
 use super::ShaCompressChip;
 
 impl ShaCompressChip {
-    pub fn execute(rt: &mut Runtime) -> (u32, u32, u32) {
-        let t0 = Register::X5;
-        let a0 = Register::X10;
-
-        // The number of cycles it takes to perform this precompile.
-        const NB_SHA_COMPRESS_CYCLES: u32 = 8 * 4 + 64 * 4 + 8 * 4;
-
-        // Temporarily set the clock to the number of cycles it takes to perform
-        // this precompile as reading `w_ptr` happens on this clock.
-        rt.clk += NB_SHA_COMPRESS_CYCLES;
-
-        // Read `w_ptr` from register a0 or x5.
-        let w_ptr = rt.register(a0);
-        let mut w = Vec::new();
-        for i in 0..64 {
-            w.push(rt.word(w_ptr + i * 4));
-        }
-
-        // Set the CPU table values with some dummy values.
-        let (fa, fb, fc) = (w_ptr, rt.rr(t0, AccessPosition::B), 0);
-        rt.rw(a0, fa);
-
-        // We'll save the current record and restore it later so that the CPU
-        // event gets emitted correctly.
-        let t = rt.record;
+    pub const NUM_CYCLES: u32 = 8 * 4 + 64 * 4 + 8 * 4;
+    pub fn execute(rt: &mut PrecompileRuntime) -> u32 {
+        // Read `w_ptr` from register a0.
+        let w_ptr = rt.register_unsafe(Register::X10);
 
         // Set the clock back to the original value and begin executing the
         // precompile.
-        rt.clk -= NB_SHA_COMPRESS_CYCLES;
         let saved_clk = rt.clk;
         let saved_w_ptr = w_ptr;
-        let saved_w = w.clone();
         let mut h_read_records = Vec::new();
         let mut w_i_read_records = Vec::new();
         let mut h_write_records = Vec::new();
@@ -46,11 +26,13 @@ impl ShaCompressChip {
         const H_START_IDX: u32 = 64;
         let mut hx = [0u32; 8];
         for i in 0..8 {
-            hx[i] = rt.mr(w_ptr + (H_START_IDX + i as u32) * 4, AccessPosition::Memory);
-            h_read_records.push(rt.record.memory);
+            let (record, value) = rt.mr(w_ptr + (H_START_IDX + i as u32) * 4);
+            h_read_records.push(record);
+            hx[i] = value;
             rt.clk += 4;
         }
 
+        let mut original_w = Vec::new();
         // Execute the "compress" phase.
         let mut a = hx[0];
         let mut b = hx[1];
@@ -63,8 +45,9 @@ impl ShaCompressChip {
         for i in 0..64 {
             let s1 = e.rotate_right(6) ^ e.rotate_right(11) ^ e.rotate_right(25);
             let ch = (e & f) ^ (!e & g);
-            let w_i = rt.mr(w_ptr + i * 4, AccessPosition::Memory);
-            w_i_read_records.push(rt.record.memory);
+            let (record, w_i) = rt.mr(w_ptr + i * 4);
+            original_w.push(w_i);
+            w_i_read_records.push(record);
             let temp1 = h
                 .wrapping_add(s1)
                 .wrapping_add(ch)
@@ -89,30 +72,25 @@ impl ShaCompressChip {
         // Execute the "finalize" phase.
         let v = [a, b, c, d, e, f, g, h];
         for i in 0..8 {
-            rt.mr(w_ptr + (H_START_IDX + i as u32) * 4, AccessPosition::Memory);
-            rt.mw(
+            let record = rt.mw(
                 w_ptr.wrapping_add((H_START_IDX + i as u32) * 4),
                 hx[i].wrapping_add(v[i]),
-                AccessPosition::Memory,
             );
-            h_write_records.push(rt.record.memory);
+            h_write_records.push(record);
             rt.clk += 4;
         }
 
         // Push the SHA extend event.
-        rt.segment.sha_compress_events.push(ShaCompressEvent {
+        rt.segment_mut().sha_compress_events.push(ShaCompressEvent {
             clk: saved_clk,
             w_and_h_ptr: saved_w_ptr,
-            w: saved_w.try_into().unwrap(),
+            w: original_w.try_into().unwrap(),
             h: hx,
             h_read_records: h_read_records.try_into().unwrap(),
             w_i_read_records: w_i_read_records.try_into().unwrap(),
             h_write_records: h_write_records.try_into().unwrap(),
         });
 
-        // Restore the original record.
-        rt.record = t;
-
-        (fa, fb, fc)
+        w_ptr
     }
 }
