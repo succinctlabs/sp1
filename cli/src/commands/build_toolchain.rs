0187330ffb8eeb9347a514c7581f517c83177e58
John Guibas <john@succinct.xyz>
2024-01-25 10:15:53 -0800

    feat: succinct build dir

--- cli/src/commands/build_toolchain.rs
+++ cli/src/commands/build_toolchain.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{path::PathBuf, process::Command};
 
 use anyhow::Result;
 use clap::Parser;
@@ -13,40 +13,47 @@ impl BuildToolchainCmd {
     pub fn run(&self) -> Result<()> {
         // Get enviroment variables.
         let github_access_token = std::env::var("GITHUB_ACCESS_TOKEN");
+        let build_dir = std::env::var("SUCCINCT_BUILD_DIR");
 
         // Clone our rust fork.
-        let repo_url = match github_access_token {
-            Ok(github_access_token) => {
-                println!("Detected GITHUB_ACCESS_TOKEN, using it to clone rust.");
-                format!(
-                    "https://{}@github.com/succinctlabs/rust",
-                    github_access_token
-                )
-            }
+        let rust_dir = match build_dir {
+            Ok(build_dir) => PathBuf::from(build_dir).join("rust"),
             Err(_) => {
-                println!("No GITHUB_ACCESS_TOKEN detected. If you get throttled by Github, set it to bypass the rate limit.");
-                "https://github.com/succinctlabs/rust".to_string()
+                let repo_url = match github_access_token {
+                    Ok(github_access_token) => {
+                        println!("Detected GITHUB_ACCESS_TOKEN, using it to clone rust.");
+                        format!(
+                            "https://{}@github.com/succinctlabs/rust",
+                            github_access_token
+                        )
+                    }
+                    Err(_) => {
+                        println!("No GITHUB_ACCESS_TOKEN detected. If you get throttled by Github, set it to bypass the rate limit.");
+                        "https://github.com/succinctlabs/rust".to_string()
+                    }
+                };
+                Command::new("git").args(["clone", &repo_url]).run()?;
+
+                // Checkout the correct branch.
+                Command::new("git")
+                    .args(["checkout", "riscv32im-succinct-zkvm"])
+                    .current_dir("rust")
+                    .run()?;
+                Command::new("git")
+                    .args(["reset", "--hard"])
+                    .current_dir("rust")
+                    .run()?;
+                Command::new("git")
+                    .args(["submodule", "update", "--init", "--recursive", "--progress"])
+                    .current_dir("rust")
+                    .run()?;
+                PathBuf::from("rust")
             }
         };
-        Command::new("git").args(["clone", &repo_url]).run()?;
-
-        // Checkout the correct branch.
-        Command::new("git")
-            .args(["checkout", "riscv32im-succinct-zkvm"])
-            .current_dir("rust")
-            .run()?;
-        Command::new("git")
-            .args(["reset", "--hard"])
-            .current_dir("rust")
-            .run()?;
-        Command::new("git")
-            .args(["submodule", "update", "--init", "--recursive", "--progress"])
-            .current_dir("rust")
-            .run()?;
 
         // Install our config.toml.
         let config_toml = include_str!("config.toml");
-        std::fs::write("rust/config.toml", config_toml)?;
+        std::fs::write(rust_dir.join("config.toml"), config_toml)?;
 
         // Build the toolchain (stage 1).
         Command::new("python3")
@@ -55,7 +62,7 @@ impl BuildToolchainCmd {
                 "-Cpasses=loweratomic",
             )
             .args(["x.py", "build"])
-            .current_dir("rust")
+            .current_dir(&rust_dir)
             .run()?;
 
         // Build the toolchain (stage 2).
@@ -65,7 +72,7 @@ impl BuildToolchainCmd {
                 "-Cpasses=loweratomic",
             )
             .args(["x.py", "build", "--stage", "2"])
-            .current_dir("rust")
+            .current_dir(&rust_dir)
             .run()?;
 
         // Remove the existing toolchain from rustup, if it exists.
@@ -79,7 +86,7 @@ impl BuildToolchainCmd {
 
         // Find the toolchain directory.
         let mut toolchain_dir = None;
-        for wentry in std::fs::read_dir("rust/build")? {
+        for wentry in std::fs::read_dir(rust_dir.join("build"))? {
             let entry = wentry?;
             let toolchain_dir_candidate = entry.path().join("stage2");
             if toolchain_dir_candidate.is_dir() {
