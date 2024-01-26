f8c5b2473a40bccdc82232121b46cee14da1f54f
John Guibas <john@succinct.xyz>
2024-01-25 21:57:06 -0800

    update toolchain download uls

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -75,10 +75,10 @@ pub fn get_target() -> &'static str {
 #[allow(unreachable_code)]
 pub fn get_toolchain_download_url() -> &'static str {
     #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
-    return "https://succinct-vm-toolchain.s3.amazonaws.com/riscv32im-succinct-zkvm-v2024-01-25.tar.gz";
+    return "https://succinct-vm-toolchain.s3.amazonaws.com/rust-toolchain-x86_64-unknown-linux-gnu.tar.gz";
 
     #[cfg(all(target_arch = "aarch64", target_os = "macos"))]
-    return "https://succinct-vm-toolchain.s3.amazonaws.com/riscv32im-succinct-zkvm-v2024-01-25.tar.gz";
+    return "https://succinct-vm-toolchain.s3.amazonaws.com/rust-toolchain-aarch64-apple-darwin-v2025-01-25.tar.gz";
 
     panic!("Unsupported architecture. Please build the toolchain from source.")
 }
