6e8e2151f50241e52b89364e06239d42b0b9beac
Hidenori Shinohara <hidenori@succinct.xyz>
2024-01-29 17:06:57 -0500

    typo

--- core/src/precompiles/edwards/ed_add.rs
+++ core/src/precompiles/edwards/ed_add.rs
@@ -10,7 +10,7 @@ use crate::operations::field::params::Limbs;
 use crate::operations::field::params::NUM_LIMBS;
 use crate::precompiles::PrecompileRuntime;
 use crate::runtime::Segment;
-use crate::utils::ec::add::create_elliptic_curve_add_event;
+use crate::utils::ec::add::create_ec_add_event;
 use crate::utils::ec::edwards::EdwardsParameters;
 use crate::utils::ec::field::FieldParameters;
 use crate::utils::ec::AffinePoint;
@@ -72,7 +72,7 @@ impl<E: EllipticCurve, EP: EdwardsParameters> EdAddAssignChip<E, EP> {
     }
 
     pub fn execute(rt: &mut PrecompileRuntime) -> u32 {
-        let event = create_elliptic_curve_add_event::<E>(rt);
+        let event = create_ec_add_event::<E>(rt);
         rt.segment_mut().ed_add_events.push(event);
         event.p_ptr + 1
     }
