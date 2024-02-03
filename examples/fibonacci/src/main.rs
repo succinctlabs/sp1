a0ba29811e76f64c39cf4103fe2a4ef6c19bcf8b
Tamir Hemo <tamir@succinct.xyz>
2024-02-02 17:00:59 -0800

    back to main

--- examples/fibonacci/src/main.rs
+++ examples/fibonacci/src/main.rs
@@ -1,12 +1,11 @@
 use succinct_core::{utils, SuccinctProver};
 
 const FIBONACCI_ELF: &[u8] =
-    include_bytes!("../../../programs/ed25519/elf/riscv32im-succinct-zkvm-elf");
+    include_bytes!("../../../programs/fibonacci/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
-    std::env::set_var("RUST_TRACER", "info");
-    // utils::setup_logger();
-    utils::setup_tracer();
+    std::env::set_var("RUST_LOG", "info");
+    utils::setup_logger();
     let prover = SuccinctProver::new();
     prover.prove(FIBONACCI_ELF);
 }
