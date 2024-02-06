75502a90c7713535f75a68458c0d7abfa58925a7
Hidenori Shinohara <hidenori@succinct.xyz>
2024-02-06 17:47:37 -0500

    remove the stuff in execute.rs

--- core/src/precompiles/poseidon2/external/execute.rs
+++ core/src/precompiles/poseidon2/external/execute.rs
@@ -6,7 +6,10 @@ use p3_field::{AbstractField, PrimeField32};
 use crate::{
     cpu::{MemoryReadRecord, MemoryWriteRecord},
     precompiles::{
-        poseidon2::{external::columns::POSEIDON2_SBOX_EXPONENT, Poseidon2ExternalEvent},
+        poseidon2::{
+            external::columns::POSEIDON2_SBOX_EXPONENT, external_linear_permute_mut,
+            Poseidon2ExternalEvent,
+        },
         PrecompileRuntime,
     },
     runtime::Register,
@@ -16,83 +19,6 @@ use super::{
     columns::{POSEIDON2_DEFAULT_FIRST_EXTERNAL_ROUNDS, POSEIDON2_ROUND_CONSTANTS},
     Poseidon2ExternalChip,
 };
-
-/// Implements the permutation given by the matrix:
-///  ```ignore
-///     M4 = [[5, 7, 1, 3],
-///           [4, 6, 1, 1],
-///           [1, 3, 5, 7],
-///           [1, 1, 4, 6]];
-///   ```
-fn m4_permute_mut<T>(input: &mut [T; 4])
-where
-    T: Add<Output = T> + Default + Clone,
-{
-    // Implements the permutation given by the matrix M4 with multiplications unrolled as
-    // additions and doublings.
-    let mut t_0 = input[0].clone();
-    t_0 = t_0 + input[1].clone();
-    let mut t_1 = input[2].clone();
-    t_1 = t_1 + input[3].clone();
-    let mut t_2 = input[1].clone();
-    t_2 = t_2.clone() + t_2.clone();
-    t_2 = t_2.clone() + t_1.clone();
-    let mut t_3 = input[3].clone();
-    t_3 = t_3.clone() + t_3.clone();
-    t_3 = t_3.clone() + t_0.clone();
-    let mut t_4 = t_1.clone();
-    t_4 = t_4.clone() + t_4.clone();
-    t_4 = t_4.clone() + t_4.clone();
-    t_4 = t_4.clone() + t_3.clone();
-    let mut t_5 = t_0.clone();
-    t_5 = t_5.clone() + t_5.clone();
-    t_5 = t_5.clone() + t_5.clone();
-    t_5 = t_5.clone() + t_2.clone();
-    let mut t_6 = t_3.clone();
-    t_6 = t_6.clone() + t_5.clone();
-    let mut t_7 = t_2.clone();
-    t_7 = t_7.clone() + t_4.clone();
-    input[0] = t_6;
-    input[1] = t_5;
-    input[2] = t_7;
-    input[3] = t_4;
-}
-
-fn matmul_m4<T, const NUM_WORDS_STATE: usize>(input: &mut [T; NUM_WORDS_STATE])
-where
-    T: Add<Output = T> + Default + Clone,
-{
-    input
-        .chunks_exact_mut(4)
-        .for_each(|x| m4_permute_mut(x.try_into().unwrap()));
-}
-
-pub fn external_linear_permute_mut<T, const NUM_WORDS_STATE: usize>(
-    input: &mut [T; NUM_WORDS_STATE],
-) where
-    T: Add<Output = T> + Default + Clone,
-{
-    match NUM_WORDS_STATE {
-        16 => {
-            // First, apply Diag(M4, ..., M4).
-            matmul_m4(input);
-
-            let t4 = NUM_WORDS_STATE / 4;
-            // Four 0's.
-            let mut stored = [T::default(), T::default(), T::default(), T::default()];
-            for l in 0..4 {
-                stored[l] = input[l].clone();
-                for j in 1..t4 {
-                    stored[l] = stored[l].clone() + input[j * 4 + l].clone();
-                }
-            }
-            for i in 0..NUM_WORDS_STATE {
-                input[i] = input[i].clone() + stored[i % 4].clone();
-            }
-        }
-        _ => unimplemented!(),
-    }
-}
 
 /// Poseidon2 external precompile execution. `NUM_WORDS_STATE` is the number of words in the state.
 impl<const NUM_WORDS_STATE: usize> Poseidon2ExternalChip<NUM_WORDS_STATE> {
