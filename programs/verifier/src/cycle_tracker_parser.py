8c467ce9f2b439d249d5d8d90e679c7ea0855e6b
Kevin Jue <kjue235@gmail.com>
2024-02-06 14:13:08 -0800

    cycle tracker parser

--- programs/verifier/src/cycle_tracker_parser.py
+++ programs/verifier/src/cycle_tracker_parser.py
@@ -1,13 +1,9 @@
 import argparse
 
-class Labels:
+class Label:
     def __init__(self, label):
         self.label = label
         self.children = {}
-        self.num_cycles = 0
-
-class Stats:
-    def __init__(self):
         self.num_total_cycles = 0
         self.num_calls = 0
 
@@ -24,11 +20,11 @@ def parse_file(tracking_filename, top_level_label):
             cycle_count = 0
 
             if '┌╴' in line:
-                line_label = line.split('┌╴')[1]
+                line_label = line.split('┌╴')[1].strip()
                 is_label_line = True
 
-            if '└─' in line:
-                cycle_count = int(line.split('└─')[1])
+            if '└╴' in line:
+                cycle_count = int(line.split('└╴')[1].replace(',', '').replace(" cycles", '').strip())
                 is_cycle_line = True
 
             if is_label_line:
@@ -38,31 +34,48 @@ def parse_file(tracking_filename, top_level_label):
                     if top_level_label != line_label:
                         continue
                     else:
-                        label = Labels(line_label)
+                        label = Label(line_label)
                         label_stack.append(label)
                 else: # The stack is not empty.  Add to the stack.
-                    label = Labels(line_label)
+                    label = Label(line_label)
                     label_stack.append(label)
+                continue
 
             if is_cycle_line:
                 if len(label_stack) == 0:
                     continue
 
                 label = label_stack.pop()
-                label.num_cycles += cycle_count
+                label.num_total_cycles += cycle_count
+                label.num_calls += 1
 
                 if len(label_stack) > 0:
                     # Add to the parent's children
                     parent_label = label_stack[-1]
                     if label.label not in parent_label.children:
-                        parent_label.children[label.label] = Stats()
-    
-                    parent_label.children[label.label].num_calls += 1
-                    parent_label.children[label.label].num_total_cycles += label.num_cycles
+                        parent_label.children[label.label] = label
+                    else:
+                        parent_label.children[label.label].num_total_cycles += label.num_total_cycles
+                        parent_label.children[label.label].num_calls += label.num_calls   
+
+                        for child in label.children:
+                            if child not in parent_label.children[label.label].children:
+                                parent_label.children[label.label].children[child] = label.children[child]
+                            else:
+                                parent_label.children[label.label].children[child].num_total_cycles += label.children[child].num_total_cycles
+                                parent_label.children[label.label].children[child].num_calls += label.children[child].num_calls
                 else:
                     top_level_labels.append(label)
+                continue
 
     return top_level_labels
+
+
+def print_label(label, num_tabs):
+    print('\t' * num_tabs + label.label + ' ' + str(label.num_calls) + ' ' + str(label.num_total_cycles))
+    for child in label.children:
+        print_label(label.children[child], num_tabs + 1)
+
 
 def main():
     parser = argparse.ArgumentParser(description='Aggregate cycle tracker numbers.')
@@ -73,9 +86,10 @@ def main():
     # Parse the command-line arguments
     args = parser.parse_args()
 
-    parse_file(args.tracking_filename, args.top_level_label)
+    top_level_labels = parse_file(args.tracking_filename, args.top_level_label)
 
-
+    for top_level_label in top_level_labels:
+        print_label(top_level_label, 0)
 
 if __name__ == '__main__':
     main()
