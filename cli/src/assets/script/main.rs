2947bc8d1fae0673aa2ba9ecb1e71b06f0f4140f
John Guibas <jtguibas@Johns-MBP.monkeybrains.net>
2024-02-09 15:15:06 -0800

    changes

---
+++ cli/src/assets/script/main.rs
@@ -0,0 +1,10 @@
+use succinct_core::{utils, SuccinctProver};
+
+const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+
+fn main() {
+    std::env::set_var("RUST_LOG", "info");
+    utils::setup_logger();
+    let prover = SuccinctProver::new();
+    prover.run_and_prove(ELF);
+}
