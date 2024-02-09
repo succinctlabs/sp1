324982ff950ae7ee9fc2876f300202fdb92fa106
Tamir Hemo <tamir@succinct.xyz>
2024-02-08 16:54:52 -0800

    machine

--- core/src/stark/mod.rs
+++ core/src/stark/mod.rs
@@ -1,6 +1,7 @@
 mod config;
 mod debug;
 mod folder;
+mod machine;
 mod permutation;
 mod prover;
 mod runtime;
@@ -12,6 +13,7 @@ mod zerofier_coset;
 pub use config::*;
 pub use debug::*;
 pub use folder::*;
+pub use machine::*;
 pub use prover::*;
 pub use types::*;
 pub use verifier::*;
