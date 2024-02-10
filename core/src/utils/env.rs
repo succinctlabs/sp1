4e0e9f83ce22f8dccd3fb08d1a0e0ed1ca2ceec8
John Guibas <jtguibas@Johns-MBP.monkeybrains.net>
2024-02-10 02:31:01 -0800

    hm

--- core/src/utils/env.rs
+++ core/src/utils/env.rs
@@ -14,7 +14,7 @@ pub fn shard_size() -> usize {
 pub fn save_disk_threshold() -> usize {
     let value = match std::env::var("SAVE_DISK_THRESHOLD") {
         Ok(val) => val.parse().unwrap(),
-        Err(_) => 128,
+        Err(_) => 256,
     };
     value
 }
