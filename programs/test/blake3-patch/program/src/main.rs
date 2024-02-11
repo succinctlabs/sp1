0ffda4d1599a4c8c66d852e46fe6f577a3cfa5a1
Hidenori Shinohara <hidenori@succinct.xyz>
2024-02-11 13:48:53 -0500

    nonzero cumulative sum (interaction error?)

--- programs/test/blake3-patch/program/src/main.rs
+++ programs/test/blake3-patch/program/src/main.rs
@@ -6,16 +6,7 @@ curta_zkvm::entrypoint!(main);
 use blake3;
 
 pub fn main() {
-    let n = curta_zkvm::io::read::<u32>();
-    let mut a = 0;
-    let mut b = 1;
-    let mut sum;
-    for _ in 1..n {
-        sum = a + b;
-        a = b;
-        b = sum;
-    }
-
-    curta_zkvm::io::write(&a);
-    curta_zkvm::io::write(&b);
+    let a : u32 = 0;
+    let bytes = a.to_le_bytes();
+    let hash = blake3::hash(&bytes);
 }
