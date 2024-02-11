6135b82639375515f673d85a491d57e1bbd00c0b
Hidenori Shinohara <hidenori@succinct.xyz>
2024-02-11 16:05:22 -0500

    another println

--- programs/test/blake3-patch/program/src/main.rs
+++ programs/test/blake3-patch/program/src/main.rs
@@ -6,6 +6,7 @@ curta_zkvm::entrypoint!(main);
 use blake3;
 
 pub fn main() {
+    println!("testing if patching is done correctly");
     let a : u32 = 0;
     let bytes = a.to_le_bytes();
     let hash = blake3::hash(&bytes);
