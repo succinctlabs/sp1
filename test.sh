33d2de9c5daeb9c2265e417d3afe27385d3216a3
Hidenori Shinohara <hidenori@succinct.xyz>
2024-02-12 15:26:44 -0500

    remove test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-cargo test --package curta-core --lib --release -- operations::field::fp_sqrt::tests --nocapture && \
-    cargo test --package curta-core --lib --release -- operations::field::fp_op::tests --nocapture && \
-    cargo test --package curta-core --lib --release -- operations::field::fp_op::tests --nocapture && \
-    cargo test --package curta-core --lib --release -- operations::field::fp_inner_product::tests --nocapture && \
-    cargo test --package curta-core --lib --release -- operations::field::fp_den::tests --nocapture 
