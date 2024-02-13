473ffc5a29a77ffabc7cd67f50a96de0d4739570
Hidenori Shinohara <hidenori@succinct.xyz>
2024-02-13 16:08:21 -0500

    remove test.sh

--- test.sh
+++
@@ -1,2 +0,0 @@
-cargo test --package curta-core --lib --release -- syscall::precompiles::sha256::compress::compress_tests --nocapture && \
-    cargo test --package curta-core --lib --release -- syscall::precompiles::sha256::extend::extend_tests --nocapture  
