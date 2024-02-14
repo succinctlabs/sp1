136af378099e697741bc4746e755f63c50fc09a9
Kevin Jue <kjue235@gmail.com>
2024-02-14 06:05:30 -0800

    changing file locations

--- programs/demo/verifier/src/verifier.rs
+++ programs/demo/verifier/src/verifier.rs
@@ -1,19 +1,15 @@
-#![no_main]
-curta_zkvm::entrypoint!(main);
-
 use curta_core::utils::BabyBearBlake3;
 use curta_core::{CurtaProofWithIO, CurtaVerifier};
 
-const FIBONACCI_ELF: &[u8] =
-    include_bytes!("../../../programs/demo/fibonacci/elf/riscv32im-curta-zkvm-elf");
+const FIBONACCI_ELF: &[u8] = include_bytes!("../../fibonacci/elf/riscv32im-curta-zkvm-elf");
 
 pub fn main() {
-    let proof_str = include_str!("../../../examples/fibonacci/proof-with-pis.json");
+    let proof_str = include_str!("../../../../examples/fibonacci/proof-with-pis.json");
     let proof: CurtaProofWithIO<BabyBearBlake3> =
         serde_json::from_str(proof_str).expect("loading proof failed");
 
     // Verify proof.
-    println!("cycle-tracker-start: verify");
     CurtaVerifier::verify(FIBONACCI_ELF, &proof).expect("verification failed");
-    println!("cycle-tracker-end: verify");
+
+    println!("verification succeeded");
 }
