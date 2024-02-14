de0cbe79d8da5668b6045e62f0788bc6d715a23d
Kevin Jue <kjue235@gmail.com>
2024-02-14 10:47:40 -0800

    added fixture

--- examples/verifier/program/src/verifier.rs
+++ examples/verifier/program/src/verifier.rs
@@ -5,7 +5,7 @@ const FIBONACCI_ELF: &[u8] =
     include_bytes!("../../../../examples/fibonacci/program/elf/riscv32im-succinct-zkvm-elf");
 
 pub fn main() {
-    let proof_str = include_str!("./fixtures/proof-with-pis.json");
+    let proof_str = include_str!("./fixtures/fib-proof-with-pis.json");
     let proof: SP1ProofWithIO<BabyBearBlake3> =
         serde_json::from_str(proof_str).expect("loading proof failed");
 
