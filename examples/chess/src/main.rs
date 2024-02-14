0e81e6106fb2e97b628922dedb02d683d275cc27
John Guibas <jtguibas@Johns-MacBook-Pro.local>
2024-02-13 21:15:41 -0800

    fix: compilation issues

--- examples/chess/src/main.rs
+++ examples/chess/src/main.rs
@@ -1,27 +1,26 @@
-use curta_core::{CurtaProver, CurtaStdin, CurtaVerifier};
+use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
 
-const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+const ELF: &[u8] = include_bytes!("../../../programs/demo/chess/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
-    let mut stdin = CurtaStdin::new();
+    let mut stdin = SP1Stdin::new();
 
     // FEN representation of a chessboard in its initial state
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
+    stdin.write(&fen);
+
     // SAN representation Queen's pawn opening
     let san = "d4".to_string();
-
-    stdin.write(&fen);
     stdin.write(&san);
 
-    let mut proof = CurtaProver::prove(ELF, stdin).expect("proving failed");
+    let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
 
     // Read output.
     let is_valid_move = proof.stdout.read::<bool>();
-
     println!("is_valid_move: {}", is_valid_move);
 
     // Verify proof.
-    CurtaVerifier::verify(ELF, &proof).expect("verification failed");
+    SP1Verifier::verify(ELF, &proof).expect("verification failed");
 
     // Save proof.
     proof
