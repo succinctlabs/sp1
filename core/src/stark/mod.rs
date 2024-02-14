9d03646e6d790be0786cc73b44872a69ad7429d7
Tamir Hemo <tamir@succinct.xyz>
2024-02-14 15:57:07 -0800

    checkpoint

--- core/src/stark/mod.rs
+++ core/src/stark/mod.rs
@@ -1,3 +1,4 @@
+mod air;
 mod chip;
 mod config;
 mod debug;
@@ -11,6 +12,7 @@ mod util;
 mod verifier;
 mod zerofier_coset;
 
+pub use air::*;
 pub use chip::*;
 pub use config::*;
 pub use debug::*;
@@ -21,6 +23,8 @@ pub use prover::*;
 pub use quotient::*;
 pub use types::*;
 pub use verifier::*;
+
+pub(crate) use air::riscv_chips;
 
 #[cfg(test)]
 pub use machine::tests;
