a068d2bcaab30e21ae3d259170e7c9db22979d1c
Kevin Jue <kjue235@gmail.com>
2024-02-13 17:18:13 -0800

    changes

--- verifier/program/src/main.rs
+++ verifier/program/src/main.rs
@@ -4,9 +4,11 @@ succinct_zkvm::entrypoint!(main);
 use curta_core::utils::BabyBearBlake3;
 use curta_core::{CurtaProofWithIO, CurtaVerifier};
 
+const ELF: &[u8] = include_bytes!("../../programs/demo/fibonacci-io/elf/riscv32im-curta-zkvm-elf");
+
 pub fn main() {
     
-    let proof_str = include_str!("../../../examples/ed25519/proof-with-pis.json");
+    let proof_str = include_str!("../../../examples/fibonacci-io/proof-with-pis.json");
     let new_proof: CurtaProofWithIO<BabyBearBlake3> =
         serde_json::from_str(proof_str).expect("loading proof failed");
     CurtaVerifier::verify(ED25519_ELF, &new_proof).expect("verification failed");
