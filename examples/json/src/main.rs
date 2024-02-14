a018aee7a8828d21bcd23c6b839c8a2999bcb47b
John Guibas <jtguibas@Johns-MacBook-Pro.local>
2024-02-13 20:11:11 -0800

    fix ci

--- examples/json/src/main.rs
+++ examples/json/src/main.rs
@@ -1,14 +1,14 @@
 //! A simple script to generate and verify the proof of a given program.
-use curta_core::{utils, CurtaProver, CurtaStdin, CurtaVerifier};
+use sp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
 
-const JSON_ELF: &[u8] = include_bytes!("../../../programs/demo/json/elf/riscv32im-curta-zkvm-elf");
+const JSON_ELF: &[u8] = include_bytes!("../../../programs/demo/json/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
     // setup tracer for logging.
     utils::setup_tracer();
 
     // Generate proof.
-    let mut stdin = CurtaStdin::new();
+    let mut stdin = SP1Stdin::new();
 
     // Sample JSON as a string input.
     let data_str = r#"
@@ -23,14 +23,14 @@ fn main() {
     stdin.write(&data_str);
     stdin.write(&key);
 
-    let mut proof = CurtaProver::prove(JSON_ELF, stdin).expect("proving failed");
+    let mut proof = SP1Prover::prove(JSON_ELF, stdin).expect("proving failed");
 
     // Read output.
     let val = proof.stdout.read::<String>();
     println!("Value of {} is {}", key, val);
 
     // Verify proof.
-    CurtaVerifier::verify(JSON_ELF, &proof).expect("verification failed");
+    SP1Verifier::verify(JSON_ELF, &proof).expect("verification failed");
 
     // Save proof.
     proof
