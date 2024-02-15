314527f62ab52f05888ca8a39476c71dfdc12d7c
dr-frmr <docterformer@protonmail.com>
2024-02-15 11:32:33 -0300

    edit fibonacci example to use `u128` and note overflow case in quickstart

--- cli/src/assets/script/main.rs
+++ cli/src/assets/script/main.rs
@@ -7,13 +7,13 @@ const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf
 fn main() {
     // Generate proof.
     let mut stdin = SP1Stdin::new();
-    let n = 500u32;
+    let n = 186u32;
     stdin.write(&n);
     let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
 
     // Read output.
-    let a = proof.stdout.read::<u32>();
-    let b = proof.stdout.read::<u32>();
+    let a = proof.stdout.read::<u128>();
+    let b = proof.stdout.read::<u128>();
     println!("a: {}", a);
     println!("b: {}", b);
 
