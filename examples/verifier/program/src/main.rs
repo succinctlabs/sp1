a55e81de48434097b29bd65fe9c521fef1dad963
Kevin Jue <kjue235@gmail.com>
2024-02-14 17:54:50 -0800

    new proof

--- examples/verifier/program/src/main.rs
+++ examples/verifier/program/src/main.rs
@@ -5,7 +5,7 @@ use sp1_core::utils::BabyBearBlake3;
 use sp1_core::{SP1ProofWithIO, SP1Verifier};
 
 const FIBONACCI_ELF: &[u8] =
-    include_bytes!("../../../../examples/fibonacci/program/elf/riscv32im-succinct-zkvm-elf");
+    include_bytes!("../../../../examples/fibonacci-io/program/elf/riscv32im-succinct-zkvm-elf");
 
 pub fn main() {
     let proof_str = include_str!("./fixtures/fib-proof-with-pis.json");
