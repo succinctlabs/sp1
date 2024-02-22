71c1cae11c9d8ec2f781417f83bee4bd0ba36d45
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-02-21 17:23:08 -0800

    custom json

--- examples/json/program/src/main.rs
+++ examples/json/program/src/main.rs
@@ -1,12 +1,37 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use serde_json::Value;
+use lib::{Account, Transaction}; // Custom structs.
+use serde_json::Value; // Generic JSON.
 
 fn main() {
+    // read generic JSON example inputs.
     let data_str = sp1_zkvm::io::read::<String>();
     let key = sp1_zkvm::io::read::<String>();
+
+    // read custom struct example inputs.
+    let mut old_account_state = sp1_zkvm::io::read::<Account>();
+    println!(
+        "account: {}",
+        serde_json::to_string(&old_account_state).unwrap()
+    );
+    let txs = sp1_zkvm::io::read::<Vec<Transaction>>();
+
+    // do stuff with generic JSON.
     let v: Value = serde_json::from_str(&data_str).unwrap();
     let val = &v[key];
+
+    // do stuff with custom struct.
+    let new_account_state = &mut old_account_state;
+    for tx in txs {
+        if tx.from == new_account_state.account_name {
+            new_account_state.balance -= tx.amount;
+        }
+        if tx.to == new_account_state.account_name {
+            new_account_state.balance += tx.amount;
+        }
+    }
+    let account_str = serde_json::to_string(&new_account_state).unwrap();
     sp1_zkvm::io::write(&val);
+    sp1_zkvm::io::write(&account_str);
 }
