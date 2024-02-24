1cbc074ecf17cc605917af9616800410d5573538
ratan kaliani <ratan@succinct.xyz>
2024-02-23 16:16:50 -0800

    perf

--- core/src/alu/mul/mod.rs
+++ core/src/alu/mul/mod.rs
@@ -430,7 +430,7 @@ mod tests {
     fn generate_trace_mul() {
         let mut shard = ExecutionRecord::default();
 
-        // Fill mul_events with 10^6 MULHSU events.
+        // Fill mul_events with 10^7 MULHSU events.
         let mut mul_events: Vec<AluEvent> = Vec::new();
         for _ in 0..10i32.pow(7) {
             mul_events.push(AluEvent::new(
