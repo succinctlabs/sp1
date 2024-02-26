e1653b87852e04b3ad24aabdf9d1a90e1a1c8666
i-m-aditya <prblmslvr.aditya@gmail.com>
2024-02-26 16:16:18 +0530

    optimizing code, fetching data from rpc and optizing search to O(nlogn)

--- examples/tendermint/program/src/main.rs
+++ examples/tendermint/program/src/main.rs
@@ -3,12 +3,11 @@ sp1_zkvm::entrypoint!(main);
 
 use core::time::Duration;
 use serde::Deserialize;
-use tendermint::{node::Id, validator::Info};
+use tendermint::validator::Info;
 use tendermint_light_client_verifier::{
-    options::Options,
-    types::{LightBlock, SignedHeader, ValidatorSet},
-    ProdVerifier, Verdict, Verifier,
+    options::Options, types::SignedHeader, ProdVerifier, Verdict, Verifier,
 };
+use utils::generate_light_block_at_given_block_height;
 mod utils;
 
 #[derive(Debug, Deserialize)]
@@ -34,70 +33,22 @@ pub struct BlockValidatorSet {
     pub total: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let peer_id: [u8; 20] = [
         0x72, 0x6b, 0xc8, 0xd2, 0x60, 0x38, 0x7c, 0xf5, 0x6e, 0xcf, 0xad, 0x3a, 0x6b, 0xf6, 0xfe,
         0xcd, 0x90, 0x3e, 0x18, 0xa2,
     ];
-    // Generate the Light Block's without testgen
-    let file_content = include_bytes!("./fixtures/1/signed_header.json");
-    let file_content_str =
-        core::str::from_utf8(file_content).expect("Failed to convert file content to string");
 
-    let commit_response: CommitResponse =
-        serde_json::from_str(file_content_str).expect("Failed to parse JSON");
-    let signed_header = commit_response.result.signed_header;
-
-    let file_content = include_bytes!("./fixtures/1/validators.json");
-    let file_content_str =
-        core::str::from_utf8(file_content).expect("Failed to convert file content to string");
-    let validators_response: ValidatorSetResponse =
-        serde_json::from_str(file_content_str).expect("Failed to parse JSON");
-    let validators = validators_response.result;
-    let validators = ValidatorSet::new(validators.validators, None);
-
-    let file_content = include_bytes!("./fixtures/1/next_validators.json");
-    let file_content_str =
-        core::str::from_utf8(file_content).expect("Failed to convert file content to string");
-    let next_validators_response: ValidatorSetResponse =
-        serde_json::from_str(file_content_str).expect("Failed to parse JSON");
-    let next_validators = next_validators_response.result;
-    let next_validators = ValidatorSet::new(next_validators.validators, None);
+    let light_block_1 = generate_light_block_at_given_block_height(10000, peer_id)
+        .await
+        .expect("Failed to generate light block 1");
 
     // Create a default light block with a valid chain-id for height `1` with a timestamp 20
     // secs before now (to be treated as trusted state)
-    let light_block_1: LightBlock =
-        LightBlock::new(signed_header, validators, next_validators, Id::new(peer_id));
-
-    // // Generate the Light Block's without testgen
-    let file_content = include_bytes!("./fixtures/2/signed_header.json");
-    let file_content_str =
-        core::str::from_utf8(file_content).expect("Failed to convert file content to string");
-
-    let commit_response: CommitResponse =
-        serde_json::from_str(file_content_str).expect("Failed to parse JSON");
-    let signed_header = commit_response.result.signed_header;
-
-    let file_content = include_bytes!("./fixtures/2/validators.json");
-    let file_content_str =
-        core::str::from_utf8(file_content).expect("Failed to convert file content to string");
-    let validators_response: ValidatorSetResponse =
-        serde_json::from_str(file_content_str).expect("Failed to parse JSON");
-    let validators = validators_response.result;
-    let validators = ValidatorSet::new(validators.validators, None);
-
-    let file_content = include_bytes!("./fixtures/2/next_validators.json");
-    let file_content_str =
-        core::str::from_utf8(file_content).expect("Failed to convert file content to string");
-    let next_validators_response: ValidatorSetResponse =
-        serde_json::from_str(file_content_str).expect("Failed to parse JSON");
-    let next_validators = next_validators_response.result;
-    let next_validators = ValidatorSet::new(next_validators.validators, None);
-
-    // Create a default light block with a valid chain-id for height `1` with a timestamp 20
-    // secs before now (to be treated as trusted state)
-    let light_block_2: LightBlock =
-        LightBlock::new(signed_header, validators, next_validators, Id::new(peer_id));
+    let light_block_2 = generate_light_block_at_given_block_height(10020, peer_id)
+        .await
+        .expect("Failed to generate light block 2");
 
     let vp = ProdVerifier::default();
     let opt = Options {
--- examples/tendermint/program/src/utils.rs
+++ examples/tendermint/program/src/utils.rs
@@ -1,26 +1,89 @@
-use tendermint::block::CommitSig;
+#![allow(dead_code)]
+use std::collections::HashMap;
+use std::error::Error;
 
-use crate::BlockValidatorSet;
+use reqwest::Client;
+use tendermint::{block::signed_header::SignedHeader, node::Id, validator::Set};
+use tendermint_light_client_verifier::types::{LightBlock, ValidatorSet};
 
-pub fn filter_top_signatures_by_power(
-    signatures: Vec<CommitSig>,
-    validators: &BlockValidatorSet,
-) -> Vec<CommitSig> {
-    let mut signatures_by_desc_power = signatures.clone();
-    signatures_by_desc_power.sort_by(|a, b| {
-        let validator_a = validators
-            .validators
-            .iter()
-            .find(|v| v.address == a.validator_address().unwrap())
-            .unwrap();
+use crate::{BlockValidatorSet, CommitResponse, ValidatorSetResponse};
 
-        let validator_b = validators
-            .validators
-            .iter()
-            .find(|v| v.address == b.validator_address().unwrap())
-            .unwrap();
+pub fn sort_signatures_by_validators_power_desc(
+    signed_header: &mut SignedHeader,
+    validators_set: &ValidatorSet,
+) {
+    let validator_powers: HashMap<_, _> = validators_set
+        .validators()
+        .iter()
+        .map(|v| (v.address, v.power()))
+        .collect();
 
-        validator_b.power().cmp(&validator_a.power())
+    signed_header.commit.signatures.sort_by(|a, b| {
+        let power_a = a
+            .validator_address()
+            .and_then(|addr| validator_powers.get(&addr))
+            .unwrap_or(&0);
+        let power_b = b
+            .validator_address()
+            .and_then(|addr| validator_powers.get(&addr))
+            .unwrap_or(&0);
+        power_b.cmp(power_a)
     });
-    signatures_by_desc_power
+}
+
+pub async fn fetch_json<T>(
+    client: &Client,
+    url: &str,
+    block_height: u64,
+) -> Result<T, Box<dyn Error>>
+where
+    T: serde::de::DeserializeOwned,
+{
+    let response = client
+        .get(url)
+        .query(&[("height", block_height.to_string().as_str())])
+        .send()
+        .await?
+        .json::<T>()
+        .await?;
+    Ok(response)
+}
+
+pub async fn generate_light_block_at_given_block_height(
+    block_height: u64,
+    peer_id: [u8; 20],
+) -> Result<LightBlock, Box<dyn Error>> {
+    let client = Client::new();
+    const BASE_URL: &str = "https://celestia-mocha-rpc.publicnode.com:443";
+
+    let commit_response =
+        fetch_json::<CommitResponse>(&client, &format!("{}/commit", BASE_URL), block_height)
+            .await?;
+    let mut signed_header = commit_response.result.signed_header;
+
+    let validator_response = fetch_json::<ValidatorSetResponse>(
+        &client,
+        &format!("{}/validators", BASE_URL),
+        block_height,
+    )
+    .await?;
+    let block_validator_set: BlockValidatorSet = validator_response.result;
+    let validators = Set::new(block_validator_set.validators, None);
+
+    let next_validator_response = fetch_json::<ValidatorSetResponse>(
+        &client,
+        &format!("{}/validators", BASE_URL),
+        block_height + 1,
+    )
+    .await?;
+    let next_block_validator_set = next_validator_response.result;
+    let next_validators = Set::new(next_block_validator_set.validators, None);
+
+    sort_signatures_by_validators_power_desc(&mut signed_header, &validators);
+    Ok(LightBlock::new(
+        signed_header,
+        validators,
+        next_validators,
+        Id::new(peer_id),
+    ))
 }
