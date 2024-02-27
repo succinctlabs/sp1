2170e50a6e4419b92e494648a2c4c596ec8de692
Tamir Hemo <tamir@succinct.xyz>
2024-02-27 15:33:09 -0800

    lifetime

--- recursion/primitives/src/lib.rs
+++ recursion/primitives/src/lib.rs
@@ -18,8 +18,8 @@ impl<SC: StarkGenericConfig> RecursiveVerifier<SC> {
         RecursiveVerifier(PhantomData)
     }
 
-    pub fn verify_shard<'a, 'b: 'a>(
-        machine: &'b RiscvStark<'a, SC>,
+    pub fn verify_shard(
+        machine: &RiscvStark<SC>,
         challenger: &mut SC::Challenger,
         proof: &ShardProof<SC>,
     ) {
