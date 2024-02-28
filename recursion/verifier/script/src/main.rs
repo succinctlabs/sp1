edf830e4470ae3c1ca86653e49eb24539ab0d4e1
Tamir Hemo <tamir@succinct.xyz>
2024-02-27 17:52:06 -0800

    init script

--- recursion/verifier/script/src/main.rs
+++ recursion/verifier/script/src/main.rs
@@ -2,28 +2,12 @@
 
 use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
 
-const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+const VERIFIER_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+
+const PROGRAM_ELF: &[u8] =
+    include_bytes!("../../../examples/fibonacci/program/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
-    // Generate proof.
-    let mut stdin = SP1Stdin::new();
-    let n = 186u32;
-    stdin.write(&n);
-    let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
-
-    // Read output.
-    let a = proof.stdout.read::<u128>();
-    let b = proof.stdout.read::<u128>();
-    println!("a: {}", a);
-    println!("b: {}", b);
-
-    // Verify proof.
-    SP1Verifier::verify(ELF, &proof).expect("verification failed");
-
-    // Save proof.
-    proof
-        .save("proof-with-io.json")
-        .expect("saving proof failed");
-
-    println!("succesfully generated and verified proof for the program!")
+    // Get the program ELF.
+    let program = Program::from(PROGRAM_ELF);
 }
