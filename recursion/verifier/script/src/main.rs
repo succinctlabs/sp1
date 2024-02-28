111d1e5c40ccaac0af96724480992ce654901d25
Tamir Hemo <tamir@succinct.xyz>
2024-02-28 14:16:44 -0800

    updated script

--- recursion/verifier/script/src/main.rs
+++ recursion/verifier/script/src/main.rs
@@ -10,8 +10,9 @@ use sp1_core::utils::BabyBearBlake3;
 use sp1_core::utils::StarkUtils;
 use sp1_core::SP1Prover;
 use sp1_core::SP1Stdin;
+use sp1_core::SP1Verifier;
 
-use verifier_script::simple_program;
+use verifier_script::{get_fixture_proof, simple_program};
 
 const VERIFIER_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
@@ -28,19 +29,38 @@ fn main() {
     let mut challenger = machine.config().challenger();
     let proof = machine.prove::<LocalProver<_>>(&pk, runtime.record, &mut challenger);
 
-    let mut challenger = machine.config().challenger();
-    machine.verify(&vk, &proof, &mut challenger).unwrap();
-
     utils::setup_logger();
-    tracing::info!(
-        "Proof generated, number of shards: {}",
-        proof.shard_proofs.len()
-    );
 
     // Write the first shard proof to stdin of the recursive verifier.
     let mut stdin = SP1Stdin::new();
+    // let proof = get_fixture_proof().proof;
     stdin.write(&proof);
 
     // Execute the recursive verifier and get the cycle counts.
-    SP1Prover::execute(VERIFIER_ELF, stdin).expect("proving failed");
+    SP1Prover::execute(VERIFIER_ELF, stdin).expect("execution failed");
+    // Generate a recursive proof.
+    // let proof = SP1Prover::prove(VERIFIER_ELF, stdin).expect("proving failed");
+    // Verify the recursive proof.
+    // SP1Verifier::verify(VERIFIER_ELF, &proof).expect("verification failed");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sp1_recursion::RISCV_STARK;
+
+    #[test]
+    fn test_main_execution() {
+        type SC = sp1_recursion::utils::BabyBearBlake3;
+
+        let config = SC::new();
+        let mut stdin = SP1Stdin::new();
+        let proof = get_fixture_proof().proof;
+
+        let vk = VerifyingKey::empty();
+        let mut challenger = config.challenger();
+        RISCV_STARK
+            .verify(&vk, &proof, &mut challenger)
+            .expect("proof verification failed");
+    }
 }
