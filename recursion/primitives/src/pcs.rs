d00a1a1a7b2ac1f2bc93ee5878ebf7589d05f40e
Kevin Jue <kjue235@gmail.com>
2024-03-01 15:33:18 -0800

    got PrimeField32 working

--- recursion/primitives/src/pcs.rs
+++ recursion/primitives/src/pcs.rs
@@ -1,24 +1,38 @@
 use itertools::izip;
-use p3_challenger::CanSample;
-use p3_commit::{Mmcs, Pcs, UnivariatePcs, UnivariatePcsWithLde};
-use p3_field::{AbstractExtensionField, AbstractField, TwoAdicField};
-use p3_fri::{verifier, FriConfig, TwoAdicFriPcs, TwoAdicFriPcsGenericConfig, VerificationError};
-use p3_matrix::dense::RowMajorMatrix;
+use p3_challenger::{CanObserve, CanSample, FieldChallenger, GrindingChallenger};
+use p3_commit::{DirectMmcs, Mmcs, Pcs, UnivariatePcs, UnivariatePcsWithLde};
+use p3_dft::TwoAdicSubgroupDft;
+use p3_field::{AbstractExtensionField, AbstractField, ExtensionField, PrimeField32, TwoAdicField};
+use p3_fri::{
+    verifier, FriConfig, TwoAdicFriPcs, TwoAdicFriPcsConfig, TwoAdicFriPcsGenericConfig,
+    VerificationError,
+};
+use p3_matrix::dense::{RowMajorMatrix, RowMajorMatrixView};
 use p3_util::{log2_strict_usize, reverse_bits_len};
 
 extern "C" {
     fn syscall_fri_fold(input_mem_ptr: *const u32, output_mem_ptr: *const *mut u32);
 }
 
-pub(crate) struct RecursiveTwoAdicFriPCS<T: TwoAdicFriPcsGenericConfig> {
-    fri: FriConfig<T::FriMmcs>,
-    dft: T::Dft,
-    mmcs: T::InputMmcs,
-    pcs: TwoAdicFriPcs<T>,
+pub(crate) trait TwoAdicPrime32FriPcsGenericConfig: TwoAdicFriPcsGenericConfig {
+    type Prime32Val: TwoAdicField + PrimeField32;
+    type Prime32Challenge: TwoAdicField + ExtensionField<Self::Prime32Val>;
+    type ChallengerPrime32: FieldChallenger<Self::Val>
+        + GrindingChallenger<Witness = Self::Val>
+        + CanObserve<<Self::FriMmcs as Mmcs<Self::Challenge>>::Commitment>
+        + CanSample<Self::Challenge>
+        + CanSample<Self::Prime32Challenge>;
 }
 
-impl<T: TwoAdicFriPcsGenericConfig> RecursiveTwoAdicFriPCS<T> {
-    pub const fn new(fri: FriConfig<T::FriMmcs>, dft: T::Dft, mmcs: T::InputMmcs) -> Self {
+pub(crate) struct RecursiveTwoAdicFriPCS<C: TwoAdicPrime32FriPcsGenericConfig> {
+    fri: FriConfig<C::FriMmcs>,
+    dft: C::Dft,
+    mmcs: C::InputMmcs,
+    pcs: TwoAdicFriPcs<C>,
+}
+
+impl<C: TwoAdicPrime32FriPcsGenericConfig> RecursiveTwoAdicFriPCS<C> {
+    pub const fn new(fri: FriConfig<C::FriMmcs>, dft: C::Dft, mmcs: C::InputMmcs) -> Self {
         let plonky3_pcs = TwoAdicFriPcs::new(fri, dft, mmcs);
         Self {
             fri,
@@ -29,32 +43,50 @@ impl<T: TwoAdicFriPcsGenericConfig> RecursiveTwoAdicFriPCS<T> {
     }
 }
 
-impl<T: TwoAdicFriPcsGenericConfig>
-    UnivariatePcsWithLde<T::Val, T::Challenge, RowMajorMatrix<T::Val>, T::Challenger>
-    for RecursiveTwoAdicFriPCS<T>
+impl<Val, Challenge, Challenger, Dft, InputMmcs, FriMmcs> TwoAdicPrime32FriPcsGenericConfig
+    for TwoAdicFriPcsConfig<Val, Challenge, Challenger, Dft, InputMmcs, FriMmcs>
+where
+    Val: TwoAdicField + PrimeField32,
+    Challenge: TwoAdicField + ExtensionField<Val>,
+    Challenger: FieldChallenger<Val>
+        + GrindingChallenger<Witness = Val>
+        + CanObserve<<FriMmcs as Mmcs<Challenge>>::Commitment>
+        + CanSample<Challenge>,
+    Dft: TwoAdicSubgroupDft<Val>,
+    InputMmcs: 'static + for<'a> DirectMmcs<Val, Mat<'a> = RowMajorMatrixView<'a, Val>>,
+    FriMmcs: DirectMmcs<Challenge>,
 {
-    type Lde<'a> = <TwoAdicFriPcs<T> as UnivariatePcsWithLde<
-        T::Val,
-        T::Challenge,
-        RowMajorMatrix<T::Val>,
-        T::Challenger,
-    >>::Lde<'a> where T: 'a;
+    type Prime32Val = Val;
+    type Prime32Challenge = Challenge;
+    type ChallengerPrime32 = Challenger;
+}
 
-    fn coset_shift(&self) -> T::Val {
-        <TwoAdicFriPcs<T> as UnivariatePcsWithLde<
-            T::Val,
-            T::Challenge,
-            RowMajorMatrix<T::Val>,
-            T::Challenger,
+impl<C: TwoAdicPrime32FriPcsGenericConfig>
+    UnivariatePcsWithLde<C::Val, C::Challenge, RowMajorMatrix<C::Val>, C::ChallengerPrime32>
+    for RecursiveTwoAdicFriPCS<C>
+{
+    type Lde<'a> = <TwoAdicFriPcs<C> as UnivariatePcsWithLde<
+        C::Val,
+        C::Challenge,
+        RowMajorMatrix<C::Val>,
+        C::Challenger,
+    >>::Lde<'a> where C: 'a;
+
+    fn coset_shift(&self) -> C::Val {
+        <TwoAdicFriPcs<C> as UnivariatePcsWithLde<
+            C::Val,
+            C::Challenge,
+            RowMajorMatrix<C::Val>,
+            C::Challenger,
         >>::coset_shift(&self.pcs)
     }
 
     fn log_blowup(&self) -> usize {
-        <TwoAdicFriPcs<T> as UnivariatePcsWithLde<
-            T::Val,
-            T::Challenge,
-            RowMajorMatrix<T::Val>,
-            T::Challenger,
+        <TwoAdicFriPcs<C> as UnivariatePcsWithLde<
+            C::Val,
+            C::Challenge,
+            RowMajorMatrix<C::Val>,
+            C::Challenger,
         >>::log_blowup(&self.pcs)
     }
 
@@ -62,58 +94,58 @@ impl<T: TwoAdicFriPcsGenericConfig>
     where
         'a: 'b,
     {
-        <TwoAdicFriPcs<T> as UnivariatePcsWithLde<
-            T::Val,
-            T::Challenge,
-            RowMajorMatrix<T::Val>,
-            T::Challenger,
+        <TwoAdicFriPcs<C> as UnivariatePcsWithLde<
+            C::Val,
+            C::Challenge,
+            RowMajorMatrix<C::Val>,
+            C::Challenger,
         >>::get_ldes(&self.pcs, prover_data)
     }
 
     fn commit_shifted_batches(
         &self,
-        polynomials: Vec<RowMajorMatrix<T::Val>>,
-        coset_shift: &[T::Val],
+        polynomials: Vec<RowMajorMatrix<C::Val>>,
+        coset_shift: &[C::Val],
     ) -> (Self::Commitment, Self::ProverData) {
         self.pcs.commit_shifted_batches(polynomials, coset_shift)
     }
 
     fn commit_shifted_batch(
         &self,
-        polynomials: RowMajorMatrix<T::Val>,
-        coset_shift: T::Val,
+        polynomials: RowMajorMatrix<C::Val>,
+        coset_shift: C::Val,
     ) -> (Self::Commitment, Self::ProverData) {
         self.pcs.commit_shifted_batch(polynomials, coset_shift)
     }
 }
 
-impl<T: TwoAdicFriPcsGenericConfig>
-    UnivariatePcs<T::Val, T::Challenge, RowMajorMatrix<T::Val>, T::Challenger>
-    for RecursiveTwoAdicFriPCS<T>
+impl<C: TwoAdicPrime32FriPcsGenericConfig>
+    UnivariatePcs<C::Val, C::Challenge, RowMajorMatrix<C::Val>, C::ChallengerPrime32>
+    for RecursiveTwoAdicFriPCS<C>
 {
     fn open_multi_batches(
         &self,
-        prover_data_and_points: &[(&Self::ProverData, &[Vec<T::Challenge>])],
-        challenger: &mut T::Challenger,
-    ) -> (p3_commit::OpenedValues<T::Challenge>, Self::Proof) {
-        <TwoAdicFriPcs<T> as UnivariatePcs<
-            T::Val,
-            T::Challenge,
-            RowMajorMatrix<T::Val>,
-            T::Challenger,
+        prover_data_and_points: &[(&Self::ProverData, &[Vec<C::Challenge>])],
+        challenger: &mut C::ChallengerPrime32,
+    ) -> (p3_commit::OpenedValues<C::Challenge>, Self::Proof) {
+        <TwoAdicFriPcs<C> as UnivariatePcs<
+            C::Val,
+            C::Challenge,
+            RowMajorMatrix<C::Val>,
+            C::Challenger,
         >>::open_multi_batches(&self.pcs, prover_data_and_points, challenger)
     }
 
     fn verify_multi_batches(
         &self,
-        commits_and_points: &[(Self::Commitment, &[Vec<T::Challenge>])],
+        commits_and_points: &[(Self::Commitment, &[Vec<C::Challenge>])],
         dims: &[Vec<p3_matrix::Dimensions>],
-        values: p3_commit::OpenedValues<T::Challenge>,
+        values: p3_commit::OpenedValues<C::Challenge>,
         proof: &Self::Proof,
-        challenger: &mut T::Challenger,
+        challenger: &mut C::ChallengerPrime32,
     ) -> Result<(), Self::Error> {
         // Batch combination challenge
-        let alpha = <T::Challenger as CanSample<T::Challenge>>::sample(challenger);
+        let alpha = <C::ChallengerPrime32 as CanSample<C::Prime32Challenge>>::sample(challenger);
 
         let fri_challenges =
             verifier::verify_shape_and_sample_challenges(&self.fri, &proof.fri_proof, challenger)
@@ -121,13 +153,13 @@ impl<T: TwoAdicFriPcsGenericConfig>
 
         let log_max_height = proof.fri_proof.commit_phase_commits.len() + self.fri.log_blowup;
 
-        let reduced_openings: Vec<[T::Challenge; 32]> = proof
+        let reduced_openings: Vec<[C::Challenge; 32]> = proof
             .query_openings
             .iter()
             .zip(&fri_challenges.query_indices)
             .map(|(query_opening, &index)| {
-                let mut ro = [T::Challenge::zero(); 32];
-                let mut alpha_pow = [T::Challenge::one(); 32];
+                let mut ro = [C::Challenge::zero(); 32];
+                let mut alpha_pow = [C::Challenge::one(); 32];
                 for (batch_opening, batch_dims, (batch_commit, batch_points), batch_at_z) in
                     izip!(query_opening, dims, commits_and_points, &values)
                 {
@@ -150,8 +182,8 @@ impl<T: TwoAdicFriPcsGenericConfig>
                         let rev_reduced_index = reverse_bits_len(index >> bits_reduced, log_height);
 
                         // A field mul with (field lookup then field exp)
-                        let x = T::Val::generator()
-                            * T::Val::two_adic_generator(log_height)
+                        let x = C::Prime32Val::generator()
+                            * C::Prime32Val::two_adic_generator(log_height)
                                 .exp_u64(rev_reduced_index as u64);
 
                         let mut array_arg: [u32; 14] = [0u32; 14];
@@ -191,7 +223,7 @@ impl<T: TwoAdicFriPcsGenericConfig>
                 }
                 Ok(ro)
             })
-            .collect::<Result<Vec<_>, <T::InputMmcs as Mmcs<T::Val>>::Error>>()
+            .collect::<Result<Vec<_>, <C::InputMmcs as Mmcs<C::Val>>::Error>>()
             .map_err(VerificationError::InputMmcsError)?;
 
         verifier::verify_challenges(
@@ -206,7 +238,7 @@ impl<T: TwoAdicFriPcsGenericConfig>
     }
 }
 
-impl<T: TwoAdicFriPcsGenericConfig> Pcs<T::Val, RowMajorMatrix<T::Val>>
+impl<T: TwoAdicPrime32FriPcsGenericConfig> Pcs<T::Val, RowMajorMatrix<T::Val>>
     for RecursiveTwoAdicFriPCS<T>
 {
     type Commitment = <TwoAdicFriPcs<T> as Pcs<T::Val, RowMajorMatrix<T::Val>>>::Commitment;
