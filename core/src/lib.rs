b79600027633270aecd2f6b94ee126028e4c34e9
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-03-01 10:53:22 -0800

    add clippy ignore back

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,16 @@
+#![allow(
+    clippy::eq_op,
+    clippy::new_without_default,
+    clippy::field_reassign_with_default,
+    clippy::unnecessary_cast,
+    clippy::cast_abs_to_unsigned,
+    clippy::needless_range_loop,
+    clippy::type_complexity,
+    clippy::unnecessary_unwrap,
+    clippy::default_constructed_unit_structs,
+    clippy::box_default
+)]
+
 extern crate alloc;
 
 pub mod air;
