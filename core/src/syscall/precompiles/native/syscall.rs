5c15af854642e3d09c3093bb9e409ccd4f87b64a
Tamir Hemo <tamir@succinct.xyz>
2024-02-29 17:09:26 -0800

    native checkpoint

--- core/src/syscall/precompiles/native/syscall.rs
+++ core/src/syscall/precompiles/native/syscall.rs
@@ -1,1 +1,10 @@
+use p3_field::PrimeField32;
 
+use crate::runtime::Syscall;
+
+use super::NativeChip;
+
+// impl<F: PrimeField32> Syscall<F> for NativeChip {
+//     fn num_extra_cycles(&self) -> u32 {}
+//     fn execute(&self, ctx: &mut crate::runtime::SyscallContext<F>) -> u32 {}
+// }
