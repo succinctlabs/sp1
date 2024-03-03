9ada644271bd2266e71d9cc8700ec4054e8c374e
Hadas <hadaszeilberger@gmail.com>
2024-03-02 21:10:50 -0500

    fixed bug, tests passing

--- core/src/field/mod.rs
+++ core/src/field/mod.rs
@@ -1,5 +1,4 @@
 pub mod event;
-use p3_matrix::{Matrix, MatrixRowSlices};
 use crate::air::FieldAirBuilder;
 use crate::air::MachineAir;
 use crate::air::SP1AirBuilder;
@@ -11,6 +10,7 @@ use core::mem::size_of;
 use p3_air::{Air, AirBuilder, BaseAir};
 use p3_field::{AbstractField, Field, PrimeField};
 use p3_matrix::dense::RowMajorMatrix;
+use p3_matrix::{Matrix, MatrixRowSlices};
 use p3_maybe_rayon::prelude::*; //{ParallelIterator, ParallelSlice,};
 use sp1_derive::AlignedBorrow;
 
@@ -18,7 +18,7 @@ use tracing::instrument;
 
 /// The number of main trace columns for `FieldLTUChip`.
 pub const NUM_FIELD_COLS: usize = size_of::<FieldLTUCols<u8>>();
-const WIDTH: usize = 1;
+const WIDTH: usize = 10;
 /// A chip that implements less than within the field.
 #[derive(Default)]
 pub struct FieldLTUChip;
@@ -62,22 +62,19 @@ impl<F: PrimeField> MachineAir<F> for FieldLTUChip {
         _output: &mut ExecutionRecord,
     ) -> RowMajorMatrix<F> {
         // Generate the trace rows for each event.
-	let events = &input.field_events;
-	let rem = events.len() % WIDTH;
-	assert_eq!(rem,0);
         let rows = input
             .field_events
             .par_chunks(WIDTH)
             .map(|events| {
                 let mut row = [F::zero(); NUM_FIELD_COLS * WIDTH];
-                let packed_cols: &mut PackedFieldLTUCols<F> = row.as_mut_slice().borrow_mut();
-                for (i, event) in events.iter().enumerate() {
-                    let mut cols = packed_cols.packed_chips[i];
+                let packed_cols: /*&mut FieldLTUCols<F>*/ &mut PackedFieldLTUCols<F> = row.as_mut_slice().borrow_mut();
+		for (i, event) in events.iter().enumerate() {
+                    let mut cols: &mut FieldLTUCols<F> = packed_cols.packed_chips[i].borrow_mut();
                     let diff = event.b.wrapping_sub(event.c).wrapping_add(1 << LTU_NB_BITS);
                     cols.b = F::from_canonical_u32(event.b);
                     cols.c = F::from_canonical_u32(event.c);
-                    for i in 0..cols.diff_bits.len() {
-                        cols.diff_bits[i] = F::from_canonical_u32((diff >> i) & 1);
+                    for j in 0..cols.diff_bits.len() {
+                        cols.diff_bits[j] = F::from_canonical_u32((diff >> j) & 1);
                     }
                     let max = 1 << LTU_NB_BITS;
                     if diff >= max {
@@ -85,21 +82,18 @@ impl<F: PrimeField> MachineAir<F> for FieldLTUChip {
                     }
                     cols.lt = F::from_bool(event.ltu);
                     cols.is_real = F::one();
-                }
+		}
                 row
             })
             .collect::<Vec<_>>();
-
+        let flat_rows = rows.into_iter().flatten().collect::<Vec<_>>();
         // Convert the trace to a row major matrix.
-        let mut trace = RowMajorMatrix::new(
-            rows.into_iter().flatten().collect::<Vec<_>>(),
-            NUM_FIELD_COLS * WIDTH,
-        );
-	println!("trace height {:?}", trace.height());
+        let mut trace = RowMajorMatrix::new(flat_rows, NUM_FIELD_COLS * WIDTH);
+
         // Pad the trace to a power of two.
         const width: usize = NUM_FIELD_COLS * WIDTH;
         pad_to_power_of_two::<width, F>(&mut trace.values);
-	println!("trace height {:?}", trace.height());
+
         trace
     }
 }
@@ -152,9 +146,9 @@ impl<AB: SP1AirBuilder> Air<AB> for FieldLTUChip {
 
 #[cfg(test)]
 mod tests {
-    use std::time::Instant;    
     use p3_baby_bear::BabyBear;
     use p3_matrix::dense::RowMajorMatrix;
+    use std::time::Instant;
 
     use crate::{
         air::MachineAir,
@@ -162,7 +156,7 @@ mod tests {
     };
     use rand::{thread_rng, Rng};
 
-    use super::{FieldLTUChip,event::FieldEvent};
+    use super::{event::FieldEvent, FieldLTUChip};
     use crate::{
         runtime::{ExecutionRecord, Opcode},
         utils::{BabyBearPoseidon2, StarkUtils},
@@ -171,7 +165,7 @@ mod tests {
     #[test]
     fn generate_trace() {
         let mut shard = ExecutionRecord::default();
-        shard.field_events = vec![FieldEvent::new(true,1,2)];
+        shard.field_events = vec![FieldEvent::new(true, 1, 2)];
         let chip = FieldLTUChip::default();
         let trace: RowMajorMatrix<BabyBear> =
             chip.generate_trace(&shard, &mut ExecutionRecord::default());
@@ -184,25 +178,24 @@ mod tests {
         let mut challenger = config.challenger();
 
         let mut shard = ExecutionRecord::default();
-        let mut result = true;	
+        let mut result = true;
         for i in 0..1000 {
             let operand_1 = thread_rng().gen_range(0..100);
-            let operand_2 = operand_1 + 2; 
-	    shard
-		.field_events
-                .push(FieldEvent::new(true, operand_1, operand_2));		
-	}
-    
+            let operand_2 = operand_1 + 2;
+            shard
+                .field_events
+                .push(FieldEvent::new(true, operand_1, operand_2));
+        }
+
         let chip = FieldLTUChip::default();
         let trace: RowMajorMatrix<BabyBear> =
             chip.generate_trace(&shard, &mut ExecutionRecord::default());
-	let proof_time = Instant::now();
+        let proof_time = Instant::now();
         let proof = prove::<BabyBearPoseidon2, _>(&config, &chip, &mut challenger, trace);
-	println!("proof time {:?}", proof_time.elapsed());
+        println!("proof time {:?}", proof_time.elapsed());
         let mut challenger = config.challenger();
-	if result == true{
+        if result == true {
             verify(&config, &chip, &mut challenger, &proof).unwrap();
-	}
-
+        }
     }
 }
