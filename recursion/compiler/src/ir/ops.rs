5467dabfa6e4491db08a17b690917fe6cef55338
Tamir Hemo <tamir@succinct.xyz>
2024-03-08 16:05:59 -0800

    rm ops

--- recursion/compiler/src/ir/ops.rs
+++
@@ -1,8 +0,0 @@
-use super::Bool;
-use super::Builder;
-use super::Variable;
-
-pub trait Eq<B: Builder, Rhs = Self>: Variable<B> {
-    type Output;
-    fn eq(&self, other: Self, builder: &mut B) -> Self::Output;
-}
