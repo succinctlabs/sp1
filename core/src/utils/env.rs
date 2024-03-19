42d564470b0647bb976021d0ee773989186b3dc9
Kevin Jue <kjue235@gmail.com>
2024-03-18 20:21:08 -0700

    check for max shard size

--- core/src/utils/env.rs
+++ core/src/utils/env.rs
@@ -1,11 +1,14 @@
 /// Gets the number of rows which by default should be used for each chip to maximize padding.
-///
-/// Some chips, such as FieldLTU, may use a constant multiple of this value to optimize performance.
 pub fn shard_size() -> usize {
     let value = match std::env::var("SHARD_SIZE") {
         Ok(val) => val.parse().unwrap(),
         Err(_) => 1 << 19,
     };
+
+    if value > 1 << 22 {
+        panic!("Shard size must be at most 2^22");
+    }
+
     assert!(value != 0 && (value & (value - 1)) == 0);
     value
 }
