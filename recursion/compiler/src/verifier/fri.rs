314515ec599597a73da54ab2668d5a15a27a649b
John Guibas <jtguibas@Johns-MBP.monkeybrains.net>
2024-03-18 17:24:04 -0700

    fri types

--- recursion/compiler/src/verifier/fri.rs
+++ recursion/compiler/src/verifier/fri.rs
@@ -1,16 +1,16 @@
+use super::types::FmtQueryProof;
+use super::types::FriConfig;
 use crate::prelude::Builder;
 use crate::prelude::Config;
 use crate::prelude::Felt;
 use crate::prelude::Usize;
+use crate::prelude::Var;
 use crate::verifier::types::Hash;
+
 use itertools::izip;
 use p3_field::AbstractField;
 use p3_field::TwoAdicField;
 use p3_matrix::Dimensions;
-use p3_util::reverse_bits_len;
-
-use super::types::FmtQueryProof;
-use super::types::FriConfig;
 
 impl<C: Config> Builder<C> {
     pub fn two_adic_generator(&mut self, bits: Usize<C::N>) -> Felt<C::F> {
@@ -37,15 +37,25 @@ impl<C: Config> Builder<C> {
         proof: &FmtQueryProof<C>,
         betas: &[Felt<C::F>],
         reduced_openings: &[Felt<C::F>; 32],
-        log_max_height: usize,
+        log_max_height: Usize<C::N>,
     ) where
         C::F: TwoAdicField,
     {
         let folded_eval: Felt<_> = self.eval(C::F::zero());
         let generator = self.two_adic_generator(Usize::Const(1));
-        let two_adic_generator = self.two_adic_generator(Usize::Const(log_max_height));
-        let power = self.reverse_bits_len(Usize::Const(index), Usize::Const(log_max_height));
+        let two_adic_generator = self.two_adic_generator(log_max_height);
+        let power = self.reverse_bits_len(Usize::Const(index), log_max_height);
         let x = self.exp_usize(two_adic_generator, power);
+
+        let start = Usize::Const(0);
+        let end = log_max_height;
+        let end_var: Var<C::N> = match log_max_height {
+            Usize::Const(log_max_height) => self.eval(C::N::from_canonical_usize(log_max_height)),
+            Usize::Var(log_max_height) => log_max_height,
+        };
+        self.range(start, end).for_each(|i, builder| {
+            let log_folded_height = builder.eval(end_var - i - C::N::one());
+        });
 
         for (log_folded_height, commit, step, &beta) in izip!(
             (0..log_max_height).rev(),
--- recursion/compiler/src/verifier/types.rs
+++ recursion/compiler/src/verifier/types.rs
@@ -1,6 +1,7 @@
-use crate::prelude::{Config, Felt};
+use crate::prelude::{Builder, Config, Felt, MemVariable, Ptr, Usize, Variable};
 use std::marker::PhantomData;
 
+/// The current verifier implementation assumes that we are using a 256-bit hash with 32-bit elements.
 pub const DIGEST_SIZE: usize = 8;
 
 /// Reference: https://github.com/Plonky3/Plonky3/blob/4809fa7bedd9ba8f6f5d3267b1592618e3776c57/merkle-tree/src/mmcs.rs#L54
@@ -25,4 +26,91 @@ pub struct FmtCommitPhaseProofStep<C: Config> {
     pub sibling_value: Felt<C::F>,
     pub opening_proof: Vec<Hash<C>>,
     pub phantom: PhantomData<C>,
+}
+
+impl<C: Config> Variable<C> for Hash<C> {
+    type Expression = Self;
+
+    fn uninit(builder: &mut Builder<C>) -> Self {
+        [
+            Felt::uninit(builder),
+            Felt::uninit(builder),
+            Felt::uninit(builder),
+            Felt::uninit(builder),
+            Felt::uninit(builder),
+            Felt::uninit(builder),
+            Felt::uninit(builder),
+            Felt::uninit(builder),
+        ]
+    }
+
+    fn assign(&self, src: Self::Expression, builder: &mut Builder<C>) {
+        builder.assign(self[0], src[0]);
+        builder.assign(self[1], src[1]);
+        builder.assign(self[2], src[2]);
+        builder.assign(self[3], src[3]);
+        builder.assign(self[4], src[4]);
+        builder.assign(self[5], src[5]);
+        builder.assign(self[6], src[6]);
+        builder.assign(self[7], src[7]);
+    }
+
+    fn assert_eq(
+        lhs: impl Into<Self::Expression>,
+        rhs: impl Into<Self::Expression>,
+        builder: &mut Builder<C>,
+    ) {
+        builder.assert_felt_eq(lhs.into()[0], rhs.into()[0]);
+        builder.assert_felt_eq(lhs.into()[1], rhs.into()[1]);
+        builder.assert_felt_eq(lhs.into()[2], rhs.into()[2]);
+        builder.assert_felt_eq(lhs.into()[3], rhs.into()[3]);
+        builder.assert_felt_eq(lhs.into()[4], rhs.into()[4]);
+        builder.assert_felt_eq(lhs.into()[5], rhs.into()[5]);
+        builder.assert_felt_eq(lhs.into()[6], rhs.into()[6]);
+        builder.assert_felt_eq(lhs.into()[7], rhs.into()[7]);
+    }
+
+    fn assert_ne(
+        lhs: impl Into<Self::Expression>,
+        rhs: impl Into<Self::Expression>,
+        builder: &mut Builder<C>,
+    ) {
+        builder.assert_felt_ne(lhs.into()[0], rhs.into()[0]);
+        builder.assert_felt_ne(lhs.into()[1], rhs.into()[1]);
+        builder.assert_felt_ne(lhs.into()[2], rhs.into()[2]);
+        builder.assert_felt_ne(lhs.into()[3], rhs.into()[3]);
+        builder.assert_felt_ne(lhs.into()[4], rhs.into()[4]);
+        builder.assert_felt_ne(lhs.into()[5], rhs.into()[5]);
+        builder.assert_felt_ne(lhs.into()[6], rhs.into()[6]);
+        builder.assert_felt_ne(lhs.into()[7], rhs.into()[7]);
+    }
+}
+
+impl<C: Config> MemVariable<C> for Hash<C> {
+    fn size_of() -> usize {
+        <Felt<C::F> as MemVariable<C>>::size_of() * DIGEST_SIZE
+    }
+
+    fn load(&self, ptr: Ptr<<C as Config>::N>, builder: &mut Builder<C>) {
+        let address = builder.eval(ptr + Usize::Const(0));
+        self[0].load(address, builder);
+        let address = builder.eval(ptr + Usize::Const(1));
+        self[1].load(address, builder);
+        let address = builder.eval(ptr + Usize::Const(2));
+        self[2].load(address, builder);
+        let address = builder.eval(ptr + Usize::Const(3));
+        self[3].load(address, builder);
+        let address = builder.eval(ptr + Usize::Const(4));
+        self[4].load(address, builder);
+        let address = builder.eval(ptr + Usize::Const(5));
+        self[5].load(address, builder);
+        let address = builder.eval(ptr + Usize::Const(6));
+        self[6].load(address, builder);
+        let address = builder.eval(ptr + Usize::Const(7));
+        self[7].load(address, builder);
+    }
+
+    fn store(&self, ptr: Ptr<<C as Config>::N>, builder: &mut Builder<C>) {
+        todo!()
+    }
 }
