b919c07e864e17a9c401e24993c9fb4a9b5fdb90
Tamir Hemo <tamir@succinct.xyz>
2024-03-20 16:54:36 -0700

    ed25519

--- recursion/compiler/examples/verifier.rs
+++ recursion/compiler/examples/verifier.rs
@@ -1,65 +1,16 @@
-use std::marker::PhantomData;
-
-use p3_air::Air;
 use p3_field::AbstractField;
-use p3_field::Field;
+use p3_field::PrimeField32;
 use rand::thread_rng;
 use rand::Rng;
 use sp1_core::air::MachineAir;
-use sp1_core::stark::ChipOpenedValues;
-use sp1_core::stark::MachineChip;
 use sp1_core::stark::RiscvAir;
-use sp1_core::stark::StarkAir;
 use sp1_core::stark::StarkGenericConfig;
-use sp1_core::stark::VerifierConstraintFolder;
 use sp1_core::utils;
 use sp1_core::utils::BabyBearPoseidon2;
 use sp1_core::SP1Prover;
 use sp1_core::SP1Stdin;
 use sp1_recursion_compiler::asm::VmBuilder;
-use sp1_recursion_compiler::ir::ExtConst;
-use sp1_recursion_compiler::ir::{Ext, Felt};
 use sp1_recursion_core::runtime::Runtime;
-
-pub fn eval_constraints<SC, A>(
-    chip: &MachineChip<SC, A>,
-    opening: &ChipOpenedValues<SC::Challenge>,
-    g: SC::Val,
-    zeta: SC::Challenge,
-    alpha: SC::Challenge,
-) -> SC::Challenge
-where
-    SC: StarkGenericConfig,
-    A: MachineAir<SC::Val> + StarkAir<SC>,
-{
-    let g_inv = g.inverse();
-    let z_h = zeta.exp_power_of_2(opening.log_degree);
-    let one = SC::Val::one();
-    let is_first_row = z_h / (zeta - one);
-    let is_last_row = z_h / (zeta - g_inv);
-    let is_transition = zeta - g_inv;
-
-    let mut folder = VerifierConstraintFolder::<SC> {
-        preprocessed: opening.preprocessed.view(),
-        main: opening.main.view(),
-        perm: opening.permutation.view(),
-        perm_challenges: &[SC::Challenge::one(), SC::Challenge::one()],
-        cumulative_sum: SC::Challenge::zero(),
-        is_first_row,
-        is_last_row,
-        is_transition,
-        alpha,
-        accumulator: SC::Challenge::zero(),
-        _marker: PhantomData,
-    };
-
-    // let monomials = (0..SC::Challenge::D)
-    //     .map(SC::Challenge::monomial)
-    //     .collect::<Vec<_>>();
-
-    chip.eval(&mut folder);
-    folder.accumulator
-}
 
 fn main() {
     type SC = BabyBearPoseidon2;
@@ -70,23 +21,16 @@ fn main() {
 
     // Generate a dummy proof.
     utils::setup_logger();
-    let elf =
-        include_bytes!("../../../examples/cycle-tracking/program/elf/riscv32im-succinct-zkvm-elf");
+    let elf = include_bytes!("../../../examples/ed25519/program/elf/riscv32im-succinct-zkvm-elf");
     let proofs = SP1Prover::prove(elf, SP1Stdin::new())
         .unwrap()
         .proof
         .shard_proofs;
-    let proof = &proofs[0];
+
+    println!("Proof generated successfully");
 
     // Extract verification metadata.
     let machine = RiscvAir::machine(SC::new());
-    let chips = machine
-        .chips()
-        .iter()
-        .filter(|chip| proof.chip_ids.contains(&chip.name()))
-        .collect::<Vec<_>>();
-    let chip = chips[0];
-    let opened_values = &proof.opened_values.chips[0];
 
     // Run the verify inside the DSL.
     let mut builder = VmBuilder::<F, EF>::default();
@@ -94,21 +38,27 @@ fn main() {
 
     let zeta_val = rng.gen::<EF>();
     let alpha_val = rng.gen::<EF>();
-    // let constraint_eval =
-    //     eval_constraints::<SC, _>(chip, opened_values, g_val, zeta_val, alpha_val);
 
-    let g: Felt<F> = builder.eval(F::one());
-
-    let zeta: Ext<F, EF> = builder.eval(zeta_val.cons());
-    let alpha: Ext<F, EF> = builder.eval(alpha_val.cons());
-
-    builder.eval_constraints_test::<SC, _>(chip, opened_values, g_val, zeta_val, alpha_val);
-
-    // builder.assert_ext_eq(constraint_eval.cons(), vm_value);
+    for shard_proof in proofs.into_iter().take(1) {
+        let chips = machine
+            .chips()
+            .iter()
+            .filter(|chip| shard_proof.chip_ids.contains(&chip.name()))
+            .collect::<Vec<_>>();
+        for (chip, values) in chips
+            .into_iter()
+            .zip(shard_proof.opened_values.chips.iter())
+        {
+            builder.eval_constraints_test::<SC, _>(chip, values, g_val, zeta_val, alpha_val)
+        }
+    }
 
     let program = builder.compile();
-    println!("Program size = {}", program.instructions.len());
 
     let mut runtime = Runtime::<F, EF>::new(&program);
     runtime.run();
+    println!(
+        "The program executed successfully, number of cycles: {}",
+        runtime.clk.as_canonical_u32() / 4
+    );
 }
