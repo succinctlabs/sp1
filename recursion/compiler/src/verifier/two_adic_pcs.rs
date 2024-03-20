06e4a146fc23243e4c1dab1677e797f7d477869d
John Guibas <jtguibas@Johns-MBP.monkeybrains.net>
2024-03-20 11:28:14 -0700

    two adic pcs

--- recursion/compiler/src/verifier/two_adic_pcs.rs
+++ recursion/compiler/src/verifier/two_adic_pcs.rs
@@ -1,8 +1,13 @@
+use p3_field::AbstractField;
+use p3_field::TwoAdicField;
+
 use super::{
     challenger::DuplexChallenger,
-    types::{Commitment, FriProof},
+    fri::{verify_challenges, verify_shape_and_sample_challenges},
+    types::{Commitment, FriConfig, FriProof},
 };
-use crate::prelude::{Array, Builder, Config, Ext, Felt, Usize};
+use crate::prelude::Var;
+use crate::prelude::{Array, Builder, Config, Ext, Felt, SymbolicExt, SymbolicFelt, Usize};
 
 pub struct BatchOpening<C: Config> {
     pub opened_values: Array<C, Array<C, Felt<C::F>>>,
@@ -27,9 +32,22 @@ pub struct TwoAdicPcsMats<C: Config> {
 #[allow(unused_variables)]
 pub fn verify_two_adic_pcs<C: Config>(
     builder: &mut Builder<C>,
+    config: &FriConfig,
     rounds: TwoAdicPcsRounds<C>,
     proof: TwoAdicPcsProof<C>,
     challenger: &mut DuplexChallenger<C>,
-) {
-    todo!()
+) where
+    C::F: TwoAdicField,
+{
+    let alpha = challenger.sample(builder);
+    let alpha: Ext<_, _> = builder.eval(SymbolicExt::Base(SymbolicFelt::Val(alpha).into()));
+
+    let fri_challenges =
+        verify_shape_and_sample_challenges(builder, config, &proof.fri_proof, challenger);
+
+    let commit_phase_commits_len = builder.materialize(proof.fri_proof.commit_phase_commits.len());
+    let log_max_height: Var<_> =
+        builder.eval(commit_phase_commits_len + C::N::from_canonical_usize(config.log_blowup));
+
+    // TODO:
 }
