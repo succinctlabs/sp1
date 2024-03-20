c2735972addf59db054c13e36a37bf250d27fca4
Tamir Hemo <tamir@succinct.xyz>
2024-03-20 10:50:30 -0700

    checkpoint

---
+++ recursion/core/src/cpu/columns/branch.rs
@@ -0,0 +1,13 @@
+use sp1_derive::AlignedBorrow;
+use std::mem::size_of;
+
+use crate::air::IsExtZeroOperation;
+
+pub const NUM_BRANCH_COLS: usize = size_of::<BranchCols<u8>>();
+
+/// The column layout for branching.
+#[derive(AlignedBorrow, Default, Debug, Clone, Copy)]
+#[repr(C)]
+pub struct BranchCols<T> {
+    is_eq_zero: IsExtZeroOperation<T>,
+}
--- recursion/core/src/cpu/columns/mod.rs
+++ recursion/core/src/cpu/columns/mod.rs
@@ -1,7 +1,4 @@
-use crate::{
-    air::{Block, IsExtZeroOperation},
-    memory::MemoryReadWriteCols,
-};
+use crate::{air::IsExtZeroOperation, memory::MemoryReadWriteCols};
 use sp1_derive::AlignedBorrow;
 
 mod alu;
@@ -23,11 +20,12 @@ pub struct CpuCols<T> {
     pub pc: T,
     pub fp: T,
 
+    pub instruction: InstructionCols<T>,
+    pub selectors: OpcodeSelectorCols<T>,
+
     pub a: MemoryReadWriteCols<T>,
     pub b: MemoryReadWriteCols<T>,
     pub c: MemoryReadWriteCols<T>,
-
-    pub instruction: InstructionCols<T>,
 
     pub alu: AluCols<T>,
 
