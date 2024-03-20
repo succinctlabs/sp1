1215d9df832f8a0744d014e142f951c7e752de1e
Kevin Jue <kjue235@gmail.com>
2024-03-20 09:54:21 -0700

    added comment regarding main constraint

--- core/src/air/builder.rs
+++ core/src/air/builder.rs
@@ -407,6 +407,11 @@ pub trait MemoryAirBuilder: BaseAirBuilder {
         let do_check: Self::Expr = do_check.into();
 
         // Subtract one since current_comp_val shouldn't equal prev_comp_val.
+        //
+        // We know that diff_minus_one will have the possible values of [-MAX_SHARD_SIZE, MAX_SHARD_SIZE - 2],
+        // since we do a range check for all clk values to be within [0, MAX_SHARD_SIZE - 1].
+        // We want to ensure that diff_minus_one is [0, MAX_SHARD_SIZE - 2], so we do a 24 bit
+        // range check on it.
         let diff_minus_one = current_comp_val - prev_comp_val - Self::Expr::one();
 
         // Verify that mem_access.ts_diff = mem_access.ts_diff_16bit_limb + mem_access.ts_diff_8bit_limb * 2^16.
