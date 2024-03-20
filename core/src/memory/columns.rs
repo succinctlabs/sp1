2dc69f31e2f0c09606e5551bdc73ca333e3277e7
Kevin Jue <kjue235@gmail.com>
2024-03-20 15:49:34 -0700

    chore: styling and comment fixes (#414)

--- core/src/memory/columns.rs
+++ core/src/memory/columns.rs
@@ -30,21 +30,21 @@ pub struct MemoryReadWriteCols<T> {
 pub struct MemoryAccessCols<T> {
     pub value: Word<T>,
 
-    // The previous shard and timestamp that this memory access is being read from.
+    /// The previous shard and timestamp that this memory access is being read from.
     pub prev_shard: T,
     pub prev_clk: T,
 
-    // This will be true if the current shard == prev_access's shard, else false.
+    /// This will be true if the current shard == prev_access's shard, else false.
     pub compare_clk: T,
 
-    // The following columns are decomposed limbs for the different between the current access's timestamp
-    // and the previous access's timestamp.  Note the actual value of the timestamp is either the
-    // accesses' shard or clk depending on the value of compare_clk.
+    /// The following columns are decomposed limbs for the difference between the current access's timestamp
+    /// and the previous access's timestamp.  Note the actual value of the timestamp is either the
+    /// accesses' shard or clk depending on the value of compare_clk.
 
-    // This column is the least significant 16 bit limb of current access timestamp - prev access timestamp.
+    /// This column is the least significant 16 bit limb of current access timestamp - prev access timestamp.
     pub diff_16bit_limb: T,
 
-    // This column is the most signficant 8 bit limb of current_comp_val - prev_comp_val.
+    /// This column is the most signficant 8 bit limb of current access timestamp - prev access timestamp.
     pub diff_8bit_limb: T,
 }
 
