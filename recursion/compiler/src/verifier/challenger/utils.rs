eacce2795a7ee736918f33bb4d553d9366f4fa0c
Tamir Hemo <tamir@succinct.xyz>
2024-03-22 14:41:43 -0700

    fix: compiler conditionals + working challenger (#430)
    Co-authored-by: John Guibas <jtguibas@Johns-MBP.monkeybrains.net>

--- recursion/compiler/src/verifier/challenger/utils.rs
+++ recursion/compiler/src/verifier/challenger/utils.rs
@@ -1,8 +1,14 @@
+use p3_field::AbstractField;
+
 use crate::prelude::{Array, Builder, Config, MemVariable};
 
 impl<C: Config> Builder<C> {
-    pub fn clear<V: MemVariable<C>>(&mut self, array: Array<C, V>) {
-        let empty = self.array::<V>(array.len());
-        self.assign(array.clone(), empty);
+    pub fn clear<V: MemVariable<C>>(&mut self, array: &mut Array<C, V>)
+    where
+        V::Expression: AbstractField,
+    {
+        self.range(0, array.len()).for_each(|i, builder| {
+            builder.set(array, i, V::Expression::zero());
+        });
     }
 }
