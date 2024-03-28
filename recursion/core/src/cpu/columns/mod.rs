6b172251082bcefbc4bba2c344b79bd0cd2d5304
Tamir Hemo <tamir@succinct.xyz>
2024-03-27 18:16:40 -0700

    delete num_cpu_cols

--- recursion/core/src/cpu/columns/mod.rs
+++ recursion/core/src/cpu/columns/mod.rs
@@ -12,8 +12,6 @@ pub use alu::*;
 pub use instruction::*;
 pub use opcode::*;
 
-pub const NUM_CPU_COLS: usize = core::mem::size_of::<CpuCols<u8>>();
-
 /// The column layout for the chip.
 #[derive(AlignedBorrow, Default, Clone, Debug)]
 #[repr(C)]
