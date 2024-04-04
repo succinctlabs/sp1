8982ac0fc2e274fd15a567bd2145ff9a58219476
John Guibas <jtguibas@gmail.com>
2024-04-03 18:02:41 -0700

    boom

--- recursion/circuit/build.rs
+++ recursion/circuit/build.rs
@@ -2,18 +2,18 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    // Get the current directory of the build script
+    // Get the current directory of the build script.
     let current_dir = env::current_dir().expect("failed to get current directory");
 
-    // Navigate to the parent directory
+    // Navigate to the parent directory.
     let parent_dir = current_dir
         .parent()
         .expect("failed to find parent directory")
         .join("groth16");
 
-    // Run `make` in the parent directory
+    // Run `make` in the parent directory.
     let status = Command::new("make")
-        .current_dir(parent_dir) // This sets the working directory for the command
+        .current_dir(parent_dir)
         .status()
         .expect("failed to build groth16");
 
