bcaee4ce6266975bfed4ddffa8c21e1309da4332
Chris Tian <chris@succinct.xyz>
2024-04-09 16:40:20 -0700

    fix

--- recursion/program/src/reduce.rs
+++ recursion/program/src/reduce.rs
@@ -69,8 +69,7 @@ pub fn const_fri_config(
             log_n: i,
             shift: Val::one(),
         };
-        let domain_value: TwoAdicMultiplicativeCosetVariable<_> =
-            builder.eval_const(constant_domain);
+        let domain_value: TwoAdicMultiplicativeCosetVariable<_> = builder.constant(constant_domain);
         builder.set(&mut subgroups, i, domain_value);
     }
     FriConfigVariable {
@@ -106,15 +105,17 @@ pub fn build_reduce() -> RecursionProgram<Val> {
     // let recursion_challenger = DuplexChallenger::read(&mut builder);
     let prep_sorted_indices = Vec::<usize>::read(&mut builder);
     let prep_domains = Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::read(&mut builder);
+    // let recursion_prep_sorted_indices = Vec::<usize>::read(&mut builder);
+    // let recursion_prep_domains = Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::read(&mut builder);
     let sp1_vk = VerifyingKey::<SC>::read(&mut builder);
     let recursion_vk = VerifyingKey::<SC>::read(&mut builder);
     let num_proofs = proofs.len();
 
     let pre_start_challenger = clone(&mut builder, &sp1_challenger);
     let pre_reconstruct_challenger = clone(&mut builder, &reconstruct_challenger);
-    let zero: Var<_> = builder.eval_const(F::zero());
-    let one: Var<_> = builder.eval_const(F::one());
-    let one_felt: Felt<_> = builder.eval_const(F::one());
+    let zero: Var<_> = builder.constant(F::zero());
+    let one: Var<_> = builder.constant(F::one());
+    let one_felt: Felt<_> = builder.constant(F::one());
     builder
         .range(Usize::Const(0), num_proofs)
         .for_each(|i, builder| {
@@ -125,7 +126,7 @@ pub fn build_reduce() -> RecursionProgram<Val> {
                 // Non-recursive proof
                 |builder| {
                     let shard_bits = builder.num2bits_f(proof.public_values.shard);
-                    let shard = builder.bits_to_num_var(&shard_bits);
+                    let shard = builder.bits2num_v(&shard_bits);
                     builder.if_eq(shard, one).then(|builder| {
                         // Initialize the current challenger
                         // let h: [BabyBear; DIGEST_SIZE] = sp1_vk.commit.into();
