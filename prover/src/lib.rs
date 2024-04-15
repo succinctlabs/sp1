b47a7736a1ab7da8ad3d6afd5e022b3cd3225101
Kevin Jue <kjue235@gmail.com>
2024-04-15 11:58:45 -0700

    don't have hard coded proof

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -360,30 +360,33 @@ mod tests {
     use sp1_recursion_circuit::{stark::build_wrap_circuit, witness::Witnessable};
     use sp1_recursion_compiler::{constraints::groth16_ffi, ir::Witness};
     use sp1_recursion_core::stark::config::BabyBearPoseidon2Outer;
+    use sp1_sdk::{ProverClient, SP1Stdin};
 
     #[test]
     // #[ignore]
     fn test_prove_sp1() {
         setup_logger();
         std::env::set_var("RECONSTRUCT_COMMITMENTS", "false");
-        let prover = SP1ProverImpl::new();
 
+        // Generate SP1 proof
         let elf =
             include_bytes!("../../examples/fibonacci/program/elf/riscv32im-succinct-zkvm-elf");
-        let proof = match std::fs::read("sp1_proof.bin") {
-            Ok(proof) => bincode::deserialize::<Proof<SP1SC>>(&proof).unwrap(),
-            Err(_) => {
-                let stdin = [bincode::serialize::<u32>(&6).unwrap()];
-                let proof = SP1ProverImpl::prove(elf, &stdin);
 
-                // save proof
-                let serialized = bincode::serialize(&proof).unwrap();
-                std::fs::write("sp1_proof.bin", serialized).unwrap();
+        type SC = BabyBearPoseidon2;
+        type F = <SC as StarkGenericConfig>::Val;
+        type A = RiscvAir<F>;
 
-                proof
-            }
-        };
+        let machine = A::machine(SC::default());
+        let (_, vk) = machine.setup(&Program::from(elf));
+        let mut challenger = machine.config().challenger();
+        let client = ProverClient::new();
+        let proof = client
+            .prove_local(elf, SP1Stdin::new(), machine.config().clone())
+            .unwrap()
+            .proof;
+        machine.verify(&vk, &proof, &mut challenger).unwrap();
 
+        let prover = SP1ProverImpl::new();
         let sp1_machine = RiscvAir::machine(SP1SC::default());
         let (_, vk) = sp1_machine.setup(&Program::from(elf));
 
