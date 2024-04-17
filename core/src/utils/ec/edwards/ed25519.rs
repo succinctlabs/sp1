379eb9c6763a316d3f001462c614ceddd3e7db50
Tamir Hemo <tamir@succinct.xyz>
2024-04-17 14:08:53 -0700

    fix witness offset

--- core/src/utils/ec/edwards/ed25519.rs
+++ core/src/utils/ec/edwards/ed25519.rs
@@ -25,7 +25,7 @@ impl FieldParameters for Ed25519BaseField {
         255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127,
     ];
 
-    const WITNESS_OFFSET: usize = 1usize << 13;
+    const WITNESS_OFFSET: usize = 1usize << 14;
 
     fn modulus() -> BigUint {
         (BigUint::one() << 255) - BigUint::from(19u32)
