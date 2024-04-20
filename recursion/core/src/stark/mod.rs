f1dfa0295428fde4c296240b94f052510f5766aa
Uma Roy <uma.roy.us@gmail.com>
2024-04-19 20:06:26 -0700

    In progress adding final layer recursion chip for skinnier air

--- recursion/core/src/stark/mod.rs
+++ recursion/core/src/stark/mod.rs
@@ -65,3 +65,47 @@ impl<F: PrimeField32 + BinomiallyExtendable<D>> RecursionAir<F> {
         chips
     }
 }
+
+#[derive(MachineAir)]
+#[sp1_core_path = "sp1_core"]
+#[execution_record_path = "crate::runtime::ExecutionRecord<F>"]
+#[program_path = "crate::runtime::RecursionProgram<F>"]
+pub enum FinalRecursionAir<F: PrimeField32 + BinomiallyExtendable<D>> {
+    Program(ProgramChip),
+    Cpu(CpuChip<F>),
+    MemoryInit(MemoryGlobalChip),
+    MemoryFinalize(MemoryGlobalChip),
+    Poseidon2(Poseidon2Chip),
+    Alu(AluChip),
+}
+
+impl<F: PrimeField32 + BinomiallyExtendable<D>> FinalRecursionAir<F> {
+    pub fn machine<SC: StarkGenericConfig<Val = F>>(config: SC) -> MachineStark<SC, Self> {
+        let chips = Self::get_all()
+            .into_iter()
+            .map(Chip::new)
+            .collect::<Vec<_>>();
+        MachineStark::new(config, chips, DIGEST_SIZE)
+    }
+
+    pub fn get_all() -> Vec<Self> {
+        let mut chips = vec![];
+        let program = ProgramChip;
+        chips.push(FinalRecursionAir::Program(program));
+        let cpu = CpuChip::default();
+        chips.push(FinalRecursionAir::Cpu(cpu));
+        let memory_init = MemoryGlobalChip {
+            kind: MemoryChipKind::Init,
+        };
+        chips.push(FinalRecursionAir::MemoryInit(memory_init));
+        let memory_finalize = MemoryGlobalChip {
+            kind: MemoryChipKind::Finalize,
+        };
+        chips.push(FinalRecursionAir::MemoryFinalize(memory_finalize));
+        let poseidon2 = Poseidon2Chip {};
+        chips.push(FinalRecursionAir::Poseidon2(poseidon2));
+        let alu = AluChip {};
+        chips.push(FinalRecursionAir::Alu(alu));
+        chips
+    }
+}
