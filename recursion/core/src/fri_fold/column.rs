f60987a683083a7a5ea5266bdf447c7d2d98081c
Kevin Jue <kjue235@gmail.com>
2024-04-21 16:39:48 -0700

    columns and event

---
+++ recursion/core/src/fri_fold/column.rs
@@ -0,0 +1,32 @@
+use sp1_derive::AlignedBorrow;
+
+use super::FriFoldEvent;
+
+#[derive(AlignedBorrow, Debug, Clone)]
+#[repr(C)]
+pub struct FriFoldCols<T> {
+    /// The parameters into the FRI fold precompile.
+    pub m: MemoryReadWriteCols<T>,
+    pub input_ptr: MemoryReadWriteCols<T>,
+
+    /// The inputs stored in memory.  All the values are just read from memory.
+    pub z: MemoryReadWriteCols<T>,
+    pub alpha: MemoryReadWriteCols<T>,
+    pub x: MemoryReadWriteCols<T>,
+    pub log_height: MemoryReadWriteCols<T>,
+    pub mat_opening_ptr: MemoryReadWriteCols<T>,
+    pub ps_at_z_ptr: MemoryReadWriteCols<T>,
+    pub alpha_pow_ptr: MemoryReadWriteCols<T>,
+    pub ro_ptr: MemoryReadWriteCols<T>,
+
+    pub p_at_x: MemoryReadWriteCols<T>,
+    pub p_at_z: MemoryReadWriteCols<T>,
+
+    /// The values here are read and then written.
+    pub alpha_pow_at_log_height: MemoryReadWriteCols<T>,
+    pub ro_at_log_height: MemoryReadWriteCols<T>,
+}
+
+impl<T: Clone> FriFoldCols<T> {
+    pub fn populate(&mut self, event: &FriFoldEvent) {}
+}
---
+++ recursion/core/src/fri_fold/mod.rs
@@ -0,0 +1,17 @@
+#[derive(Debug, Clone)]
+pub struct FriFoldEvent<F: PrimeField32, EF: ExtensionField<F>> {
+    pub z: EF,
+    pub alpha: EF,
+    pub x: F,
+    pub log_height: usize,
+    pub mat_opening_ptr: usize,
+    pub ps_at_x_ptr: usize,
+    pub alpha_pow_ptr: usize,
+    pub ro_ptr: usize,
+
+    pub p_at_x: EF,
+    pub p_at_z: EF,
+
+    pub alpha_pow_at_log_height: EF,
+    pub ro_at_log_height: EF,
+}
