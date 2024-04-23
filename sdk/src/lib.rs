1165a65093cfdce52ac89fdcd55e75b883f195f7
Matt Stam <mattstam@live.com>
2024-04-23 15:42:34 -0700

    fix: return fulfilled proof (#573)

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -95,7 +95,7 @@ impl ProverClient {
 
             match status.status() {
                 ProofStatus::ProofFulfilled => {
-                    maybe_proof.unwrap();
+                    return Ok(maybe_proof.unwrap());
                 }
                 ProofStatus::ProofClaimed => {
                     if !is_claimed {
