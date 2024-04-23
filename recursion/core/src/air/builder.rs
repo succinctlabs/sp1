a8a313d4d4190df0736353632cfecd075d5f1bb1
Kevin Jue <kjue235@gmail.com>
2024-04-23 15:26:20 -0700

    remove unneeded builder traits

--- recursion/core/src/air/builder.rs
+++ recursion/core/src/air/builder.rs
@@ -3,23 +3,9 @@ use sp1_core::air::{BaseAirBuilder, ExtensionAirBuilder, MemoryAirBuilder, Progr
 
 use crate::air::Block;
 
-pub trait BlockAirBuilder: BaseAirBuilder {
-    fn assert_is_field<I: Into<Self::Expr> + Clone>(&mut self, block: &Block<I>) {
-        self.assert_zero(block.0[1].clone());
-        self.assert_zero(block.0[2].clone());
-        self.assert_zero(block.0[3].clone());
-    }
-}
-
 pub trait SP1RecursionAirBuilder:
-    BaseAirBuilder
-    + MemoryAirBuilder
-    + ProgramAirBuilder
-    + ExtensionAirBuilder
-    + BlockAirBuilder
-    + AirBuilderWithPublicValues
+    BaseAirBuilder + ExtensionAirBuilder + AirBuilderWithPublicValues
 {
 }
 
-impl<AB: BaseAirBuilder> BlockAirBuilder for AB {}
 impl<AB: BaseAirBuilder + AirBuilderWithPublicValues> SP1RecursionAirBuilder for AB {}
