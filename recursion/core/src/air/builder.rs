e12a4cd0150a030537f6cd069fdb7d431a078851
Kevin Jue <kjue235@gmail.com>
2024-04-23 15:28:24 -0700

    added a comment

--- recursion/core/src/air/builder.rs
+++ recursion/core/src/air/builder.rs
@@ -1,6 +1,7 @@
 use p3_air::AirBuilderWithPublicValues;
 use sp1_core::air::{BaseAirBuilder, ExtensionAirBuilder};
 
+/// Builder for the SP1 recursion machine AIRs.
 pub trait SP1RecursionAirBuilder:
     BaseAirBuilder + ExtensionAirBuilder + AirBuilderWithPublicValues
 {
