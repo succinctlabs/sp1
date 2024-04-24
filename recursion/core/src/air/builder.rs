0fdd45659649f22b0038143f3755beeafe246fa3
Kevin Jue <kjue235@gmail.com>
2024-04-24 16:40:33 -0700

    added constraint

--- recursion/core/src/air/builder.rs
+++ recursion/core/src/air/builder.rs
@@ -1,11 +1,13 @@
-use crate::memory::{MemoryAccessTimestampCols, MemoryCols};
 use core::iter::{once, repeat};
-use p3_air::AirBuilderWithPublicValues;
+use p3_air::{AirBuilder, AirBuilderWithPublicValues};
 use p3_field::AbstractField;
 use sp1_core::{
     air::{AirInteraction, BaseAirBuilder, MachineAirBuilder},
     lookup::InteractionKind,
 };
+
+use crate::memory::{MemoryAccessTimestampCols, MemoryCols};
+use crate::range_check::RangeCheckOpcode;
 
 use super::Block;
 /// A trait which contains all helper methods for building SP1 recursion machine AIRs.
@@ -18,7 +20,7 @@ impl<AB: AirBuilderWithPublicValues + RecursionMemoryAirBuilder> SP1RecursionAir
 impl<AB: BaseAirBuilder + AirBuilderWithPublicValues> RecursionMemoryAirBuilder for AB {}
 impl<AB: BaseAirBuilder + AirBuilderWithPublicValues> RangeCheckAirBuilder for AB {}
 
-pub trait RecursionMemoryAirBuilder: BaseAirBuilder {
+pub trait RecursionMemoryAirBuilder: RangeCheckAirBuilder {
     fn recursion_eval_memory_access<E: Into<Self::Expr> + Clone>(
         &mut self,
         timestamp: impl Into<Self::Expr>,
@@ -93,13 +95,57 @@ pub trait RecursionMemoryAirBuilder: BaseAirBuilder {
         ));
     }
 
-    fn eval_memory_access_timestamp<E: Into<Self::Expr>>(
+    fn eval_memory_access_timestamp<E: Into<Self::Expr> + Clone>(
         &mut self,
-        _timestamp: impl Into<Self::Expr>,
-        _mem_access: &impl MemoryAccessTimestampCols<E>,
-        _is_real: impl Into<Self::Expr>,
+        timestamp: impl Into<Self::Expr>,
+        mem_access: &impl MemoryAccessTimestampCols<E>,
+        is_real: impl Into<Self::Expr> + Clone,
     ) {
-        // TODO: check that mem_access.prev_clk < clk if is_real.
+        let diff_minus_one: Self::Expr =
+            timestamp.into() - mem_access.prev_timestamp().clone().into() - Self::Expr::one();
+
+        // Verify that mem_access.ts_diff = mem_access.ts_diff_16bit_limb
+        // + mem_access.ts_diff_8bit_limb * 2^16.
+        self.eval_range_check_28bits(
+            diff_minus_one,
+            mem_access.diff_16bit_limb().clone(),
+            mem_access.diff_12bit_limb().clone(),
+            is_real.clone(),
+        );
+    }
+
+    /// Verifies the inputted value is within 24 bits.
+    ///
+    /// This method verifies that the inputted is less than 2^24 by doing a 16 bit and 8 bit range
+    /// check on it's limbs.  It will also verify that the limbs are correct.  This method is needed
+    /// since the memory access timestamp check (see [Self::verify_mem_access_ts]) needs to assume
+    /// the clk is within 24 bits.
+    fn eval_range_check_28bits(
+        &mut self,
+        value: impl Into<Self::Expr>,
+        limb_16: impl Into<Self::Expr> + Clone,
+        limb_12: impl Into<Self::Expr> + Clone,
+        is_real: impl Into<Self::Expr> + Clone,
+    ) {
+        // Verify that value = limb_16 + limb_8 * 2^16.
+        self.when(is_real.clone()).assert_eq(
+            value,
+            limb_16.clone().into()
+                + limb_12.clone().into() * Self::Expr::from_canonical_u32(1 << 16),
+        );
+
+        // Send the range checks for the limbs.
+        self.send_range_check(
+            Self::Expr::from_canonical_u8(RangeCheckOpcode::U16 as u8),
+            limb_16,
+            is_real.clone(),
+        );
+
+        self.send_range_check(
+            Self::Expr::from_canonical_u8(RangeCheckOpcode::U12 as u8),
+            limb_12,
+            is_real,
+        )
     }
 }
 
