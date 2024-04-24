123e3702cc12264598f60e0d336ca581eda63985
John Guibas <jtguibas@gmail.com>
2024-04-24 16:07:25 -0700

    boom

--- recursion/core/src/stark/poseidon2.rs
+++ recursion/core/src/stark/poseidon2.rs
@@ -38,3 +38,18 @@ pub fn bn254_poseidon2_rc3() -> Vec<[Bn254Fr; 3]> {
         })
         .collect()
 }
+
+pub fn bn254_poseidon2_rc4() -> Vec<[Bn254Fr; 4]> {
+    RC3.iter()
+        .map(|vec| {
+            let result: [Bn254Fr; 3] = vec
+                .iter()
+                .cloned()
+                .map(bn254_from_ark_ff)
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+            [result[0], result[1], result[2], result[2]]
+        })
+        .collect()
+}
