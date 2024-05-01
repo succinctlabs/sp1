ab2d47602d0a43fd43cb45894e75822af4263759
John Guibas <jtguibas@gmail.com>
2024-05-01 12:42:31 -0700

    bom

--- prover/scripts/e2e.rs
+++ prover/scripts/e2e.rs
@@ -25,56 +25,58 @@ pub fn main() {
 
     let elf = include_bytes!("../../examples/fibonacci/program/elf/riscv32im-succinct-zkvm-elf");
 
-    tracing::info!("initializing prover");
-    let prover = SP1Prover::new();
+    // tracing::info!("initializing prover");
+    // let prover = SP1Prover::new();
 
-    tracing::info!("setup elf");
-    let (pk, vk) = prover.setup(elf);
+    // tracing::info!("setup elf");
+    // let (pk, vk) = prover.setup(elf);
 
-    tracing::info!("prove core");
-    let stdin = SP1Stdin::new();
-    let core_proof = prover.prove_core(&pk, &stdin);
+    // tracing::info!("prove core");
+    // let stdin = SP1Stdin::new();
+    // let core_proof = prover.prove_core(&pk, &stdin);
 
-    tracing::info!("reduce");
-    let reduced_proof = prover.reduce(&vk, core_proof, vec![]);
+    // tracing::info!("reduce");
+    // let reduced_proof = prover.reduce(&vk, core_proof, vec![]);
 
-    tracing::info!("compress");
-    let compressed_proof = prover.compress(&vk, reduced_proof);
+    // tracing::info!("compress");
+    // let compressed_proof = prover.compress(&vk, reduced_proof);
 
-    tracing::info!("wrap");
-    let wrapped_proof = prover.wrap_bn254(&vk, compressed_proof);
+    // tracing::info!("wrap");
+    // let wrapped_proof = prover.wrap_bn254(&vk, compressed_proof);
 
-    tracing::info!("building verifier constraints");
-    let constraints = tracing::info_span!("wrap circuit")
-        .in_scope(|| build_wrap_circuit(&prover.wrap_vk, wrapped_proof.clone()));
+    // tracing::info!("building verifier constraints");
+    // let constraints = tracing::info_span!("wrap circuit")
+    //     .in_scope(|| build_wrap_circuit(&prover.wrap_vk, wrapped_proof.clone()));
 
-    tracing::info!("building template witness");
-    let mut witness = Witness::default();
-    wrapped_proof.write(&mut witness);
-    let vk_commit: OuterDigest = prover.wrap_vk.commit.into();
-    vk_commit.write(&mut witness);
-    prover.wrap_vk.pc_start.write(&mut witness);
+    // tracing::info!("building template witness");
+    // let mut witness = Witness::default();
+    // wrapped_proof.write(&mut witness);
+    // let vk_commit: OuterDigest = prover.wrap_vk.commit.into();
+    // vk_commit.write(&mut witness);
+    // prover.wrap_vk.pc_start.write(&mut witness);
 
-    // tracing::info!("sanity check gnark test");
-    // Groth16Prover::test(constraints.clone(), witness.clone());
+    // // tracing::info!("sanity check gnark test");
+    // // Groth16Prover::test(constraints.clone(), witness.clone());
 
-    tracing::info!("sanity check gnark build");
-    Groth16Prover::build(
-        constraints.clone(),
-        witness.clone(),
-        args.build_dir.clone().into(),
-    );
+    // tracing::info!("sanity check gnark build");
+    // Groth16Prover::build(
+    //     constraints.clone(),
+    //     witness.clone(),
+    //     args.build_dir.clone().into(),
+    // );
 
     tracing::info!("sanity check gnark prove");
     let groth16_prover = Groth16Prover::new(args.build_dir.clone().into());
 
-    tracing::info!("gnark prove");
-    let proof = groth16_prover.prove(witness.clone());
+    // tracing::info!("gnark prove");
+    // let proof = groth16_prover.prove(witness.clone());
 
-    tracing::info!("gnark cancel");
-    groth16_prover.cancel();
+    // tracing::info!("gnark cancel");
+    // groth16_prover.cancel();
 
-    let elf = include_bytes!("../../tests/keccak256/elf/riscv32im-succinct-zkvm-elf");
+    let elf = include_bytes!(
+        "../../examples/fibonacci-benchmark/program/elf/riscv32im-succinct-zkvm-elf"
+    );
 
     tracing::info!("initializing prover");
     let prover = SP1Prover::new();
