4e85a10df1abf1666e8409970bd7565521fb33a1
John Guibas <jtguibas@gmail.com>
2024-05-05 17:45:35 -0700

    fix

--- recursion/core/src/stark/utils.rs
+++ recursion/core/src/stark/utils.rs
@@ -58,7 +58,7 @@ pub fn run_test_recursion(
     }
 
     if test_config == TestConfig::All || test_config == TestConfig::SkinnyDeg7 {
-        let machine = RecursionAirSkinnyDeg7::machine(BabyBearPoseidon2::default());
+        let machine = RecursionAirSkinnyDeg7::machine(BabyBearPoseidon2::compressed());
         let (pk, vk) = machine.setup(&program);
         let record = runtime.record.clone();
         let result = run_test_machine(record, machine, pk, vk);
