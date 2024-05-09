4dbfdf1f86db0179853048237a13952216a14999
John Guibas <jtguibas@gmail.com>
2024-05-09 12:49:05 -0700

    fix: proof aggregation issue

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -836,6 +836,7 @@ mod tests {
     #[test]
     #[serial]
     fn test_e2e_with_deferred_proofs() {
+        env::set_var("SHARD_BATCH_SIZE", "1");
         setup_logger();
 
         // Test program which proves the Keccak-256 hash of various inputs.
