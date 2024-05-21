4cbf76b6edd06c04de49538fa68bcf8805d960d3
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-05-21 15:01:20 -0700

    feat: move groth16 feature flag into ffi

--- recursion/gnark-ffi/src/groth16.rs
+++ recursion/gnark-ffi/src/groth16.rs
@@ -38,9 +38,7 @@ impl Groth16Prover {
 
     /// Executes the prover in testing mode with a circuit definition and witness.
     pub fn test<C: Config>(constraints: Vec<Constraint>, witness: Witness<C>) {
-        cfg_if! {
-            if #[cfg(feature = "groth16")] {
-                let serialized = serde_json::to_string(&constraints).unwrap();
+        let serialized = serde_json::to_string(&constraints).unwrap();
 
         // Write constraints.
         let mut constraints_file = tempfile::NamedTempFile::new().unwrap();
@@ -56,75 +54,56 @@ impl Groth16Prover {
             witness_file.path().to_str().unwrap(),
             constraints_file.path().to_str().unwrap(),
         );
-            }
-            else {
-                panic!("groth16 feature not enabled")
-            }
-        }
     }
 
     /// Builds the groth16 circuit locally.
     pub fn build<C: Config>(constraints: Vec<Constraint>, witness: Witness<C>, build_dir: PathBuf) {
-        cfg_if! {
-            if #[cfg(feature = "groth16")] {
-                let serialized = serde_json::to_string(&constraints).unwrap();
+        let serialized = serde_json::to_string(&constraints).unwrap();
 
-                // Write constraints.
-                let constraints_path = build_dir.join("constraints_groth16.json");
-                let mut file = File::create(constraints_path).unwrap();
-                file.write_all(serialized.as_bytes()).unwrap();
+        // Write constraints.
+        let constraints_path = build_dir.join("constraints_groth16.json");
+        let mut file = File::create(constraints_path).unwrap();
+        file.write_all(serialized.as_bytes()).unwrap();
 
-                // Write witness.
-                let witness_path = build_dir.join("witness_groth16.json");
-                let gnark_witness = GnarkWitness::new(witness);
-                let mut file = File::create(witness_path).unwrap();
-                let serialized = serde_json::to_string(&gnark_witness).unwrap();
-                file.write_all(serialized.as_bytes()).unwrap();
+        // Write witness.
+        let witness_path = build_dir.join("witness_groth16.json");
+        let gnark_witness = GnarkWitness::new(witness);
+        let mut file = File::create(witness_path).unwrap();
+        let serialized = serde_json::to_string(&gnark_witness).unwrap();
+        file.write_all(serialized.as_bytes()).unwrap();
 
-                build_groth16(build_dir.to_str().unwrap());
+        build_groth16(build_dir.to_str().unwrap());
 
-                // Extend the built verifier with the sp1 verifier contract.
-                let groth16_verifier_path = build_dir.join("SP1Verifier.sol");
+        // Extend the built verifier with the sp1 verifier contract.
+        let groth16_verifier_path = build_dir.join("SP1Verifier.sol");
 
-                // Open the file in append mode.
-                let mut groth16_verifier_file = OpenOptions::new()
-                    .append(true)
-                    .open(groth16_verifier_path)
-                    .expect("failed to open file");
+        // Open the file in append mode.
+        let mut groth16_verifier_file = OpenOptions::new()
+            .append(true)
+            .open(groth16_verifier_path)
+            .expect("failed to open file");
 
-                // Write the string to the file
-                let sp1_verifier_str = include_str!("../assets/SP1Verifier.txt");
-                groth16_verifier_file
-                    .write_all(sp1_verifier_str.as_bytes())
-                    .expect("Failed to write to file");
-            }
-            else {
-                panic!("groth16 feature not enabled")
-            }
-        }
+        // Write the string to the file
+        let sp1_verifier_str = include_str!("../assets/SP1Verifier.txt");
+        groth16_verifier_file
+            .write_all(sp1_verifier_str.as_bytes())
+            .expect("Failed to write to file");
     }
 
     /// Generates a Groth16 proof by sending a request to the Gnark server.
     pub fn prove<C: Config>(&self, witness: Witness<C>, build_dir: PathBuf) -> Groth16Proof {
-        cfg_if! {
-            if #[cfg(feature = "groth16")] {
-                // Write witness.
-                let mut witness_file = tempfile::NamedTempFile::new().unwrap();
-                let gnark_witness = GnarkWitness::new(witness);
-                let serialized = serde_json::to_string(&gnark_witness).unwrap();
-                witness_file.write_all(serialized.as_bytes()).unwrap();
+        // Write witness.
+        let mut witness_file = tempfile::NamedTempFile::new().unwrap();
+        let gnark_witness = GnarkWitness::new(witness);
+        let serialized = serde_json::to_string(&gnark_witness).unwrap();
+        witness_file.write_all(serialized.as_bytes()).unwrap();
 
-                let serialized = serde_json::to_string(&gnark_witness).unwrap();
+        let serialized = serde_json::to_string(&gnark_witness).unwrap();
 
-                prove_groth16(
-                    build_dir.to_str().unwrap(),
-                    witness_file.path().to_str().unwrap(),
-                )
-            } else {
-                panic!("groth16 feature not enabled")
-
-            }
-        }
+        prove_groth16(
+            build_dir.to_str().unwrap(),
+            witness_file.path().to_str().unwrap(),
+        )
     }
 
     /// Verify a Groth16 proof and verify that the supplied vkey_hash and committed_values_digest match.
@@ -135,19 +114,13 @@ impl Groth16Prover {
         committed_values_digest: &BigUint,
         build_dir: &Path,
     ) {
-        cfg_if! {
-            if #[cfg(feature = "groth16")] {
-                verify_groth16(
-                    build_dir.to_str().unwrap(),
-                    &proof.raw_proof,
-                    &vkey_hash.to_string(),
-                    &committed_values_digest.to_string(),
-                )
-                .expect("failed to verify proof")
-            } else {
-                panic!("groth16 feature not enabled")
-            }
-        }
+        verify_groth16(
+            build_dir.to_str().unwrap(),
+            &proof.raw_proof,
+            &vkey_hash.to_string(),
+            &committed_values_digest.to_string(),
+        )
+        .expect("failed to verify proof")
     }
 }
 
