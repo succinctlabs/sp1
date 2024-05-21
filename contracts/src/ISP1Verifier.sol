daafde6f781fe7963bdf2c9a50babe298d40da6f
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-05-21 12:32:46 -0700

    update contracts

--- contracts/src/ISP1Verifier.sol
+++ contracts/src/ISP1Verifier.sol
@@ -2,25 +2,6 @@
 /// @author Succinct Labs
 /// @notice This contracts implements a solidity verifier for SP1.
 interface ISP1Verifier {
-    /// @notice Deserializes a proof from the given bytes.
-    /// @param proofBytes The proof bytes.
-    function deserializeProof(
-        bytes memory proofBytes
-    )
-        external
-        pure
-        returns (
-            uint256[8] memory proof,
-            uint256[2] memory commitments,
-            uint256[2] memory commitmentPok
-        );
-
-    /// @notice Hashes the public values to a field elements inside Bn254.
-    /// @param publicValues The public values.
-    function hashPublicValues(
-        bytes memory publicValues
-    ) external pure returns (bytes32);
-
     /// @notice Verifies a proof with given public values and vkey.
     /// @param vkey The verification key for the RISC-V program.
     /// @param publicValues The public values encoded as bytes.
