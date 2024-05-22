6c67b6f2d5378ec8e0d6723946a4aa3d52d742db
Kevin Jue <kjue235@gmail.com>
2024-05-22 11:13:26 -0700

    chore(recursion): document IR (#737)

--- recursion/compiler/src/asm/compiler.rs
+++ recursion/compiler/src/asm/compiler.rs
@@ -467,18 +467,12 @@ impl<F: PrimeField32 + TwoAdicField, EF: ExtensionField<F> + TwoAdicField> AsmCo
                     }
                     _ => unimplemented!(),
                 },
-                DslIr::Num2BitsF(_, _) => unimplemented!(),
-                DslIr::Num2BitsV(_, _) => unimplemented!(),
                 DslIr::Poseidon2PermuteBabyBear(dst, src) => match (dst, src) {
                     (Array::Dyn(dst, _), Array::Dyn(src, _)) => {
                         self.push(AsmInstruction::Poseidon2Permute(dst.fp(), src.fp()), trace)
                     }
                     _ => unimplemented!(),
                 },
-                DslIr::ReverseBitsLen(_, _, _) => unimplemented!(),
-                DslIr::TwoAdicGenerator(_, _) => unimplemented!(),
-                DslIr::ExpUsizeV(_, _, _) => unimplemented!(),
-                DslIr::ExpUsizeF(_, _, _) => unimplemented!(),
                 DslIr::Error() => self.push(AsmInstruction::Trap, trace),
                 DslIr::PrintF(dst) => self.push(AsmInstruction::PrintF(dst.fp()), trace),
                 DslIr::PrintV(dst) => self.push(AsmInstruction::PrintV(dst.fp()), trace),
