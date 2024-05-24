d6b14fbd39ef564943b9c8c5c694e2eed33369ce
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-05-23 18:00:52 -0700

    fix tests with affine point

--- tests/bn254-mul/src/main.rs
+++ tests/bn254-mul/src/main.rs
@@ -15,7 +15,7 @@ pub fn main() {
         0, 0, 0, 0,
     ];
 
-    let mut a_point = AffinePoint::<Bn254, 16>::from_le_bytes(a);
+    let mut a_point = AffinePoint::<Bn254, 16>::from_le_bytes(&a);
 
     // scalar.
     // 3
