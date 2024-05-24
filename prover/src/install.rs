1f44d542f2ccfa7b76cd0d9ce24e80117f93ddc6
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-05-23 17:25:34 -0700

    add todo

--- prover/src/install.rs
+++ prover/src/install.rs
@@ -10,6 +10,7 @@ use crate::utils::block_on;
 pub const PLONK_BN254_ARTIFACTS_URL_BASE: &str = "https://sp1-circuits.s3-us-east-2.amazonaws.com";
 
 /// The current version of the plonk bn254 artifacts.
+/// TODO: Update with artifacts that include SP1Verifier.txt, SP1MockVerifier.txt, etc.
 pub const PLONK_BN254_ARTIFACTS_COMMIT: &str = "d7b2513f";
 
 /// Install the latest plonk bn254 artifacts.
