77b12902745bfa9fb942df44317100f6c0b098a6
John Guibas <jtguibas@gmail.com>
2024-05-29 11:58:24 -0700

    fix: issue 7 revision

--- core/src/cpu/air/ecall.rs
+++ core/src/cpu/air/ecall.rs
@@ -35,10 +35,14 @@ impl CpuChip {
         let syscall_id = syscall_code[0];
         let send_to_table = syscall_code[1];
 
-        // When is_ecall_instruction == true AND send_to_table == true, ecall_mul_send_to_table should be true.
-        builder
-            .when(is_ecall_instruction.clone())
-            .assert_eq(local.ecall_mul_send_to_table, send_to_table);
+        // Handle cases:
+        // - is_ecall_instruction = 1 => ecall_mul_send_to_table == send_to_table
+        // - is_ecall_instruction = 0 => ecall_mul_send_to_table == 0
+        builder.assert_eq(
+            local.ecall_mul_send_to_table,
+            send_to_table * is_ecall_instruction.clone(),
+        );
+
         builder.send_syscall(
             local.shard,
             local.channel,
