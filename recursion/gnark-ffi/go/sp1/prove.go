aad1502c02efc05153ad03266144752d11ee7dce
ratan kaliani <ratan@succinct.xyz>
2024-05-29 18:28:57 -0700

    lint

--- recursion/gnark-ffi/go/sp1/prove.go
+++ recursion/gnark-ffi/go/sp1/prove.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/consensys/gnark-crypto/ecc"
-	plonk "github.com/consensys/gnark/backend/plonk"
+	"github.com/consensys/gnark/backend/plonk"
 	"github.com/consensys/gnark/frontend"
 )
 
