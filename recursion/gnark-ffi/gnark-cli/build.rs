efc330fdddc35c18126024e8b9909c2be4f6392b
Bruce Collie <brucecollie82@gmail.com>
2024-05-31 21:59:13 +0100

    Build bindings; expose rough CLI
    This PR reinstates the relevant parts of the non-docker build script
    (i.e. using bindgen to build the Go / CGo bindings), and sketches an
    as-yet-unimplemented CLI for the tool to be run through docker.

--- recursion/gnark-ffi/gnark-cli/build.rs
+++ recursion/gnark-ffi/gnark-cli/build.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::path::PathBuf;
+
+#[allow(deprecated)]
+use bindgen::CargoCallbacks;
+
 fn main() {
-    println!("cargo:rustc-link-lib=static={}", "sp1gnark");
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = PathBuf::from(&out_dir);
+    let lib_name = "sp1gnark";
+
+    // Generate bindings using bindgen
+    let header_path = format!("/usr/include/lib{}.h", lib_name);
+    let bindings = bindgen::Builder::default()
+        .header(header_path)
+        .parse_callbacks(Box::new(CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file(dest_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    println!("cargo:rustc-link-lib=static={}", lib_name);
 }
--- recursion/gnark-ffi/gnark-cli/src/main.rs
+++ recursion/gnark-ffi/gnark-cli/src/main.rs
@@ -1,5 +1,79 @@
 mod babybear;
 
+use clap::{Args, Parser, Subcommand};
+
+#[allow(warnings, clippy::all)]
+mod bind {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+use bind::*;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Build(BuildArgs),
+    Prove(ProveArgs),
+    Verify(VerifyArgs),
+    Test(TestArgs),
+}
+
+#[derive(Debug, Args)]
+struct BuildArgs {
+    data_dir: String,
+}
+
+#[derive(Debug, Args)]
+struct ProveArgs {
+    data_dir: String,
+    witness_path: String,
+}
+
+#[derive(Debug, Args)]
+struct VerifyArgs {
+    data_dir: String,
+    proof: String,
+    vkey_hash: String,
+    committed_values_digest: String,
+}
+
+#[derive(Debug, Args)]
+struct TestArgs {
+    witness_json: String,
+    constraints_json: String,
+}
+
+fn run_build(args: BuildArgs) {
+    dbg!(args);
+    todo!();
+}
+
+fn run_prove(args: ProveArgs) {
+    dbg!(args);
+    todo!();
+}
+
+fn run_verify(args: VerifyArgs) {
+    dbg!(args);
+    todo!();
+}
+
+fn run_test(args: TestArgs) {
+    dbg!(args);
+    todo!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Build(args) => run_build(args),
+        Command::Prove(args) => run_prove(args),
+        Command::Verify(args) => run_verify(args),
+        Command::Test(args) => run_test(args),
+    }
 }
