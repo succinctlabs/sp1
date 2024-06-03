1ed165e1a829cb80c0e6fda5a4a571a4ec16b830
Chris Tian <chris@succinct.xyz>
2024-06-03 13:31:52 -0700

    fix

--- zkvm/precompiles/src/uint256_div.rs
+++ zkvm/precompiles/src/uint256_div.rs
@@ -40,13 +40,15 @@ pub fn uint256_div(x: &mut [u8; 32], y: &[u8; 32]) -> [u8; 32] {
 
             let mut quotient_times_y = [0u8; 32];
             let zero = [0u32; 8];
-            sys_bigint(
-                quotient_times_y.as_mut_ptr() as *mut [u32; 8],
-                0,
-                quotient_bytes.as_ptr() as *const [u32; 8],
-                y.as_ptr() as *const [u32; 8],
-                zero.as_ptr() as *const [u32; 8]
-            );
+            unsafe {
+                sys_bigint(
+                    quotient_times_y.as_mut_ptr() as *mut [u32; 8],
+                    0,
+                    quotient_bytes.as_ptr() as *const [u32; 8],
+                    y.as_ptr() as *const [u32; 8],
+                    zero.as_ptr() as *const [u32; 8]
+                );
+            }
 
             let quotient_times_divisor = BigUint::from_bytes_le(&quotient_times_y);
             assert_eq!(quotient_times_divisor, dividend - remainder);
