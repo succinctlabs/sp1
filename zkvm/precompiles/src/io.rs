b0f511bd3a9b216e441d1177e958ff7b91d1dbb2
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-06-05 17:19:39 -0700

    fix

--- zkvm/precompiles/src/io.rs
+++ zkvm/precompiles/src/io.rs
@@ -54,11 +54,11 @@ pub fn read_vec() -> Vec<u8> {
 
 pub fn read<T: DeserializeOwned>() -> T {
     let vec = read_vec();
+    unimplemented!();
     bincode::deserialize(&vec).expect("deserialization failed")
 }
 
 pub fn commit<T: Serialize>(value: &T) {
-    unimplemented!();
     let writer = SyscallWriter {
         fd: FD_PUBLIC_VALUES,
     };
