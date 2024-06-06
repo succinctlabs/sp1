940687fda1918ef80d82d23076f70e588346873a
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-06-05 17:29:34 -0700

    readd unimplemented

--- zkvm/precompiles/src/io.rs
+++ zkvm/precompiles/src/io.rs
@@ -55,6 +55,7 @@ pub fn read_vec() -> Vec<u8> {
 pub fn read<T: DeserializeOwned>() -> T {
     let vec = read_vec();
     println!("vec: {:?}", vec);
+    unimplemented!();
     bincode::deserialize(&vec).expect("deserialization failed")
 }
 
