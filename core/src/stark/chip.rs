105b45f9d2c85e47e09d8e0f08a630525904cc98
Tamir Hemo <tamir@succinct.xyz>
2024-06-10 17:12:47 -0700

    fix: remove mutable requirement (#892)
    Co-authored-by: tamirhemo <tamirhmo@gmai.com>

--- core/src/stark/chip.rs
+++ core/src/stark/chip.rs
@@ -113,7 +113,7 @@ where
     pub fn generate_permutation_trace<EF: ExtensionField<F>>(
         &self,
         preprocessed: Option<&RowMajorMatrix<F>>,
-        main: &mut RowMajorMatrix<F>,
+        main: &RowMajorMatrix<F>,
         random_elements: &[EF],
     ) -> RowMajorMatrix<EF>
     where
