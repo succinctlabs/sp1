d5a1423ffe4740b154b60764f17b201c7d94e80e
John Guibas <jtguibas@gmail.com>
2024-06-12 10:12:14 -0700

    fix: no mangle

--- zkvm/entrypoint/src/syscalls/sys.rs
+++ zkvm/entrypoint/src/syscalls/sys.rs
@@ -23,6 +23,7 @@ static SYS_RAND_WARNING: std::sync::Once = std::sync::Once::new();
 /// # Safety
 ///
 /// Make sure that `buf` has at least `nwords` words.
+#[no_mangle]
 pub unsafe extern "C" fn sys_rand(recv_buf: *mut u8, words: usize) {
     SYS_RAND_WARNING.call_once(|| {
         println!("WARNING: Using insecure random number generator.");
