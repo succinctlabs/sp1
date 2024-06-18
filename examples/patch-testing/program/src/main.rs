f97896b034e3eaafdb7b149c86b0417409ff7f96
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-06-18 12:10:56 -0700

    fix

--- examples/patch-testing/program/src/main.rs
+++ examples/patch-testing/program/src/main.rs
@@ -8,34 +8,32 @@ use sha2_v0_10_6::{Digest as Digest_10_6, Sha256 as Sha256_10_6};
 use sha2_v0_9_8::{Digest as Digest_9_8, Sha256 as Sha256_9_8};
 use tiny_keccak::{Hasher, Keccak};
 
+/// To add testing for a new patch, add a new case to the function below.
 fn main() {
-    let num_cases = 5;
     let input = [1u8; 32];
 
     let sig: Signature = sp1_zkvm::io::read();
     let vk: VerificationKey = sp1_zkvm::io::read();
     let msg: Vec<u8> = sp1_zkvm::io::read_vec();
 
-    for _ in 0..num_cases {
-        // Test Keccak.
-        let mut hasher = Keccak::v256();
-        hasher.update(&input);
-        let mut output = [0u8; 32];
-        hasher.finalize(&mut output);
+    // Test Keccak.
+    let mut hasher = Keccak::v256();
+    hasher.update(&input);
+    let mut output = [0u8; 32];
+    hasher.finalize(&mut output);
 
-        // Test SHA256.
-        let mut sha256_9_8 = Sha256_9_8::new();
-        sha256_9_8.update(input);
-        let _ = sha256_9_8.finalize();
+    // Test SHA256.
+    let mut sha256_9_8 = Sha256_9_8::new();
+    sha256_9_8.update(input);
+    let _ = sha256_9_8.finalize();
 
-        let mut sha256_10_6 = Sha256_10_6::new();
-        sha256_10_6.update(input);
-        let _ = sha256_10_6.finalize();
+    let mut sha256_10_6 = Sha256_10_6::new();
+    sha256_10_6.update(input);
+    let _ = sha256_10_6.finalize();
 
-        // let mut sha256_10_8 = Sha256_10_8::new();
-        // sha256_10_8.update(input);
-        // let output_10_8 = sha256_10_8.finalize();
-    }
+    // let mut sha256_10_8 = Sha256_10_8::new();
+    // sha256_10_8.update(input);
+    // let output_10_8 = sha256_10_8.finalize();
 
     // Test curve25519-dalek-ng.
     let y = CompressedEdwardsY(input);
--- examples/patch-testing/script/src/main.rs
+++ examples/patch-testing/script/src/main.rs
@@ -4,8 +4,8 @@ use sp1_sdk::{utils, ProverClient, SP1Stdin};
 
 const PATCH_TEST_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
+/// This script is used to test that SP1 patches are correctly applied and syscalls are triggered.
 fn main() {
-    // Generate proof.
     utils::setup_logger();
 
     let mut stdin = SP1Stdin::new();
@@ -25,7 +25,7 @@ fn main() {
         .execute(PATCH_TEST_ELF, stdin)
         .expect("proving failed");
 
-    // Confirm that there was at least 1 SHA_COMPUTE syscall.
+    // Confirm there was at least 1 SHA_COMPUTE syscall.
     assert!(report
         .syscall_counts
         .contains_key(&sp1_core::runtime::SyscallCode::SHA_COMPRESS));
@@ -33,7 +33,7 @@ fn main() {
         .syscall_counts
         .contains_key(&sp1_core::runtime::SyscallCode::SHA_EXTEND));
 
-    // Confirm there were ED25519_COMPUTE syscalls.
+    // Confirm there was at least 1 ED25519_COMPUTE syscalls.
     assert!(report
         .syscall_counts
         .contains_key(&sp1_core::runtime::SyscallCode::ED_ADD));
@@ -41,7 +41,7 @@ fn main() {
         .syscall_counts
         .contains_key(&sp1_core::runtime::SyscallCode::ED_DECOMPRESS));
 
-    // Confirm that there was at least 1 KECCAK_PERMUTE syscall.
+    // Confirm there was at least 1 KECCAK_PERMUTE syscall.
     assert!(report
         .syscall_counts
         .contains_key(&sp1_core::runtime::SyscallCode::KECCAK_PERMUTE));
