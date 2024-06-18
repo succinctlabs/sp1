df81519172ef02e76291a3ecc1b55ae32aaf6a9a
Tej Qu Nair <tej.q.nair@gmail.com>
2024-06-18 10:06:41 -0700

    feat: (breaking changes to SDK API) use builder pattern for SDK execute/prove/verify (#940)
    - CLI test now fails because of the breaking changes
    - Refactor a lot of the parallel three-type proof code in SDK to use the new `SP1Proof` enum.

--- core/src/utils/options.rs
+++ core/src/utils/options.rs
@@ -3,7 +3,22 @@ use std::env;
 const DEFAULT_SHARD_SIZE: usize = 1 << 22;
 const DEFAULT_SHARD_BATCH_SIZE: usize = 16;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SP1ProverOpts {
+    pub core_opts: SP1CoreOpts,
+    pub recursion_opts: SP1CoreOpts,
+}
+
+impl Default for SP1ProverOpts {
+    fn default() -> Self {
+        Self {
+            core_opts: SP1CoreOpts::default(),
+            recursion_opts: SP1CoreOpts::recursion(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct SP1CoreOpts {
     pub shard_size: usize,
     pub shard_batch_size: usize,
