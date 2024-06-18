6779e2add2f9b9900df4209d57e5b853faeb1db2
John Guibas <jtguibas@gmail.com>
2024-06-18 11:42:52 -0700

    feat: optimize byte trace gen

--- core/src/bytes/trace.rs
+++ core/src/bytes/trace.rs
@@ -9,6 +9,7 @@ use super::{
 };
 use crate::{
     air::MachineAir,
+    bytes::ByteOpcode,
     runtime::{ExecutionRecord, Program},
 };
 
@@ -28,10 +29,7 @@ impl<F: Field> MachineAir<F> for ByteChip<F> {
     }
 
     fn generate_preprocessed_trace(&self, _program: &Self::Program) -> Option<RowMajorMatrix<F>> {
-        // OPT: We should be able to make this a constant. Also, trace / map should be separate.
-        // Since we only need the trace and not the map, we can just pass 0 as the shard.
-        let (trace, _) = Self::trace_and_map(0);
-
+        let trace = Self::trace_and_map(0);
         Some(trace)
     }
 
@@ -44,23 +42,23 @@ impl<F: Field> MachineAir<F> for ByteChip<F> {
         input: &ExecutionRecord,
         _output: &mut ExecutionRecord,
     ) -> RowMajorMatrix<F> {
-        let shard = input.index;
-        let (_, event_map) = Self::trace_and_map(shard);
-
         let mut trace = RowMajorMatrix::new(
             vec![F::zero(); NUM_BYTE_MULT_COLS * NUM_ROWS],
             NUM_BYTE_MULT_COLS,
         );
 
+        let shard = input.index;
         for (lookup, mult) in input.byte_lookups[&shard].iter() {
-            let (row, index) = event_map[lookup];
+            let row = if lookup.opcode != ByteOpcode::U16Range {
+                ((lookup.b << 8) + lookup.c) as usize
+            } else {
+                lookup.a1 as usize
+            };
+            let index = lookup.opcode as usize;
             let channel = lookup.channel as usize;
+
             let cols: &mut ByteMultCols<F> = trace.row_mut(row).borrow_mut();
-
-            // Update the trace multiplicity
             cols.mult_channels[channel].multiplicities[index] += F::from_canonical_usize(*mult);
-
-            // Set the shard column as the current shard.
             cols.shard = F::from_canonical_u32(shard);
         }
 
