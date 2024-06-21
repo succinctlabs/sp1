dfdab8e9020d5a42ffa78a442b306771d4d273e5
Chris Tian <chris@succinct.xyz>
2024-06-21 19:04:08 +0000

    fix

--- core/src/utils/options.rs
+++ core/src/utils/options.rs
@@ -2,6 +2,8 @@ use std::env;
 
 const DEFAULT_SHARD_SIZE: usize = 1 << 22;
 const DEFAULT_SHARD_BATCH_SIZE: usize = 16;
+const DEFAULT_SHARD_CHUNKING_MULTIPLIER: usize = 1;
+const DEFAULT_RECONSTRUCT_COMMITMENTS: bool = true;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct SP1ProverOpts {
@@ -37,17 +39,28 @@ impl Default for SP1CoreOpts {
                 |_| DEFAULT_SHARD_BATCH_SIZE,
                 |s| s.parse::<usize>().unwrap_or(DEFAULT_SHARD_BATCH_SIZE),
             ),
-            shard_chunking_multiplier: 1,
-            reconstruct_commitments: true,
+            shard_chunking_multiplier: env::var("SHARD_CHUNKING_MULTIPLIER").map_or_else(
+                |_| DEFAULT_SHARD_CHUNKING_MULTIPLIER,
+                |s| {
+                    s.parse::<usize>()
+                        .unwrap_or(DEFAULT_SHARD_CHUNKING_MULTIPLIER)
+                },
+            ),
+            reconstruct_commitments: env::var("RECONSTRUCT_COMMITMENTS").map_or_else(
+                |_| DEFAULT_RECONSTRUCT_COMMITMENTS,
+                |s| s.parse::<bool>().unwrap_or(DEFAULT_RECONSTRUCT_COMMITMENTS),
+            ),
         }
     }
 }
 
 impl SP1CoreOpts {
     pub fn recursion() -> Self {
-        let mut opts = Self::default();
-        opts.reconstruct_commitments = false;
-        opts.shard_size = DEFAULT_SHARD_SIZE;
-        opts
+        Self {
+            shard_size: DEFAULT_SHARD_SIZE,
+            shard_batch_size: DEFAULT_SHARD_BATCH_SIZE,
+            shard_chunking_multiplier: DEFAULT_SHARD_CHUNKING_MULTIPLIER,
+            reconstruct_commitments: false,
+        }
     }
 }
