69ee4ded33377325ab8db2434978a0873f1ea722
Tej Qu Nair <tej.q.nair@gmail.com>
2024-06-26 12:09:45 -0700

    add chip, without memory

--- recursion/core-v2/src/add.rs
+++ recursion/core-v2/src/add.rs
@@ -7,17 +7,20 @@ use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::Matrix;
 use sp1_core::air::MachineAir;
 use sp1_core::air::SP1AirBuilder;
-use sp1_core::runtime::ExecutionRecord;
+// use sp1_core::runtime::ExecutionRecord;
 use sp1_core::runtime::Program;
 use sp1_core::utils::pad_rows_fixed;
+use sp1_core::utils::pad_to_power_of_two;
 use sp1_derive::AlignedBorrow;
 use std::borrow::BorrowMut;
 use tracing::instrument;
 
+use crate::*;
+
 pub const NUM_ADD_COLS: usize = core::mem::size_of::<AddCols<u8>>();
 
 #[derive(Default)]
-pub struct AddChip<const DEGREE: usize> {}
+pub struct AddChip {}
 
 #[derive(AlignedBorrow, Debug, Clone, Copy)]
 #[repr(C)]
@@ -28,14 +31,14 @@ pub struct AddCols<T: Copy> {
     pub is_real: T,
 }
 
-impl<F, const DEGREE: usize> BaseAir<F> for AddChip<DEGREE> {
+impl<F> BaseAir<F> for AddChip {
     fn width(&self) -> usize {
         NUM_ADD_COLS
     }
 }
 
-impl<F: PrimeField32, const DEGREE: usize> MachineAir<F> for AddChip<DEGREE> {
-    type Record = ExecutionRecord;
+impl<F: PrimeField32> MachineAir<F> for AddChip {
+    type Record = ExecutionRecord<F>;
 
     type Program = Program;
 
@@ -47,22 +50,47 @@ impl<F: PrimeField32, const DEGREE: usize> MachineAir<F> for AddChip<DEGREE> {
         // This is a no-op.
     }
 
-    fn generate_trace(
-        &self,
-        input: &ExecutionRecord,
-        _: &mut ExecutionRecord,
-    ) -> RowMajorMatrix<F> {
-        let mut rows = vec![F::zero(); NUM_ADD_COLS * 4];
+    fn generate_trace(&self, input: &Self::Record, _: &mut Self::Record) -> RowMajorMatrix<F> {
+        let add_events = input.add_events.clone();
 
-        RowMajorMatrix::new(rows, NUM_ADD_COLS)
+        // Generate the trace rows & corresponding records for each chunk of events in parallel.
+        let rows = add_events
+            .into_iter()
+            .map(|event| {
+                let mut row = [F::zero(); NUM_ADD_COLS];
+
+                assert_eq!(event.opcode, Opcode::Add);
+
+                let AluEvent { a, b, c, .. } = event;
+
+                let cols: &mut AddCols<_> = row.as_mut_slice().borrow_mut();
+                *cols = AddCols {
+                    a,
+                    b,
+                    c,
+                    is_real: F::one(),
+                };
+
+                row
+            })
+            .collect::<Vec<_>>();
+
+        // Convert the trace to a row major matrix.
+        let mut trace =
+            RowMajorMatrix::new(rows.into_iter().flatten().collect::<Vec<_>>(), NUM_ADD_COLS);
+
+        // Pad the trace to a power of two.
+        pad_to_power_of_two::<NUM_ADD_COLS, F>(&mut trace.values);
+
+        trace
     }
 
-    fn included(&self, record: &Self::Record) -> bool {
+    fn included(&self, _record: &Self::Record) -> bool {
         true
     }
 }
 
-impl<AB, const DEGREE: usize> Air<AB> for AddChip<DEGREE>
+impl<AB> Air<AB> for AddChip
 where
     AB: SP1AirBuilder,
 {
@@ -84,3 +112,91 @@ and then also fill in generate_trace and eval and write test (look at add_sub in
 you will also need to write your own execution record struct but look at recursion-core for how we did that
 
 */
+
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+    use p3_baby_bear::BabyBear;
+    use p3_baby_bear::DiffusionMatrixBabyBear;
+    use p3_field::AbstractField;
+    use p3_matrix::dense::RowMajorMatrix;
+    use p3_matrix::Matrix;
+    use p3_poseidon2::Poseidon2;
+    use p3_poseidon2::Poseidon2ExternalMatrixGeneral;
+
+    use rand::{thread_rng, Rng};
+    use std::time::Instant;
+
+    use sp1_core::{air::MachineAir, utils::uni_stark_verify};
+    use sp1_core::{
+        stark::StarkGenericConfig,
+        utils::{uni_stark_prove, BabyBearPoseidon2},
+    };
+
+    use super::*;
+
+    #[test]
+    fn generate_trace() {
+        let shard = ExecutionRecord::<BabyBear> {
+            add_events: vec![AluEvent {
+                opcode: Opcode::Add,
+                a: BabyBear::one(),
+                b: BabyBear::one(),
+                c: BabyBear::two(),
+            }],
+            // ..Default::default()
+        };
+        let chip = AddChip::default();
+        let trace: RowMajorMatrix<BabyBear> =
+            chip.generate_trace(&shard, &mut ExecutionRecord::default());
+        println!("{:?}", trace.values)
+    }
+
+    #[test]
+    fn prove_babybear() {
+        let config = BabyBearPoseidon2::compressed();
+        let mut challenger = config.challenger();
+
+        let chip = AddChip::default();
+
+        let test_xs = (1..8).map(BabyBear::from_canonical_u32).collect_vec();
+
+        let test_ys = (1..8).map(BabyBear::from_canonical_u32).collect_vec();
+
+        let mut input_exec = ExecutionRecord::<BabyBear>::default();
+        for (x, y) in test_xs.into_iter().cartesian_product(test_ys) {
+            input_exec.add_events.push(AluEvent {
+                opcode: Opcode::Add,
+                a: x + y,
+                b: x,
+                c: y,
+            });
+        }
+        println!("input exec: {:?}", input_exec.add_events.len());
+        let trace: RowMajorMatrix<BabyBear> =
+            chip.generate_trace(&input_exec, &mut ExecutionRecord::<BabyBear>::default());
+        println!(
+            "trace dims is width: {:?}, height: {:?}",
+            trace.width(),
+            trace.height()
+        );
+
+        let start = Instant::now();
+        let proof = uni_stark_prove(&config, &chip, &mut challenger, trace);
+        let duration = start.elapsed().as_secs_f64();
+        println!("proof duration = {:?}", duration);
+
+        let mut challenger: p3_challenger::DuplexChallenger<
+            BabyBear,
+            Poseidon2<BabyBear, Poseidon2ExternalMatrixGeneral, DiffusionMatrixBabyBear, 16, 7>,
+            16,
+            8,
+        > = config.challenger();
+        let start = Instant::now();
+        uni_stark_verify(&config, &chip, &mut challenger, &proof)
+            .expect("expected proof to be valid");
+
+        let duration = start.elapsed().as_secs_f64();
+        println!("verify duration = {:?}", duration);
+    }
+}
--- recursion/core-v2/src/lib.rs
+++ recursion/core-v2/src/lib.rs
@@ -1,16 +1,61 @@
+use std::collections::HashMap;
+
+use p3_field::PrimeField32;
 use sp1_core::stark::MachineRecord;
 
-mod add;
+pub mod add;
 
-struct Address;
+// #[derive(Clone, Debug)]
+// pub struct Address;
+// pub type Address = u32;
 
-struct BinOp {
-    arg1: Address,
-    arg2: Address,
-    dest: Address,
+// I don't think events should depend on the field being used,
+// but I don't want to implement encoding or memory yet
+#[derive(Clone, Debug)]
+pub struct AluEvent<F> {
+    pub opcode: Opcode,
+    pub a: F,
+    pub b: F,
+    pub c: F,
 }
 
-enum Opcode {
-    Add(BinOp),
-    Mul(BinOp),
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Opcode {
+    Add,
+    Mul,
+}
+
+#[derive(Clone, Default, Debug)]
+pub struct ExecutionRecord<F> {
+    pub add_events: Vec<AluEvent<F>>,
+    // _data: std::marker::PhantomData<F>,
+    // pub vars: HashMap<Address, u32>,
+}
+
+impl<F: PrimeField32> MachineRecord for ExecutionRecord<F> {
+    type Config = ();
+
+    fn index(&self) -> u32 {
+        todo!()
+    }
+
+    fn set_index(&mut self, _index: u32) {
+        todo!()
+    }
+
+    fn stats(&self) -> hashbrown::HashMap<String, usize> {
+        todo!()
+    }
+
+    fn append(&mut self, _other: &mut Self) {
+        todo!()
+    }
+
+    fn shard(self, _config: &Self::Config) -> Vec<Self> {
+        todo!()
+    }
+
+    fn public_values<T: p3_field::AbstractField>(&self) -> Vec<T> {
+        todo!()
+    }
 }
