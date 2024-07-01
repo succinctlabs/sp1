2a5adbafd734147fe436c581b645282e7f46dd85
Tej Qu Nair <tej.q.nair@gmail.com>
2024-07-01 14:40:38 -0700

    feat: make `AddressValue.value` a `Block` (#998)

--- recursion/core-v2/src/lib.rs
+++ recursion/core-v2/src/lib.rs
@@ -1,4 +1,6 @@
-use p3_field::PrimeField32;
+use std::iter::once;
+
+use p3_field::{AbstractField, PrimeField32};
 use sp1_core::{air::PublicValues, stark::MachineRecord};
 
 pub mod alu;
@@ -16,9 +18,9 @@ pub mod program;
 #[derive(Clone, Debug)]
 pub struct AluEvent<F> {
     pub opcode: Opcode,
-    pub out: AddressValue<F>,
-    pub in1: AddressValue<F>,
-    pub in2: AddressValue<F>,
+    pub out: AddressValueBase<F>,
+    pub in1: AddressValueBase<F>,
+    pub in2: AddressValueBase<F>,
     pub mult: F, // number of times we need this value in the future
 }
 
@@ -38,12 +40,49 @@ pub enum MemAccessKind {
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct AddressValue<F> {
     addr: F,
-    val: F,
+    val: Block<F>,
 }
 
 impl<F> AddressValue<F> {
+    pub fn new(addr: F, val: Block<F>) -> Self {
+        Self { addr, val }
+    }
+
+    pub fn iter(&self) -> std::iter::Chain<std::iter::Once<&F>, std::slice::Iter<F>> {
+        once(&self.addr).chain(self.val.0.iter())
+    }
+
+    pub fn iter_mut(
+        &mut self,
+    ) -> std::iter::Chain<std::iter::Once<&mut F>, std::slice::IterMut<F>> {
+        once(&mut self.addr).chain(self.val.0.iter_mut())
+    }
+}
+
+/// Used for base field computations.
+/// Should not be read/written to memory directly. Instead, convert to `AddressValue`.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct AddressValueBase<F> {
+    addr: F,
+    val: F,
+}
+
+impl<F> AddressValueBase<F> {
     pub fn new(addr: F, val: F) -> Self {
         Self { addr, val }
+    }
+}
+
+// TODO this is not what we want and doesn't work in `eval`
+impl<F: Default> From<AddressValueBase<F>> for AddressValue<F> {
+    fn from(other: AddressValueBase<F>) -> Self {
+        let AddressValueBase { addr, val } = other;
+        let mut padded_val = <[F; D]>::default();
+        padded_val[0] = val;
+        Self {
+            addr,
+            val: Block(padded_val),
+        }
     }
 }
 
@@ -111,6 +150,7 @@ impl<F: PrimeField32> MachineRecord for ExecutionRecord<F> {
 use p3_field::Field;
 use serde::{Deserialize, Serialize};
 use sp1_core::air::MachineProgram;
+use sp1_recursion_core::{air::Block, runtime::D};
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct RecursionProgram<F> {
