40edfdd189267268b3c561690d84b92e6add588c
John Guibas <jtguibas@gmail.com>
2024-07-02 12:31:28 -0700

    chore: cleanup zkvm/lib

--- zkvm/lib/src/io.rs
+++ zkvm/lib/src/io.rs
@@ -4,20 +4,25 @@ use crate::{syscall_hint_len, syscall_hint_read};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::alloc::Layout;
+use std::io::Result;
 use std::io::Write;
 
-const FD_HINT: u32 = 4;
+/// The file descriptor for public values.
 pub const FD_PUBLIC_VALUES: u32 = 3;
-// Runtime hook file descriptors. Make sure these match the FDs in the HookRegistry.
-// The default hooks can be found in `core/src/runtime/hooks.rs`.
+
+/// The file descriptor for hints.
+pub const FD_HINT: u32 = 4;
+
+/// The file descriptor for the `ecreover` hook.
 pub const FD_ECRECOVER_HOOK: u32 = 5;
 
-pub struct SyscallWriter {
+/// A writer that writes to a file descriptor inside the zkVM.
+struct SyscallWriter {
     fd: u32,
 }
 
-impl std::io::Write for SyscallWriter {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+impl Write for SyscallWriter {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
         let nbytes = buf.len();
         let write_buf = buf.as_ptr();
         unsafe {
@@ -26,19 +31,26 @@ impl std::io::Write for SyscallWriter {
         Ok(nbytes)
     }
 
-    fn flush(&mut self) -> std::io::Result<()> {
+    fn flush(&mut self) -> Result<()> {
         Ok(())
     }
 }
 
+/// Read a buffer from the input stream.
+///
+/// ### Examples
+/// ```no_run
+/// let data: Vec<u8> = sp1_zkvm::io::read_vec();
+/// ```
 pub fn read_vec() -> Vec<u8> {
-    let len = unsafe { syscall_hint_len() };
     // Round up to the nearest multiple of 4 so that the memory allocated is in whole words
+    let len = unsafe { syscall_hint_len() };
     let capacity = (len + 3) / 4 * 4;
 
     // Allocate a buffer of the required length that is 4 byte aligned
     let layout = Layout::from_size_align(capacity, 4).expect("vec is too large");
     let ptr = unsafe { std::alloc::alloc(layout) };
+
     // SAFETY:
     // 1. `ptr` was allocated using alloc
     // 2. We assuume that the VM global allocator doesn't dealloc
@@ -46,6 +58,7 @@ pub fn read_vec() -> Vec<u8> {
     // 4/5. Length is 0
     // 7. Layout::from_size_align already checks this
     let mut vec = unsafe { Vec::from_raw_parts(ptr, 0, capacity) };
+
     // Read the vec into uninitialized memory. The syscall assumes the memory is uninitialized,
     // which should be true because the allocator does not dealloc, so a new alloc should be fresh.
     unsafe {
@@ -55,11 +68,43 @@ pub fn read_vec() -> Vec<u8> {
     vec
 }
 
+/// Read a deserializable object from the input stream.
+///
+/// ### Examples
+/// ```no_run
+/// use serde::{Deserialize, Serialize};
+///
+/// #[derive(Serialize, Deserialize)]
+/// struct MyStruct {
+///     a: u32,
+///     b: u32,
+/// }
+///
+/// let data: MyStruct = sp1_zkvm::io::read();
+/// ```
 pub fn read<T: DeserializeOwned>() -> T {
     let vec = read_vec();
     bincode::deserialize(&vec).expect("deserialization failed")
 }
 
+/// Commit a serializable object to the public values stream.
+///
+/// ### Examples
+/// ```no_run
+/// use serde::{Deserialize, Serialize};
+///
+/// #[derive(Serialize, Deserialize)]
+/// struct MyStruct {
+///     a: u32,
+///     b: u32,
+/// }
+///
+/// let data = MyStruct {
+///     a: 1,
+///     b: 2,
+/// };
+/// sp1_zkvm::io::commit(&data);
+/// ```
 pub fn commit<T: Serialize>(value: &T) {
     let writer = SyscallWriter {
         fd: FD_PUBLIC_VALUES,
@@ -67,6 +112,13 @@ pub fn commit<T: Serialize>(value: &T) {
     bincode::serialize_into(writer, value).expect("serialization failed");
 }
 
+/// Commit bytes to the public values stream.
+///
+/// ### Examples
+/// ```no_run
+/// let data = vec![1, 2, 3, 4];
+/// sp1_zkvm::io::commit_slice(&data);
+/// ```
 pub fn commit_slice(buf: &[u8]) {
     let mut my_writer = SyscallWriter {
         fd: FD_PUBLIC_VALUES,
@@ -74,17 +126,48 @@ pub fn commit_slice(buf: &[u8]) {
     my_writer.write_all(buf).unwrap();
 }
 
+/// Hint a serializable object to the hint stream.
+///
+/// ### Examples
+/// ```no_run
+/// use serde::{Deserialize, Serialize};
+///
+/// #[derive(Serialize, Deserialize)]
+/// struct MyStruct {
+///     a: u32,
+///     b: u32,
+/// }
+///
+/// let data = MyStruct {
+///     a: 1,
+///     b: 2,
+/// };
+/// sp1_zkvm::io::hint(&data);
+/// ```
 pub fn hint<T: Serialize>(value: &T) {
     let writer = SyscallWriter { fd: FD_HINT };
     bincode::serialize_into(writer, value).expect("serialization failed");
 }
 
+/// Hint bytes to the hint stream.
+///
+/// ### Examples
+/// ```no_run
+/// let data = vec![1, 2, 3, 4];
+/// sp1_zkvm::io::hint_slice(&data);
+/// ```
 pub fn hint_slice(buf: &[u8]) {
     let mut my_reader = SyscallWriter { fd: FD_HINT };
     my_reader.write_all(buf).unwrap();
 }
 
-/// Write the data `buf` to the file descriptor `fd` using `Write::write_all` .
+/// Write the data `buf` to the file descriptor `fd`.
+///
+/// ### Examples
+/// ```no_run
+/// let data = vec![1, 2, 3, 4];
+/// sp1_zkvm::io::write(3, &data);
+/// ```
 pub fn write(fd: u32, buf: &[u8]) {
     SyscallWriter { fd }.write_all(buf).unwrap();
 }
--- zkvm/lib/src/lib.rs
+++ zkvm/lib/src/lib.rs
@@ -1,13 +1,16 @@
-//! Precompiles for SP1 zkVM.
+//! Precompiles for the SP1 zkVM.
 //!
 //! Specifically, this crate contains user-friendly functions that call SP1 syscalls. Syscalls are
 //! also declared here for convenience. In order to avoid duplicate symbol errors, the syscall
 //! function impls must live in sp1-zkvm, which is only imported into the end user program crate.
 //! In contrast, sp1-precompiles can be imported into any crate in the dependency tree.
 
+#[cfg(feature = "bls12381")]
 pub mod bls12381;
+#[cfg(feature = "bn254")]
 pub mod bn254;
 pub mod io;
+#[cfg(feature = "secp256k1")]
 pub mod secp256k1;
 pub mod unconstrained;
 pub mod utils;
