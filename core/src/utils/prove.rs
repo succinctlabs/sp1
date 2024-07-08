9f364340155da62fc17365d5f555491526ffe3cc
Tamir Hemo <tamir@succinct.xyz>
2024-07-08 16:14:18 -0700

    fix

--- core/src/utils/prove.rs
+++ core/src/utils/prove.rs
@@ -201,7 +201,9 @@ where
 
         // Update the public values & prover state for the shards which do not contain "cpu events"
         // before committing to them.
-        state.execution_shard += 1;
+        if !is_last_checkpoint {
+            state.execution_shard += 1;
+        }
         for record in deferred.iter_mut() {
             state.shard += 1;
             state.previous_init_addr_bits = record.public_values.previous_init_addr_bits;
@@ -269,7 +271,9 @@ where
 
         // Update the public values & prover state for the shards which do not contain "cpu events"
         // before committing to them.
-        state.execution_shard += 1;
+        if !is_last_checkpoint {
+            state.execution_shard += 1;
+        }
         for record in deferred.iter_mut() {
             state.shard += 1;
             state.previous_init_addr_bits = record.public_values.previous_init_addr_bits;
