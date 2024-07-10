5365024008b5b09f14c527e80b55dff7c46154a2
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-07-09 17:09:30 -0700

    clean

--- build/src/lib.rs
+++ build/src/lib.rs
@@ -4,7 +4,7 @@ use std::{
     fs,
     io::{BufRead, BufReader},
     path::PathBuf,
-    process::{exit, Command, Stdio},
+    process::{exit, ChildStderr, ChildStdout, Command, Stdio},
     thread,
 };
 
@@ -58,6 +58,123 @@ fn get_docker_image(tag: &str) -> String {
     })
 }
 
+/// Modify the command to build the program in the docker container. Mounts the entire workspace
+/// and sets the working directory to the program directory.
+fn get_docker_cmd(
+    command: &mut Command,
+    args: &BuildArgs,
+    workspace_root: &Utf8PathBuf,
+    program_dir: &Utf8PathBuf,
+) -> Result<()> {
+    let image = get_docker_image(&args.tag);
+
+    let docker_check = Command::new("docker")
+        .args(["info"])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status()
+        .context("failed to run docker command")?;
+
+    if !docker_check.success() {
+        eprintln!("docker is not installed or not running: https://docs.docker.com/get-docker/");
+        exit(1);
+    }
+
+    // Mount the entire workspace, and set the working directory to the program dir.
+    let workspace_root_path = format!("{}:/root/program", workspace_root);
+    let program_dir_path = format!(
+        "/root/program/{}",
+        program_dir.strip_prefix(workspace_root).unwrap()
+    );
+    let mut child_args = vec![
+        "run".to_string(),
+        "--rm".to_string(),
+        "--platform".to_string(),
+        "linux/amd64".to_string(),
+        "-v".to_string(),
+        workspace_root_path,
+        "-w".to_string(),
+        program_dir_path,
+        image,
+        "prove".to_string(),
+        "build".to_string(),
+    ];
+
+    // Add the `cargo prove build` arguments to the child process command, while ignoring
+    // `--docker`, as this command will be invoked in a docker container.
+    add_cargo_prove_build_args(&mut child_args, args.clone(), true);
+
+    command.args(&child_args);
+    Ok(())
+}
+
+/// Modify the command to build the program in the local environment.
+fn get_build_cmd(
+    command: &mut Command,
+    args: &BuildArgs,
+    program_dir: &Utf8PathBuf,
+    build_target: String,
+) -> Result<()> {
+    let rust_flags = [
+        "-C".to_string(),
+        "passes=loweratomic".to_string(),
+        "-C".to_string(),
+        "link-arg=-Ttext=0x00200800".to_string(),
+        "-C".to_string(),
+        "panic=abort".to_string(),
+    ];
+
+    let mut cargo_args = vec![
+        "build".to_string(),
+        "--release".to_string(),
+        "--target".to_string(),
+        build_target,
+    ];
+
+    // Add the `cargo prove build` arguments to the child process command, while ignoring
+    // `--docker`, as we already know it's unused in this conditional.
+    add_cargo_prove_build_args(&mut cargo_args, args.clone(), true);
+
+    // Ensure the Cargo.lock doesn't update.
+    cargo_args.push("--locked".to_string());
+
+    command
+        .current_dir(program_dir.clone())
+        .env("RUSTUP_TOOLCHAIN", "succinct")
+        .env("CARGO_ENCODED_RUSTFLAGS", rust_flags.join("\x1f"))
+        .args(&cargo_args);
+    Ok(())
+}
+
+/// Add the [sp1] or [docker] prefix to the output of the child process depending on the context.
+fn handle_cmd_output(
+    stdout: BufReader<ChildStdout>,
+    stderr: BufReader<ChildStderr>,
+    build_with_helper: bool,
+    docker: bool,
+) {
+    let msg = if build_with_helper && docker {
+        "[sp1] [docker] "
+    } else if build_with_helper {
+        "[sp1] "
+    } else if docker {
+        "[docker] "
+    } else {
+        ""
+    };
+    // Pipe stdout and stderr to the parent process with [docker] prefix
+    let stdout_handle = thread::spawn(move || {
+        stdout.lines().for_each(|line| {
+            println!("{} {}", msg, line.unwrap());
+        });
+    });
+    stderr.lines().for_each(|line| {
+        eprintln!("{} {}", msg, line.unwrap());
+    });
+
+    stdout_handle.join().unwrap();
+}
+
 /// Build a program with the specified BuildArgs. program_dir is specified as an argument when
 /// the program is built via build_program in sp1-helper.
 pub fn build_program(args: &BuildArgs, program_dir: Option<PathBuf>) -> Result<Utf8PathBuf> {
@@ -77,131 +194,33 @@ pub fn build_program(args: &BuildArgs, program_dir: Option<PathBuf>) -> Result<U
     let root_package_name = root_package.as_ref().map(|p| &p.name);
 
     let build_target = "riscv32im-succinct-zkvm-elf".to_string();
-    if args.docker {
-        let image = get_docker_image(&args.tag);
-
-        let docker_check = Command::new("docker")
-            .args(["info"])
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .status()
-            .context("failed to run docker command")?;
-
-        if !docker_check.success() {
-            eprintln!(
-                "docker is not installed or not running: https://docs.docker.com/get-docker/"
-            );
-            exit(1);
-        }
-
-        let workspace_root_path = format!("{}:/root/program", metadata.workspace_root);
-        let mut child_args = vec![
-            "run".to_string(),
-            "--rm".to_string(),
-            "--platform".to_string(),
-            "linux/amd64".to_string(),
-            "-v".to_string(),
-            workspace_root_path,
-            image,
-            "prove".to_string(),
-            "build".to_string(),
-        ];
-
-        // Add the `cargo prove build` arguments to the child process command, while ignoring
-        // `--docker`, as this command will be invoked in a docker container.
-        add_cargo_prove_build_args(&mut child_args, args.clone(), true);
-
-        let mut child = Command::new("docker")
-            .args(&child_args)
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()
-            .context("failed to spawn command")?;
-
-        let stdout = BufReader::new(child.stdout.take().unwrap());
-        let stderr = BufReader::new(child.stderr.take().unwrap());
-
-        // Pipe stdout and stderr to the parent process with [docker] prefix
-        let stdout_handle = thread::spawn(move || {
-            stdout.lines().for_each(|line| {
-                if build_with_helper {
-                    println!("[sp1] [docker] {}", line.unwrap());
-                } else {
-                    println!("[docker] {}", line.unwrap());
-                }
-            });
-        });
-        stderr.lines().for_each(|line| {
-            if build_with_helper {
-                eprintln!("[sp1] [docker] {}", line.unwrap());
-            } else {
-                eprintln!("[docker] {}", line.unwrap());
-            }
-        });
-
-        stdout_handle.join().unwrap();
-
-        let result = child.wait()?;
-        if !result.success() {
-            // Error message is already printed by cargo
-            exit(result.code().unwrap_or(1))
-        }
+    let mut cmd = if args.docker {
+        let mut docker_cmd = Command::new("docker");
+        get_docker_cmd(
+            &mut docker_cmd,
+            args,
+            &metadata.workspace_root,
+            &program_dir,
+        )?;
+        docker_cmd
     } else {
-        let rust_flags = [
-            "-C".to_string(),
-            "passes=loweratomic".to_string(),
-            "-C".to_string(),
-            "link-arg=-Ttext=0x00200800".to_string(),
-            "-C".to_string(),
-            "panic=abort".to_string(),
-        ];
+        let mut build_cmd = Command::new("cargo");
+        get_build_cmd(&mut build_cmd, args, &program_dir, build_target.clone())?;
+        build_cmd
+    };
 
-        let mut cargo_args = vec![
-            "build".to_string(),
-            "--release".to_string(),
-            "--target".to_string(),
-            build_target.clone(),
-        ];
-
-        // Add the `cargo prove build` arguments to the child process command, while ignoring
-        // `--docker`, as we already know it's unused in this conditional.
-        add_cargo_prove_build_args(&mut cargo_args, args.clone(), true);
-
-        // Ensure the Cargo.lock doesn't update.
-        cargo_args.push("--locked".to_string());
-
-        let mut child = Command::new("cargo")
-            .current_dir(program_dir.clone())
-            .env("RUSTUP_TOOLCHAIN", "succinct")
-            .env("CARGO_ENCODED_RUSTFLAGS", rust_flags.join("\x1f"))
-            .args(&cargo_args)
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()
-            .context("Failed to spawn command")?;
-
-        if build_with_helper {
-            let stdout = BufReader::new(child.stdout.take().unwrap());
-            let stderr = BufReader::new(child.stderr.take().unwrap());
-
-            // Pipe stdout and stderr to the parent process with [sp1] prefix
-            let stdout_handle = thread::spawn(move || {
-                stdout.lines().for_each(|line| {
-                    println!("[sp1] {}", line.unwrap());
-                });
-            });
-            stderr.lines().for_each(|line| {
-                eprintln!("[sp1] {}", line.unwrap());
-            });
-
-            stdout_handle.join().unwrap();
-        }
-
-        let result = child.wait()?;
-
-        if !result.success() {
-            exit(result.code().unwrap_or(1))
-        }
+    let mut child = cmd
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .context("failed to spawn command")?;
+    let stdout = BufReader::new(child.stdout.take().unwrap());
+    let stderr = BufReader::new(child.stderr.take().unwrap());
+    handle_cmd_output(stdout, stderr, build_with_helper, args.docker);
+    let result = child.wait()?;
+    if !result.success() {
+        // Error message is already printed by cargo.
+        exit(result.code().unwrap_or(1))
     }
 
     let elf_path = metadata
