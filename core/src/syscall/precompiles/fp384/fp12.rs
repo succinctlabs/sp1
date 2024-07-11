c13c50a0fbb34d6c64d0785eb83dcab223b99557
Bhargav Annem <bhargav.annem@gmail.com>
2024-07-10 19:02:10 -0700

    wip: columns

--- core/src/syscall/precompiles/fp384/fp12.rs
+++ core/src/syscall/precompiles/fp384/fp12.rs
@@ -2,7 +2,7 @@ use crate::air::{BaseAirBuilder, MachineAir, Polynomial, SP1AirBuilder, WORD_SIZ
 use crate::bytes::event::ByteRecord;
 use crate::memory::{value_as_limbs, MemoryReadCols, MemoryWriteCols};
 use crate::operations::field::field_op::{FieldOpCols, FieldOperation};
-use crate::operations::field::params::NumWords;
+use crate::operations::field::params::{FieldParameters, NumWords};
 use crate::operations::field::params::{Limbs, NumLimbs};
 use crate::operations::IsZeroOperation;
 use crate::runtime::{ExecutionRecord, Program, Syscall, SyscallCode};
@@ -27,6 +27,7 @@ use p3_matrix::Matrix;
 use serde::{Deserialize, Serialize};
 use sp1_derive::AlignedBorrow;
 use std::borrow::{Borrow, BorrowMut};
+use std::marker::PhantomData;
 use std::mem::size_of;
 use typenum::Unsigned;
 
@@ -57,70 +58,70 @@ const NUM_FP_MULS: usize = 144;
 #[repr(C)]
 pub struct Fp6MulCols<T> {
     // c0:
-    // self.c0.c0 * b.c0.c0 - self.c0.c1 * b.c0.c1 + self.c1.c0 * b20_m_b21
-    //     - self.c1.c1 * b20_p_b21
-    //     + self.c2.c0 * b10_m_b11
-    //     - self.c2.c1 * b10_p_b11,
-    pub c0_b10_p_b11: FieldOpCols<T, U384Field>, // self.c1.c0 + self.c1.c1
-    pub c0_b10_m_b11: FieldOpCols<T, U384Field>, // self.c1.c0 - self.c1.c1
-    pub c0_b20_p_b21: FieldOpCols<T, U384Field>, // self.c2.c0 + self.c2.c1
-    pub c0_b20_m_b21: FieldOpCols<T, U384Field>, // self.c2.c0 - self.c2.c1
+    // a.c0.c0 * b.c0.c0 - a.c0.c1 * b.c0.c1 + a.c1.c0 * b20_m_b21
+    //     - a.c1.c1 * b20_p_b21
+    //     + a.c2.c0 * b10_m_b11
+    //     - a.c2.c1 * b10_p_b11,
+    pub c0_b10_p_b11: FieldOpCols<T, U384Field>, // a.c1.c0 + b.c1.c1
+    pub c0_b10_m_b11: FieldOpCols<T, U384Field>, // a.c1.c0 - b.c1.c1
+    pub c0_b20_p_b21: FieldOpCols<T, U384Field>, // a.c2.c0 + b.c2.c1
+    pub c0_b20_m_b21: FieldOpCols<T, U384Field>, // a.c2.c0 - b.c2.c1
 
-    pub c0_a00_t_b00: FieldOpCols<T, U384Field>, // self.c0.c0 * self.b.c0.c0
-    pub c0_a01_t_b01: FieldOpCols<T, U384Field>, // self.c0.c1 * self.b.c0.c1
-    pub c0_a10_t_b20_m_b21: FieldOpCols<T, U384Field>, // self.c1.c0 * (self.c2.c0 - self.c2.c1)
-    pub c0_a11_t_b20_p_b21: FieldOpCols<T, U384Field>, // self.c1.c1 * (self.c2.c0 + self.c2.c1)
-    pub c0_a20_t_b10_m_b11: FieldOpCols<T, U384Field>, // self.c2.c0 * (self.c1.c0 - self.c1.c1)
-    pub c0_a21_t_b10_p_b11: FieldOpCols<T, U384Field>, // self.c2.c1 * (self.c1.c0 + self.c1.c1)
+    pub c0_a00_t_b00: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c0
+    pub c0_a01_t_b01: FieldOpCols<T, U384Field>, // a.c0.c1 * b.c0.c1
+    pub c0_a10_t_b20_m_b21: FieldOpCols<T, U384Field>, // a.c1.c0 * (a.c2.c0 - b.c2.c1)
+    pub c0_a11_t_b20_p_b21: FieldOpCols<T, U384Field>, // a.c1.c1 * (a.c2.c0 + b.c2.c1)
+    pub c0_a20_t_b10_m_b11: FieldOpCols<T, U384Field>, // a.c2.c0 * (a.c1.c0 - b.c1.c1)
+    pub c0_a21_t_b10_p_b11: FieldOpCols<T, U384Field>, // a.c2.c1 * (a.c1.c0 + b.c1.c1)
 
-    pub c0_m_a01_t_b01: FieldOpCols<T, U384Field>, // self.c0.c0 * self.b.c0.c0 - self.c0.c1 * self.b.c0.c1
-    pub c0_p_a10_t_b20_m_b21: FieldOpCols<T, U384Field>, // self.c0.c0 * b.c0.c0 - self.c0.c1 * b.c0.c1 + self.c1.c0 * b20_m_b21
-    pub c0_m_a11_t_b20_p_b21: FieldOpCols<T, U384Field>, // self.c0.c0 * b.c0.c0 - self.c0.c1 * b.c0.c1 + self.c1.c0 * b20_m_b21 - self.c1.c1 * b20_p_b21
-    pub c0_p_a20_t_b10_m_b11: FieldOpCols<T, U384Field>, // self.c0.c0 * b.c0.c0 - self.c0.c1 * b.c0.c1 + self.c1.c0 * b20_m_b21 - self.c1.c1 * b20_p_b21 + self.c2.c0 * b10_m_b11
-    pub c0: FieldOpCols<T, U384Field>, // self.c0.c0 * b.c0.c0 - self.c0.c1 * b.c0.c1 + self.c1.c0 * b20_m_b21 - self.c1.c1 * b20_p_b21 + self.c2.c0 * b10_m_b11 - self.c2.c1 * b10_p_b11
+    pub c0_m_a01_t_b01: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c0 - a.c0.c1 * b.c0.c1
+    pub c0_p_a10_t_b20_m_b21: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c0 - a.c0.c1 * b.c0.c1 + a.c1.c0 * b20_m_b21
+    pub c0_m_a11_t_b20_p_b21: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c0 - a.c0.c1 * b.c0.c1 + a.c1.c0 * b20_m_b21 - a.c1.c1 * b20_p_b21
+    pub c0_p_a20_t_b10_m_b11: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c0 - a.c0.c1 * b.c0.c1 + a.c1.c0 * b20_m_b21 - a.c1.c1 * b20_p_b21 + a.c2.c0 * b10_m_b11
+    pub c0: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c0 - a.c0.c1 * b.c0.c1 + a.c1.c0 * b20_m_b21 - a.c1.c1 * b20_p_b21 + a.c2.c0 * b10_m_b11 - a.c2.c1 * b10_p_b11
 
-    // c1: self.c0.c0 * b.c0.c1
-    // + self.c0.c1 * b.c0.c0
-    // + self.c1.c0 * b20_p_b21
-    // + self.c1.c1 * b20_m_b21
-    // + self.c2.c0 * b10_p_b11
-    // + self.c2.c1 * b10_m_b11,
-    pub c1_a00_t_b01: FieldOpCols<T, U384Field>, // self.c0.c0 * self.b.c0.c1
-    pub c1_a01_t_b00: FieldOpCols<T, U384Field>, // self.c0.c1 * self.b.c0.c0
-    pub c1_a10_t_b20_p_b21: FieldOpCols<T, U384Field>, // self.c1.c0 * (self.c2.c0 + self.c2.c1)
-    pub c1_a11_t_b20_m_b21: FieldOpCols<T, U384Field>, // self.c1.c1 * (self.c2.c0 - self.c2.c1)
-    pub c1_a20_t_b10_p_b11: FieldOpCols<T, U384Field>, // self.c2.c0 * (self.c1.c0 + self.c1.c1)
-    pub c1_a21_t_b10_m_b11: FieldOpCols<T, U384Field>, // self.c2.c1 * (self.c1.c0 - self.c1.c1)
+    // c1: a.c0.c0 * b.c0.c1
+    // + a.c0.c1 * b.c0.c0
+    // + a.c1.c0 * b20_p_b21
+    // + a.c1.c1 * b20_m_b21
+    // + a.c2.c0 * b10_p_b11
+    // + a.c2.c1 * b10_m_b11,
+    pub c1_a00_t_b01: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c1
+    pub c1_a01_t_b00: FieldOpCols<T, U384Field>, // a.c0.c1 * b.c0.c0
+    pub c1_a10_t_b20_p_b21: FieldOpCols<T, U384Field>, // a.c1.c0 * (a.c2.c0 + a.c2.c1)
+    pub c1_a11_t_b20_m_b21: FieldOpCols<T, U384Field>, // a.c1.c1 * (a.c2.c0 - a.c2.c1)
+    pub c1_a20_t_b10_p_b11: FieldOpCols<T, U384Field>, // a.c2.c0 * (a.c1.c0 + a.c1.c1)
+    pub c1_a21_t_b10_m_b11: FieldOpCols<T, U384Field>, // a.c2.c1 * (a.c1.c0 - a.c1.c1)
 
-    pub c1_p_a01_t_b00: FieldOpCols<T, U384Field>, // self.c0.c0 * self.b.c0.c1 + self.c0.c1 * self.b.c0.c0
-    pub c1_p_a10_t_b20_p_b21: FieldOpCols<T, U384Field>, // self.c0.c0 * b.c0.c1 + self.c0.c1 * b.c0.c0 + self.c1.c0 * b20_p_b21
-    pub c1_p_a11_t_b20_m_b21: FieldOpCols<T, U384Field>, // self.c0.c0 * b.c0.c1 + self.c0.c1 * b.c0.c0 + self.c1.c0 * b20_p_b21 + self.c1.c1 * b20_m_b21
-    pub c1_p_a20_t_b10_p_b11: FieldOpCols<T, U384Field>, // self.c0.c0 * b.c0.c1 + self.c0.c1 * b.c0.c0 + self.c1.c0 * b20_p_b21 + self.c1.c1 * b20_m_b21 + self.c2.c0 * b10_p_b11
-    pub c1: FieldOpCols<T, U384Field>, // self.c0.c0 * b.c0.c1 + self.c0.c1 * b.c0.c0 + self.c1.c0 * b20_p_b21 + self.c1.c1 * b20_m_b21 + self.c2.c0 * b10_p_b11 + self.c2.c1 * b10_m_b11
+    pub c1_p_a01_t_b00: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c1 + a.c0.c1 * b.c0.c0
+    pub c1_p_a10_t_b20_p_b21: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c1 + a.c0.c1 * b.c0.c0 + a.c1.c0 * b20_p_b21
+    pub c1_p_a11_t_b20_m_b21: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c1 + a.c0.c1 * b.c0.c0 + a.c1.c0 * b20_p_b21 + a.c1.c1 * b20_m_b21
+    pub c1_p_a20_t_b10_p_b11: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c1 + a.c0.c1 * b.c0.c0 + a.c1.c0 * b20_p_b21 + a.c1.c1 * b20_m_b21 + a.c2.c0 * b10_p_b11
+    pub c1: FieldOpCols<T, U384Field>, // a.c0.c0 * b.c0.c1 + a.c0.c1 * b.c0.c0 + a.c1.c0 * b20_p_b21 + a.c1.c1 * b20_m_b21 + a.c2.c0 * b10_p_b11 + a.c2.c1 * b10_m_b11
 }
 
 #[derive(Debug, Clone, AlignedBorrow)]
 #[repr(C)]
 pub struct Fp6AddCols<T> {
-    pub a00_p_b00: FieldOpCols<T, U384Field>, // self.c0.c0 + self.b.c0.c0
-    pub a01_p_b01: FieldOpCols<T, U384Field>, // self.c0.c1 + self.b.c0.c1
-    pub a10_p_b10: FieldOpCols<T, U384Field>, // self.c1.c0 + self.b.c1.c0
-    pub a11_p_b11: FieldOpCols<T, U384Field>, // self.c1.c1 + self.b.c1.c1
-    pub a20_p_b20: FieldOpCols<T, U384Field>, // self.c2.c0 + self.b.c2.c0
-    pub a21_p_b21: FieldOpCols<T, U384Field>, // self.c2.c1 + self.b.c2.c1
+    pub a00_p_b00: FieldOpCols<T, U384Field>, // a.c0.c0 + b.c0.c0
+    pub a01_p_b01: FieldOpCols<T, U384Field>, // a.c0.c1 + b.c0.c1
+    pub a10_p_b10: FieldOpCols<T, U384Field>, // a.c1.c0 + b.c1.c0
+    pub a11_p_b11: FieldOpCols<T, U384Field>, // a.c1.c1 + b.c1.c1
+    pub a20_p_b20: FieldOpCols<T, U384Field>, // a.c2.c0 + b.c2.c0
+    pub a21_p_b21: FieldOpCols<T, U384Field>, // a.c2.c1 + b.c2.c1
 }
 
 #[derive(Debug, Clone, AlignedBorrow)]
 #[repr(C)]
 pub struct Fp6MulByNonResidue<T> {
-    pub c00: FieldOpCols<T, U384Field>, // self.c2.c0 - self.c2.c1
-    pub c01: FieldOpCols<T, U384Field>, // self.c2.c0 + self.c2.c1
+    pub c00: FieldOpCols<T, U384Field>, // a.c2.c0 - a.c2.c1
+    pub c01: FieldOpCols<T, U384Field>, // a.c2.c0 + a.c2.c1
 
-    pub c10: FieldOpCols<T, U384Field>, // self.c0.c0
-    pub c11: FieldOpCols<T, U384Field>, // self.c0.c1
+    pub c10: FieldOpCols<T, U384Field>, // a.c0.c0
+    pub c11: FieldOpCols<T, U384Field>, // a.c0.c1
 
-    pub c20: FieldOpCols<T, U384Field>, // self.c1.c0
-    pub c21: FieldOpCols<T, U384Field>, // self.c1.c1
+    pub c20: FieldOpCols<T, U384Field>, // a.c1.c0
+    pub c21: FieldOpCols<T, U384Field>, // a.c1.c1
 }
 
 #[derive(Debug, Clone, AlignedBorrow)]
@@ -129,10 +130,10 @@ pub struct AuxFp12MulCols<T> {
     pub aa: Fp6MulCols<T>,
     pub bb: Fp6MulCols<T>,
     pub o: Fp6AddCols<T>,
-    pub y1: Fp6AddCols<T>,         // self.c1 + self.c0
-    pub y2: Fp6MulCols<T>,         // (self.c1 + self.c0) * self.o
-    pub y3: Fp6AddCols<T>,         // (self.c1 + self.c0) * o  - aa
-    pub y: Fp6AddCols<T>,          // (self.c1 + self.c0) * o  - aa - bb
+    pub y1: Fp6AddCols<T>,         // a.c1 + a.c0
+    pub y2: Fp6MulCols<T>,         // (a.c1 + a.c0) * a.o
+    pub y3: Fp6AddCols<T>,         // (a.c1 + a.c0) * o  - aa
+    pub y: Fp6AddCols<T>,          // (a.c1 + a.c0) * o  - aa - bb
     pub x1: Fp6MulByNonResidue<T>, // bb * non_residue
     pub x: Fp6AddCols<T>,          // bb * non_residue + aa
 }
@@ -154,15 +155,19 @@ pub struct Fp12MulCols<T> {
 }
 
 #[derive(Default)]
-pub struct Fp12MulChip;
+pub struct Fp12MulChip<E> {
+    _marker: PhantomData<E>,
+}
 
-impl Fp12MulChip {
+impl<E: FieldParameters> Fp12MulChip<E> {
     pub const fn new() -> Self {
-        Self
+        Self {
+            _marker: PhantomData,
+        }
     }
 }
 
-impl<F: PrimeField32> MachineAir<F> for Fp12MulChip {
+impl<F: PrimeField32, E: FieldParameters> MachineAir<F> for Fp12MulChip {
     type Record = ExecutionRecord;
 
     type Program = Program;
@@ -199,6 +204,7 @@ impl<F: PrimeField32> MachineAir<F> for Fp12MulChip {
                                 ))
                             })
                             .collect_vec();
+                        let modulus = BigUint::from_bytes_le(E::MODULUS);
 
                         // Assign basic values to the columns.
                         cols.is_real = F::one();
@@ -222,7 +228,98 @@ impl<F: PrimeField32> MachineAir<F> for Fp12MulChip {
                             );
                         }
 
-                        // Populate the output columns.
+                        // // Populate the output columns.
+                        // let a000 = &x[0];
+                        // let a001 = &x[1];
+                        // let a010 = &x[2];
+                        // let a011 = &x[3];
+                        // let a020 = &x[4];
+                        // let a021 = &x[5];
+                        // let a100 = &x[6];
+                        // let a101 = &x[7];
+                        // let a110 = &x[8];
+                        // let a111 = &x[9];
+                        // let a120 = &x[10];
+                        // let a121 = &x[11];
+
+                        // let b000 = &y[0];
+                        // let b001 = &y[1];
+                        // let b010 = &y[2];
+                        // let b011 = &y[3];
+                        // let b020 = &y[4];
+                        // let b021 = &y[5];
+                        // let b100 = &y[6];
+                        // let b101 = &y[7];
+                        // let b110 = &y[8];
+                        // let b111 = &y[9];
+                        // let b120 = &y[10];
+                        // let b121 = &y[11];
+
+                        let mul = |dest: FieldOpCols<F, U384Field>,
+                                   a: &BigUint,
+                                   b: &BigUint|
+                         -> BigUint {
+                            dest.populate_with_modulus(
+                                &mut new_byte_lookup_events,
+                                event.shard,
+                                event.channel,
+                                a,
+                                b,
+                                &modulus,
+                                FieldOperation::Mul,
+                            );
+                            (a * b) % &modulus
+                        };
+
+                        let add = |dest: FieldOpCols<F, U384Field>,
+                                   a: &BigUint,
+                                   b: &BigUint|
+                         -> BigUint {
+                            dest.populate_with_modulus(
+                                &mut new_byte_lookup_events,
+                                event.shard,
+                                event.channel,
+                                a,
+                                b,
+                                &modulus,
+                                FieldOperation::Mul,
+                            );
+                            (a * b) % &modulus
+                        };
+
+                        let fp12_mul = |dest: Fp6MulCols<F>, a: [&BigUint; 6], b: [&BigUint; 6]| {
+                            let a00 = a[0];
+                            let a01 = a[1];
+                            let a10 = a[2];
+                            let a11 = a[3];
+                            let a20 = a[4];
+                            let a21 = a[5];
+
+                            let b00 = b[0];
+                            let b01 = b[1];
+                            let b10 = b[2];
+                            let b11 = b[3];
+                            let b20 = b[4];
+                            let b21 = b[5];
+
+                            let c0_b10_p_b11 = &mul(dest.c0_b10_p_b11, b10, b11);
+                            let c0_b10_m_b11 = &mul(dest.c0_b10_m_b11, b10, b11);
+                            let c0_b20_p_b21 = &mul(dest.c0_b20_p_b21, b20, b21);
+                            let c0_b20_m_b21 = &mul(dest.c0_b20_m_b21, b20, b21);
+
+                            let c0_a00_t_b00 = &mul(dest.c0_a00_t_b00, a00, b00);
+                            let c0_a01_t_b01 = &mul(dest.c0_a01_t_b01, a01, b01);
+                            let c0_a10_t_b20_m_b21 =
+                                mul(dest.c0_a10_t_b20_m_b21, a10, c0_b20_m_b21);
+                            let c0_a11_t_b20_p_b21 =
+                                &mul(dest.c0_a11_t_b20_p_b21, a11, c0_b20_p_b21);
+                            let c0_a20_t_b10_m_b11 =
+                                &mul(dest.c0_a20_t_b10_m_b11, a20, c0_b10_m_b11);
+                            let c0_a21_t_b10_p_b11 =
+                                &mul(dest.c0_a21_t_b10_p_b11, a21, c0_b10_p_b11);
+
+                            let c0_m_a01_t_b01 = &mul(dest.c0_m_a01_t_b01, a00, b00);
+                        };
 
                         row
                     })
@@ -234,5 +331,17 @@ impl<F: PrimeField32> MachineAir<F> for Fp12MulChip {
 
     fn included(&self, shard: &Self::Record) -> bool {
         todo!()
+    }
+
+    fn generate_dependencies(&self, input: &Self::Record, output: &mut Self::Record) {
+        self.generate_trace(input, output);
+    }
+
+    fn preprocessed_width(&self) -> usize {
+        0
+    }
+
+    fn generate_preprocessed_trace(&self, _program: &Self::Program) -> Option<RowMajorMatrix<F>> {
+        None
     }
 }
