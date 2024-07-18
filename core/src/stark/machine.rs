fd7297dd368f74649fdb164acb6ee8695e132a1e
John Guibas <jtguibas@gmail.com>
2024-07-18 16:11:23 -0700

    feat: multi-threaded tracing (#1124)

--- core/src/stark/machine.rs
+++ core/src/stark/machine.rs
@@ -244,12 +244,14 @@ impl<SC: StarkGenericConfig, A: MachineAir<Val<SC>>> StarkMachine<SC, A> {
         let chips = self.chips();
         records.iter_mut().for_each(|record| {
             chips.iter().for_each(|chip| {
-                let mut output = A::Record::default();
-                chip.generate_dependencies(record, &mut output);
-                record.append(&mut output);
+                tracing::debug_span!("chip dependencies", chip = chip.name()).in_scope(|| {
+                    let mut output = A::Record::default();
+                    chip.generate_dependencies(record, &mut output);
+                    record.append(&mut output);
+                });
             });
-            record.register_nonces(opts);
-        });
+            tracing::debug_span!("register nonces").in_scope(|| record.register_nonces(opts));
+        })
     }
 
     pub const fn config(&self) -> &SC {
