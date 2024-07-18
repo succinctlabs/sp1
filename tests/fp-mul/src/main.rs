7289c74a54717421214333cabdfef96dcb7cf797
Bhargav Annem <bhargav.annem@gmail.com>
2024-07-18 12:18:54 -0700

    test(fp12-mul): build fp12-mul test

--- tests/fp-mul/src/main.rs
+++ tests/fp-mul/src/main.rs
@@ -3,19 +3,17 @@ sp1_zkvm::entrypoint!(main);
 
 use num::{BigUint, One};
 use rand::Rng;
-use sp1_zkvm::precompiles::bigint_mulmod::sys_fp_bigint;
+use sp1_zkvm::syscalls::sys_fp_bigint;
 
-fn fp384_mul(x: &[u8; 48], y: &[u8; 48], modulus: &[u8; 48]) -> [u8; 48] {
-    println!("cycle-tracker-start: fp384_mul");
-    let mut result = [0u32; 8];
-    sys_bigint(
+fn fp_mul(x: &[u8; 48], y: &[u8; 48], modulus: &[u8; 48]) -> [u8; 48] {
+    let mut result = [0u32; 12];
+    sys_fp_bigint(
         result.as_mut_ptr() as *mut [u32; 12],
         0,
         x.as_ptr() as *const [u32; 12],
         y.as_ptr() as *const [u32; 12],
         modulus.as_ptr() as *const [u32; 12],
     );
-    println!("cycle-tracker-end: fp384_mul");
     bytemuck::cast::<[u32; 12], [u8; 48]>(result)
 }
 
@@ -30,9 +28,9 @@ fn main() {
     for _ in 0..50 {
         // Test with random numbers.
         let mut rng = rand::thread_rng();
-        let mut x: [u8; 48] = rng.gen();
-        let mut y: [u8; 48] = rng.gen();
-        let modulus: [u8; 48] = rng.gen();
+        let mut x: [u8; 48] = [0; 48].map(|_| rng.gen());
+        let mut y: [u8; 48] = [0; 48].map(|_| rng.gen());
+        let modulus: [u8; 48] = [0; 48].map(|_| rng.gen());
 
         // Convert byte arrays to BigUint
         let modulus_big = BigUint::from_bytes_le(&modulus);
@@ -41,7 +39,7 @@ fn main() {
         let y_big = BigUint::from_bytes_le(&y);
         y = biguint_to_bytes_le(&y_big % &modulus_big);
 
-        let result_bytes = uint256_mul(&x, &y, &modulus);
+        let result_bytes = fp_mul(&x, &y, &modulus);
 
         let result = (x_big * y_big) % modulus_big;
         let result_syscall = BigUint::from_bytes_le(&result_bytes);
@@ -55,8 +53,8 @@ fn main() {
     for _ in 0..50 {
         // Test with random numbers.
         let mut rng = rand::thread_rng();
-        let mut x: [u8; 48] = rng.gen();
-        let mut y: [u8; 48] = rng.gen();
+        let mut x: [u8; 48] = [0; 48].map(|_| rng.gen());
+        let mut y: [u8; 48] = [0; 48].map(|_| rng.gen());
 
         // Convert byte arrays to BigUint
         let x_big = BigUint::from_bytes_le(&x);
@@ -64,7 +62,7 @@ fn main() {
         let y_big = BigUint::from_bytes_le(&y);
         y = biguint_to_bytes_le(&y_big % &modulus_big);
 
-        let result_bytes = fp384_mul(&x, &y, &modulus);
+        let result_bytes = fp_mul(&x, &y, &modulus);
 
         let result = (x_big * y_big) % &modulus_big;
         let result_syscall = BigUint::from_bytes_le(&result_bytes);
@@ -74,15 +72,15 @@ fn main() {
 
     // Test with random numbers.
     let mut rng = rand::thread_rng();
-    let x: [u8; 48] = rng.gen();
+    let x: [u8; 48] = [0; 48].map(|_| rng.gen());
 
     // Hardcoded edge case: Multiplying by 1
-    let modulus = [0u8; 32];
+    let modulus = [0u8; 48];
 
     let mut one: [u8; 48] = [0; 48];
     one[0] = 1; // Least significant byte set to 1, represents the number 1
     let original_x = x; // Copy original x value before multiplication by 1
-    let result_one = fp384_mul(&x, &one, &modulus);
+    let result_one = fp_mul(&x, &one, &modulus);
     assert_eq!(
         result_one, original_x,
         "Multiplying by 1 should yield the same number."
@@ -90,7 +88,7 @@ fn main() {
 
     // Hardcoded edge case: Multiplying by 0
     let zero: [u8; 48] = [0; 48]; // Represents the number 0
-    let result_zero = fp384_mul(&x, &zero, &modulus);
+    let result_zero = fp_mul(&x, &zero, &modulus);
     assert_eq!(result_zero, zero, "Multiplying by 0 should yield 0.");
 
     println!("All tests passed successfully!");
