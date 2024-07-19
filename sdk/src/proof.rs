bd880e3156d28dd9cbbb18e65fed0097b508bb7e
Chris T. <chris@succinct.xyz>
2024-07-18 19:29:41 -0700

    feat: v1.0.0-rc.1 (#1126)

--- sdk/src/proof.rs
+++ sdk/src/proof.rs
@@ -47,6 +47,22 @@ impl SP1ProofWithPublicValues {
             _ => unimplemented!(),
         }
     }
+
+    /// For Plonk proofs, returns the proof in a byte encoding the onchain verifier accepts.
+    /// The bytes consist of the first four bytes of Plonk vkey hash followed by the encoded proof.
+    pub fn bytes(&self) -> Vec<u8> {
+        match &self.proof {
+            SP1Proof::Plonk(plonk_proof) => {
+                let mut bytes = Vec::with_capacity(4 + plonk_proof.encoded_proof.len());
+                bytes.extend_from_slice(&plonk_proof.plonk_vkey_hash[..4]);
+                bytes.extend_from_slice(
+                    &hex::decode(&plonk_proof.encoded_proof).expect("Invalid Plonk proof"),
+                );
+                bytes
+            }
+            _ => unimplemented!("only Plonk proofs are verifiable onchain"),
+        }
+    }
 }
 
 pub type SP1CoreProofVerificationError = MachineVerificationError<CoreSC>;
