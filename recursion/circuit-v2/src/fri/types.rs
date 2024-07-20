69971549807903f5fa65d8cdc3bb49af1e6b1de9
Tej Qu Nair <tej.q.nair@gmail.com>
2024-07-19 18:21:24 -0700

    wip (won't compile)

---
+++ recursion/circuit-v2/src/fri/types.rs
@@ -0,0 +1,72 @@
+use p3_commit::TwoAdicMultiplicativeCoset;
+use sp1_recursion_compiler::prelude::*;
+
+use crate::fri::TwoAdicMultiplicativeCosetVariable;
+
+pub type DigestVariable<C> = Vec<Felt<<C as Config>::F>>;
+
+#[derive(Clone)]
+pub struct FriConfigVariable<C: Config> {
+    pub log_blowup: Var<C::N>,
+    pub blowup: Var<C::N>,
+    pub num_queries: Var<C::N>,
+    pub proof_of_work_bits: Var<C::N>,
+    pub generators: Vec<Felt<C::F>>,
+    pub subgroups: Vec<TwoAdicMultiplicativeCosetVariable<C>>,
+}
+
+#[derive(Clone)]
+pub struct FriProofVariable<C: Config> {
+    pub commit_phase_commits: Vec<DigestVariable<C>>,
+    pub query_proofs: Vec<FriQueryProofVariable<C>>,
+    pub final_poly: Ext<C::F, C::EF>,
+    pub pow_witness: Felt<C::F>,
+}
+
+#[derive(Clone)]
+pub struct FriQueryProofVariable<C: Config> {
+    pub commit_phase_openings: Vec<FriCommitPhaseProofStepVariable<C>>,
+}
+
+#[derive(Clone)]
+pub struct FriCommitPhaseProofStepVariable<C: Config> {
+    pub sibling_value: Ext<C::F, C::EF>,
+    pub opening_proof: Vec<DigestVariable<C>>,
+}
+
+#[derive(Clone)]
+pub struct FriChallengesVariable<C: Config> {
+    pub query_indices: Vec<Felt<C::F>>,
+    pub betas: Vec<Ext<C::F, C::EF>>,
+}
+
+#[derive(Clone)]
+pub struct DimensionsVariable<C: Config> {
+    pub height: Var<C::N>,
+}
+
+#[derive(Clone)]
+pub struct TwoAdicPcsProofVariable<C: Config> {
+    pub fri_proof: FriProofVariable<C>,
+    pub query_openings: Vec<Vec<BatchOpeningVariable<C>>>,
+}
+
+#[derive(Clone)]
+pub struct BatchOpeningVariable<C: Config> {
+    pub opened_values: Vec<Vec<Ext<C::F, C::EF>>>,
+    pub opening_proof: Vec<Vec<Felt<C::F>>>,
+}
+
+#[derive(Clone)]
+pub struct TwoAdicPcsRoundVariable<C: Config> {
+    pub batch_commit: DigestVariable<C>,
+    pub mats: Vec<TwoAdicPcsMatsVariable<C>>,
+}
+
+#[allow(clippy::type_complexity)]
+#[derive(Clone)]
+pub struct TwoAdicPcsMatsVariable<C: Config> {
+    pub domain: TwoAdicMultiplicativeCoset<C::F>,
+    pub points: Vec<Ext<C::F, C::EF>>,
+    pub values: Vec<Vec<Ext<C::F, C::EF>>>,
+}
