82dcffad26586b690224b33ff266dba04db8a7da
Tej Qu Nair <tej.q.nair@gmail.com>
2024-07-23 20:08:02 -0700

    hash in ir with test

--- recursion/compiler/src/circuit/builder.rs
+++ recursion/compiler/src/circuit/builder.rs
@@ -3,15 +3,16 @@
 use p3_field::AbstractField;
 
 use crate::prelude::*;
-use sp1_recursion_core_v2::{poseidon2_skinny::WIDTH, NUM_BITS};
+use sp1_recursion_core_v2::{poseidon2_skinny::WIDTH, DIGEST_SIZE, HASH_RATE, NUM_BITS};
 
 pub trait CircuitV2Builder<C: Config> {
     fn num2bits_v2_f(&mut self, num: Felt<C::F>) -> Vec<Felt<C::F>>;
     fn exp_reverse_bits_v2(&mut self, input: Felt<C::F>, power_bits: Vec<Felt<C::F>>)
         -> Felt<C::F>;
     fn poseidon2_permute_v2_skinny(&mut self, state: [Felt<C::F>; WIDTH]) -> [Felt<C::F>; WIDTH];
-    fn fri_fold_v2(&mut self, input: CircuitV2FriFoldInput<C>) -> CircuitV2FriFoldOutput<C>;
     fn poseidon2_permute_v2_wide(&mut self, state: [Felt<C::F>; WIDTH]) -> [Felt<C::F>; WIDTH];
+    fn poseidon2_hash_v2(&mut self, array: &[Felt<C::F>]) -> [Felt<C::F>; DIGEST_SIZE];
+    fn fri_fold_v2(&mut self, input: CircuitV2FriFoldInput<C>) -> CircuitV2FriFoldOutput<C>;
 }
 
 impl<C: Config> CircuitV2Builder<C> for Builder<C> {
@@ -61,6 +62,18 @@ impl<C: Config> CircuitV2Builder<C> for Builder<C> {
         self.operations
             .push(DslIr::CircuitV2Poseidon2PermuteBabyBearWide(output, array));
         output
+    }
+    /// Applies the Poseidon2 permutation to the given array.
+    ///
+    /// Reference: [p3_symmetric::PaddingFreeSponge]
+    fn poseidon2_hash_v2(&mut self, input: &[Felt<C::F>]) -> [Felt<C::F>; DIGEST_SIZE] {
+        // static_assert(RATE < WIDTH)
+        let mut state = core::array::from_fn(|_| self.eval(C::F::zero()));
+        for input_chunk in input.chunks(HASH_RATE) {
+            state[..input_chunk.len()].copy_from_slice(input_chunk);
+            state = self.poseidon2_permute_v2_skinny(state);
+        }
+        state[..DIGEST_SIZE].try_into().unwrap()
     }
     /// Runs FRI fold.
     fn fri_fold_v2(&mut self, input: CircuitV2FriFoldInput<C>) -> CircuitV2FriFoldOutput<C> {
