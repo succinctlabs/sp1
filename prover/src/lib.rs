6bf8c22f8a0c0e93ee6c82d0829507e8c8a245c5
John Guibas <jtguibas@gmail.com>
2024-07-25 12:55:22 -0700

    debug

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -418,7 +418,7 @@ impl<C: SP1ProverComponents> SP1Prover<C> {
     }
 
     /// Reduce shards proofs to a single shard proof using the recursion prover.
-    #[instrument(name = "compress", level = "info", skip_all)]
+    #[instrument(name = "compress", level = "debug", skip_all)]
     pub fn compress(
         &self,
         vk: &SP1VerifyingKey,
