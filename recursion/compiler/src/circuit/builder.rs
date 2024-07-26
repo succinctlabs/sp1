0b06d95ac8ca69e3812b01f605c3dc2c6dc3653d
Kevin Jue <kjue235@gmail.com>
2024-07-26 11:17:53 -0700

    some cleanup

--- recursion/compiler/src/circuit/builder.rs
+++ recursion/compiler/src/circuit/builder.rs
@@ -20,7 +20,7 @@ pub trait CircuitV2Builder<C: Config> {
     ) -> [Felt<C::F>; DIGEST_SIZE];
     fn fri_fold_v2(&mut self, input: CircuitV2FriFoldInput<C>) -> CircuitV2FriFoldOutput<C>;
     fn ext2felt_v2(&mut self, ext: Ext<C::F, C::EF>) -> [Felt<C::F>; D];
-    fn register_pv_hash_v2(&mut self, pv_elm: Felt<C::F>);
+    fn register_pv_elm_v2(&mut self, pv_elm: Felt<C::F>);
     fn commit_pv_hash_v2(&mut self, pv_hash: [Felt<C::F>; DIGEST_SIZE]);
 }
 
@@ -130,11 +130,11 @@ impl<C: Config> CircuitV2Builder<C> for Builder<C> {
 
         felts
     }
-
-    fn register_pv_hash_v2(&mut self, pv_elm: Felt<C::F>) {
+    /// Registers a public values element.
+    fn register_pv_elm_v2(&mut self, pv_elm: Felt<C::F>) {
         self.operations.push(DslIr::CircuitV2RegisterPVElm(pv_elm));
     }
-
+    // Commits a public values hash.
     fn commit_pv_hash_v2(&mut self, pv_hash: [Felt<C::F>; DIGEST_SIZE]) {
         self.operations.push(DslIr::CircuitV2CommitPVHash(pv_hash));
     }
