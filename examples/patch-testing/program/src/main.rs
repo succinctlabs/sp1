483de123cbe20f56094958646c677a7b27c5f050
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-07-25 18:04:23 -0700

    example

--- examples/patch-testing/program/src/main.rs
+++ examples/patch-testing/program/src/main.rs
@@ -42,7 +42,7 @@ fn test_secp256k1_patch() {
 
 /// Emits ED_ADD and ED_DECOMPRESS syscalls.
 fn test_ed25519_consensus() {
-    // Example from ed25519-consensus docs.
+    // Example signature and message.
     let vk = hex!("9194c3ead03f5848111db696fe1196fbbeffc69342d51c7cf5e91c502de91eb4");
     let msg = hex!("656432353531392d636f6e73656e7375732074657374206d657373616765");
     let sig = hex!("69261ea5df799b20fc6eeb49aa79f572c8f1e2ba88b37dff184cc55d4e3653d876419bffcc47e5343cdd5fd78121bb32f1c377a5ed505106ad37f19980218f0d");
