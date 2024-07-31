b7cfdfe62c9e21d75ecb5f1ff5438472b3c03e9f
John Guibas <jtguibas@gmail.com>
2024-07-31 16:47:19 -0700

    with inv e opt

--- recursion/gnark-ffi/go/sp1/babybear/babybear.go
+++ recursion/gnark-ffi/go/sp1/babybear/babybear.go
@@ -240,10 +240,10 @@ func (c *Chip) MulEF(a ExtensionVariable, b Variable) ExtensionVariable {
 }
 
 func (c *Chip) InvE(in ExtensionVariable) ExtensionVariable {
-	in.Value[0] = c.ReduceSlow(in.Value[0])
-	in.Value[1] = c.ReduceSlow(in.Value[1])
-	in.Value[2] = c.ReduceSlow(in.Value[2])
-	in.Value[3] = c.ReduceSlow(in.Value[3])
+	// in.Value[0] = c.ReduceSlow(in.Value[0])
+	// in.Value[1] = c.ReduceSlow(in.Value[1])
+	// in.Value[2] = c.ReduceSlow(in.Value[2])
+	// in.Value[3] = c.ReduceSlow(in.Value[3])
 
 	result, err := c.api.Compiler().NewHint(InvEHint, 4, in.Value[0].Value, in.Value[1].Value, in.Value[2].Value, in.Value[3].Value)
 	if err != nil {
@@ -353,10 +353,16 @@ func InvFHint(_ *big.Int, inputs []*big.Int, results []*big.Int) error {
 }
 
 func InvEHint(_ *big.Int, inputs []*big.Int, results []*big.Int) error {
-	a := C.uint(inputs[0].Uint64())
-	b := C.uint(inputs[1].Uint64())
-	c := C.uint(inputs[2].Uint64())
-	d := C.uint(inputs[3].Uint64())
+	p := big.NewInt(15*(1<<27) + 1)
+	aReduced := new(big.Int).Mod(inputs[0], p)
+	bReduced := new(big.Int).Mod(inputs[1], p)
+	cReduced := new(big.Int).Mod(inputs[2], p)
+	dReduced := new(big.Int).Mod(inputs[3], p)
+
+	a := C.uint(aReduced.Uint64())
+	b := C.uint(bReduced.Uint64())
+	c := C.uint(cReduced.Uint64())
+	d := C.uint(dReduced.Uint64())
 
 	ainv := C.babybearextinv(a, b, c, d, 0)
 	binv := C.babybearextinv(a, b, c, d, 1)
