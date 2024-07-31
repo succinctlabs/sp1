a82de5c7e10bc6a28a9098b4798ced1d39cde3c2
John Guibas <jtguibas@gmail.com>
2024-07-31 11:03:48 -0700

    hm

--- recursion/gnark-ffi/go/sp1/babybear/babybear.go
+++ recursion/gnark-ffi/go/sp1/babybear/babybear.go
@@ -276,7 +276,7 @@ func (c *Chip) ToBinary(in Variable) []frontend.Variable {
 }
 
 func (p *Chip) reduceFast(x Variable) Variable {
-	if x.NbBits >= uint(120) {
+	if x.NbBits >= uint(160) {
 		return Variable{
 			Value:  p.reduceWithMaxBits(x.Value, uint64(x.NbBits)),
 			NbBits: 31,
