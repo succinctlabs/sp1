ed7a602f8cc7415e39e1801100e6194de52a8ccb
John Guibas <jtguibas@gmail.com>
2024-07-31 17:23:52 -0700

    subef opt

--- recursion/compiler/src/ir/types.rs
+++ recursion/compiler/src/ir/types.rs
@@ -1041,6 +1041,9 @@ impl<F: Field, EF: ExtensionField<F>> Ext<F, EF> {
                 (SymbolicExt::Val(lhs, _), SymbolicExt::Val(rhs, _)) => {
                     builder.push(DslIr::SubE(*self, *lhs, *rhs));
                 }
+                (SymbolicExt::Val(lhs, _), SymbolicExt::Base(rhs, _)) => {
+                    builder.push(DslIr::SubEF(*self, *lhs, *rhs));
+                }
                 (SymbolicExt::Val(lhs, _), rhs) => {
                     let rhs_value = Self::uninit(builder);
                     rhs_value.assign_with_caches(rhs.clone(), builder, ext_cache, base_cache);
