153929d860aad0182a3489261d44c509139287e9
John Guibas <jtguibas@gmail.com>
2024-08-01 12:24:02 -0700

    testing

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -24,3 +24,33 @@ pub struct CompressRequestPayload {
     pub proof: SP1CoreProof,
     pub deferred_proofs: Vec<ShardProof<InnerSC>>,
 }
+
+#[cfg(test)]
+mod tests {
+    use sp1_core::utils::tests::FIBONACCI_ELF;
+    use sp1_prover::components::DefaultProverComponents;
+    use sp1_prover::{SP1CoreProof, SP1Prover};
+    use twirp::url::Url;
+    use twirp::Client;
+
+    use crate::SP1Stdin;
+    use crate::{proto::api::ProverServiceClient, ProveCoreRequestPayload};
+
+    #[tokio::test]
+    async fn test_prove_core() {
+        let client = Client::from_base_url(Url::parse("http://127.0.0.1:3000").unwrap()).unwrap();
+
+        let prover = SP1Prover::<DefaultProverComponents>::new();
+        let (pk, vk) = prover.setup(FIBONACCI_ELF);
+        let payload = ProveCoreRequestPayload {
+            pk,
+            stdin: SP1Stdin::new(),
+        };
+        let request = crate::proto::api::ProveCoreRequest {
+            data: bincode::serialize(&payload).unwrap(),
+        };
+        let proof = client.prove_core(request).await.unwrap();
+        let proof: SP1CoreProof = bincode::deserialize(&proof.result).unwrap();
+        prover.verify(&proof.proof, &vk).unwrap();
+    }
+}
