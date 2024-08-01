17bd65924aee7f95d69ac685bdaecec376fedda0
John Guibas <jtguibas@gmail.com>
2024-08-01 11:22:24 -0700

    fix

--- server/build.rs
+++ server/build.rs
@@ -3,7 +3,6 @@ fn main() {
 
     let mut config = prost_build::Config::new();
     config
-        .protoc_arg("--experimental_allow_proto3_optional")
         .out_dir("src/proto")
         .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
         .service_generator(twirp_build::service_generator())
