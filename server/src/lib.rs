881f83a0f649ff830c0b6d56a39dd83b818d262d
John Guibas <jtguibas@gmail.com>
2024-08-01 11:33:53 -0700

    types

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -2,3 +2,14 @@
 pub mod proto {
     pub mod api;
 }
+
+use serde::{Deserialize, Serialize};
+use sp1_core::io::SP1Stdin;
+use sp1_core::utils::SP1ProverOpts;
+use sp1_prover::types::SP1ProvingKey;
+
+#[derive(Serialize, Deserialize)]
+pub struct ProveCoreRequestPayload {
+    pk: SP1ProvingKey,
+    stdin: SP1Stdin,
+}
