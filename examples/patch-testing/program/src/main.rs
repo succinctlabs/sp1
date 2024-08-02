ce2a7592ccffd0d057030252e4435c724b26bbf3
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-08-02 13:47:20 -0700

    add program

--- examples/patch-testing/program/src/main.rs
+++ examples/patch-testing/program/src/main.rs
@@ -44,7 +44,10 @@ fn test_ed25519_dalek() {
 
     let vk = Ed25519DalekVerifiyingKey::from_bytes(&vk).unwrap();
     let sig = Ed25519DalekSignature::from_bytes(&sig);
+
+    println!("cycle-tracker-start: ed25519-dalek verify");
     vk.verify(&msg, &sig).unwrap();
+    println!("cycle-tracker-end: ed25519-dalek verify");
 }
 
 /// Emits ED_ADD and ED_DECOMPRESS syscalls.
@@ -56,39 +59,61 @@ fn test_ed25519_consensus() {
 
     let vk: Ed25519ConsensusVerificationKey = vk.try_into().unwrap();
     let sig: Ed25519ConsensusSignature = sig.into();
+
+    println!("cycle-tracker-start: ed25519-consensus verify");
     vk.verify(&sig, &msg).unwrap();
+    println!("cycle-tracker-end: ed25519-consensus verify");
 }
 
-/// Emits ED_DECOMPRESSz syscalls.
+/// Emits ED_DECOMPRESS syscall.
 fn test_curve25519_dalek_ng() {
     let input = [1u8; 32];
     let y = CompressedEdwardsY_dalek_ng(input);
-    let _ = y.decompress();
+
+    println!("cycle-tracker-start: curve25519-dalek-ng decompress");
+    let decompressed_key = y.decompress();
+    println!("cycle-tracker-end: curve25519-dalek-ng decompress");
+
+    let compressed_key = decompressed_key.unwrap().compress();
+    assert_eq!(compressed_key, y);
 }
 
-/// Emits ED_DECOMPRESS syscalls.
+/// Emits ED_DECOMPRESS syscall.
 fn test_curve25519_dalek() {
     let input = [1u8; 32];
     let y = CompressedEdwardsY_dalek(input);
-    let _ = y.decompress();
+
+    println!("cycle-tracker-start: curve25519-dalek decompress");
+    let decompressed_key = y.decompress().unwrap();
+    println!("cycle-tracker-end: curve25519-dalek decompress");
+
+    let compressed_key = decompressed_key.compress();
+    assert_eq!(compressed_key, y);
 }
 
 /// Emits KECCAK_PERMUTE syscalls.
 fn test_keccak() {
     let input = [1u8; 32];
-    let _ = keccak256(input);
+    let expected_output = hex!("cebc8882fecbec7fb80d2cf4b312bec018884c2d66667c67a90508214bd8bafc");
+
+    let output = keccak256(input);
+    assert_eq!(output, expected_output);
 }
 
 /// Emits SHA_COMPRESS and SHA_EXTEND syscalls.
 fn test_sha256() {
     let input = [1u8; 32];
+    let expected_output = hex!("72cd6e8422c407fb6d098690f1130b7ded7ec2f7f5e1d30bd9d521f015363793");
+
     let mut sha256_9_8 = Sha256_9_8::new();
     sha256_9_8.update(input);
-    let _ = sha256_9_8.finalize();
+    let output_9_8: [u8; 32] = sha256_9_8.finalize().into();
+    assert_eq!(output_9_8, expected_output);
 
     let mut sha256_10_6 = Sha256_10_6::new();
     sha256_10_6.update(input);
-    let _ = sha256_10_6.finalize();
+    let output_10_6: [u8; 32] = sha256_10_6.finalize().into();
+    assert_eq!(output_10_6, expected_output);
 
     // Can't have two different sha256 versions for the same major version.
     // let mut sha256_10_8 = Sha256_10_8::new();
@@ -104,11 +129,11 @@ fn test_k256_patch() {
     ).expect("could not parse signature");
     let expected = address!("2c7536E3605D9C16a7a3D7b1898e529396a65c23");
 
-    assert_eq!(
-        sig.recover_address_from_msg("Some data")
-            .expect("could not recover address"),
-        expected
-    );
+    println!("cycle-tracker-start: k256 verify");
+    let recovered_address = sig.recover_address_from_msg("Some data").expect("could not recover address");
+    println!("cycle-tracker-end: k256 verify");
+
+    assert_eq!(recovered_address, expected);
 }
 
 /// Emits SECP256K1_ADD, SECP256K1_DOUBLE, and SECP256K1_DECOMPRESS syscalls.
@@ -126,48 +151,33 @@ fn test_secp256k1_patch() {
     let message = Secp256k1Message::from_digest_slice(&message_bytes)
         .expect("Message could not be created from bytes");
     let expected = "04e76c446148ca6c558910ee241e7dde6d96a7fe3d5a30c00e65aceabe0af9fd2dd131ee7b5d38edafa79eac5110608be0ce01866c1f1a868596b6d991711699c4";
+
+    println!("cycle-tracker-start: secp256k1 verify");
     let public_key = secp
         .recover_ecdsa(&message, &signature) // Use the new context to call recover
-        .expect("could not recover public key")
+        .expect("could not recover public key");
+    println!("cycle-tracker-end: secp256k1 verify");
+
+    let serialized_key = public_key
         .serialize_uncompressed();
 
     // Use the message in the recover_ecdsa call
-    assert_eq!(hex::encode(public_key), expected);
+    assert_eq!(hex::encode(serialized_key), expected);
 }
 
 /// To add testing for a new patch, add a new case to the function below.
 fn main() {
     // TODO: Specify which syscalls are linked to each function invocation, iterate
     // over this list that is shared between the program and script.
-    println!("cycle-tracker-start: keccak");
     test_keccak();
-    println!("cycle-tracker-end: keccak");
-
-    println!("cycle-tracker-start: sha256");
     test_sha256();
-    println!("cycle-tracker-end: sha256");
 
-    println!("cycle-tracker-start: curve25519-dalek-ng");
     test_curve25519_dalek_ng();
-    println!("cycle-tracker-end: curve25519-dalek-ng");
-
-    println!("cycle-tracker-start: curve25519-dalek");
     test_curve25519_dalek();
-    println!("cycle-tracker-end: curve25519-dalek");
 
-    println!("cycle-tracker-start: ed25519-dalek");
     test_ed25519_dalek();
-    println!("cycle-tracker-end: ed25519-dalek");
-
-    println!("cycle-tracker-start: ed25519");
     test_ed25519_consensus();
-    println!("cycle-tracker-end: ed25519");
 
-    println!("cycle-tracker-start: k256");
     test_k256_patch();
-    println!("cycle-tracker-end: k256");
-
-    println!("cycle-tracker-start: secp256k1");
     test_secp256k1_patch();
-    println!("cycle-tracker-end: secp256k1");
 }
