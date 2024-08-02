bdde43367c2cdfa1fdf0926ca9ff2d436e1ac353
Ubuntu <ubuntu@ip-172-31-23-59.ec2.internal>
2024-08-02 04:32:23 +0000

    hm

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -197,75 +197,75 @@ fn cleanup_container(container_name: &str) {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use sp1_core::utils;
-    use sp1_core::utils::tests::FIBONACCI_ELF;
-    use sp1_prover::components::DefaultProverComponents;
-    use sp1_prover::{InnerSC, SP1CoreProof, SP1Prover, SP1ReduceProof};
-    use twirp::url::Url;
-    use twirp::Client;
+// #[cfg(test)]
+// mod tests {
+//     use sp1_core::utils;
+//     use sp1_core::utils::tests::FIBONACCI_ELF;
+//     use sp1_prover::components::DefaultProverComponents;
+//     use sp1_prover::{InnerSC, SP1CoreProof, SP1Prover, SP1ReduceProof};
+//     use twirp::url::Url;
+//     use twirp::Client;
 
-    use crate::SP1Stdin;
-    use crate::{proto::api::ProverServiceClient, ProveCoreRequestPayload};
-    use crate::{CompressRequestPayload, SP1ProverServer};
+//     use crate::SP1Stdin;
+//     use crate::{proto::api::ProverServiceClient, ProveCoreRequestPayload};
+//     use crate::{CompressRequestPayload, SP1ProverServer};
 
-    #[ignore]
-    #[test]
-    fn test_client() {
-        utils::setup_logger();
+//     #[ignore]
+//     #[test]
+//     fn test_client() {
+//         utils::setup_logger();
 
-        let client = SP1ProverServer::new();
+//         let client = SP1ProverServer::new();
 
-        let prover = SP1Prover::<DefaultProverComponents>::new();
-        let (pk, vk) = prover.setup(FIBONACCI_ELF);
+//         let prover = SP1Prover::<DefaultProverComponents>::new();
+//         let (pk, vk) = prover.setup(FIBONACCI_ELF);
 
-        println!("proving core");
-        let proof = client.prove_core(&pk, &SP1Stdin::new()).unwrap();
+//         println!("proving core");
+//         let proof = client.prove_core(&pk, &SP1Stdin::new()).unwrap();
 
-        println!("verifying core");
-        prover.verify(&proof.proof, &vk).unwrap();
+//         println!("verifying core");
+//         prover.verify(&proof.proof, &vk).unwrap();
 
-        println!("proving compress");
-        let proof = client.compress(&vk, proof, vec![]).unwrap();
+//         println!("proving compress");
+//         let proof = client.compress(&vk, proof, vec![]).unwrap();
 
-        println!("verifying compress");
-        prover.verify_compressed(&proof, &vk).unwrap();
-    }
+//         println!("verifying compress");
+//         prover.verify_compressed(&proof, &vk).unwrap();
+//     }
 
-    #[ignore]
-    #[tokio::test]
-    async fn test_prove_core() {
-        let client =
-            Client::from_base_url(Url::parse("http://localhost:3000/twirp/").unwrap()).unwrap();
+//     #[ignore]
+//     #[tokio::test]
+//     async fn test_prove_core() {
+//         let client =
+//             Client::from_base_url(Url::parse("http://localhost:3000/twirp/").unwrap()).unwrap();
 
-        let prover = SP1Prover::<DefaultProverComponents>::new();
-        let (pk, vk) = prover.setup(FIBONACCI_ELF);
-        let payload = ProveCoreRequestPayload {
-            pk,
-            stdin: SP1Stdin::new(),
-        };
-        let request = crate::proto::api::ProveCoreRequest {
-            data: bincode::serialize(&payload).unwrap(),
-        };
-        let proof = client.prove_core(request).await.unwrap();
-        let proof: SP1CoreProof = bincode::deserialize(&proof.result).unwrap();
-        prover.verify(&proof.proof, &vk).unwrap();
+//         let prover = SP1Prover::<DefaultProverComponents>::new();
+//         let (pk, vk) = prover.setup(FIBONACCI_ELF);
+//         let payload = ProveCoreRequestPayload {
+//             pk,
+//             stdin: SP1Stdin::new(),
+//         };
+//         let request = crate::proto::api::ProveCoreRequest {
+//             data: bincode::serialize(&payload).unwrap(),
+//         };
+//         let proof = client.prove_core(request).await.unwrap();
+//         let proof: SP1CoreProof = bincode::deserialize(&proof.result).unwrap();
+//         prover.verify(&proof.proof, &vk).unwrap();
 
-        tracing::info!("compress");
-        let payload = CompressRequestPayload {
-            vk: vk.clone(),
-            proof,
-            deferred_proofs: vec![],
-        };
-        let request = crate::proto::api::CompressRequest {
-            data: bincode::serialize(&payload).unwrap(),
-        };
-        let compressed_proof = client.compress(request).await.unwrap();
-        let compressed_proof: SP1ReduceProof<InnerSC> =
-            bincode::deserialize(&compressed_proof.result).unwrap();
+//         tracing::info!("compress");
+//         let payload = CompressRequestPayload {
+//             vk: vk.clone(),
+//             proof,
+//             deferred_proofs: vec![],
+//         };
+//         let request = crate::proto::api::CompressRequest {
+//             data: bincode::serialize(&payload).unwrap(),
+//         };
+//         let compressed_proof = client.compress(request).await.unwrap();
+//         let compressed_proof: SP1ReduceProof<InnerSC> =
+//             bincode::deserialize(&compressed_proof.result).unwrap();
 
-        tracing::info!("verify compressed");
-        prover.verify_compressed(&compressed_proof, &vk).unwrap();
-    }
-}
+//         tracing::info!("verify compressed");
+//         prover.verify_compressed(&compressed_proof, &vk).unwrap();
+//     }
+// }
