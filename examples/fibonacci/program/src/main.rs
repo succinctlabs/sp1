26cc557aaeac7411e0bc1e261d6a6b65e45697d4
dongchangYoo <yoodc8167@gmail.com>
2024-08-05 09:51:13 +0900

    feat(example): update fibonacci example

--- examples/fibonacci/program/src/main.rs
+++ examples/fibonacci/program/src/main.rs
@@ -8,6 +8,13 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
+use alloy_sol_types::{sol, SolType};
+
+/// The public values encoded as a tuple that can be easily deserialized inside Solidity.
+type PublicValuesTuple = sol! {
+    tuple(uint32, uint32, uint32)
+};
+
 pub fn main() {
     // Read an input to the program.
     //
@@ -15,23 +22,24 @@ pub fn main() {
     // from the prover.
     let n = sp1_zkvm::io::read::<u32>();
 
-    // Write n to public input
-    sp1_zkvm::io::commit(&n);
+    if n > 186 {
+        panic!(
+            "This fibonacci program doesn't support n > 186, as it would overflow a 32-bit integer."
+        );
+    }
 
     // Compute the n'th fibonacci number, using normal Rust code.
-    let mut a = 0;
-    let mut b = 1;
+    let mut a = 0u32;
+    let mut b = 1u32;
     for _ in 0..n {
-        let mut c = a + b;
-        c %= 7919; // Modulus to prevent overflow.
+        let c = a + b;
         a = b;
         b = c;
     }
 
-    // Write the output of the program.
-    //
-    // Behind the scenes, this also compiles down to a custom system call which handles writing
-    // outputs to the prover.
-    sp1_zkvm::io::commit(&a);
-    sp1_zkvm::io::commit(&b);
+    // Encode the public values of the program.
+    let bytes = PublicValuesTuple::abi_encode(&(n, a, b));
+
+    // Commit to the public values of the program.
+    sp1_zkvm::io::commit_slice(&bytes);
 }
