77aefa40b6798236b2dc9915f0fc2f5fdf38f52b
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-08-06 12:30:06 -0700

    1.2.0

--- crates/helper/src/lib.rs
+++ crates/helper/src/lib.rs
@@ -1,5 +1,5 @@
 #[deprecated(
-    since = "1.1.1",
+    since = "1.2.0",
     note = "Please use `sp1_build::build_program` and `sp1_build::build_program_with_args` directly"
 )]
 pub use sp1_build::{build_program, build_program_with_args};
