2ca75086a9040adbb44e6eda6674612b5ab69bf1
Chris Tian <chris@succinct.xyz>
2024-08-06 18:02:49 -0700

    fixed trace methods

--- core/src/stark/chip.rs
+++ core/src/stark/chip.rs
@@ -184,6 +184,16 @@ where
         <A as MachineAir<F>>::generate_preprocessed_trace(&self.air, program)
     }
 
+    fn generate_fixed_trace(
+        &self,
+        input: &A::Record,
+        output: &mut A::Record,
+        fixed_log2_rows: Option<usize>,
+    ) -> RowMajorMatrix<F> {
+        self.air
+            .generate_fixed_trace(input, output, fixed_log2_rows)
+    }
+
     fn generate_trace(&self, input: &A::Record, output: &mut A::Record) -> RowMajorMatrix<F> {
         self.air.generate_trace(input, output)
     }
@@ -194,6 +204,10 @@ where
 
     fn included(&self, shard: &Self::Record) -> bool {
         self.air.included(shard)
+    }
+
+    fn min_rows(&self, shard: &Self::Record) -> usize {
+        self.air.min_rows(shard)
     }
 }
 
