031f2b8ece3fd22bc0bb6b4b110fa21d526d28a5
Kevin Jue <kjue235@gmail.com>
2024-08-09 09:25:52 -0700

    added tests

--- core/src/memory/local.rs
+++ core/src/memory/local.rs
@@ -111,3 +111,84 @@ where
 {
     fn eval(&self, builder: &mut AB) {}
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::lookup::{debug_interactions_with_all_chips, InteractionKind};
+    use crate::runtime::tests::simple_program;
+    use crate::runtime::Runtime;
+    use crate::stark::RiscvAir;
+    use crate::syscall::precompiles::sha256::extend_tests::sha_extend_program;
+    use crate::utils::{setup_logger, BabyBearPoseidon2, SP1CoreOpts};
+    use p3_baby_bear::BabyBear;
+
+    #[test]
+    fn test_local_memory_generate_trace() {
+        let program = simple_program();
+        let mut runtime = Runtime::new(program, SP1CoreOpts::default());
+        runtime.run().unwrap();
+        let shard = runtime.record.clone();
+
+        let chip: MemoryLocalChip = MemoryLocalChip::new(MemoryChipType::Initialize);
+
+        let trace: RowMajorMatrix<BabyBear> =
+            chip.generate_trace(&shard, &mut ExecutionRecord::default());
+        println!("{:?}", trace.values);
+
+        let chip: MemoryLocalChip = MemoryLocalChip::new(MemoryChipType::Finalize);
+        let trace: RowMajorMatrix<BabyBear> =
+            chip.generate_trace(&shard, &mut ExecutionRecord::default());
+        println!("{:?}", trace.values);
+
+        for mem_event in shard.global_memory_finalize_events {
+            println!("{:?}", mem_event);
+        }
+    }
+
+    // #[test]
+    // fn test_memory_lookup_interactions() {
+    //     setup_logger();
+    //     let program = sha_extend_program();
+    //     let program_clone = program.clone();
+    //     let mut runtime = Runtime::new(program, SP1CoreOpts::default());
+    //     runtime.run().unwrap();
+    //     let machine: crate::stark::StarkMachine<BabyBearPoseidon2, RiscvAir<BabyBear>> =
+    //         RiscvAir::machine(BabyBearPoseidon2::new());
+    //     let (pkey, _) = machine.setup(&program_clone);
+    //     let opts = SP1CoreOpts::default();
+    //     machine.generate_dependencies(&mut runtime.records, &opts);
+
+    //     let shards = runtime.records;
+    //     assert_eq!(shards.len(), 2);
+    //     debug_interactions_with_all_chips::<BabyBearPoseidon2, RiscvAir<BabyBear>>(
+    //         &machine,
+    //         &pkey,
+    //         &shards,
+    //         vec![InteractionKind::Memory],
+    //     );
+    // }
+
+    // #[test]
+    // fn test_byte_lookup_interactions() {
+    //     setup_logger();
+    //     let program = sha_extend_program();
+    //     let program_clone = program.clone();
+    //     let mut runtime = Runtime::new(program, SP1CoreOpts::default());
+    //     runtime.run().unwrap();
+    //     let machine = RiscvAir::machine(BabyBearPoseidon2::new());
+    //     let (pkey, _) = machine.setup(&program_clone);
+    //     let opts = SP1CoreOpts::default();
+    //     machine.generate_dependencies(&mut runtime.records, &opts);
+
+    //     let shards = runtime.records;
+    //     assert_eq!(shards.len(), 2);
+    //     debug_interactions_with_all_chips::<BabyBearPoseidon2, RiscvAir<BabyBear>>(
+    //         &machine,
+    //         &pkey,
+    //         &shards,
+    //         vec![InteractionKind::Byte],
+    //     );
+    // }
+}
