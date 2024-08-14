2c26cedf668a5c90c503e70422a84f6eca5916f1
John Guibas <jtguibas@gmail.com>
2024-08-13 23:28:04 -0700

    more opts

--- core/src/runtime/utils.rs
+++ core/src/runtime/utils.rs
@@ -41,35 +41,6 @@ impl<'a> Runtime<'a> {
             }
         }
 
-        // If RUST_LOG is set to "trace", then log the current state of the runtime every cycle.
-        let width = 12;
-        let registers = self.registers();
-        log::trace!(
-            "clk={} [pc=0x{:x?}] {:<width$?} |         x0={:<width$} x1={:<width$} x2={:<width$} x3={:<width$} x4={:<width$} x5={:<width$} x6={:<width$} x7={:<width$} x8={:<width$} x9={:<width$} x10={:<width$} x11={:<width$} x12={:<width$} x13={:<width$} x14={:<width$} x15={:<width$} x16={:<width$} x17={:<width$} x18={:<width$}",
-            self.state.global_clk,
-            self.state.pc,
-            instruction,
-            registers[0],
-            registers[1],
-            registers[2],
-            registers[3],
-            registers[4],
-            registers[5],
-            registers[6],
-            registers[7],
-            registers[8],
-            registers[9],
-            registers[10],
-            registers[11],
-            registers[12],
-            registers[13],
-            registers[14],
-            registers[15],
-            registers[16],
-            registers[17],
-            registers[18],
-        );
-
         if !self.unconstrained && self.state.global_clk % 10_000_000 == 0 {
             log::info!(
                 "clk = {} pc = 0x{:x?}",
