67780ceadb156fc846b715cb9a1897043497fa98
John Guibas <jtguibas@gmail.com>
2024-08-14 17:13:12 -0700

    fix deleted ssz code

--- examples/ssz-withdrawals/script/src/main.rs
+++ examples/ssz-withdrawals/script/src/main.rs
@@ -10,28 +10,22 @@ fn main() {
     let stdin = SP1Stdin::new();
     let client = ProverClient::new();
     let (pk, vk) = client.setup(ELF);
+    let proof = client.prove(&pk, stdin).run().expect("proving failed");
 
-    let executor = client
-        .execute(ELF, stdin.clone())
-        .run()
-        .expect("proving failed");
+    // Verify proof.
+    client.verify(&proof, &vk).expect("verification failed");
 
-    // let proof = client.prove(&pk, stdin).run().expect("proving failed");
+    // Test a round trip of proof serialization and deserialization.
+    proof
+        .save("proof-with-pis.bin")
+        .expect("saving proof failed");
+    let deserialized_proof =
+        SP1ProofWithPublicValues::load("proof-with-pis.bin").expect("loading proof failed");
 
-    // // Verify proof.
-    // client.verify(&proof, &vk).expect("verification failed");
+    // Verify the deserialized proof.
+    client
+        .verify(&deserialized_proof, &vk)
+        .expect("verification failed");
 
-    // // Test a round trip of proof serialization and deserialization.
-    // proof
-    //     .save("proof-with-pis.bin")
-    //     .expect("saving proof failed");
-    // let deserialized_proof =
-    //     SP1ProofWithPublicValues::load("proof-with-pis.bin").expect("loading proof failed");
-
-    // // Verify the deserialized proof.
-    // client
-    //     .verify(&deserialized_proof, &vk)
-    //     .expect("verification failed");
-
-    // println!("successfully generated and verified proof for the program!")
+    println!("successfully generated and verified proof for the program!")
 }
