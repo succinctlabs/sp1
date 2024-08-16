152b3abbea832aa9f9dcc2213054e554f1a90633
John Guibas <jtguibas@gmail.com>
2024-08-15 22:47:38 -0700

    fix

--- crates/sdk/src/provers/utils.rs
+++ crates/sdk/src/provers/utils.rs
@@ -1,10 +1,10 @@
 use sysinfo::System;
 
 /// The minimum amount of RAM required to generate a plonk proof.
-pub const PLONK_MEMORY_GB_REQUIREMENT: u64 = 64;
+const PLONK_MEMORY_GB_REQUIREMENT: u64 = 64;
 
 /// Checks if there is enough RAM to generate a plonk proof.
-pub fn enough_ram_for_plonk() -> bool {
+fn enough_ram_for_plonk() -> bool {
     let total_ram_gb = System::new_all().total_memory() / 1_000_000_000;
     total_ram_gb >= (PLONK_MEMORY_GB_REQUIREMENT - 4)
 }
