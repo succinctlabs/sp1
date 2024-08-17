73188005e8f75e6b2bad7e7b99fb3e053835d65f
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-08-16 17:50:25 -0700

    fix

--- crates/sdk/build.rs
+++ crates/sdk/build.rs
@@ -1,8 +1,3 @@
 fn main() {
-    #[cfg(debug_assertions)]
-    compile_error!(
-        "sp1-sdk must be used in release mode. Please compile with the --release flag."
-    );
-
     vergen::EmitBuilder::builder().build_timestamp().git_sha(true).emit().unwrap();
 }
