1e02fe9deb28464550e8d6fffa1876ae2351b4c0
erabinov <erabinovich92@gmail.com>
2024-08-20 10:26:32 -0700

    multifield challenger

--- crates/recursion/circuit-v2/src/challenger.rs
+++ crates/recursion/circuit-v2/src/challenger.rs
@@ -1,6 +1,7 @@
-use p3_field::AbstractField;
+use p3_field::{AbstractField, Field};
 use sp1_recursion_compiler::{
     circuit::CircuitV2Builder,
+    ir::{DslIr, Var},
     prelude::{Builder, Config, Ext, Felt},
 };
 use sp1_recursion_core_v2::{
@@ -8,6 +9,11 @@ use sp1_recursion_core_v2::{
     runtime::{HASH_RATE, PERMUTATION_WIDTH},
     NUM_BITS,
 };
+
+// Constants for the Multifield challenger.
+pub const SPONGE_SIZE: usize = 3;
+pub const DIGEST_SIZE: usize = 1;
+pub const RATE: usize = 16;
 
 // use crate::{DigestVariable, VerifyingKeyVariable};
 
@@ -228,6 +234,164 @@ impl<C: Config> FieldChallengerVariable<C, Felt<C::F>> for DuplexChallengerVaria
         self.output_buffer.clear();
         self.output_buffer.extend_from_slice(&self.sponge_state);
     }
+}
+
+#[derive(Clone)]
+pub struct MultiField32ChallengerVariable<C: Config> {
+    sponge_state: [Var<C::N>; 3],
+    input_buffer: Vec<Felt<C::F>>,
+    output_buffer: Vec<Felt<C::F>>,
+    num_f_elms: usize,
+}
+
+impl<C: Config> MultiField32ChallengerVariable<C> {
+    pub fn new(builder: &mut Builder<C>) -> Self {
+        MultiField32ChallengerVariable::<C> {
+            sponge_state: [
+                builder.eval(C::N::zero()),
+                builder.eval(C::N::zero()),
+                builder.eval(C::N::zero()),
+            ],
+            input_buffer: vec![],
+            output_buffer: vec![],
+            num_f_elms: C::N::bits() / 64,
+        }
+    }
+
+    pub fn duplexing(&mut self, builder: &mut Builder<C>) {
+        assert!(self.input_buffer.len() <= self.num_f_elms * SPONGE_SIZE);
+
+        for (i, f_chunk) in self.input_buffer.chunks(self.num_f_elms).enumerate() {
+            self.sponge_state[i] = reduce_32(builder, f_chunk);
+        }
+        self.input_buffer.clear();
+
+        // TODO make this a method for the builder.
+        builder.push(DslIr::CircuitPoseidon2Permute(self.sponge_state));
+
+        self.output_buffer.clear();
+        for &pf_val in self.sponge_state.iter() {
+            let f_vals = split_32(builder, pf_val, self.num_f_elms);
+            for f_val in f_vals {
+                self.output_buffer.push(f_val);
+            }
+        }
+    }
+
+    pub fn observe(&mut self, builder: &mut Builder<C>, value: Felt<C::F>) {
+        self.output_buffer.clear();
+
+        self.input_buffer.push(value);
+        if self.input_buffer.len() == self.num_f_elms * SPONGE_SIZE {
+            self.duplexing(builder);
+        }
+    }
+
+    pub fn observe_commitment(
+        &mut self,
+        builder: &mut Builder<C>,
+        value: [Var<C::N>; DIGEST_SIZE],
+    ) {
+        for i in 0..DIGEST_SIZE {
+            let f_vals: Vec<Felt<C::F>> = split_32(builder, value[i], self.num_f_elms);
+            for f_val in f_vals {
+                self.observe(builder, f_val);
+            }
+        }
+    }
+
+    pub fn sample(&mut self, builder: &mut Builder<C>) -> Felt<C::F> {
+        if !self.input_buffer.is_empty() || self.output_buffer.is_empty() {
+            self.duplexing(builder);
+        }
+
+        self.output_buffer.pop().expect("output buffer should be non-empty")
+    }
+
+    pub fn sample_ext(&mut self, builder: &mut Builder<C>) -> Ext<C::F, C::EF> {
+        let a = self.sample(builder);
+        let b = self.sample(builder);
+        let c = self.sample(builder);
+        let d = self.sample(builder);
+        builder.felts2ext(&[a, b, c, d])
+    }
+
+    pub fn sample_bits(&mut self, builder: &mut Builder<C>, bits: usize) -> Vec<Var<C::N>> {
+        let rand_f = self.sample(builder);
+        let rand_f_bits = builder.num2bits_v2_f(rand_f, NUM_BITS);
+        let mut result = vec![];
+        for bit in &rand_f_bits[0..bits] {
+            result.push(builder.felt2var_circuit(*bit));
+        }
+        result
+    }
+
+    pub fn check_witness(&mut self, builder: &mut Builder<C>, bits: usize, witness: Felt<C::F>) {
+        self.observe(builder, witness);
+        let element = self.sample_bits(builder, bits);
+        for bit in element {
+            builder.assert_var_eq(bit, C::N::from_canonical_usize(0));
+        }
+    }
+}
+
+impl<C: Config> CanObserveVariable<C, Felt<C::F>> for MultiField32ChallengerVariable<C> {
+    fn observe(&mut self, builder: &mut Builder<C>, value: Felt<C::F>) {
+        MultiField32ChallengerVariable::observe(self, builder, value);
+    }
+}
+
+impl<C: Config> CanSampleVariable<C, Felt<C::F>> for MultiField32ChallengerVariable<C> {
+    fn sample(&mut self, builder: &mut Builder<C>) -> Felt<C::F> {
+        MultiField32ChallengerVariable::sample(self, builder)
+    }
+}
+
+impl<C: Config> CanSampleBitsVariable<C, Var<C::N>> for MultiField32ChallengerVariable<C> {
+    fn sample_bits(&mut self, builder: &mut Builder<C>, bits: usize) -> Vec<Var<C::N>> {
+        MultiField32ChallengerVariable::sample_bits(self, builder, bits)
+    }
+}
+
+impl<C: Config> FieldChallengerVariable<C, Var<C::N>> for MultiField32ChallengerVariable<C> {
+    fn sample_ext(&mut self, builder: &mut Builder<C>) -> Ext<C::F, C::EF> {
+        MultiField32ChallengerVariable::sample_ext(self, builder)
+    }
+
+    fn check_witness(&mut self, builder: &mut Builder<C>, bits: usize, witness: Felt<C::F>) {
+        MultiField32ChallengerVariable::check_witness(self, builder, bits, witness);
+    }
+
+    fn duplexing(&mut self, builder: &mut Builder<C>) {
+        MultiField32ChallengerVariable::duplexing(self, builder);
+    }
+}
+
+pub fn reduce_32<C: Config>(builder: &mut Builder<C>, vals: &[Felt<C::F>]) -> Var<C::N> {
+    let mut power = C::N::one();
+    let result: Var<C::N> = builder.eval(C::N::zero());
+    for val in vals.iter() {
+        let val = builder.felt2var_circuit(*val);
+        builder.assign(result, result + val * power);
+        power *= C::N::from_canonical_u64(1u64 << 32);
+    }
+    result
+}
+
+pub fn split_32<C: Config>(builder: &mut Builder<C>, val: Var<C::N>, n: usize) -> Vec<Felt<C::F>> {
+    let bits = builder.num2bits_v_circuit(val, 256);
+    let mut results = Vec::new();
+    for i in 0..n {
+        let result: Felt<C::F> = builder.eval(C::F::zero());
+        for j in 0..64 {
+            let bit = bits[i * 64 + j];
+            let t = builder.eval(result + C::F::from_wrapped_u64(1 << j));
+            let z = builder.select_f(bit, t, result);
+            builder.assign(result, z);
+        }
+        results.push(result);
+    }
+    results
 }
 
 #[cfg(test)]
