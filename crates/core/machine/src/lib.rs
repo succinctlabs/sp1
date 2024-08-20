9d445b65745e872a5777ef6a73c0ba0fd9205676
Bhargav Annem <bhargav.annem@gmail.com>
2024-08-20 12:20:43 -0700

    update version

--- crates/core/machine/src/lib.rs
+++ crates/core/machine/src/lib.rs
@@ -30,4 +30,4 @@ pub mod utils;
 /// This string should be updated whenever any step in verifying an SP1 proof changes, including
 /// core, recursion, and plonk-bn254. This string is used to download SP1 artifacts and the gnark
 /// docker image.
-pub const SP1_CIRCUIT_VERSION: &str = "v1.2.0-experimental";
+pub const SP1_CIRCUIT_VERSION: &str = "v1.2.0-rc1";
