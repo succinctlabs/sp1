88f8ffefdad14c926fd527a1a67093ce9b472f9b
John Guibas <jtguibas@gmail.com>
2024-08-23 14:00:49 -0700

    opt

--- crates/core/machine/src/utils/prove.rs
+++ crates/core/machine/src/utils/prove.rs
@@ -235,6 +235,7 @@ where
                             // "cpu events".
                             let mut state = state.lock().unwrap();
                             for record in records.iter_mut() {
+                                println!("record: {:?}", record.stats());
                                 state.shard += 1;
                                 state.execution_shard = record.public_values.execution_shard;
                                 state.start_pc = record.public_values.start_pc;
