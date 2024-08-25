0756de0e93d8956bce16cf4feb2d95ba86acf4d9
Kevin Jue <kjue235@gmail.com>
2024-08-25 13:11:53 -0700

    log the shard

--- crates/core/machine/src/utils/prove.rs
+++ crates/core/machine/src/utils/prove.rs
@@ -510,7 +510,8 @@ where
             let mut shard_proofs = Vec::new();
             tracing::debug_span!("phase 2 prover").in_scope(|| {
                 for (records, traces) in p2_records_and_traces_rx.into_iter() {
-                    tracing::debug_span!("batch").in_scope(|| {
+                    let shard = records[0].public_values.shard;
+                    tracing::debug_span!("batch", shard).in_scope(|| {
                         let span = tracing::Span::current().clone();
                         shard_proofs.par_extend(
                             records.into_par_iter().zip(traces.into_par_iter()).map(
