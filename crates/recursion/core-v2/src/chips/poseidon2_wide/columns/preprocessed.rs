152bcfd4df8cadca6cd988bd7143bf66e56fc911
Tej Qu Nair <tej.q.nair@gmail.com>
2024-08-26 12:07:22 -0700

    perf: remove 15 cols from poseidon2 wide (#1397)

--- crates/recursion/core-v2/src/chips/poseidon2_wide/columns/preprocessed.rs
+++ crates/recursion/core-v2/src/chips/poseidon2_wide/columns/preprocessed.rs
@@ -1,9 +1,14 @@
 use sp1_derive::AlignedBorrow;
 
-use crate::chips::{mem::MemoryAccessCols, poseidon2_wide::WIDTH};
+use crate::{
+    chips::{mem::MemoryAccessCols, poseidon2_wide::WIDTH},
+    Address,
+};
 
 #[derive(AlignedBorrow, Clone, Copy, Debug)]
 #[repr(C)]
 pub struct Poseidon2PreprocessedCols<T: Copy> {
-    pub memory_preprocessed: [MemoryAccessCols<T>; 2 * WIDTH],
+    pub input: [Address<T>; WIDTH],
+    pub output: [MemoryAccessCols<T>; WIDTH],
+    pub is_real_neg: T,
 }
