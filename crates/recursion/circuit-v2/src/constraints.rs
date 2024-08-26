cba22420a21f8957b22b8860ea5e3b228d4ad53e
erabinov <erabinovich92@gmail.com>
2024-08-26 07:43:48 -0700

    comment

--- crates/recursion/circuit-v2/src/constraints.rs
+++ crates/recursion/circuit-v2/src/constraints.rs
@@ -151,7 +151,7 @@ where
                             - C::F::one();
                         (
                             {
-                                // We use the precomputed powers of zeta to compute the value of
+                                // We use the precomputed powers of zeta to compute (inline) the value of
                                 // `other_domain.zp_at_point_variable(builder, zeta)`.
                                 let z: Ext<_, _> = builder.eval(
                                     zetas[other_domain.log_n] * SymbolicFelt::from_f(shift_power)
