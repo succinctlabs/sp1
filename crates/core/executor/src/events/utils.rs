71788761c82ddab0271d68c81a210d08b28e76d8
Shaked Regev <shaked@succinct.xyz>
2024-08-28 16:46:39 -0700

    down to 8 errors

--- crates/core/executor/src/events/utils.rs
+++ crates/core/executor/src/events/utils.rs
@@ -1,22 +1,52 @@
+use serde::Deserialize;
+use serde::Serialize;
 use std::{
     fmt::Display,
     iter::{Map, Peekable},
 };
 
 use rand::{thread_rng, Rng};
-
-/// Creates a new ALU lookup id.
-#[must_use]
-pub fn create_alu_lookup_id() -> u128 {
-    let mut rng = thread_rng();
-    rng.gen()
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, Default, Eq, Hash, PartialEq)]
+pub struct LookupId {
+    pub a: u32,
+    pub b: u32,
+    pub c: u32,
+    pub d: u32,
 }
 
-/// Creates a new ALU lookup ids.
+// /// Creates a new ALU lookup id.
+// #[must_use]
+// pub fn create_alu_lookup_id() -> u128 {
+//     let mut rng = thread_rng();
+//     rng.gen()
+// }
+
+/// Creates a new ALU lookup id with LookupId4
 #[must_use]
-pub fn create_alu_lookups() -> [u128; 6] {
+pub fn create_alu_lookup_id() -> LookupId {
     let mut rng = thread_rng();
-    [rng.gen(), rng.gen(), rng.gen(), rng.gen(), rng.gen(), rng.gen()]
+    LookupId { a: rng.gen(), b: rng.gen(), c: rng.gen(), d: rng.gen() }
+}
+
+// /// Creates a new ALU lookup ids.
+// #[must_use]
+// pub fn create_alu_lookups() -> [u128; 6] {
+//     let mut rng = thread_rng();
+//     [rng.gen(), rng.gen(), rng.gen(), rng.gen(), rng.gen(), rng.gen()]
+// }
+
+/// Creates a new ALU lookup id with LookupId4
+#[must_use]
+pub fn create_alu_lookups() -> [LookupId; 6] {
+    let mut rng = thread_rng();
+    [
+        LookupId { a: rng.gen(), b: rng.gen(), c: rng.gen(), d: rng.gen() },
+        LookupId { a: rng.gen(), b: rng.gen(), c: rng.gen(), d: rng.gen() },
+        LookupId { a: rng.gen(), b: rng.gen(), c: rng.gen(), d: rng.gen() },
+        LookupId { a: rng.gen(), b: rng.gen(), c: rng.gen(), d: rng.gen() },
+        LookupId { a: rng.gen(), b: rng.gen(), c: rng.gen(), d: rng.gen() },
+        LookupId { a: rng.gen(), b: rng.gen(), c: rng.gen(), d: rng.gen() },
+    ]
 }
 
 /// Returns sorted and formatted rows of a table of counts (e.g. `opcode_counts`).
