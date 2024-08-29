03be9331e800602411a7dd67b5bca36cd3240a5c
Shaked Regev <shaked@succinct.xyz>
2024-08-28 17:36:37 -0700

    fixed warnings

--- crates/core/executor/src/events/utils.rs
+++ crates/core/executor/src/events/utils.rs
@@ -24,26 +24,12 @@ pub struct LookupId {
     pub d: u32,
 }
 
-// /// Creates a new ALU lookup id.
-// #[must_use]
-// pub fn create_alu_lookup_id() -> u128 {
-//     let mut rng = thread_rng();
-//     rng.gen()
-// }
-
 /// Creates a new ALU lookup id with ``LookupId``
 #[must_use]
 pub fn create_alu_lookup_id() -> LookupId {
     let mut rng = thread_rng();
     LookupId { a: rng.gen(), b: rng.gen(), c: rng.gen(), d: rng.gen() }
 }
-
-// /// Creates a new ALU lookup ids.
-// #[must_use]
-// pub fn create_alu_lookups() -> [u128; 6] {
-//     let mut rng = thread_rng();
-//     [rng.gen(), rng.gen(), rng.gen(), rng.gen(), rng.gen(), rng.gen()]
-// }
 
 /// Creates a new ALU lookup id with ``LookupId``
 #[must_use]
