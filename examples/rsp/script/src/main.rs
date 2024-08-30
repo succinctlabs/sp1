c2363fbd7ffb9f49e6e0f01e0ae8329f5c4898d7
Yuwen Zhang <yuwen01@gmail.com>
2024-08-30 13:35:30 -0700

    small cleanup

--- examples/rsp/script/src/main.rs
+++ examples/rsp/script/src/main.rs
@@ -1,19 +1,15 @@
-// use reth_primitives::B256;
+use reth_primitives::B256;
 use rsp_client_executor::{
     io::ClientExecutorInput, CHAIN_ID_ETH_MAINNET,
 };
-use sp1_sdk::{ProverClient, SP1Stdin};
+use sp1_sdk::{utils, ProverClient, SP1Stdin};
 use std::path::PathBuf;
-use tracing_subscriber::{
-    filter::EnvFilter, fmt, prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt,
-};
 
 fn main() -> () {
-   // Initialize the logger.
-    tracing_subscriber::registry().with(fmt::layer()).with(EnvFilter::from_default_env()).init();
+    // Initialize the logger.
+    utils::setup_logger();
 
-    let client_input = try_load_input_from_cache(
-        &PathBuf::from("input/"),
+    let client_input = load_input_from_cache(
         CHAIN_ID_ETH_MAINNET,
         20526624,
     ).unwrap();
@@ -24,7 +20,7 @@ fn main() -> () {
     // Setup the proving key and verification key.
     let (pk, vk) = client.setup(include_bytes!("../../eth-program/elf/riscv32im-succinct-zkvm-elf"));
 
-    // Execute the block inside the zkVM.
+    // Write the block to the program's stdin.
     let mut stdin = SP1Stdin::new();
     let buffer = bincode::serialize(&client_input).unwrap();
     stdin.write_vec(buffer);
@@ -32,11 +28,11 @@ fn main() -> () {
     // Only execute the program.
     let (mut public_values, execution_report) =
         client.execute(&pk.elf, stdin.clone()).run().unwrap();
-    println!("Finished executing the block in {} ccycles", execution_report.total_instruction_count());
+    println!("Finished executing the block in {} cycles", execution_report.total_instruction_count());
 
     // Read the block hash.
-    // let block_hash = public_values.read::<B256>();
-    // println!("success: block_hash={block_hash}");
+    let block_hash = public_values.read::<B256>();
+    println!("success: block_hash={block_hash}");
 
     // Actually generate the proof. It is strongly recommended you use the network prover
     // given the size of these programs.
@@ -47,13 +43,11 @@ fn main() -> () {
     // client.verify(&proof, &vk).expect("proof verification should succeed");
 }
 
-fn try_load_input_from_cache(
-    cache_dir:&PathBuf,
+fn load_input_from_cache(
     chain_id: u64,
     block_number: u64,
 ) -> eyre::Result<ClientExecutorInput> {
     let cache_path = PathBuf::from(format!("./input/{}/{}.bin", chain_id, block_number));
-    println!("pwd: {:?}", std::env::current_dir());
     
     if cache_path.exists() {
         // TODO: prune the cache if invalid instead
