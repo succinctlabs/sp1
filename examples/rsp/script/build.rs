9bc37568e26bb416396d7a31c8aaa9e1954e24ae
Yuwen Zhang <yuwen01@gmail.com>
2024-08-30 17:13:29 -0700

    fix style issues, add ci test

--- examples/rsp/script/build.rs
+++ examples/rsp/script/build.rs
@@ -2,7 +2,7 @@ use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
     build_program_with_args(
-        &format!("../{}", "eth-program"),
+        &format!("../{}", "program"),
         BuildArgs { ignore_rust_version: true, ..Default::default() },
     );
 }
