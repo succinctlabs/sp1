4cb928c18237709e64de47f8c81ffbd137a2d074
Tamir Hemo <tamir@succinct.xyz>
2024-09-04 16:41:33 -0700

    shape config

--- crates/core/machine/src/riscv/shape.rs
+++ crates/core/machine/src/riscv/shape.rs
@@ -3,7 +3,12 @@ use p3_field::PrimeField32;
 use sp1_core_executor::{CoreShape, ExecutionRecord, Program};
 use sp1_stark::air::MachineAir;
 
-use super::RiscvAir;
+use crate::memory::{MemoryChipType, MemoryProgramChip};
+
+use super::{
+    AddSubChip, BitwiseChip, CpuChip, DivRemChip, LtChip, MemoryChip, MulChip, ProgramChip,
+    RiscvAir, ShiftLeft, ShiftRightChip,
+};
 
 /// A structure that enables fixing the shape of an executionrecord.
 pub struct CoreShapeConfig<F: PrimeField32> {
@@ -60,8 +65,53 @@ impl<F: PrimeField32> CoreShapeConfig<F> {
     }
 }
 
-// impl<F: PrimeField32> Default for CoreShapeConfig<F> {
-//     fn default() -> Self {
-//         let mut allowed_heights = HashMap::new();
-//     }
-// }
+impl<F: PrimeField32> Default for CoreShapeConfig<F> {
+    fn default() -> Self {
+        let mut allowed_heights = HashMap::new();
+
+        // Preprocessed chip heights.
+        let program_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+        let program_memory_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+
+        // Core chip heights.
+        let cpu_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+        let divrem_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+        let add_sub_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+        let bitwise_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+        let mul_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+        let shift_right_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+        let shift_left_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+        let lt_heights = vec![22, 21, 20, 19, 18, 17, 16, 10];
+        let memory_init_heights = vec![22, 21, 20, 19, 18, 16];
+        let memory_final_heights = vec![22, 21, 20, 19, 18, 16];
+
+        // Get allowed heights for preprocessed chips.
+        allowed_heights.extend([
+            (RiscvAir::Program(ProgramChip::default()), program_heights),
+            (RiscvAir::ProgramMemory(MemoryProgramChip::default()), program_memory_heights),
+        ]);
+
+        // Get the heights of core chips.
+
+        allowed_heights.extend([
+            (RiscvAir::Cpu(CpuChip::default()), cpu_heights),
+            (RiscvAir::DivRem(DivRemChip::default()), divrem_heights),
+            (RiscvAir::Add(AddSubChip::default()), add_sub_heights),
+            (RiscvAir::Bitwise(BitwiseChip::default()), bitwise_heights),
+            (RiscvAir::Mul(MulChip::default()), mul_heights),
+            (RiscvAir::ShiftRight(ShiftRightChip::default()), shift_right_heights),
+            (RiscvAir::ShiftLeft(ShiftLeft::default()), shift_left_heights),
+            (RiscvAir::Lt(LtChip::default()), lt_heights),
+            (
+                RiscvAir::MemoryInit(MemoryChip::new(MemoryChipType::Initialize)),
+                memory_init_heights,
+            ),
+            (
+                RiscvAir::MemoryFinal(MemoryChip::new(MemoryChipType::Finalize)),
+                memory_final_heights,
+            ),
+        ]);
+
+        Self { allowed_heights }
+    }
+}
