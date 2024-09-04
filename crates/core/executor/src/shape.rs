bfeb434ccfb9025b69f3f9782378af2f93732b61
Tamir Hemo <tamir@succinct.xyz>
2024-09-04 12:07:55 -0700

    comment fmt

--- crates/core/executor/src/shape.rs
+++ crates/core/executor/src/shape.rs
@@ -4,8 +4,12 @@ use serde::{Deserialize, Serialize};
 /// The shape of a core proof.
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct CoreShape {
-    /// The id of the shape. Used for enumeration of the possible proof shapes.
+    /// The id of the shape.
+    ///
+    /// The `id` is used for enumeration of the possible proof shapes.
     pub id: usize,
-    /// The shape of the proof. Keys are the chip names and values are the log-heights of the chips.
+    /// The shape of the proof.
+    ///
+    /// Keys are the chip names and values are the log-heights of the chips.
     pub shape: HashMap<String, usize>,
 }
