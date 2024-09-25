6e99159bd261a904980e5a05ca2fef429142cca6
umadayal <uma.dayal@yahoo.com>
2024-09-25 11:24:40 -0700

    removed commented out code

--- tests/u256x2048-mul/src/main.rs
+++ tests/u256x2048-mul/src/main.rs
@@ -17,85 +17,14 @@ fn u2048_to_bytes_le(x: &BigUint) -> [u8; 256] {
     bytes.try_into().unwrap()
 }
 
-fn u256x2048_mul(a: [u8; 32], b: [u8; 256]) -> (BigUint, BigUint) {
-    let a_big = BigUint::from_bytes_le(&a);
-    // println!("a_big: {}", a_big);
-    // let a_bytes = u256_to_bytes_le(&a_big);
-    let b_big = BigUint::from_bytes_le(&b);
-    // let b_bytes = u2048_to_bytes_le(&b_big);
-    // println!("b_big: {}", b_big);
-
-    let mut lo: [u32; 64] = [0; 64];
-    let mut hi: [u32; 8] = [0; 8];
-
-    syscall_u256x2048_mul(
-        a.as_ptr() as *const [u32; 8],
-        b.as_ptr() as *const [u32; 64],
-        lo.as_mut_ptr() as *mut [u32; 64],
-        hi.as_mut_ptr() as *mut [u32; 8],
-    );
-
-    // bytemuck::cast::<[u32; 64], [u8; 256]>(lo);
-    // bytemuck::cast::<[u32; 8], [u8; 32]>(hi);
-    let lo_bytes: [u8; 256] = bytemuck::cast::<[u32; 64], [u8; 256]>(lo);
-    let hi_bytes: [u8; 32] = bytemuck::cast::<[u32; 8], [u8; 32]>(hi);
-
-    let lo_big = BigUint::from_bytes_le(&lo_bytes);
-    let hi_big = BigUint::from_bytes_le(&hi_bytes);
-    (lo_big, hi_big)
-}
-
 pub fn main() {
-    // for _ in 0..50 {
-    //     let mut rng = rand::thread_rng();
-    //     let mut a: [u8; 32] = rng.gen();
-    //     let mut b: [u8; 256] = [0; 256];
-    //     rng.fill(&mut b);
-    //     let a_big = BigUint::from_bytes_le(&a);
-    //     // println!("a_big: {}", a_big);
-    //     a = u256_to_bytes_le(&a_big);
-    //     let b_big = BigUint::from_bytes_le(&b);
-    //     b = u2048_to_bytes_le(&b_big);
-    //     // println!("b_big: {}", b_big);
-
-    //     let mut lo: [u32; 64] = [0; 64];
-    //     let mut hi: [u32; 8] = [0; 8];
-
-    //     syscall_u256x2048_mul(
-    //         a.as_ptr() as *const [u32; 8],
-    //         b.as_ptr() as *const [u32; 64],
-    //         lo.as_mut_ptr() as *mut [u32; 64],
-    //         hi.as_mut_ptr() as *mut [u32; 8],
-    //     );
-
-    //     // bytemuck::cast::<[u32; 64], [u8; 256]>(lo);
-    //     // bytemuck::cast::<[u32; 8], [u8; 32]>(hi);
-    //     let lo_bytes: [u8; 256] = bytemuck::cast::<[u32; 64], [u8; 256]>(lo);
-    //     let hi_bytes: [u8; 32] = bytemuck::cast::<[u32; 8], [u8; 32]>(hi);
-
-    //     let lo_big = BigUint::from_bytes_le(&lo_bytes);
-    //     let hi_big = BigUint::from_bytes_le(&hi_bytes);
-    //     // println!("hi: {}", hi_big);
-    //     // println!("lo: {}", lo_big);
-
-    //     let result_syscall = (hi_big << 2048) + lo_big;
-    //     let result = a_big * b_big;
-    //     // println!("result: {}", result);
-    //     // println!("lo_true: {}", result.clone() % (BigUint::one() << 2048));
-
-    //     assert_eq!(result, result_syscall);
-    // }
-
-    // let mut rng = rand::thread_rng();
     let mut a_max: [u8; 32] = [0xff; 32];
     let mut b_max: [u8; 256] = [0xff; 256];
 
     let a_max_big = BigUint::from_bytes_le(&a_max);
-    // println!("a_big: {}", a_big);
     a_max = u256_to_bytes_le(&a_max_big);
     let b_max_big = BigUint::from_bytes_le(&b_max);
     b_max = u2048_to_bytes_le(&b_max_big);
-    // println!("b_big: {}", b_big);
 
     let mut lo_max: [u32; 64] = [0; 64];
     let mut hi_max: [u32; 8] = [0; 8];
@@ -107,8 +36,6 @@ pub fn main() {
         hi_max.as_mut_ptr() as *mut [u32; 8],
     );
 
-    // bytemuck::cast::<[u32; 64], [u8; 256]>(lo);
-    // bytemuck::cast::<[u32; 8], [u8; 32]>(hi);
     let lo_max_bytes: [u8; 256] = bytemuck::cast::<[u32; 64], [u8; 256]>(lo_max);
     let hi_max_bytes: [u8; 32] = bytemuck::cast::<[u32; 8], [u8; 32]>(hi_max);
 
@@ -117,7 +44,7 @@ pub fn main() {
 
     let result_max_syscall = (hi_max_big << 2048) + lo_max_big;
     let result_max = a_max_big * b_max_big;
-    // assert_eq!(result_max, result_max_syscall);
+    assert_eq!(result_max, result_max_syscall);
 
     println!("All tests passed successfully!");
 }
