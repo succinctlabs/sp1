8a0d4bc49a95df9c3c91ce2d7ebde4a77258f2fe
Alan Li <alan@brevis.network>
2024-09-25 19:47:01 +0800

    modify to use only one shard for proving

--- examples/fibonacci/script/src/main.rs
+++ examples/fibonacci/script/src/main.rs
@@ -1,9 +1,10 @@
+use std::sync::Arc;
 use clap::Parser;
 use sp1_sdk::{ProverClient, SP1Stdin};
 use std::time::SystemTime;
 use sp1_core_executor::{Executor, ExecutorMode, Program};
 use sp1_core_machine::riscv::RiscvAir;
-use sp1_stark::{baby_bear_poseidon2::BabyBearPoseidon2, StarkGenericConfig, SP1CoreOpts, CpuProver, MachineProver};
+use sp1_stark::{baby_bear_poseidon2::BabyBearPoseidon2, StarkGenericConfig, SP1CoreOpts, CpuProver, MachineProver, MachineRecord};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
@@ -37,7 +38,6 @@ fn main() {
 
     // Setup fibonacci program
     let program = Program::from(ELF).unwrap();
-    // println!("program: {:?}", program);
     let mut runtime = Executor::new(program, SP1CoreOpts::default());
 
     runtime.executor_mode = ExecutorMode::Trace;
@@ -55,34 +55,60 @@ fn main() {
     runtime.run().unwrap();
 
 
+    assert_eq!(
+        runtime.records.len(),
+        2,
+        "We could only test for one record for now and the last is the final one",
+    );
+    let mut record = runtime.records[0].clone();
+    assert!(record.memory_initialize_events.is_empty());
+    assert!(record.memory_finalize_events.is_empty());
+    runtime.records[1]
+        .memory_initialize_events
+        .clone_into(&mut record.memory_initialize_events);
+    // runtime.records[1].memory_initialize_events = vec![];
+    runtime.records[1]
+        .memory_finalize_events
+        .clone_into(&mut record.memory_finalize_events);
+    // runtime.records[1].memory_finalize_events = vec![];
+    let program = record.program.clone();
+    println!("shard size: {}", runtime.shard_size);
+    for rcd in &runtime.records {
+        println!("record events: {:?}", rcd.stats());
+    }
+    println!("final record events: {:?}", record.stats());
+
+
     // Setup the prover.
     println!(
-        "\n Setup prover (at {} sec)..",
-        start.elapsed().unwrap().as_secs()
+        "\n Setup prover (at {} ms)..",
+        start.elapsed().unwrap().as_millis()
     );
 
     let config = BabyBearPoseidon2::new();
     let machine = RiscvAir::machine(config);
     let prover = <CpuProver<_, _>>::new(machine);
-    let (pk, vk) = prover.setup(runtime.program.as_ref());
+    let (pk, vk) = prover.setup(&program);
 
     // Generate the proof
     println!(
-        "\n Generating proof (at {} sec)..",
-        start.elapsed().unwrap().as_secs()
+        "\n Generating proof (at {} ms)..",
+        start.elapsed().unwrap().as_millis()
     );
     let mut challenger = prover.config().challenger();
-    //let records = vec![runtime.record];
-    let records = runtime.records;
+    let mut records = vec![record];
     let proof = prover.prove(&pk, records, &mut challenger, SP1CoreOpts::default()).unwrap();
 
 
-    println!("Successfully generated proof!");
+    println!(
+        "Successfully generated proof (at {} ms)..",
+        start.elapsed().unwrap().as_millis()
+    );
 
     // Verify the proof.
     println!(
-        "\n Verifying proof (at {} sec)..",
-        start.elapsed().unwrap().as_secs()
+        "\n Verifying proof (at {} ms)..",
+        start.elapsed().unwrap().as_millis()
     );
     let mut challenger = prover.config().challenger();
     let _ = prover.machine().verify(&vk, &proof, &mut challenger);
