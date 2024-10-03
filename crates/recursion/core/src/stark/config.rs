1b04e5766640016c18d2b82243d9cede3c544a91
Tamir Hemo <tamir@succinct.xyz>
2024-10-03 10:10:50 -0700

    test blowup

--- crates/recursion/core/src/stark/config.rs
+++ crates/recursion/core/src/stark/config.rs
@@ -81,10 +81,10 @@ pub fn outer_fri_config() -> FriConfig<OuterChallengeMmcs> {
     } else {
         match std::env::var("FRI_QUERIES") {
             Ok(value) => value.parse().unwrap(),
-            Err(_) => 25,
+            Err(_) => 34,
         }
     };
-    FriConfig { log_blowup: 4, num_queries, proof_of_work_bits: 16, mmcs: challenge_mmcs }
+    FriConfig { log_blowup: 3, num_queries, proof_of_work_bits: 16, mmcs: challenge_mmcs }
 }
 
 /// The FRI config for outer recursion.
