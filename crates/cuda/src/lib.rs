6dde7dfd600937a895b3feeaf621c065f9456c3f
John Guibas <jtguibas@gmail.com>
2024-10-08 10:56:47 -0700

    update cuda img

--- crates/cuda/src/lib.rs
+++ crates/cuda/src/lib.rs
@@ -84,7 +84,7 @@ impl SP1CudaProver {
     /// [SP1ProverClient] that can be used to communicate with the container.
     pub fn new() -> Result<Self, Box<dyn StdError>> {
         let container_name = "sp1-gpu";
-        let image_name = "public.ecr.aws/succinct-labs/sp1-gpu:fc6aeba";
+        let image_name = "public.ecr.aws/succinct-labs/sp1-gpu:a0a64fe";
 
         let cleaned_up = Arc::new(AtomicBool::new(false));
         let cleanup_name = container_name;
