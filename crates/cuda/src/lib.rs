01a7581499ea000243f478cad2753daaa3048c58
John Guibas <jtguibas@gmail.com>
2024-10-08 23:00:58 -0700

    update gpu image with new shapes

--- crates/cuda/src/lib.rs
+++ crates/cuda/src/lib.rs
@@ -84,7 +84,7 @@ impl SP1CudaProver {
     /// [SP1ProverClient] that can be used to communicate with the container.
     pub fn new() -> Result<Self, Box<dyn StdError>> {
         let container_name = "sp1-gpu";
-        let image_name = "public.ecr.aws/succinct-labs/sp1-gpu:4b00830";
+        let image_name = "public.ecr.aws/succinct-labs/sp1-gpu:a4e646b";
 
         let cleaned_up = Arc::new(AtomicBool::new(false));
         let cleanup_name = container_name;
