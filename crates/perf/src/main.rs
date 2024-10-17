195b624eed8f757d587921207a43b602afa0d610
jtguibas <jtguibas@gmail.com>
2024-10-17 18:38:52 +0000

    test deferred proofs on cpu

--- crates/perf/src/main.rs
+++ crates/perf/src/main.rs
@@ -1,8 +1,10 @@
 use std::time::{Duration, Instant};
 
 use clap::{command, Parser, ValueEnum};
+use sp1_core_executor::programs::tests::VERIFY_PROOF_ELF;
 use sp1_cuda::SP1CudaProver;
 use sp1_prover::components::DefaultProverComponents;
+use sp1_prover::HashableKey;
 use sp1_sdk::{self, ProverClient, SP1Context, SP1Prover, SP1Stdin};
 use sp1_stark::SP1ProverOpts;
 
@@ -17,7 +19,7 @@ struct PerfArgs {
     pub mode: ProverMode,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Default, Debug, Clone)]
 #[allow(dead_code)]
 struct PerfResult {
     pub cycles: u64,
@@ -73,13 +75,13 @@ fn main() {
 
             let proofs = stdin.proofs.into_iter().map(|(proof, _)| proof).collect::<Vec<_>>();
             let (compress_proof, compress_duration) =
-                time_operation(|| prover.compress(&vk, core_proof, proofs, opts).unwrap());
+                time_operation(|| prover.compress(&vk, core_proof.clone(), proofs, opts).unwrap());
 
             let (_, verify_compressed_duration) =
                 time_operation(|| prover.verify_compressed(&compress_proof, &vk));
 
             let (shrink_proof, shrink_duration) =
-                time_operation(|| prover.shrink(compress_proof, opts).unwrap());
+                time_operation(|| prover.shrink(compress_proof.clone(), opts).unwrap());
 
             let (_, verify_shrink_duration) =
                 time_operation(|| prover.verify_shrink(&shrink_proof, &vk));
@@ -89,6 +91,30 @@ fn main() {
 
             let (_, verify_wrap_duration) =
                 time_operation(|| prover.verify_wrap_bn254(&wrapped_bn254_proof, &vk));
+
+            // Generate a proof that verifies two deferred proofs from the proof above.
+            let (pk_verify_proof, vk_verify_proof) = prover.setup(VERIFY_PROOF_ELF);
+            let pv = core_proof.public_values.to_vec();
+
+            let mut stdin = SP1Stdin::new();
+            let vk_u32 = vk.hash_u32();
+            stdin.write::<[u32; 8]>(&vk_u32);
+            stdin.write::<Vec<Vec<u8>>>(&vec![pv.clone(), pv.clone()]);
+            stdin.write_proof(compress_proof.clone(), vk.vk.clone());
+            stdin.write_proof(compress_proof.clone(), vk.vk.clone());
+
+            let context = SP1Context::default();
+            let (core_proof, _) = time_operation(|| {
+                prover.prove_core(&pk_verify_proof, &stdin, opts, context).unwrap()
+            });
+            let deferred_proofs =
+                stdin.proofs.into_iter().map(|(proof, _)| proof).collect::<Vec<_>>();
+            let (compress_proof, _) = time_operation(|| {
+                prover
+                    .compress(&vk_verify_proof, core_proof.clone(), deferred_proofs, opts)
+                    .unwrap()
+            });
+            prover.verify_compressed(&compress_proof, &vk_verify_proof).unwrap();
 
             let result = PerfResult {
                 cycles,
@@ -132,12 +158,11 @@ fn main() {
             let (_, verify_shrink_duration) =
                 time_operation(|| prover.verify_shrink(&shrink_proof, &vk));
 
-            let (_, wrap_duration) = time_operation(|| server.wrap_bn254(shrink_proof).unwrap());
+            let (wrapped_bn254_proof, wrap_duration) =
+                time_operation(|| server.wrap_bn254(shrink_proof).unwrap());
 
-            // TODO: Verify wrapped bn254 proofs.
-            // let (_, verify_wrap_duration) =
-            //     time_operation(|| prover.verify_wrap_bn254(&wrapped_bn254_proof, &vk));
-            let verify_wrap_duration = Duration::from_secs(0);
+            let (_, verify_wrap_duration) =
+                time_operation(|| prover.verify_wrap_bn254(&wrapped_bn254_proof, &vk));
 
             let result = PerfResult {
                 cycles,
