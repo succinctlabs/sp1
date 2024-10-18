e16516da51e216f9153c94a04eace949c9963739
Tamir Hemo <tamir@succinct.xyz>
2024-10-17 19:45:47 -0700

    fix: connect deferred vk root (#1649)
    Co-authored-by: jtguibas <jtguibas@gmail.com>

--- crates/perf/run_s3.sh
+++ crates/perf/run_s3.sh
@@ -14,9 +14,10 @@ aws s3 cp s3://sp1-testing-suite/$s3_path/program.bin /tmp/program.bin
 aws s3 cp s3://sp1-testing-suite/$s3_path/stdin.bin /tmp/stdin.bin
 
 # Set environment variables
-export RUSTFLAGS="-Copt-level=3 -Ctarget-cpu=native"
+export RUSTFLAGS="-Copt-level=3 -Ctarget-cpu=native -Cdebuginfo=2"
 export RUST_BACKTRACE=1
 export RUST_LOG=debug
+export SP1_DEBUG=1
 
 # Run moongate-perf
-cargo run --release -p sp1-perf -- --program /tmp/program.bin --stdin /tmp/stdin.bin --mode $stage
+cargo run -p sp1-perf -- --program /tmp/program.bin --stdin /tmp/stdin.bin --mode $stage
