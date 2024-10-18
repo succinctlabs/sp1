990519b5182537303bbb370be6af19246c016b0d
N <n@push0.dev>
2024-10-18 12:49:40 -0700

    chore: License

--- crates/cli/src/commands/profile.rs
+++ crates/cli/src/commands/profile.rs
@@ -1,3 +1,23 @@
+//! RISC-V tracing and profiling for SP1 traces. This tool can be used to analyze function call graphs and
+//! instruction counts from a trace file from SP1 execution by setting the `TRACE_FILE` env
+//! variable.
+//
+// Adapted from Sovereign's RISC-V tracer tool: https://github.com/Sovereign-Labs/riscv-cycle-tracer.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Modified by Succinct Labs on Oct 18, 2024.
+
 use clap::Parser;
 use gecko_profile::{Frame, ProfileBuilder, ThreadBuilder};
 use std::path::PathBuf;
