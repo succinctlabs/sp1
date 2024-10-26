b7b55a6657a1f26e9afcbd61e0c321732541af1f
Yuwen Zhang <yuwen01@gmail.com>
2024-10-25 17:35:36 -0700

    fix up tests

--- crates/verifier/src/tests.rs
+++ crates/verifier/src/tests.rs
@@ -1,10 +1,10 @@
-use sp1_sdk::{HashableKey, ProverClient, SP1ProofWithPublicValues, SP1Stdin};
+use sp1_sdk::{HashableKey, ProverClient, SP1Stdin};
 
 extern crate std;
 
 #[test]
 fn test_verify_groth16() {
-    let client = ProverClient::local();
+    let client = ProverClient::new();
     let elf = include_bytes!("../../../tests/fibonacci/elf/riscv32im-succinct-zkvm-elf");
     let (pk, vk) = client.setup(elf);
     let mut stdin = SP1Stdin::new();
@@ -33,7 +33,7 @@ fn test_verify_groth16() {
 #[cfg(feature = "getrandom")]
 #[test]
 fn test_verify_plonk() {
-    let client = ProverClient::local();
+    let client = ProverClient::new();
     let elf = include_bytes!("../../../tests/fibonacci/elf/riscv32im-succinct-zkvm-elf");
     let (pk, vk) = client.setup(elf);
     let mut stdin = SP1Stdin::new();
@@ -42,7 +42,7 @@ fn test_verify_plonk() {
     // Generate proof & verify.
     let sp1_proof_with_public_values = client.prove(&pk, stdin).plonk().run().unwrap();
 
-    let proof = sp1_proof_with_public_values.bytes();
+    let proof = sp1_proof_with_public_values.raw_with_checksum();
     let public_inputs = sp1_proof_with_public_values.public_values.to_vec();
 
     // This vkey hash was derived by calling `vk.bytes32()` on the verifying key.
@@ -54,25 +54,5 @@ fn test_verify_plonk() {
 
     if !is_valid {
         panic!("Plonk proof is invalid");
-    }
-}
-
-#[test]
-fn test_verify_groth16_from_binary() {
-    // Generate proof & verify.
-    let sp1_proof_with_public_values =
-        SP1ProofWithPublicValues::load("test_binaries/fibonacci_groth16.bin").unwrap();
-
-    let proof = sp1_proof_with_public_values.bytes();
-    let public_inputs = sp1_proof_with_public_values.public_values.to_vec();
-    let vkey_hash = "0x0051835c0ba4b1ce3e6c5f4c5ab88a41e3eb1bc725d383f12255028ed76bd9a7";
-    // let vkey_hash = "0x002df46ca9d137cd164b6fcb2e5db55213aaacf217fad88e29d608aeabe3285c";
-
-    let is_valid =
-        crate::Groth16Verifier::verify(&proof, &public_inputs, vkey_hash, &crate::GROTH16_VK_BYTES)
-            .expect("Groth16 proof is invalid");
-
-    if !is_valid {
-        panic!("Groth16 proof is invalid");
     }
 }
