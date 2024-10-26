d06f503d57d72836dd7add4ca9a3f95294e8624a
mattstam <codeguy0112358@gmail.com>
2024-10-25 18:17:54 -0700

    increase timeout

--- crates/sdk/src/network-v2/prover.rs
+++ crates/sdk/src/network-v2/prover.rs
@@ -19,7 +19,7 @@ use {crate::block_on, tokio::time::sleep};
 use crate::provers::{CpuProver, ProofOpts, ProverType};
 
 /// The timeout for a proof request to be fulfilled.
-const TIMEOUT_SECS: u64 = 3600;
+const TIMEOUT_SECS: u64 = 14400;
 
 /// The default cycle limit for a proof request.
 const DEFAULT_CYCLE_LIMIT: u64 = 1_000_000_000;
