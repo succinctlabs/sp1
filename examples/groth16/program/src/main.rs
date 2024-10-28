f4b97edb4aa8f0713acdbe5923af780531a2e7f3
Yuwen Zhang <yuwen01@gmail.com>
2024-10-28 01:01:26 -0700

    small cleanup

--- examples/groth16/program/src/main.rs
+++ examples/groth16/program/src/main.rs
@@ -12,9 +12,9 @@ pub fn main() {
     let sp1_vkey_hash: String = sp1_zkvm::io::read();
 
     // Verify the groth16 proof.
-    let groth16_vk = &sp1_verifier::GROTH16_VK_BYTES;
+    let groth16_vk = *sp1_verifier::GROTH16_VK_BYTES;
     println!("cycle-tracker-start: verify");
-    let result = Groth16Verifier::verify(&proof, &sp1_public_values, &sp1_vkey_hash, &groth16_vk);
+    let result = Groth16Verifier::verify(&proof, &sp1_public_values, &sp1_vkey_hash, groth16_vk);
     println!("cycle-tracker-end: verify");
 
     match result {
