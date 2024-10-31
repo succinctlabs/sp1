eee73c35b2cead78e04d08aa6609b7fef494aa58
erabinov <erabinovich92@gmail.com>
2024-10-30 19:08:22 -0700

    changed cuda

--- crates/prover/src/lib.rs
+++ crates/prover/src/lib.rs
@@ -699,6 +699,9 @@ impl<C: SP1ProverComponents> SP1Prover<C> {
                     loop {
                         let received = { input_rx.lock().unwrap().recv() };
                         if let Ok((index, height, input)) = received {
+                            if index == 2 * num_first_layer_inputs - 2 {
+                                break;
+                            }
                             // Get the program and witness stream.
                             let (program, witness_stream) = tracing::debug_span!(
                                 "get program and witness stream"
@@ -785,7 +788,7 @@ impl<C: SP1ProverComponents> SP1Prover<C> {
             let proofs_sync = Arc::new(TurnBasedSync::new());
             let (proofs_tx, proofs_rx) =
                 sync_channel::<(usize, usize, StarkVerifyingKey<InnerSC>, ShardProof<InnerSC>)>(
-                    num_first_layer_inputs * 2,
+                    num_first_layer_inputs * 2 - 1,
                 );
             let proofs_tx = Arc::new(Mutex::new(proofs_tx));
             let proofs_rx = Arc::new(Mutex::new(proofs_rx));
@@ -800,6 +803,9 @@ impl<C: SP1ProverComponents> SP1Prover<C> {
                     loop {
                         let received = { record_and_trace_rx.lock().unwrap().recv() };
                         if let Ok((index, height, program, record, traces)) = received {
+                            if index == 2*num_first_layer_inputs-2{
+                                break
+                            }
                             tracing::debug_span!("batch").in_scope(|| {
                                 // Get the keys.
                                 let (pk, vk) = tracing::debug_span!("Setup compress program")
