0a26f9f9e5a84b03612a9c80acb87455a4f94d83
rkm0959 <rkm0959@gmail.com>
2024-11-01 14:51:58 -0700

    chore: fix comments and logging for select precompile (#1738)

--- crates/recursion/compiler/src/ir/instructions.rs
+++ crates/recursion/compiler/src/ir/instructions.rs
@@ -120,8 +120,7 @@ pub enum DslIr<C: Config> {
     /// Inverts an extension field element (ext = 1 / ext).
     InvE(Ext<C::F, C::EF>, Ext<C::F, C::EF>),
 
-    // Selecting Digest.
-    /// Selects the digest based on a bit (should_swap, first result, second result, first input, second input)
+    /// Selects order of felts based on a bit (should_swap, first result, second result, first input, second input)
     Select(Felt<C::F>, Felt<C::F>, Felt<C::F>, Felt<C::F>, Felt<C::F>),
 
     // Control flow.
