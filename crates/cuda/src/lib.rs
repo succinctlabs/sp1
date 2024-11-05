5647629f3bbd2ed7567797ebc0097dbb3f8e1b96
erabinov <erabinovich92@gmail.com>
2024-11-05 14:52:24 -0800

    docker

--- crates/cuda/src/lib.rs
+++ crates/cuda/src/lib.rs
@@ -91,7 +91,7 @@ impl SP1CudaProver {
     /// [SP1ProverClient] that can be used to communicate with the container.
     pub fn new() -> Result<Self, Box<dyn StdError>> {
         let container_name = "sp1-gpu";
-        let image_name = "public.ecr.aws/succinct-labs/sp1-gpu:7e66232";
+        let image_name = "public.ecr.aws/succinct-labs/sp1-gpu:295889b";
 
         let cleaned_up = Arc::new(AtomicBool::new(false));
         let cleanup_name = container_name;
