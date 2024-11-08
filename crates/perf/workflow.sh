05020626da83f5de4d7acc7c87bb0d9d54c6f260
mattstam <codeguy0112358@gmail.com>
2024-11-08 15:10:14 -0800

    concurrent

--- crates/perf/workflow.sh
+++ crates/perf/workflow.sh
@@ -65,6 +65,28 @@ NETWORK_WORKLOADS=(
     "op-succinct-op-sepolia-range-17685896-17685897"
     "op-succinct-op-sepolia-range-17985900-17985905"
     "op-succinct-op-sepolia-range-18129400-18129401"
+    "fibonacci-17k"
+    "ssz-withdrawals"
+    "tendermint"
+    "rsp-20526624"
+    "rsa"
+    "regex"
+    "chess"
+    "json"
+    "blobstream-01j6z63fgafrc8jeh0k12gbtvw"
+    "blobstream-01j6z95bdme9svevmfyc974bja"
+    "blobstream-01j6z9ak0ke9srsppgywgke6fj"
+    "vector-01j6xsv35re96tkgyda115320t"
+    "vector-01j6xzy366ff5tbkzcrs8pma02"
+    "vector-01j6y06de0fdaafemr8b1t69z3"
+    "raiko-a7-10"
+    "op-succinct-op-sepolia-1818303090-18303120"
+    "op-succinct-op-sepolia-18200000-18200030" 
+    "op-succinct-op-sepolia-18250000-18250030"
+    "op-succinct-op-sepolia-18303044-18303074"
+    "op-succinct-op-sepolia-range-17685896-17685897"
+    "op-succinct-op-sepolia-range-17985900-17985905"
+    "op-succinct-op-sepolia-range-18129400-18129401"
 )
 
 # Create a JSON object with the list of workloads.
@@ -76,4 +98,3 @@ WORKLOADS=$(jq -n \
 
 # Run the workflow with the list of workloads.
 echo $WORKLOADS | gh workflow run suite.yml --ref $GIT_REF --json
-
