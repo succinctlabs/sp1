7c126c8b3fa59fbd5f81ef995ba09d54ad59daff
leruaa <aurelien.catinon@gmail.com>
2024-11-12 14:08:21 -0800

    fix: prover scripts not added as binaries

--- crates/test-artifacts/src/lib.rs
+++ crates/test-artifacts/src/lib.rs
@@ -68,3 +68,5 @@ pub const BN254_FP_ELF: &[u8] = include_elf!("bn254-fp-test");
 pub const BN254_FP2_ADDSUB_ELF: &[u8] = include_elf!("bn254-fp2-addsub-test");
 
 pub const BN254_FP2_MUL_ELF: &[u8] = include_elf!("bn254-fp2-mul-test");
+
+pub const TENDERMINT_BENCHMARK_ELF: &[u8] = include_elf!("tendermint-benchmark-program");
