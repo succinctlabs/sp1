588286b6c7ecc6c2aa8a668951c64b47dece8b67
Aurélien <3535019+leruaa@users.noreply.github.com>
2024-11-13 15:19:43 -0800

    feat: stop printing unused syscalls in execution report (#1787)

--- crates/core/machine/src/utils/span.rs
+++ crates/core/machine/src/utils/span.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, fmt::Display, hash::Hash, iter::once};
 
-use sp1_core_executor::events::sorted_table_lines;
+use sp1_core_executor::events::{format_table_line, sorted_table_lines};
 use thiserror::Error;
 
 /// A builder to create a [`Span`].
@@ -116,13 +116,15 @@ where
     /// Format and yield lines describing this span. Appropriate for logging.
     pub fn lines(&self) -> Vec<String> {
         let Self { name, cts: instr_cts, children } = self;
+        let (width, lines) = sorted_table_lines(instr_cts);
+        let lines = lines.map(|(label, count)| format_table_line(&width, &label, count));
 
         once(format!("{}", name))
             .chain(
                 children
                     .iter()
                     .flat_map(|c| c.lines())
-                    .chain(sorted_table_lines(instr_cts))
+                    .chain(lines)
                     .map(|line| format!("│  {line}")),
             )
             .chain(once(format!("└╴ {} total", self.total())))
