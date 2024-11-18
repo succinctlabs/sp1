37cce1ce3c78528ed71ef510799b5fff2db7694c
John Guibas <jtguibas@gmail.com>
2024-11-17 22:23:30 -0800

    hm

--- examples/fibonacci/script/src/main.rs
+++ examples/fibonacci/script/src/main.rs
@@ -1,7 +1,10 @@
+use sp1_core_executor::{Executor, Program};
 use sp1_sdk::{include_elf, utils, ProverClient, SP1ProofWithPublicValues, SP1Stdin};
-
+use sp1_stark::SP1CoreOpts;
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_elf!("fibonacci-program");
+const PROGRAM: &[u8] = include_bytes!("../../program_taiko");
+const STDIN: &[u8] = include_bytes!("../../stdin_taiko");
 
 fn main() {
     // Setup logging.
@@ -17,38 +20,47 @@ fn main() {
 
     // Create a `ProverClient` method.
     let client = ProverClient::new();
+    let program: Vec<u8> = bincode::deserialize(PROGRAM).unwrap();
+    let program = Program::from(&program).unwrap();
 
-    // Execute the program using the `ProverClient.execute` method, without generating a proof.
-    let (_, report) = client.execute(ELF, stdin.clone()).run().unwrap();
-    println!("executed program with {} cycles", report.total_instruction_count());
+    // let program: Program = bincode::deserialize(PROGRAM).unwrap();
+    let stdin: SP1Stdin = bincode::deserialize(STDIN).unwrap();
 
-    // Generate the proof for the given program and input.
-    let (pk, vk) = client.setup(ELF);
-    let mut proof = client.prove(&pk, stdin).run().unwrap();
+    let mut runtime = Executor::new(program, SP1CoreOpts::default());
+    runtime.write_vecs(&stdin.buffer);
+    runtime.run().unwrap();
 
-    println!("generated proof");
+    // // Execute the program using the `ProverClient.execute` method, without generating a proof.
+    // let (_, report) = client.execute(ELF, stdin.clone()).run().unwrap();
+    // println!("executed program with {} cycles", report.total_instruction_count());
 
-    // Read and verify the output.
-    //
-    // Note that this output is read from values committed to in the program using
-    // `sp1_zkvm::io::commit`.
-    let _ = proof.public_values.read::<u32>();
-    let a = proof.public_values.read::<u32>();
-    let b = proof.public_values.read::<u32>();
+    // // Generate the proof for the given program and input.
+    // let (pk, vk) = client.setup(ELF);
+    // let mut proof = client.prove(&pk, stdin).run().unwrap();
 
-    println!("a: {}", a);
-    println!("b: {}", b);
+    // println!("generated proof");
 
-    // Verify proof and public values
-    client.verify(&proof, &vk).expect("verification failed");
+    // // Read and verify the output.
+    // //
+    // // Note that this output is read from values committed to in the program using
+    // // `sp1_zkvm::io::commit`.
+    // let _ = proof.public_values.read::<u32>();
+    // let a = proof.public_values.read::<u32>();
+    // let b = proof.public_values.read::<u32>();
 
-    // Test a round trip of proof serialization and deserialization.
-    proof.save("proof-with-pis.bin").expect("saving proof failed");
-    let deserialized_proof =
-        SP1ProofWithPublicValues::load("proof-with-pis.bin").expect("loading proof failed");
+    // println!("a: {}", a);
+    // println!("b: {}", b);
 
-    // Verify the deserialized proof.
-    client.verify(&deserialized_proof, &vk).expect("verification failed");
+    // // Verify proof and public values
+    // client.verify(&proof, &vk).expect("verification failed");
 
-    println!("successfully generated and verified proof for the program!")
+    // // Test a round trip of proof serialization and deserialization.
+    // proof.save("proof-with-pis.bin").expect("saving proof failed");
+    // let deserialized_proof =
+    //     SP1ProofWithPublicValues::load("proof-with-pis.bin").expect("loading proof failed");
+
+    // // Verify the deserialized proof.
+    // client.verify(&deserialized_proof, &vk).expect("verification failed");
+
+    // println!("successfully generated and verified proof for the program!")
 }
