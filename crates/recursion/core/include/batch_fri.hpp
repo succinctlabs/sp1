d1703659017860463ad6a9715af91dde6b239fd7
Gautham Elango <gautham.gg@gmail.com>
2024-11-19 11:18:16 -0800

    Update batch_fri.hpp

--- crates/recursion/core/include/batch_fri.hpp
+++ crates/recursion/core/include/batch_fri.hpp
@@ -4,9 +4,9 @@
 
 namespace recursion::batch_fri {
 template <class F> __SP1_HOSTDEV__ void event_to_row(const sp1_recursion_core_sys::BatchFRIEvent<F> &io, sp1_recursion_core_sys::BatchFRICols<F> &cols) {
-    cols.acc = sp1_recursion_core_sys::Block<F>{io.base_vec.p_at_x};
+    cols.acc = io.ext_single.acc;
     cols.alpha_pow = io.ext_vec.alpha_pow;
     cols.p_at_z = io.ext_vec.p_at_z;
-    cols.p_at_x = io.ext_single.acc._0[0];
+    cols.p_at_x = io.base_vec.p_at_x;
 }
 } // namespace recursion::batch_fri
