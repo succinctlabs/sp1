2c5627d6b59f052228ffd6e032ab020e115fd2fb
Gautham Elango <gautham.gg@gmail.com>
2024-11-19 09:25:16 -0800

    fix build

--- crates/recursion/core/include/alu_base.hpp
+++ crates/recursion/core/include/alu_base.hpp
@@ -1,9 +1,5 @@
-#pragma once
-
-#include "sp1_recursion_core_sys-cbindgen.hpp"
-
 namespace recursion::alu_base {
-template <class F> __SP1_HOSTDEV__ void event_to_row(const BaseAluIo& event, BaseAluCols<F>& cols) {
+template <class F> __SP1_HOSTDEV__ void event_to_row() {
   // TODO
 }
 } // namespace recursion::alu_base
