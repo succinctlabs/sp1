af5693c13fa351ea39a8474bef7573e972acf58e
Gautham Elango <gautham.gg@gmail.com>
2024-11-19 10:38:54 -0800

    Update alu_ext.hpp

--- crates/recursion/core/include/alu_ext.hpp
+++ crates/recursion/core/include/alu_ext.hpp
@@ -4,8 +4,6 @@
 
 namespace recursion::alu_ext {
 template <class F> __SP1_HOSTDEV__ void event_to_row(const sp1_recursion_core_sys::ExtAluIo<sp1_recursion_core_sys::Block<F>> &io, sp1_recursion_core_sys::ExtAluValueCols<F> &cols) {
-  cols.vals.out = sp1_recursion_core_sys::Block<F>{io.out};
-  cols.vals.in1 = sp1_recursion_core_sys::Block<F>{io.in1};
-  cols.vals.in2 = sp1_recursion_core_sys::Block<F>{io.in2};
+  cols.vals = io;
 }
 } // namespace recursion::alu_ext
