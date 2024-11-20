911e59b5484440be6d393c30f4a3e62d1bd4577a
N <n@push0.dev>
2024-11-20 15:08:26 -0800

    chore: deprecate cargo prove (#1792)

--- crates/cli/src/bin/cargo-prove.rs
+++ crates/cli/src/bin/cargo-prove.rs
@@ -3,8 +3,7 @@ use clap::{Parser, Subcommand};
 use sp1_cli::{
     commands::{
         build::BuildCmd, build_toolchain::BuildToolchainCmd,
-        install_toolchain::InstallToolchainCmd, new::NewCmd, prove::ProveCmd, trace::TraceCmd,
-        vkey::VkeyCmd,
+        install_toolchain::InstallToolchainCmd, new::NewCmd, trace::TraceCmd, vkey::VkeyCmd,
     },
     SP1_VERSION_MESSAGE,
 };
@@ -19,17 +18,13 @@ pub enum Cargo {
 #[command(author, about, long_about = None, args_conflicts_with_subcommands = true, version = SP1_VERSION_MESSAGE)]
 pub struct ProveCli {
     #[clap(subcommand)]
-    pub command: Option<ProveCliCommands>,
-
-    #[clap(flatten)]
-    pub prove: ProveCmd,
+    pub command: ProveCliCommands,
 }
 
 #[derive(Subcommand)]
 pub enum ProveCliCommands {
     New(NewCmd),
     Build(BuildCmd),
-    Prove(ProveCmd),
     BuildToolchain(BuildToolchainCmd),
     InstallToolchain(InstallToolchainCmd),
     Trace(TraceCmd),
@@ -38,11 +33,10 @@ pub enum ProveCliCommands {
 
 fn main() -> Result<()> {
     let Cargo::Prove(args) = Cargo::parse();
-    let command = args.command.unwrap_or(ProveCliCommands::Prove(args.prove));
-    match command {
+
+    match args.command {
         ProveCliCommands::New(cmd) => cmd.run(),
         ProveCliCommands::Build(cmd) => cmd.run(),
-        ProveCliCommands::Prove(cmd) => cmd.run(),
         ProveCliCommands::BuildToolchain(cmd) => cmd.run(),
         ProveCliCommands::InstallToolchain(cmd) => cmd.run(),
         ProveCliCommands::Trace(cmd) => cmd.run(),
