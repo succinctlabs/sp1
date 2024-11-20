13e44e23f6e6a046425d941f170efb89cc09379e
Gautham Elango <gautham.gg@gmail.com>
2024-11-19 17:48:24 -0800

    fmt

--- crates/recursion/core/include/alu_base.hpp
+++ crates/recursion/core/include/alu_base.hpp
@@ -2,22 +2,28 @@
 
 #include "prelude.hpp"
 
-namespace sp1_recursion_core_sys::alu_base {
-template <class F> __SP1_HOSTDEV__ void event_to_row(const BaseAluEvent<F> &event, BaseAluValueCols<F> &cols) {
-  cols.vals = event;
-}
+namespace sp1_recursion_core_sys::alu_base
+{
+    template <class F>
+    __SP1_HOSTDEV__ void event_to_row(const BaseAluEvent<F> &event, BaseAluValueCols<F> &cols)
+    {
+        cols.vals = event;
+    }
 
-template <class F> __SP1_HOSTDEV__ void instr_to_row(
-    const BaseAluInstr<F> &instr,
-    BaseAluAccessCols<F> &access) {
-    access.addrs = instr.addrs;
-    access.is_add = F(0);
-    access.is_sub = F(0);
-    access.is_mul = F(0);
-    access.is_div = F(0);
-    access.mult = instr.mult;
+    template <class F>
+    __SP1_HOSTDEV__ void instr_to_row(
+        const BaseAluInstr<F> &instr,
+        BaseAluAccessCols<F> &access)
+    {
+        access.addrs = instr.addrs;
+        access.is_add = F(0);
+        access.is_sub = F(0);
+        access.is_mul = F(0);
+        access.is_div = F(0);
+        access.mult = instr.mult;
 
-    switch (instr.opcode) {
+        switch (instr.opcode)
+        {
         case BaseAluOpcode::AddF:
             access.is_add = F(1);
             break;
@@ -30,6 +36,6 @@ template <class F> __SP1_HOSTDEV__ void instr_to_row(
         case BaseAluOpcode::DivF:
             access.is_div = F(1);
             break;
+        }
     }
-}
 } // namespace sp1_recursion_core_sys::alu_base
--- crates/recursion/core/include/prelude.hpp
+++ crates/recursion/core/include/prelude.hpp
@@ -3,21 +3,23 @@
 #include "sp1-recursion-core-sys-cbindgen.hpp"
 
 #ifndef __CUDACC__
-    #define __SP1_HOSTDEV__
-    #define __SP1_INLINE__ inline
-    #include <array>
+#define __SP1_HOSTDEV__
+#define __SP1_INLINE__ inline
+#include <array>
 
-namespace sp1_recursion_core_sys {
-template<class T, std::size_t N>
-using array_t = std::array<T, N>;
-}  // namespace sp1
+namespace sp1_recursion_core_sys
+{
+    template <class T, std::size_t N>
+    using array_t = std::array<T, N>;
+} // namespace sp1
 #else
-    #define __SP1_HOSTDEV__ __host__ __device__
-    #define __SP1_INLINE__ __forceinline__
-    #include <cuda/std/array>
+#define __SP1_HOSTDEV__ __host__ __device__
+#define __SP1_INLINE__ __forceinline__
+#include <cuda/std/array>
 
-namespace sp1_recursion_core_sys {
-template<class T, std::size_t N>
-using array_t = cuda::std::array<T, N>;
-}  // namespace sp1
+namespace sp1_recursion_core_sys
+{
+    template <class T, std::size_t N>
+    using array_t = cuda::std::array<T, N>;
+} // namespace sp1
 #endif
