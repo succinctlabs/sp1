c81f0ee174a5a961e2f89ed22e15d1d39916eeea
Gautham Elango <gautham.gg@gmail.com>
2024-11-20 04:25:20 -0800

    merge fixes

--- crates/recursion/core/include/poseidon2_skinny.hpp
+++ crates/recursion/core/include/poseidon2_skinny.hpp
@@ -2,15 +2,13 @@
 
 #include "prelude.hpp"
 
-namespace sp1_recursion_core_sys::poseidon2_skinny
-{
-    template <class F>
-    __SP1_HOSTDEV__ void event_to_row(const Poseidon2Event<F> &event, size_t len, Poseidon2<F> *cols)
-    {
-    }
+namespace sp1_recursion_core_sys::poseidon2_skinny {
+template <class F>
+__SP1_HOSTDEV__ void event_to_row(const Poseidon2Event<F>& event, size_t len,
+                                  Poseidon2<F>* cols) {}
 
-    template <class F>
-    __SP1_HOSTDEV__ void instr_to_row(const Poseidon2Instr<F> &instr, size_t i, size_t len, Poseidon2PreprocessedCols<F> *cols)
-    {
-    }
-} // namespace sp1_recursion_core_sys::poseidon2_skinny
+template <class F>
+__SP1_HOSTDEV__ void instr_to_row(const Poseidon2Instr<F>& instr, size_t i,
+                                  size_t len,
+                                  Poseidon2PreprocessedCols<F>* cols) {}
+}  // namespace sp1_recursion_core_sys::poseidon2_skinny
--- crates/recursion/core/include/poseidon2_wide.hpp
+++ crates/recursion/core/include/poseidon2_wide.hpp
@@ -2,26 +2,22 @@
 
 #include "prelude.hpp"
 
-namespace sp1_recursion_core_sys::poseidon2_wide
-{
-    template <class T>
-    __SP1_HOSTDEV__ void event_to_row(
-        const T (&input)[WIDTH],
-        T (*external_rounds_state)[WIDTH],
-        T internal_rounds_state[WIDTH],
-        T internal_rounds_s0[NUM_INTERNAL_ROUNDS - 1],
-        T external_sbox[WIDTH][NUM_EXTERNAL_ROUNDS],
-        T internal_sbox[NUM_INTERNAL_ROUNDS],
-        T output_state[WIDTH])
-    {
-        for (size_t i = 0; i < WIDTH; i++)
-        {
-            external_rounds_state[0][i] = input[i];
-        }
-    }
+namespace sp1_recursion_core_sys::poseidon2_wide {
+template <class T>
+__SP1_HOSTDEV__ void event_to_row(const T (&input)[WIDTH],
+                                  T (*external_rounds_state)[WIDTH],
+                                  T internal_rounds_state[WIDTH],
+                                  T internal_rounds_s0[NUM_INTERNAL_ROUNDS - 1],
+                                  T external_sbox[WIDTH][NUM_EXTERNAL_ROUNDS],
+                                  T internal_sbox[NUM_INTERNAL_ROUNDS],
+                                  T output_state[WIDTH]) {
+  for (size_t i = 0; i < WIDTH; i++) {
+    external_rounds_state[0][i] = input[i];
+  }
+}
 
-    template <class T>
-    __SP1_HOSTDEV__ void instr_to_row(const Poseidon2SkinnyInstr<T> &instr, size_t len, Poseidon2PreprocessedCols<T> *cols)
-    {
-    }
-} // namespace sp1_recursion_core_sys::poseidon2_wide
+template <class T>
+__SP1_HOSTDEV__ void instr_to_row(const Poseidon2SkinnyInstr<T>& instr,
+                                  size_t len,
+                                  Poseidon2PreprocessedCols<T>* cols) {}
+}  // namespace sp1_recursion_core_sys::poseidon2_wide
