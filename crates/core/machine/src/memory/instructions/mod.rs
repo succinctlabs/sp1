dab1ea6a9645bc000cd58b55be53ff738ce81333
Kevin Jue <kjue235@gmail.com>
2024-12-13 12:38:12 -0800

    refactor of memory failure test casees

--- crates/core/machine/src/memory/instructions/mod.rs
+++ crates/core/machine/src/memory/instructions/mod.rs
@@ -35,44 +35,42 @@ mod tests {
         utils::run_malicious_test,
     };
 
-    #[test]
-    fn test_malicious_sw() {
-        let instructions = vec![
-            Instruction::new(Opcode::ADD, 29, 0, 5, false, true), // Set the stored value to 5.
-            Instruction::new(Opcode::ADD, 30, 0, 100, false, true), // Set the address to 100.
-            Instruction::new(Opcode::SW, 29, 30, 0, false, true),
-        ];
-        let program = Program::new(instructions, 0, 0);
-        let stdin = SP1Stdin::new();
+    enum FailureType {
+        ConstraintsFailing,
+        CumulativeSumFailing,
+    }
 
-        type P = CpuProver<BabyBearPoseidon2, RiscvAir<BabyBear>>;
-
-        let malicious_trace_pv_generator =
-            |prover: &P,
-             record: &mut ExecutionRecord|
-             -> Vec<(String, RowMajorMatrix<Val<BabyBearPoseidon2>>)> {
-                // Create a malicious record where the incorrect value is saved to memory.
-                let mut malicious_record = record.clone();
-                if let MemoryRecordEnum::Write(mem_write_record) =
-                    &mut malicious_record.memory_instr_events[0].mem_access
-                {
-                    mem_write_record.value = 8;
-                }
-                prover.generate_traces(&malicious_record)
-            };
-
-        let result =
-            run_malicious_test::<P>(program, stdin, Box::new(malicious_trace_pv_generator));
-        assert!(result.is_err() && result.unwrap_err().is_constraints_failing());
+    struct TestCase {
+        opcode: Opcode,
+        incorrect_value: u32,
+        failure_type: FailureType,
     }
 
     #[test]
-    fn test_malicious_sh_sb() {
-        for opcode in [Opcode::SH, Opcode::SB] {
+    fn test_malicious_stores() {
+        let test_cases = vec![
+            TestCase {
+                opcode: Opcode::SW,
+                incorrect_value: 8,
+                failure_type: FailureType::ConstraintsFailing,
+            }, // The correct value is 0xDEADBEEF.
+            TestCase {
+                opcode: Opcode::SH,
+                incorrect_value: 0xDEADBEEF,
+                failure_type: FailureType::ConstraintsFailing,
+            }, // The correct value is 0xBEEF.
+            TestCase {
+                opcode: Opcode::SB,
+                incorrect_value: 0xDEADBEEF,
+                failure_type: FailureType::ConstraintsFailing,
+            }, // The correct value is 0xEF.
+        ];
+
+        for test_case in test_cases {
             let instructions = vec![
-                Instruction::new(Opcode::ADD, 29, 0, 0xDEADBEEF, false, true), // Set the stored value to 0xDEADBEEF.
+                Instruction::new(Opcode::ADD, 29, 0, 0xDEADBEEF, false, true), // Set the stored value to 5.
                 Instruction::new(Opcode::ADD, 30, 0, 100, false, true), // Set the address to 100.
-                Instruction::new(opcode, 29, 30, 0, false, true),
+                Instruction::new(test_case.opcode, 29, 30, 0, false, true),
             ];
             let program = Program::new(instructions, 0, 0);
             let stdin = SP1Stdin::new();
@@ -80,63 +78,71 @@ mod tests {
             type P = CpuProver<BabyBearPoseidon2, RiscvAir<BabyBear>>;
 
             let malicious_trace_pv_generator =
-                |prover: &P,
-                 record: &mut ExecutionRecord|
-                 -> Vec<(String, RowMajorMatrix<Val<BabyBearPoseidon2>>)> {
-                    // Create a malicious record where the full word is saved to memory.
-                    // The correct memory value is 0xBEEF for SH and 0xEF for SB.
+                move |prover: &P,
+                      record: &mut ExecutionRecord|
+                      -> Vec<(String, RowMajorMatrix<Val<BabyBearPoseidon2>>)> {
+                    // Create a malicious record where the incorrect value is saved to memory.
                     let mut malicious_record = record.clone();
                     if let MemoryRecordEnum::Write(mem_write_record) =
                         &mut malicious_record.memory_instr_events[0].mem_access
                     {
-                        mem_write_record.value = 0xDEADBEEF;
+                        mem_write_record.value = test_case.incorrect_value;
                     }
                     prover.generate_traces(&malicious_record)
                 };
 
             let result =
                 run_malicious_test::<P>(program, stdin, Box::new(malicious_trace_pv_generator));
-            assert!(result.is_err() && result.unwrap_err().is_constraints_failing());
+
+            match test_case.failure_type {
+                FailureType::ConstraintsFailing => {
+                    assert!(result.is_err() && result.unwrap_err().is_constraints_failing());
+                }
+                FailureType::CumulativeSumFailing => {
+                    assert!(
+                        result.is_err() && result.unwrap_err().is_local_cumulative_sum_failing()
+                    );
+                }
+            }
         }
     }
 
     #[test]
-    fn test_malicious_lw() {
-        let instructions = vec![
-            Instruction::new(Opcode::ADD, 29, 0, 5, false, true), // Set the stored value to 5.
-            Instruction::new(Opcode::ADD, 30, 0, 100, false, true), // Set the address to 100.
-            Instruction::new(Opcode::SW, 29, 30, 0, false, true), // Store the value to memory.
-            Instruction::new(Opcode::LW, 25, 30, 0, false, true), // Load the value from memory.
+    fn test_malicious_loads() {
+        let test_cases = vec![
+            TestCase {
+                opcode: Opcode::LW,
+                incorrect_value: 8,
+                failure_type: FailureType::ConstraintsFailing,
+            }, // The correct value is 0xDEADBEEF.
+            TestCase {
+                opcode: Opcode::LH,
+                incorrect_value: 0xDEADBEEF,
+                failure_type: FailureType::CumulativeSumFailing,
+            }, // The correct value is 0xFFFFBEEF.
+            TestCase {
+                opcode: Opcode::LHU,
+                incorrect_value: 0xDEADBEEF,
+                failure_type: FailureType::ConstraintsFailing,
+            }, // The correct value is 0xBEEF.
+            TestCase {
+                opcode: Opcode::LB,
+                incorrect_value: 0xDEADBEEF,
+                failure_type: FailureType::CumulativeSumFailing,
+            }, // The correct value is 0xEF.
+            TestCase {
+                opcode: Opcode::LBU,
+                incorrect_value: 0xDEADBEEF,
+                failure_type: FailureType::ConstraintsFailing,
+            }, // The correct value is 0xFFFFEF.
         ];
-        let program = Program::new(instructions, 0, 0);
-        let stdin = SP1Stdin::new();
 
-        type P = CpuProver<BabyBearPoseidon2, RiscvAir<BabyBear>>;
-
-        let malicious_trace_pv_generator =
-            |prover: &P,
-             record: &mut ExecutionRecord|
-             -> Vec<(String, RowMajorMatrix<Val<BabyBearPoseidon2>>)> {
-                // Create a malicious record where the incorrect value is loaded from memory.
-                let mut malicious_record = record.clone();
-                malicious_record.cpu_events[3].a = 8;
-                malicious_record.memory_instr_events[1].a = 8;
-                prover.generate_traces(&malicious_record)
-            };
-
-        let result =
-            run_malicious_test::<P>(program, stdin, Box::new(malicious_trace_pv_generator));
-        assert!(result.is_err() && result.unwrap_err().is_constraints_failing());
-    }
-
-    #[test]
-    fn test_malicious_lh_lb_lhu_lbu() {
-        for opcode in [Opcode::LH, Opcode::LB, Opcode::LHU, Opcode::LBU] {
+        for test_case in test_cases {
             let instructions = vec![
-                Instruction::new(Opcode::ADD, 29, 0, 0xDEADBEEF, false, true), // Set the stored value to 5.
+                Instruction::new(Opcode::ADD, 29, 0, 0xDEADBEEF, false, true), // Set the stored value to 0xDEADBEEF.
                 Instruction::new(Opcode::ADD, 30, 0, 100, false, true), // Set the address to 100.
                 Instruction::new(Opcode::SW, 29, 30, 0, false, true), // Store the value to memory.
-                Instruction::new(opcode, 25, 30, 0, false, true),     // Load the value from memory.
+                Instruction::new(test_case.opcode, 25, 30, 0, false, true), // Load the value from memory.
             ];
             let program = Program::new(instructions, 0, 0);
             let stdin = SP1Stdin::new();
@@ -144,28 +150,28 @@ mod tests {
             type P = CpuProver<BabyBearPoseidon2, RiscvAir<BabyBear>>;
 
             let malicious_trace_pv_generator =
-                |prover: &P,
-                 record: &mut ExecutionRecord|
-                 -> Vec<(String, RowMajorMatrix<Val<BabyBearPoseidon2>>)> {
+                move |prover: &P,
+                      record: &mut ExecutionRecord|
+                      -> Vec<(String, RowMajorMatrix<Val<BabyBearPoseidon2>>)> {
                     // Create a malicious record where the incorrect value is loaded from memory.
-                    // The correct `a` value is 0xFFFFBEEF for LH, 0xFFFFFEF for LB, 0xBEEF for LHU, 0xEF for LBU.
                     let mut malicious_record = record.clone();
-                    malicious_record.cpu_events[3].a = 0xDEADBEEF;
-                    malicious_record.memory_instr_events[1].a = 0xDEADBEEF;
+                    malicious_record.cpu_events[3].a = test_case.incorrect_value;
+                    malicious_record.memory_instr_events[1].a = test_case.incorrect_value;
                     prover.generate_traces(&malicious_record)
                 };
 
             let result =
                 run_malicious_test::<P>(program, stdin, Box::new(malicious_trace_pv_generator));
 
-            match opcode {
-                Opcode::LH | Opcode::LB => assert!(
-                    result.is_err() && result.unwrap_err().is_local_cumulative_sum_failing()
-                ),
-                Opcode::LHU | Opcode::LBU => {
-                    assert!(result.is_err() && result.unwrap_err().is_constraints_failing())
+            match test_case.failure_type {
+                FailureType::ConstraintsFailing => {
+                    assert!(result.is_err() && result.unwrap_err().is_constraints_failing());
                 }
-                _ => unreachable!(),
+                FailureType::CumulativeSumFailing => {
+                    assert!(
+                        result.is_err() && result.unwrap_err().is_local_cumulative_sum_failing()
+                    );
+                }
             }
         }
     }
