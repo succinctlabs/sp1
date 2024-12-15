92ae76f00b086072920a516439cf9b1e589c8f20
Bill <web3_jack@qq.com>
2024-12-15 15:06:48 +0800

    Update mod.rs

--- crates/core/executor/src/syscalls/mod.rs
+++ crates/core/executor/src/syscalls/mod.rs
@@ -81,149 +81,53 @@ pub trait Syscall: Send + Sync {
 pub fn default_syscall_map() -> HashMap<SyscallCode, Arc<dyn Syscall>> {
     let mut syscall_map = HashMap::<SyscallCode, Arc<dyn Syscall>>::default();
 
-    syscall_map.insert(SyscallCode::HALT, Arc::new(HaltSyscall));
+    // Use an array to store syscall tuples to simplify insertion
+    let syscalls: &[(SyscallCode, Arc<dyn Syscall>)] = &[
+        (SyscallCode::HALT, Arc::new(HaltSyscall)),
+        (SyscallCode::SHA_EXTEND, Arc::new(Sha256ExtendSyscall)),
+        (SyscallCode::SHA_COMPRESS, Arc::new(Sha256CompressSyscall)),
+        (SyscallCode::ED_ADD, Arc::new(EdwardsAddAssignSyscall::<Ed25519>::new())),
+        (SyscallCode::ED_DECOMPRESS, Arc::new(EdwardsDecompressSyscall::<Ed25519Parameters>::new())),
+        (SyscallCode::KECCAK_PERMUTE, Arc::new(Keccak256PermuteSyscall)),
+        (SyscallCode::SECP256K1_ADD, Arc::new(WeierstrassAddAssignSyscall::<Secp256k1>::new())),
+        (SyscallCode::SECP256K1_DOUBLE, Arc::new(WeierstrassDoubleAssignSyscall::<Secp256k1>::new())),
+        (SyscallCode::SECP256K1_DECOMPRESS, Arc::new(WeierstrassDecompressSyscall::<Secp256k1>::new())),
+        (SyscallCode::SECP256R1_ADD, Arc::new(WeierstrassAddAssignSyscall::<Secp256r1>::new())),
+        (SyscallCode::SECP256R1_DOUBLE, Arc::new(WeierstrassDoubleAssignSyscall::<Secp256r1>::new())),
+        (SyscallCode::SECP256R1_DECOMPRESS, Arc::new(WeierstrassDecompressSyscall::<Secp256r1>::new())),
+        (SyscallCode::BN254_ADD, Arc::new(WeierstrassAddAssignSyscall::<Bn254>::new())),
+        (SyscallCode::BN254_DOUBLE, Arc::new(WeierstrassDoubleAssignSyscall::<Bn254>::new())),
+        (SyscallCode::BLS12381_ADD, Arc::new(WeierstrassAddAssignSyscall::<Bls12381>::new())),
+        (SyscallCode::BLS12381_DOUBLE, Arc::new(WeierstrassDoubleAssignSyscall::<Bls12381>::new())),
+        (SyscallCode::UINT256_MUL, Arc::new(Uint256MulSyscall)),
+        (SyscallCode::U256XU2048_MUL, Arc::new(U256xU2048MulSyscall)),
+        (SyscallCode::BLS12381_FP_ADD, Arc::new(FpOpSyscall::<Bls12381BaseField>::new(FieldOperation::Add))),
+        (SyscallCode::BLS12381_FP_SUB, Arc::new(FpOpSyscall::<Bls12381BaseField>::new(FieldOperation::Sub))),
+        (SyscallCode::BLS12381_FP_MUL, Arc::new(FpOpSyscall::<Bls12381BaseField>::new(FieldOperation::Mul))),
+        (SyscallCode::BLS12381_FP2_ADD, Arc::new(Fp2AddSubSyscall::<Bls12381BaseField>::new(FieldOperation::Add))),
+        (SyscallCode::BLS12381_FP2_SUB, Arc::new(Fp2AddSubSyscall::<Bls12381BaseField>::new(FieldOperation::Sub))),
+        (SyscallCode::BLS12381_FP2_MUL, Arc::new(Fp2MulSyscall::<Bls12381BaseField>::new())),
+        (SyscallCode::BN254_FP_ADD, Arc::new(FpOpSyscall::<Bn254BaseField>::new(FieldOperation::Add))),
+        (SyscallCode::BN254_FP_SUB, Arc::new(FpOpSyscall::<Bn254BaseField>::new(FieldOperation::Sub))),
+        (SyscallCode::BN254_FP_MUL, Arc::new(FpOpSyscall::<Bn254BaseField>::new(FieldOperation::Mul))),
+        (SyscallCode::BN254_FP2_ADD, Arc::new(Fp2AddSubSyscall::<Bn254BaseField>::new(FieldOperation::Add))),
+        (SyscallCode::BN254_FP2_SUB, Arc::new(Fp2AddSubSyscall::<Bn254BaseField>::new(FieldOperation::Sub))),
+        (SyscallCode::BN254_FP2_MUL, Arc::new(Fp2MulSyscall::<Bn254BaseField>::new())),
+        (SyscallCode::ENTER_UNCONSTRAINED, Arc::new(EnterUnconstrainedSyscall)),
+        (SyscallCode::EXIT_UNCONSTRAINED, Arc::new(ExitUnconstrainedSyscall)),
+        (SyscallCode::WRITE, Arc::new(WriteSyscall)),
+        (SyscallCode::COMMIT, Arc::new(CommitSyscall)),
+        (SyscallCode::COMMIT_DEFERRED_PROOFS, Arc::new(CommitDeferredSyscall)),
+        (SyscallCode::VERIFY_SP1_PROOF, Arc::new(VerifySyscall)),
+        (SyscallCode::HINT_LEN, Arc::new(HintLenSyscall)),
+        (SyscallCode::HINT_READ, Arc::new(HintReadSyscall)),
+        (SyscallCode::BLS12381_DECOMPRESS, Arc::new(WeierstrassDecompressSyscall::<Bls12381>::new())),
+    ];
 
-    syscall_map.insert(SyscallCode::SHA_EXTEND, Arc::new(Sha256ExtendSyscall));
-
-    syscall_map.insert(SyscallCode::SHA_COMPRESS, Arc::new(Sha256CompressSyscall));
-
-    syscall_map.insert(SyscallCode::ED_ADD, Arc::new(EdwardsAddAssignSyscall::<Ed25519>::new()));
-
-    syscall_map.insert(
-        SyscallCode::ED_DECOMPRESS,
-        Arc::new(EdwardsDecompressSyscall::<Ed25519Parameters>::new()),
-    );
-
-    syscall_map.insert(SyscallCode::KECCAK_PERMUTE, Arc::new(Keccak256PermuteSyscall));
-
-    syscall_map.insert(
-        SyscallCode::SECP256K1_ADD,
-        Arc::new(WeierstrassAddAssignSyscall::<Secp256k1>::new()),
-    );
-
-    syscall_map.insert(
-        SyscallCode::SECP256K1_DOUBLE,
-        Arc::new(WeierstrassDoubleAssignSyscall::<Secp256k1>::new()),
-    );
-
-    syscall_map.insert(
-        SyscallCode::SECP256K1_DECOMPRESS,
-        Arc::new(WeierstrassDecompressSyscall::<Secp256k1>::new()),
-    );
-
-    syscall_map.insert(
-        SyscallCode::SECP256R1_ADD,
-        Arc::new(WeierstrassAddAssignSyscall::<Secp256r1>::new()),
-    );
-
-    syscall_map.insert(
-        SyscallCode::SECP256R1_DOUBLE,
-        Arc::new(WeierstrassDoubleAssignSyscall::<Secp256r1>::new()),
-    );
-
-    syscall_map.insert(
-        SyscallCode::SECP256R1_DECOMPRESS,
-        Arc::new(WeierstrassDecompressSyscall::<Secp256r1>::new()),
-    );
-
-    syscall_map
-        .insert(SyscallCode::BN254_ADD, Arc::new(WeierstrassAddAssignSyscall::<Bn254>::new()));
-
-    syscall_map.insert(
-        SyscallCode::BN254_DOUBLE,
-        Arc::new(WeierstrassDoubleAssignSyscall::<Bn254>::new()),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BLS12381_ADD,
-        Arc::new(WeierstrassAddAssignSyscall::<Bls12381>::new()),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BLS12381_DOUBLE,
-        Arc::new(WeierstrassDoubleAssignSyscall::<Bls12381>::new()),
-    );
-
-    syscall_map.insert(SyscallCode::UINT256_MUL, Arc::new(Uint256MulSyscall));
-
-    syscall_map.insert(SyscallCode::U256XU2048_MUL, Arc::new(U256xU2048MulSyscall));
-
-    syscall_map.insert(
-        SyscallCode::BLS12381_FP_ADD,
-        Arc::new(FpOpSyscall::<Bls12381BaseField>::new(FieldOperation::Add)),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BLS12381_FP_SUB,
-        Arc::new(FpOpSyscall::<Bls12381BaseField>::new(FieldOperation::Sub)),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BLS12381_FP_MUL,
-        Arc::new(FpOpSyscall::<Bls12381BaseField>::new(FieldOperation::Mul)),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BLS12381_FP2_ADD,
-        Arc::new(Fp2AddSubSyscall::<Bls12381BaseField>::new(FieldOperation::Add)),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BLS12381_FP2_SUB,
-        Arc::new(Fp2AddSubSyscall::<Bls12381BaseField>::new(FieldOperation::Sub)),
-    );
-
-    syscall_map
-        .insert(SyscallCode::BLS12381_FP2_MUL, Arc::new(Fp2MulSyscall::<Bls12381BaseField>::new()));
-
-    syscall_map.insert(
-        SyscallCode::BN254_FP_ADD,
-        Arc::new(FpOpSyscall::<Bn254BaseField>::new(FieldOperation::Add)),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BN254_FP_SUB,
-        Arc::new(FpOpSyscall::<Bn254BaseField>::new(FieldOperation::Sub)),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BN254_FP_MUL,
-        Arc::new(FpOpSyscall::<Bn254BaseField>::new(FieldOperation::Mul)),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BN254_FP2_ADD,
-        Arc::new(Fp2AddSubSyscall::<Bn254BaseField>::new(FieldOperation::Add)),
-    );
-
-    syscall_map.insert(
-        SyscallCode::BN254_FP2_SUB,
-        Arc::new(Fp2AddSubSyscall::<Bn254BaseField>::new(FieldOperation::Sub)),
-    );
-
-    syscall_map
-        .insert(SyscallCode::BN254_FP2_MUL, Arc::new(Fp2MulSyscall::<Bn254BaseField>::new()));
-
-    syscall_map.insert(SyscallCode::ENTER_UNCONSTRAINED, Arc::new(EnterUnconstrainedSyscall));
-
-    syscall_map.insert(SyscallCode::EXIT_UNCONSTRAINED, Arc::new(ExitUnconstrainedSyscall));
-
-    syscall_map.insert(SyscallCode::WRITE, Arc::new(WriteSyscall));
-
-    syscall_map.insert(SyscallCode::COMMIT, Arc::new(CommitSyscall));
-
-    syscall_map.insert(SyscallCode::COMMIT_DEFERRED_PROOFS, Arc::new(CommitDeferredSyscall));
-
-    syscall_map.insert(SyscallCode::VERIFY_SP1_PROOF, Arc::new(VerifySyscall));
-
-    syscall_map.insert(SyscallCode::HINT_LEN, Arc::new(HintLenSyscall));
-
-    syscall_map.insert(SyscallCode::HINT_READ, Arc::new(HintReadSyscall));
-
-    syscall_map.insert(
-        SyscallCode::BLS12381_DECOMPRESS,
-        Arc::new(WeierstrassDecompressSyscall::<Bls12381>::new()),
-    );
+    // Iterate over the array and insert syscalls
+    for (code, syscall) in syscalls {
+        syscall_map.insert(*code, syscall.clone());
+    }
 
     syscall_map
 }
