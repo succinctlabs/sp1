db67ebb2a9039936e7a3e95816112ab9717ef8f4
rkm0959 <rkm0959@gmail.com>
2024-12-24 17:38:50 +0000

    add non-canonical test

--- patch-testing/curve25519-dalek/src/lib.rs
+++ patch-testing/curve25519-dalek/src/lib.rs
@@ -1,17 +1,19 @@
 #[sp1_test::sp1_test("curve25519_decompress")]
-fn test_decompressed_expected_value(stdin: &mut sp1_sdk::SP1Stdin) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
+fn test_decompressed_expected_value(
+    stdin: &mut sp1_sdk::SP1Stdin,
+) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
     use curve25519_dalek::edwards::CompressedEdwardsY;
     use curve25519_dalek::edwards::EdwardsPoint;
     use rand::distributions::Distribution;
     use rand::distributions::WeightedIndex;
-    use sp1_test::DEFAULT_CORPUS_COUNT; 
+    use sp1_test::DEFAULT_CORPUS_COUNT;
 
     let dist = rand::distributions::WeightedIndex::new([9_usize, 1]).unwrap();
 
     /// Flips a bit in the compressed point with probability 0.1.
     ///
     /// Returns true if a bit was flipped. With probablity .5 this is not a valid compressed point.
-    fn bork_point(compressed: &mut CompressedEdwardsY, dist: &WeightedIndex<usize>) -> bool { 
+    fn bork_point(compressed: &mut CompressedEdwardsY, dist: &WeightedIndex<usize>) -> bool {
         if dist.sample(&mut rand::thread_rng()) == 1 {
             let bit = 1 << 2;
             compressed.0[0] ^= bit;
@@ -21,7 +23,7 @@ fn test_decompressed_expected_value(stdin: &mut sp1_sdk::SP1Stdin) -> impl FnOnc
 
         false
     }
-    
+
     let how_many_points = DEFAULT_CORPUS_COUNT as usize;
     stdin.write(&how_many_points);
 
@@ -32,7 +34,7 @@ fn test_decompressed_expected_value(stdin: &mut sp1_sdk::SP1Stdin) -> impl FnOnc
         let mut compressed = rand_point.compress();
 
         if bork_point(&mut compressed, &dist) {
-            // if point has been borked lets just make it cant be decompressed. 
+            // if point has been borked lets just make it cant be decompressed.
             if compressed.decompress().is_some() {
                 continue;
             }
@@ -44,12 +46,52 @@ fn test_decompressed_expected_value(stdin: &mut sp1_sdk::SP1Stdin) -> impl FnOnc
 
         stdin.write(&compressed);
     }
-    
-    assert!(decompress_outputs.iter().any(|x| x.is_none()), "Expected at least one decompressed point to be None");
+
+    assert!(
+        decompress_outputs.iter().any(|x| x.is_none()),
+        "Expected at least one decompressed point to be None"
+    );
 
     move |mut public| {
         for decompressed in decompress_outputs {
-            assert_eq!(decompressed, public.read::<Option<EdwardsPoint>>()); 
+            assert_eq!(decompressed, public.read::<Option<EdwardsPoint>>());
+        }
+    }
+}
+
+#[sp1_test::sp1_test("curve25519_decompress")]
+fn test_decompressed_noncanonical(
+    stdin: &mut sp1_sdk::SP1Stdin,
+) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
+    use curve25519_dalek::edwards::CompressedEdwardsY;
+    use curve25519_dalek::edwards::EdwardsPoint;
+    use rand::distributions::Distribution;
+    use rand::distributions::WeightedIndex;
+    use sp1_test::DEFAULT_CORPUS_COUNT;
+
+    let how_many_points = 1 as usize;
+    stdin.write(&how_many_points);
+
+    let mut decompress_outputs = Vec::new();
+    while decompress_outputs.len() < how_many_points {
+        let mut bytes: [u8; 32] = [0; 32];
+        for i in 0..32 {
+            bytes[i] = 255;
+        }
+        bytes[0] = 253;
+        bytes[31] = 127;
+        let compressed = CompressedEdwardsY(bytes);
+        decompress_outputs.push(compressed.decompress());
+        stdin.write(&compressed);
+    }
+
+    move |mut public| {
+        for decompressed in decompress_outputs {
+            let public_val = public.read::<Option<EdwardsPoint>>();
+            assert!(public_val.is_none());
+            assert!(decompressed.is_some());
+
+            // assert_eq!(decompressed, public_val);
         }
     }
 }
