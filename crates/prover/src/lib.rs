068ec7fb6fcd326eb38a079982bb2cfb767dc643
jtguibas <jtguibas@gmail.com>
2024-12-24 00:27:55 +0000

    verify vk on

--- crates/prover/src/lib.rs
+++ crates/prover/src/lib.rs
@@ -199,7 +199,7 @@ impl<C: SP1ProverComponents> SP1Prover<C> {
             .then_some(RecursionShapeConfig::default());
 
         let vk_verification =
-            env::var("VERIFY_VK").map(|v| v.eq_ignore_ascii_case("true")).unwrap_or(false);
+            env::var("VERIFY_VK").map(|v| v.eq_ignore_ascii_case("true")).unwrap_or(true);
         tracing::info!("vk verification: {}", vk_verification);
 
         // Read the shapes from the shapes directory and deserialize them into memory.
