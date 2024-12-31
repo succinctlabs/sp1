9fc911b79c04c65b7d2263e81891ab4632a19f39
nhtyy <n@push0.dev>
2024-12-30 17:25:40 -0800

    tests: bls sqrt coverage

--- patch-testing/bls12-381/program/bin/base.rs
+++
@@ -1,4 +0,0 @@
-#![no_main]
-sp1_zkvm::entrypoint!(main);
-
-pub fn main() {}
--- patch-testing/bls12-381/src/lib.rs
+++ patch-testing/bls12-381/src/lib.rs
@@ -1,10 +1,36 @@
-#[sp1_test::sp1_test("bls12_381_test", gpu, prove)]
+
+#[sp1_test::sp1_test("bls12_381_test_sqrt", gpu, prove)]
 pub fn test_verify_rand_lte_100(
     stdin: &mut sp1_sdk::SP1Stdin,
 ) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
-    let _ = stdin;
+    use bls12_381::fp::Fp;
 
-    |_| {
+    let times: u8 = 100; 
+    stdin.write(&times);
 
+    let mut unpatched_results: Vec<Option<Vec<u8>>> = Vec::new();
+
+    while unpatched_results.len() < times as usize {
+        let rand_bytes = rand::random::<[u8; 48]>();
+        let Some(rand) = Fp::from_bytes(&rand_bytes).into_option() else {
+            continue;
+        };
+        
+        stdin.write(&rand_bytes.to_vec());
+
+        let sqrt_bytes = rand.sqrt().into_option().map(|v| v.to_bytes().to_vec());
+
+        unpatched_results.push(sqrt_bytes);
+    }
+
+
+    |mut public| {
+        for res in unpatched_results {
+            let zk_res = public.read::<Option<Vec<u8>>>();
+
+            assert_eq!(res, zk_res);
+        }
     }
 }
+
+
