fd6609b01b0079169484eb8830840756b9f3dfe2
N <n@push0.dev>
2025-01-08 21:35:14 -0800

    chore: bump rc.9 (#206)
    * chore: bump rc.9
    
    * fix: workflow executed script sourced
    
    * fix: explicit exit
    
    * fix: try seperating step
    
    * bash insanity

--- .github/scripts/pkg-checks.sh
+++ .github/scripts/pkg-checks.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -x
+
 # Move into the workspace root
 pushd "$(git rev-parse --show-toplevel)" || exit 1
 
+# Initialize error flag
+error_occurred=0
+
 # Loop over all the packages in the workspace
-cargo metadata --format-version=1 --no-deps | \
-jq -c '.packages[] | {publish: .publish, manifest_path: .manifest_path}' | \
 while IFS= read -r pkg_json; do
     echo "pkg_json: $pkg_json"
 
@@ -14,7 +17,6 @@ while IFS= read -r pkg_json; do
     manifest_path=$(echo "$pkg_json" | jq -r '.manifest_path')
     
     # Skip if the package is marked as not published
-    # Note: `publish` is null if true. So we corece to empty if null.
     if [ -n "$publish" ]; then
         echo "Skipping unpublished package at $manifest_path"
         continue
@@ -29,22 +31,20 @@ while IFS= read -r pkg_json; do
     # Capture the stdin/stdout of `cargo package`
     package_output=$(cargo package 2>&1)
 
-    # Check if the output mentions `Cargo.lock`
-    # If it does, then we skip this package but still print the whole output just in case.
-    # This can happen in cbindgen builds, and does not cause an issue for the release.
+    # Check if cargo package failed
     if [ $? -ne 0 ]; then
         echo "Error: Packaging failed"
         echo "$package_output"
 
-        if ! echo "$package_output" | grep -q "Cargo.lock"; then
-            echo "$package_output"
+        if echo "$package_output" | grep -iq "cargo.lock"; then
+            echo "SP1: Only Cargo.lock was modified, this is fine."
+        else
+            echo "Error not related to Cargo.lock, marking as failed."
             exit 1
         fi
-
-        echo "SP1: Only Cargo.lock was modified, this is fine."
     fi
 
-    popd || exit 1
-done
+    popd
+done < <(cargo metadata --format-version=1 --no-deps | jq -c '.packages[] | {publish: .publish, manifest_path: .manifest_path}')
 
-popd || exit 1
+popd
