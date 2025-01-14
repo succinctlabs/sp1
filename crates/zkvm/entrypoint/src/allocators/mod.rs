209669eb375314d3419e4574bde1e43ff818d338
N <n@push0.dev>
2025-01-13 22:08:10 -0800

    fix: embedded allocator takes precedence (#222)
    * fix: embedded allocator should take precedence if enabled
    
    * fix: example
    
    * fix: add period

--- crates/zkvm/entrypoint/src/allocators/mod.rs
+++ crates/zkvm/entrypoint/src/allocators/mod.rs
@@ -1,4 +1,8 @@
-#[cfg(feature = "bump")]
+//! Allocators for the SP1 zkVM.
+//!
+//! The `embedded` allocator takes precedence if enabled.
+
+#[cfg(all(feature = "bump", not(feature = "embedded")))]
 mod bump;
 
 #[cfg(feature = "embedded")]
