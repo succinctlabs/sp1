cfaf84227fdc8f8469990d1b453a45fafd22eb9f
nhtyy <n@push0.dev>
2025-02-03 12:44:55 -0800

    feat: elliptic-curve crate affine points

---
+++ crates/zkvm/lib/src/ecdsa.rs
@@ -0,0 +1,74 @@
+use crate::utils::AffinePoint as SP1AffinePointTrait;
+
+use elliptic_curve::{
+    ff, generic_array::typenum::consts::U32, subtle::CtOption, CurveArithmetic, FieldBytes,
+    PrimeField,
+};
+use std::ops::Neg;
+
+pub mod affine;
+pub use affine::AffinePoint;
+
+pub mod projective;
+pub use projective::ProjectivePoint;
+
+pub mod scalar;
+pub use scalar::Scalar;
+
+/// NOTE: Currently, the only supported ECDSA curves are secp256k1 and secp256r1.
+/// These both have 16 limbs in their field elements.
+const FIELD_LIMBS: usize = 16;
+const FIELD_BYTES_SIZE_USIZE: usize = 32;
+#[allow(non_camel_case_types)]
+type FIELD_BYTES_SIZE = U32;
+
+/// A weisertstrass curve for ECDSA.
+/// Note: This trait is only implemented for 32 byte curves.
+pub trait ECDSACurve
+where
+    Self: CurveArithmetic<FieldBytesSize = FIELD_BYTES_SIZE, Scalar = Scalar<Self::ScalarImpl>>,
+{
+    type FieldElement: Field<Self> + Neg<Output = Self::FieldElement>;
+
+    type ScalarImpl: PrimeField;
+
+    /// The number of limbs in the field element.
+    ///
+    /// Note: At the moment, the only supported ECDSA curvers are secp256k1 and secp256r1.
+    /// These both have 16 limbs in their field elements.
+    type SP1AffinePoint: ECDSAPoint;
+
+    /// The `a` coefficient in the curve equation.
+    const EQUATION_A: Self::FieldElement;
+
+    /// The `b` coefficient in the curve equation.
+    const EQUATION_B: Self::FieldElement;
+}
+
+/// Alias trait for the [`ff::PrimeField`] with 32 byte field elements.
+pub trait Field<C: ECDSACurve>: ff::PrimeField {
+    /// Create an instance of self from a FieldBytes.
+    fn from_bytes(bytes: &FieldBytes<C>) -> CtOption<Self>;
+
+    /// Convert self to a FieldBytes.
+    ///
+    /// Note: Implentors should ensure these methods normalize first.
+    fn to_bytes(&self) -> FieldBytes<C>;
+}
+
+pub type FieldElement<C> = <C as ECDSACurve>::FieldElement;
+
+/// Alias trait for the [`SP1AffinePointTrait`] with 32 byte field elements.
+pub trait ECDSAPoint:
+    SP1AffinePointTrait<FIELD_LIMBS> + Clone + Copy + std::fmt::Debug + Send + Sync
+{
+    #[inline]
+    fn from(x: &[u8], y: &[u8]) -> Self {
+        <Self as SP1AffinePointTrait<FIELD_LIMBS>>::from(x, y)
+    }
+}
+
+impl<P> ECDSAPoint for P where
+    P: SP1AffinePointTrait<FIELD_LIMBS> + Clone + Copy + std::fmt::Debug + Send + Sync
+{
+}
---
+++ crates/zkvm/lib/src/ecdsa/affine.rs
@@ -0,0 +1,219 @@
+use super::{
+    ECDSACurve, ECDSAPoint, Field, FieldElement, SP1AffinePointTrait, FIELD_BYTES_SIZE_USIZE,
+};
+
+use elliptic_curve::{
+    ff::Field as _,
+    group::GroupEncoding,
+    point::{AffineCoordinates, DecompactPoint, DecompressPoint},
+    sec1::{self, CompressedPoint, EncodedPoint, FromEncodedPoint, ToEncodedPoint},
+    subtle::{Choice, ConditionallySelectable, ConstantTimeEq, CtOption},
+    zeroize::DefaultIsZeroes,
+    FieldBytes, PrimeField,
+};
+use std::ops::Neg;
+
+#[derive(Clone, Copy, Debug)]
+pub struct AffinePoint<C: ECDSACurve> {
+    pub inner: C::SP1AffinePoint,
+}
+
+impl<C: ECDSACurve> AffinePoint<C> {
+    pub fn from_field_elements_unchecked(x: FieldElement<C>, y: FieldElement<C>) -> Self {
+        let mut x_slice = x.to_bytes();
+        let x_slice = x_slice.as_mut_slice();
+        x_slice.reverse();
+
+        let mut y_slice = y.to_bytes();
+        let y_slice = y_slice.as_mut_slice();
+        y_slice.reverse();
+
+        AffinePoint { inner: <C::SP1AffinePoint as ECDSAPoint>::from(x_slice, y_slice) }
+    }
+
+    /// # Panics
+    /// - if the point is the identity point.
+    /// - if we have non canon represntations of the field elements.
+    pub fn field_elements(&self) -> (FieldElement<C>, FieldElement<C>) {
+        if self.is_identity().into() {
+            return (FieldElement::<C>::ZERO, FieldElement::<C>::ZERO);
+        }
+
+        let bytes = self.inner.to_le_bytes();
+
+        let mut x_bytes: [u8; FIELD_BYTES_SIZE_USIZE] =
+            bytes[..FIELD_BYTES_SIZE_USIZE].try_into().unwrap();
+
+        x_bytes.reverse();
+
+        let mut y_bytes: [u8; FIELD_BYTES_SIZE_USIZE] =
+            bytes[FIELD_BYTES_SIZE_USIZE..].try_into().unwrap();
+
+        y_bytes.reverse();
+
+        let x = FieldElement::<C>::from_bytes(&x_bytes.into()).unwrap();
+        let y = FieldElement::<C>::from_bytes(&y_bytes.into()).unwrap();
+        (x, y)
+    }
+
+    pub fn generator() -> Self {
+        AffinePoint { inner: C::SP1AffinePoint::GENERATOR }
+    }
+
+    pub fn identity() -> Self {
+        AffinePoint { inner: C::SP1AffinePoint::identity() }
+    }
+
+    pub fn is_identity(&self) -> Choice {
+        Choice::from(self.inner.is_identity() as u8)
+    }
+}
+
+impl<C: ECDSACurve> FromEncodedPoint<C> for AffinePoint<C> {
+    fn from_encoded_point(point: &EncodedPoint<C>) -> CtOption<Self> {
+        match point.coordinates() {
+            sec1::Coordinates::Identity => CtOption::new(Self::identity(), 1.into()),
+            sec1::Coordinates::Compact { x } => Self::decompact(x),
+            sec1::Coordinates::Compressed { x, y_is_odd } => {
+                AffinePoint::<C>::decompress(x, Choice::from(y_is_odd as u8))
+            }
+            sec1::Coordinates::Uncompressed { x, y } => {
+                let x = FieldElement::<C>::from_bytes(x);
+                let y = FieldElement::<C>::from_bytes(y);
+
+                x.and_then(|x| {
+                    y.and_then(|y| {
+                        // Check that the point is on the curve
+                        let lhs = (y * y).neg();
+                        let rhs = (x * x * x) + (C::EQUATION_A * x) + C::EQUATION_B;
+
+                        let point = Self::from_field_elements_unchecked(x, y);
+
+                        CtOption::new(point, (lhs + rhs).is_zero())
+                    })
+                })
+            }
+        }
+    }
+}
+
+impl<C: ECDSACurve> ToEncodedPoint<C> for AffinePoint<C> {
+    fn to_encoded_point(&self, compress: bool) -> EncodedPoint<C> {
+        // If the point is the identity point, we can just return the identity point.
+        if self.is_identity().into() {
+            return EncodedPoint::<C>::identity();
+        }
+
+        let (x, y) = self.field_elements();
+
+        EncodedPoint::<C>::from_affine_coordinates(&x.to_bytes(), &y.to_bytes(), compress)
+    }
+}
+
+impl<C: ECDSACurve> DecompressPoint<C> for AffinePoint<C> {
+    fn decompress(x_bytes: &FieldBytes<C>, y_is_odd: Choice) -> CtOption<Self> {
+        FieldElement::<C>::from_bytes(x_bytes).and_then(|x| {
+            let alpha = (x * x * x) + (C::EQUATION_A * x) + C::EQUATION_B;
+            let beta = alpha.sqrt();
+
+            beta.map(|beta| {
+                let y = FieldElement::<C>::conditional_select(
+                    &beta.neg(),
+                    &beta,
+                    beta.is_odd().ct_eq(&y_is_odd),
+                );
+
+                AffinePoint::from_field_elements_unchecked(x, y)
+            })
+        })
+    }
+}
+
+impl<C: ECDSACurve> DecompactPoint<C> for AffinePoint<C> {
+    fn decompact(x_bytes: &FieldBytes<C>) -> CtOption<Self> {
+        Self::decompress(x_bytes, Choice::from(0))
+    }
+}
+
+impl<C: ECDSACurve> AffineCoordinates for AffinePoint<C> {
+    type FieldRepr = FieldBytes<C>;
+
+    fn x(&self) -> FieldBytes<C> {
+        let (x, _) = self.field_elements();
+
+        x.to_bytes()
+    }
+
+    fn y_is_odd(&self) -> Choice {
+        let (_, y) = self.field_elements();
+
+        y.is_odd()
+    }
+}
+
+impl<C: ECDSACurve> ConditionallySelectable for AffinePoint<C> {
+    fn conditional_select(a: &Self, b: &Self, choice: Choice) -> Self {
+        // In the vm, we dont care about constant time selection.
+        if choice.into() {
+            *b
+        } else {
+            *a
+        }
+    }
+}
+
+impl<C: ECDSACurve> ConstantTimeEq for AffinePoint<C> {
+    fn ct_eq(&self, other: &Self) -> Choice {
+        let (x1, y1) = self.field_elements();
+        let (x1, y1) = (x1, y1);
+
+        let (x2, y2) = other.field_elements();
+        let (x2, y2) = (x2, y2);
+
+        x1.ct_eq(&x2) & y1.ct_eq(&y2)
+    }
+}
+
+impl<C: ECDSACurve> PartialEq for AffinePoint<C> {
+    fn eq(&self, other: &Self) -> bool {
+        self.ct_eq(other).into()
+    }
+}
+
+impl<C: ECDSACurve> Eq for AffinePoint<C> {}
+
+impl<C: ECDSACurve> Default for AffinePoint<C> {
+    fn default() -> Self {
+        AffinePoint::identity()
+    }
+}
+
+impl<C: ECDSACurve> DefaultIsZeroes for AffinePoint<C> {}
+
+impl<C: ECDSACurve> GroupEncoding for AffinePoint<C> {
+    type Repr = CompressedPoint<C>;
+
+    fn from_bytes(bytes: &Self::Repr) -> CtOption<Self> {
+        EncodedPoint::<C>::from_bytes(bytes)
+            .map(|point| CtOption::new(point, Choice::from(1)))
+            .unwrap_or_else(|_| {
+                // SEC1 identity encoding is technically 1-byte 0x00, but the
+                // `GroupEncoding` API requires a fixed-width `Repr`
+                let is_identity = bytes.ct_eq(&Self::Repr::default());
+                CtOption::new(EncodedPoint::<C>::identity(), is_identity)
+            })
+            .and_then(|point| Self::from_encoded_point(&point))
+    }
+
+    fn from_bytes_unchecked(bytes: &Self::Repr) -> CtOption<Self> {
+        // No unchecked conversion possible for compressed points
+        Self::from_bytes(bytes)
+    }
+
+    fn to_bytes(&self) -> Self::Repr {
+        let encoded = self.to_encoded_point(true);
+        let mut result = CompressedPoint::<C>::default();
+        result[..encoded.len()].copy_from_slice(encoded.as_bytes());
+        result
+    }
+}
