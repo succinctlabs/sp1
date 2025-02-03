797f6177e034c12c4d614db230d7e90dacd92e8d
umadayal <uma.dayal@yahoo.com>
2025-02-03 11:08:17 -0800

    cleanup

--- crates/prover/src/lib.rs
+++ crates/prover/src/lib.rs
@@ -274,13 +274,8 @@ impl<C: SP1ProverComponents> SP1Prover<C> {
         &self,
         elf: &[u8],
     ) -> (SP1ProvingKey, DeviceProvingKey<C>, Program, SP1VerifyingKey) {
-        let setup_start = Instant::now();
         let program = self.get_program(elf).unwrap();
-        tracing::info!("SETUP: Got program in {:?}", setup_start.elapsed());
-
-        let core_setup_start = Instant::now();
         let (pk, vk) = self.core_prover.setup(&program);
-        tracing::info!("SETUP: Core setup completed in {:?}", core_setup_start.elapsed());
 
         let vk = SP1VerifyingKey { vk };
         let pk = SP1ProvingKey {
