a643d0aec85b38b8617d8e567fd34ce2e17635e5
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-02-04 19:21:15 -0800

    chore: remove vergen dep

--- crates/sdk/build.rs
+++ crates/sdk/build.rs
@@ -3,6 +3,4 @@ fn main() {
     if std::env::var("SP1_CI_IN_PROGRESS").is_ok() {
         println!("cargo::rustc-cfg=sp1_ci_in_progress");
     }
-
-    vergen::EmitBuilder::builder().build_timestamp().git_sha(true).emit().unwrap();
 }
