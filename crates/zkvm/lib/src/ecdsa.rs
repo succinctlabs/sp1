a95c384ac41bd18fcda33b3fab319ca2ee1e36fd
nhtyy <n@push0.dev>
2025-02-06 22:31:29 -0800

    fix: no run doc tests

--- crates/zkvm/lib/src/ecdsa.rs
+++ crates/zkvm/lib/src/ecdsa.rs
@@ -103,7 +103,7 @@ where
 /// Required by the [`CurveArithmetic`] trait.
 ///
 /// Note: In current Rust we cannot write:
-/// ```rust, no_run
+/// ```ignore,no_run
 /// impl<C: ECDSACurve> From<Scalar<C>> for C::Uint {
 ///     fn from(scalar: Scalar<C>) -> Self {
 ///         scalar.0.into()
