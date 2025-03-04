507de3dfb1d5c6480265feb6036097aa3560eb9e
Tej Qu Nair <tej.q.nair@gmail.com>
2025-03-03 22:04:03 -0800

    feat: estimate prover gas while executing (#1974)
    Co-authored-by: tamirhemo <tamirhmo@gmail.com>

--- crates/core/machine/src/utils/prove.rs
+++ crates/core/machine/src/utils/prove.rs
@@ -1,5 +1,6 @@
 use p3_matrix::dense::RowMajorMatrix;
 use std::{
+    error::Error,
     fs::File,
     io::{self, Seek, SeekFrom},
     str::FromStr,
@@ -11,9 +12,9 @@ use std::{
 };
 use web_time::Instant;
 
-use crate::riscv::RiscvAir;
 use crate::shape::CoreShapeConfig;
 use crate::utils::test::MaliciousTracePVGeneratorType;
+use crate::{riscv::RiscvAir, shape::Shapeable};
 use p3_maybe_rayon::prelude::*;
 use sp1_stark::MachineProvingKey;
 use sp1_stark::StarkVerifyingKey;
@@ -27,6 +28,7 @@ use crate::{
     utils::{chunk_vec, concurrency::TurnBasedSync},
 };
 use sp1_core_executor::{
+    estimator::RecordEstimator,
     events::{format_table_line, sorted_table_lines},
     ExecutionState, RiscvAirId,
 };
@@ -72,6 +74,7 @@ where
         proof_tx,
         shape_tx,
         malicious_trace_pv_generator,
+        None,
     )?;
 
     let _: Vec<_> = shape_rx.iter().collect();
@@ -93,6 +96,7 @@ pub fn prove_core_stream<SC: StarkGenericConfig, P: MachineProver<SC, RiscvAir<S
     proof_tx: Sender<ShardProof<SC>>,
     shape_and_done_tx: Sender<(OrderedShape, bool)>,
     malicious_trace_pv_generator: Option<MaliciousTracePVGeneratorType<SC::Val, P>>, // This is used for failure test cases that generate malicious traces and public values.
+    gas_calculator: Option<Box<dyn FnOnce(&RecordEstimator) -> Result<u64, Box<dyn Error>> + '_>>,
 ) -> Result<(Vec<u8>, u64), SP1CoreProverError>
 where
     SC::Val: PrimeField32,
@@ -102,7 +106,7 @@ where
     PcsProverData<SC>: Send + Sync,
 {
     // Setup the runtime.
-    let mut runtime = Executor::with_context(program.clone(), opts, context);
+    let mut runtime = Box::new(Executor::with_context(program.clone(), opts, context));
     runtime.maximal_shapes = shape_config.map(|config| {
         config.maximal_core_shapes(opts.shard_size.ilog2() as usize).into_iter().collect()
     });
@@ -110,6 +114,10 @@ where
     for proof in stdin.proofs.iter() {
         let (proof, vk) = proof.clone();
         runtime.write_proof(proof, vk);
+    }
+    // Set the record estimator to collect data for gas calculation.
+    if gas_calculator.is_some() {
+        runtime.record_estimator = Some(Box::default());
     }
 
     #[cfg(feature = "debug")]
@@ -158,7 +166,7 @@ where
 
                         // If we've reached the final checkpoint, break out of the loop.
                         if done {
-                            break Ok(runtime.state.public_values_stream);
+                            break Ok(runtime);
                         }
 
                         // Update the index.
@@ -445,12 +453,30 @@ where
                             .map(|(record, main_traces)| {
                                 let _span = span.enter();
 
-                                let main_data = prover.commit(&record, main_traces);
+                                let shard = record.shard();
+                                let before = Instant::now();
 
-                                let opening_span = tracing::debug_span!("opening").entered();
-                                let proof =
-                                    prover.open(pk, main_data, &mut challenger.clone()).unwrap();
-                                opening_span.exit();
+                                let main_data = tracing::debug_span!("commit", shard)
+                                    .in_scope(|| prover.commit(&record, main_traces));
+
+                                let proof = tracing::debug_span!("opening", shard).in_scope(|| {
+                                    prover.open(pk, main_data, &mut challenger.clone()).unwrap()
+                                });
+
+                                let elapsed = before.elapsed();
+
+                                // Log the shard heights/shape as well as how long it took to prove.
+                                let debug_shapes = record.shape.as_ref().map(|shape| {
+                                    shape
+                                        .iter()
+                                        .filter_map(|(&k, &v)| (v > 0).then_some((k, v)))
+                                        .collect::<Vec<_>>()
+                                });
+                                tracing::debug!(
+                                    "proving shard {shard} took {} ns. shape: {:?}",
+                                    elapsed.as_nanos(),
+                                    debug_shapes
+                                );
 
                                 #[cfg(debug_assertions)]
                                 {
@@ -485,7 +511,9 @@ where
         });
 
         // Wait until the checkpoint generator handle has fully finished.
-        let public_values_stream = checkpoint_generator_handle.join().unwrap().unwrap();
+        let runtime = checkpoint_generator_handle.join().unwrap().unwrap();
+        let gas = gas_calculator.map(|calc| calc(runtime.record_estimator.as_ref().unwrap()));
+        let public_values_stream = runtime.state.public_values_stream;
 
         // Wait until the records and traces have been fully generated for phase 2.
         p2_record_and_trace_gen_handles.into_iter().for_each(|handle| handle.join().unwrap());
@@ -494,13 +522,21 @@ where
         p2_prover_handle.join().unwrap();
 
         // Log some of the `ExecutionReport` information.
-        let report_aggregate = report_aggregate.lock().unwrap();
+        let mut report_aggregate = report_aggregate.lock().unwrap();
         tracing::info!(
             "execution report (totals): total_cycles={}, total_syscall_cycles={}, touched_memory_addresses={}",
             report_aggregate.total_instruction_count(),
             report_aggregate.total_syscall_count(),
             report_aggregate.touched_memory_addresses,
         );
+        match gas {
+            Some(Ok(gas)) => {
+                tracing::info!("execution report (gas): {}", gas);
+                report_aggregate.gas = Some(gas);
+            }
+            Some(Err(err)) => tracing::error!("Encountered error while calculating gas: {}", err),
+            None => (),
+        }
 
         // Print the opcode and syscall count tables like `du`: sorted by count (descending) and
         // with the count in the first column.
