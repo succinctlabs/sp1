02e156da0794d6184e714ecf90a5c926ae66de6b
leruaa <aurelien.catinon@gmail.com>
2025-04-07 13:17:03 -0700

    chore: add explanation for the `-D__ILP32__` flag

--- crates/build/src/command/local.rs
+++ crates/build/src/command/local.rs
@@ -27,6 +27,8 @@ pub(crate) fn create_local_command(
         }
     }
 
+    // the following flag is added to avoid build failure on ring:
+    // https://github.com/briansmith/ring/blob/bcf68dd27a071ff1947b6327d4c6bde526e24b60/include/ring-core/target.h#L47
     command.env("CFLAGS", "-D__ILP32__");
 
     let parsed_version = {
