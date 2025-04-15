e4c8e490c0d0e1305c74dd8ce3e556295f89fac9
Aurélien <3535019+leruaa@users.noreply.github.com>
2025-04-15 15:51:26 +0200

    test: add secp256k1 v0.30.0 patch testing (#2234)

--- patch-testing/secp256k1/build.rs
+++ patch-testing/secp256k1/build.rs
@@ -1,3 +1,12 @@
+use sp1_build::BuildArgs;
+
 fn main() {
-    sp1_build::build_program("./program");
+    sp1_build::build_program_with_args(
+        "./program",
+        BuildArgs { features: vec!["v0-29-1".to_string()], ..Default::default() },
+    );
+    sp1_build::build_program_with_args(
+        "./program",
+        BuildArgs { features: vec!["v0-30-0".to_string()], ..Default::default() },
+    );
 }
--- patch-testing/secp256k1/src/lib.rs
+++ patch-testing/secp256k1/src/lib.rs
@@ -1,82 +1,107 @@
 #[cfg(test)]
-use secp256k1::{Message, PublicKey, Secp256k1};
+mod tests {
+    use secp256k1::{Message, PublicKey, Secp256k1};
 
-#[sp1_test::sp1_test("secp256k1_recover", syscalls = [SECP256K1_DOUBLE, SECP256K1_ADD])]
-fn test_recover_rand_lte_100(
-    stdin: &mut sp1_sdk::SP1Stdin,
-) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
-    let times = 100_u8;
-
-    stdin.write(&times);
-
-    let secp = Secp256k1::new();
-
-    let mut pubkeys = Vec::with_capacity(times.into());
-    for _ in 0..times {
-        let mut rng = rand::thread_rng();
-        let (secret, public) = secp.generate_keypair(&mut rng);
-
-        pubkeys.push(public);
-
-        let msg = rand::random::<[u8; 32]>();
-        let msg = Message::from_digest_slice(&msg).unwrap();
-
-        let signature = secp.sign_ecdsa_recoverable(&msg, &secret);
-
-        // Verify that the unpatched version of this function recovers as expected.
-        assert_eq!(secp.recover_ecdsa(&msg, &signature).unwrap(), public);
-
-        let (recid, sig) = signature.serialize_compact();
-
-        let recid = recid.to_i32();
-
-        stdin.write(&recid);
-        stdin.write(msg.as_ref());
-        stdin.write_slice(sig.as_slice());
+    #[sp1_test::sp1_test("secp256k1_recover", syscalls = [SECP256K1_DOUBLE, SECP256K1_ADD], prove)]
+    fn test_recover_rand_lte_100(
+        stdin: &mut sp1_sdk::SP1Stdin,
+    ) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
+        recover_rand_lte_100(stdin)
     }
 
-    move |mut public| {
-        println!("checking publioc values");
-        for key in pubkeys {
-            assert_eq!(public.read::<Option<PublicKey>>(), Some(key));
-        }
-    }
-}
-
-#[sp1_test::sp1_test("secp256k1_verify", syscalls = [SECP256K1_DOUBLE, SECP256K1_ADD])]
-fn test_verify_rand_lte_100(
-    stdin: &mut sp1_sdk::SP1Stdin,
-) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
-    let times = 100_u8;
-    stdin.write(&times);
-
-    let secp = Secp256k1::new();
-
-    for _ in 0..times {
-        let mut rng = rand::thread_rng();
-        let (secret, public) = secp.generate_keypair(&mut rng);
-
-        let msg = rand::random::<[u8; 32]>();
-        let msg = Message::from_digest_slice(&msg).unwrap();
-
-        let signature = secp.sign_ecdsa(&msg, &secret);
-
-        // verify the unpatched version of the function verifies as expected
-        assert!(secp.verify_ecdsa(&msg, &signature, &public).is_ok());
-
-        let msg = msg.as_ref().to_vec();
-        let signature = signature.serialize_der().to_vec();
-
-        stdin.write_vec(msg);
-        stdin.write_vec(signature);
-        stdin.write(&public);
+    #[sp1_test::sp1_test("secp256k1_recover_v0_30_0", syscalls = [SECP256K1_DOUBLE, SECP256K1_ADD], prove)]
+    fn test_recover_v0_30_0_rand_lte_100(
+        stdin: &mut sp1_sdk::SP1Stdin,
+    ) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
+        recover_rand_lte_100(stdin)
     }
 
-    move |mut public| {
+    fn recover_rand_lte_100(
+        stdin: &mut sp1_sdk::SP1Stdin,
+    ) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
+        let times = 100_u8;
+
+        stdin.write(&times);
+
+        let secp = Secp256k1::new();
+
+        let mut pubkeys = Vec::with_capacity(times.into());
         for _ in 0..times {
-            assert!(public.read::<bool>());
+            let mut rng = rand::thread_rng();
+            let (secret, public) = secp.generate_keypair(&mut rng);
+
+            pubkeys.push(public);
+
+            let msg = rand::random::<[u8; 32]>();
+            let msg = Message::from_digest_slice(&msg).unwrap();
+
+            let signature = secp.sign_ecdsa_recoverable(&msg, &secret);
+
+            // Verify that the unpatched version of this function recovers as expected.
+            assert_eq!(secp.recover_ecdsa(&msg, &signature).unwrap(), public);
+
+            let (recid, sig) = signature.serialize_compact();
+
+            let recid = recid.to_i32();
+
+            stdin.write(&recid);
+            stdin.write(msg.as_ref());
+            stdin.write_slice(sig.as_slice());
+        }
+
+        move |mut public| {
+            println!("checking public values");
+            for key in pubkeys {
+                assert_eq!(public.read::<Option<PublicKey>>(), Some(key));
+            }
+        }
+    }
+
+    #[sp1_test::sp1_test("secp256k1_verify", syscalls = [SECP256K1_DOUBLE, SECP256K1_ADD], prove)]
+    fn test_verify_rand_lte_100(
+        stdin: &mut sp1_sdk::SP1Stdin,
+    ) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
+        verify_rand_lte_100(stdin)
+    }
+
+    #[sp1_test::sp1_test("secp256k1_verify_v0_30_0", syscalls = [SECP256K1_DOUBLE, SECP256K1_ADD], prove)]
+    fn test_verify_v0_30_0_rand_lte_100(
+        stdin: &mut sp1_sdk::SP1Stdin,
+    ) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
+        verify_rand_lte_100(stdin)
+    }
+
+    fn verify_rand_lte_100(stdin: &mut sp1_sdk::SP1Stdin) -> impl FnOnce(sp1_sdk::SP1PublicValues) {
+        let times = 100_u8;
+        stdin.write(&times);
+
+        let secp = Secp256k1::new();
+
+        for _ in 0..times {
+            let mut rng = rand::thread_rng();
+            let (secret, public) = secp.generate_keypair(&mut rng);
+
+            let msg = rand::random::<[u8; 32]>();
+            let msg = Message::from_digest_slice(&msg).unwrap();
+
+            let signature = secp.sign_ecdsa(&msg, &secret);
+
+            // verify the unpatched version of the function verifies as expected
+            assert!(secp.verify_ecdsa(&msg, &signature, &public).is_ok());
+
+            let msg = msg.as_ref().to_vec();
+            let signature = signature.serialize_der().to_vec();
+
+            stdin.write_vec(msg);
+            stdin.write_vec(signature);
+            stdin.write(&public);
+        }
+
+        move |mut public| {
+            for _ in 0..times {
+                assert!(public.read::<bool>());
+            }
         }
     }
 }
-
 // add cases for fail verify, although its not patched
