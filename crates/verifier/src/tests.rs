fe9069441b10253e2420cf6b752e3ec291017b1b
leruaa <aurelien.catinon@gmail.com>
2025-04-16 04:28:56 -0700

    fix: fmt

--- crates/verifier/src/tests.rs
+++ crates/verifier/src/tests.rs
@@ -1,8 +1,7 @@
 use rstest::rstest;
 use serial_test::serial;
 use sp1_sdk::{install::try_install_circuit_artifacts, HashableKey, ProverClient, SP1Stdin};
-use test_artifacts::FIBONACCI_BLAKE3_ELF;
-use test_artifacts::FIBONACCI_ELF;
+use test_artifacts::{FIBONACCI_BLAKE3_ELF, FIBONACCI_ELF};
 
 use crate::{error::Error, Groth16Error, PlonkError};
 
