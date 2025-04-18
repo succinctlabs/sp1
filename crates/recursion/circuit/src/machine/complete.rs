cf78cca8b027600b6e8e781678e61ecda073676b
Tomass <155266802+zeroprooff@users.noreply.github.com>
2025-04-18 10:28:38 +0200

    Update complete.rs

--- crates/recursion/circuit/src/machine/complete.rs
+++ crates/recursion/circuit/src/machine/complete.rs
@@ -42,7 +42,7 @@ pub(crate) fn assert_complete<C: Config<F = BabyBear>>(
     // shard that contains CPU.
     builder.assert_felt_ne(is_complete * *next_shard, C::F::one());
 
-    // Assert that that an execution shard is present.
+    // Assert that an execution shard is present.
     builder.assert_felt_eq(is_complete * (*contains_execution_shard - C::F::one()), C::F::zero());
     // Assert that the start execution shard is equal to 1.
     builder.assert_felt_eq(is_complete * (*start_execution_shard - C::F::one()), C::F::zero());
