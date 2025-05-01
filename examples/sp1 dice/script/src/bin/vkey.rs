0cf5ebb4cd2bdbc15d61f06bd9edfe0353189f30
Nihat <85920289+zacnider@users.noreply.github.com>
2025-05-01 20:19:04 +0300

    Update vkey.rs

--- examples/sp1 dice/script/src/bin/vkey.rs
+++ examples/sp1 dice/script/src/bin/vkey.rs
@@ -1,4 +1,4 @@
-use sp1_sdk::{include_elf, HashableKey, ProverClient};
+use sp1_sdk::{include_elf, HashableKey, Prover, ProverClient};
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
 pub const DICE_ELF: &[u8] = include_elf!("dice-game-program");
