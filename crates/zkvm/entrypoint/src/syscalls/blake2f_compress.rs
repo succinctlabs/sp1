e9f3a46b8b2f7f9db97819eed323e83b61932f0c
Hobbes <hobbes@hobbes.com>
2025-06-02 16:25:35 -0700

    blake2f compress: Add to zkvm

---
+++ crates/zkvm/entrypoint/src/syscalls/blake2f_compress.rs
@@ -0,0 +1,25 @@
+#[cfg(target_os = "zkvm")]
+use core::arch::asm;
+
+/// Executes the Blake2f compress operation.
+///
+/// ### Safety
+///
+/// The caller must ensure that `w` is valid pointer to data that is aligned along a four byte
+/// boundary.
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern "C" fn syscall_blake2f_compress(state: *mut [u32; 212]) {
+    #[cfg(target_os = "zkvm")]
+    unsafe {
+        asm!(
+            "ecall",
+            in("t0") crate::syscalls::BLAKE2F_COMPRESS,
+            in("a0") state,
+            in("a1") 0
+        );
+    }
+
+    #[cfg(not(target_os = "zkvm"))]
+    unreachable!()
+}
