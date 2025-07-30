135449fedf88ec9ddcac9086e218b216fb5f1f02
Matt Stam <codeguy0112358@gmail.com>
2025-07-30 15:39:28 -0700

    fix(sdk): set error parameter in network (#2400)

--- crates/sdk/src/network/client.rs
+++ crates/sdk/src/network/client.rs
@@ -263,6 +263,7 @@ impl NetworkClient {
         not_bid_by: Option<Vec<u8>>,
         execute_fail_cause: Option<i32>,
         settlement_status: Option<i32>,
+        error: Option<i32>,
     ) -> Result<GetFilteredProofRequestsResponse> {
         self.with_retry(
             || {
@@ -291,6 +292,7 @@ impl NetworkClient {
                             not_bid_by,
                             execute_fail_cause,
                             settlement_status,
+                            error,
                         })
                         .await?
                         .into_inner())
