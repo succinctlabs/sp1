e958698884055056f532c7b9fe9b14d157e1d583
0xernesto <78889960+0xernesto@users.noreply.github.com>
2025-08-22 14:19:02 -0700

    fix

--- crates/sdk/src/network/prover.rs
+++ crates/sdk/src/network/prover.rs
@@ -466,7 +466,7 @@ impl NetworkProver {
     /// Cancels a proof request by updating the deadline to the current time.
     #[cfg(not(feature = "reserved-capacity"))]
     pub async fn cancel_request(&self, request_id: B256) -> Result<()> {
-        self.client.cancel_request(request_id).await?
+        self.client.cancel_request(request_id).await
     }
 
     /// Waits for a proof to be generated and returns the proof. If a timeout is supplied, the
