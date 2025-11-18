0fcc92d52c0d2b057182d9b4c23c26788a0cd43c
Aurélien <3535019+leruaa@users.noreply.github.com>
2025-11-18 17:15:07 +0100

    chore: bump Rust `succinct` toolchain to v1.91.1 (#2494)

--- crates/core/executor/src/syscalls/write.rs
+++ crates/core/executor/src/syscalls/write.rs
@@ -196,12 +196,12 @@ fn handle_cycle_tracker_command(rt: &mut Executor, command: CycleTrackerCommand)
             if let Some(total_cycles) = end_cycle_tracker(rt, &name) {
                 rt.report
                     .cycle_tracker
-                    .entry(name.to_string())
+                    .entry(name.clone())
                     .and_modify(|cycles| *cycles += total_cycles)
                     .or_insert(total_cycles);
                 rt.report
                     .invocation_tracker
-                    .entry(name.to_string())
+                    .entry(name.clone())
                     .and_modify(|invocations| *invocations += 1)
                     .or_insert(1);
             }
