0a0b83146360e50aa72a698daf08b63f044b8ac3
Tobias Bergkvist <tobias@bergkv.ist>
2026-03-13 20:38:08 +0100

    Fix test_compress_tree test

--- crates/prover/src/worker/controller/compress.rs
+++ crates/prover/src/worker/controller/compress.rs
@@ -605,6 +605,7 @@ mod test_utils {
             (TaskType::PlonkWrap, Duration::from_millis(20)..Duration::from_millis(100)),
             (TaskType::Groth16Wrap, Duration::from_millis(20)..Duration::from_millis(100)),
             (TaskType::ExecuteOnly, Duration::from_millis(20)..Duration::from_millis(100)),
+            (TaskType::CoreExecute, Duration::from_millis(20)..Duration::from_millis(100)),
         ]);
 
         for _ in 0..num_iterations {
