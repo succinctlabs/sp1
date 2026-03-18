48592ef718b3aec31bde99f9538c9e7803070831
tamirhemo <tamirhmo@gmail.com>
2026-03-18 17:52:40 +0000

    dorroh

--- slop/crates/algebra/src/pure.rs
+++ slop/crates/algebra/src/pure.rs
@@ -1,16 +1,23 @@
 //! Abstract algebraic traits and constructions
 
-use std::ops::{Add, Mul, Sub};
+use std::{
+    fmt::Debug,
+    iter::{Product, Sum},
+    ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign},
+};
 
-use p3_field::AbstractField;
+use p3_field::{AbstractField, Field};
 use serde::{Deserialize, Serialize};
 
-pub trait Module<R>: Add<R, Output = Self> + Sub<R, Output = Self> + Mul<R, Output = Self> {}
+pub trait Module<R>:
+    Clone + Add<R, Output = Self> + Sub<R, Output = Self> + Mul<R, Output = Self> + Neg<Output = Self>
+{
+}
 
 impl<R, E> Module<R> for E
 where
     R: AbstractField,
-    E: Add<R, Output = E> + Sub<R, Output = E> + Mul<R, Output = E>,
+    E: Clone + Add<R, Output = E> + Sub<R, Output = E> + Mul<R, Output = E> + Neg<Output = E>,
 {
 }
 
@@ -29,5 +36,191 @@ pub enum Dorroh<F, E> {
 impl<F, E> From<F> for Dorroh<F, E> {
     fn from(value: F) -> Self {
         Self::Constant(value)
+    }
+}
+
+impl<F: Default, E> Default for Dorroh<F, E> {
+    fn default() -> Self {
+        Self::Constant(F::default())
+    }
+}
+
+impl<F, E> Add for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F> + Add<Output = E>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        match (self, rhs) {
+            (Self::Constant(a), Self::Constant(b)) => Self::Constant(a + b),
+            (Self::Element(a), Self::Element(b)) => Self::Element(a + b),
+            (Self::Constant(a), Self::Element(b)) => Self::Element(b + a),
+            (Self::Element(a), Self::Constant(b)) => Self::Element(a + b),
+        }
+    }
+}
+
+impl<F, E> AddAssign for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F> + Add<Output = E> + Copy,
+{
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl<F, E> Sub for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F> + Sub<Output = E>,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        match (self, rhs) {
+            (Self::Constant(a), Self::Constant(b)) => Self::Constant(a - b),
+            (Self::Element(a), Self::Element(b)) => Self::Element(a - b),
+            (Self::Constant(a), Self::Element(b)) => Self::Element(-b + a),
+            (Self::Element(a), Self::Constant(b)) => Self::Element(a - b),
+        }
+    }
+}
+
+impl<F, E> SubAssign for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F> + Sub<Output = E> + Copy,
+{
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl<F, E> Neg for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F>,
+{
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        match self {
+            Self::Constant(a) => Self::Constant(-a),
+            Self::Element(a) => Self::Element(-a),
+        }
+    }
+}
+
+impl<F, E> Mul for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F> + Mul<Output = Self>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        match (self, rhs) {
+            (Self::Constant(a), Self::Constant(b)) => Self::Constant(a * b),
+            (Self::Element(a), Self::Element(b)) => a * b,
+            (Self::Constant(a), Self::Element(b)) => Self::Element(b * a),
+            (Self::Element(a), Self::Constant(b)) => Self::Element(a * b),
+        }
+    }
+}
+
+impl<F, E> MulAssign for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F> + Mul<Output = Self> + Copy,
+{
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
+    }
+}
+
+impl<F, E> Sum for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F> + Mul<Output = Self> + Add<Output = E> + Sub<Output = E> + Copy + Debug,
+{
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::zero(), |acc, x| acc + x)
+    }
+}
+
+impl<F, E> Product for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F> + Mul<Output = Self> + Add<Output = E> + Sub<Output = E> + Copy + Debug,
+{
+    fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::one(), |acc, x| acc * x)
+    }
+}
+
+impl<F, E> AbstractField for Dorroh<F, E>
+where
+    F: Field,
+    E: Module<F> + Mul<Output = Self> + Add<Output = E> + Sub<Output = E> + Copy + Debug,
+{
+    type F = F;
+
+    fn zero() -> Self {
+        Self::Constant(F::zero())
+    }
+
+    fn one() -> Self {
+        Self::Constant(F::one())
+    }
+
+    fn two() -> Self {
+        Self::Constant(F::two())
+    }
+
+    fn neg_one() -> Self {
+        Self::Constant(F::neg_one())
+    }
+
+    fn from_f(f: Self::F) -> Self {
+        Self::Constant(f)
+    }
+
+    fn from_bool(b: bool) -> Self {
+        Self::Constant(F::from_bool(b))
+    }
+
+    fn from_canonical_u8(n: u8) -> Self {
+        Self::Constant(F::from_canonical_u8(n))
+    }
+
+    fn from_canonical_u16(n: u16) -> Self {
+        Self::Constant(F::from_canonical_u16(n))
+    }
+
+    fn from_canonical_u32(n: u32) -> Self {
+        Self::Constant(F::from_canonical_u32(n))
+    }
+
+    fn from_canonical_u64(n: u64) -> Self {
+        Self::Constant(F::from_canonical_u64(n))
+    }
+
+    fn from_canonical_usize(n: usize) -> Self {
+        Self::Constant(F::from_canonical_usize(n))
+    }
+
+    fn from_wrapped_u32(n: u32) -> Self {
+        Self::Constant(F::from_wrapped_u32(n))
+    }
+
+    fn from_wrapped_u64(n: u64) -> Self {
+        Self::Constant(F::from_wrapped_u64(n))
+    }
+
+    fn generator() -> Self {
+        Self::Constant(F::generator())
     }
 }
